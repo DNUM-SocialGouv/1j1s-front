4849c2b793ff2fd10b6cb2737f9eec13799d0920
Olivia Peny-Coblentz <oliv@octo.com>
2022-04-14 14:53:42 +0200

    setup react testing library

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   globalSetup: "./jest.setup.ts",
+  testEnvironment: "jest-environment-jsdom",
 };
 
 export default config;
