58f29c6821d32e1a62eb49a63e711af3082d84e7
olivocto <102825843+olivocto@users.noreply.github.com>
2022-04-20 14:06:08 +0200

    build: ajout react testing library

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,10 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   globalSetup: "./jest.setup.ts",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  testEnvironment: "jest-environment-jsdom",
 };
 
 export default config;
---
+++ src/__tests__/CardEmploiUnJUnS.test.tsx
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+
+import { render, screen } from "@testing-library/react";
+
+import { CardEmploiUnJUnS } from "../client/components/CardEmploiUnJUnS";
+
+describe("CardEmploiUnJUnS", () => {
+  it("renders CardEmploiUnJUnS component", () => {
+    render(<CardEmploiUnJUnS />);
+
+    const cardEmploi = screen.getByTestId("lien-emploi");
+
+    expect(cardEmploi).toBeInTheDocument();
+  });
+
+  it("redirects the user when clicking on the card", () => {
+    render(<CardEmploiUnJUnS />);
+
+    const cardEmploi = screen.getByTestId("lien-emploi");
+
+    expect(cardEmploi).toBeInTheDocument();
+  });
+});
