71eb00665801dacebcd26f7a2302334bb306553b
Olivia Peny-Coblentz <oliv@octo.com>
2022-04-22 10:17:32 +0200

    feat: ajout test session storage

---
+++ tests/client/utils/sessionStorage.util.test.ts
@@ -0,0 +1,10 @@
+import StorageService from '../../../src/client/utils/sessionStorage.util'
+
+describe("StorageService", () => {
+  describe("getItem", () => {
+    it("return le session id du session storage", () => {
+      StorageService.setItem(StorageService.Key.SESSION_ID, 'ma-session-id')
+      expect(StorageService.getItem(StorageService.Key.SESSION_ID)).toEqual('ma-session-id')
+    })
+  })
+})
