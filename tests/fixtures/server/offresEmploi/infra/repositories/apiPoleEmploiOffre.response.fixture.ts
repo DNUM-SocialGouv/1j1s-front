74589b1bcfdd582f7d6746a1a253a5f0148f46ce
Ali El Amrani <alib.elamrani@gmail.com>
2022-05-20 16:41:25 +0200

    feat : fixture et tests apiPoleEmploiOffreMapper

--- tests/fixtures/server/offresEmploi/infra/repositories/apiPoleEmploiOffre.response.fixture.ts
+++ tests/fixtures/server/offresEmploi/infra/repositories/apiPoleEmploiOffre.response.fixture.ts
@@ -20,3 +20,46 @@ function anOffreEmploiResponseCompétence(override?: Partial<OffreEmploiResponse
     ...override,
   };
 }
+
+export function anOffreEmploiResponseFormationList(): OffreEmploiResponse.Formation[] {
+  return [
+    anOffreEmploiResponseFormation(),
+    anOffreEmploiResponseFormation({ commentaire: 'Licence pro commerce', niveauLibelle: 'Bac+3 et plus ou équivalents' }),
+  ];
+}
+
+export function anOffreEmploiResponseFormationListAvecFormationNonDéfinie(): OffreEmploiResponse.Formation[] {
+  return [
+    anOffreEmploiResponseFormation(),
+    anOffreEmploiResponseFormation({ commentaire: undefined, niveauLibelle: undefined }),
+  ];
+}
+
+function anOffreEmploiResponseFormation(override?: Partial<OffreEmploiResponse.Formation>): OffreEmploiResponse.Formation {
+  return {
+    commentaire: 'DE docteur en médecine',
+    niveauLibelle: 'Bac+5 et plus ou équivalents',
+    ...override,
+  };
+}
+
+export function anOffreEmploiResponseQualitéeProfessionnelleList(): OffreEmploiResponse.QualitéeProfessionnelle[] {
+  return [
+    anOffreEmploiResponseQualitéeProfessionnelle(),
+    anOffreEmploiResponseQualitéeProfessionnelle({ libelle: 'Capacité de décision' }),
+  ];
+}
+
+export function anOffreEmploiResponseQualitéeProfessionnelleListAvecQualitéeNonDéfinie(): OffreEmploiResponse.QualitéeProfessionnelle[] {
+  return [
+    anOffreEmploiResponseQualitéeProfessionnelle(),
+    anOffreEmploiResponseQualitéeProfessionnelle({ libelle: undefined }),
+  ];
+}
+
+function anOffreEmploiResponseQualitéeProfessionnelle(override?: Partial<OffreEmploiResponse.QualitéeProfessionnelle>): OffreEmploiResponse.QualitéeProfessionnelle {
+  return {
+    libelle: 'Capacité d\'adaptation',
+    ...override,
+  };
+}
--- tests/server/offresEmploi/infra/repositories/apiPoleEmploiOffre.mapper.test.ts
+++ tests/server/offresEmploi/infra/repositories/apiPoleEmploiOffre.mapper.test.ts
@@ -1,49 +1,139 @@
 import {
-  anOffreEmploiResponseCompétenceList, anOffreEmploiResponseCompétenceListAvecCompétenceNonDéfinie,
+  anOffreEmploiResponseCompétenceList,
+  anOffreEmploiResponseCompétenceListAvecCompétenceNonDéfinie,
+  anOffreEmploiResponseFormationList,
+  anOffreEmploiResponseFormationListAvecFormationNonDéfinie,
+  anOffreEmploiResponseQualitéeProfessionnelleList,
+  anOffreEmploiResponseQualitéeProfessionnelleListAvecQualitéeNonDéfinie,
 } from '@tests/fixtures/server/offresEmploi/infra/repositories/apiPoleEmploiOffre.response.fixture';
 
-import { mapCompétence } from '~/server/offresEmploi/infra/repositories/apiPoleEmploiOffre.mapper';
+import { OffreEmploi } from '~/server/offresEmploi/domain/offreEmploi';
+import {
+  mapCompétence,
+  mapFormation,
+  mapQualitéeProfessionnelle,
+} from '~/server/offresEmploi/infra/repositories/apiPoleEmploiOffre.mapper';
 
-describe('mapCompétence', () => {
-  describe('quand la réponse des compétences est undefined', () => {
-    it('retourne undefined', () => {
-      //given
-      const compétenceResponse = undefined;
-      //when
-      const mappedOffreEmploiCompétenceList = mapCompétence(compétenceResponse);
-      const résultatAttendu = undefined;
-      //then
-      expect(mappedOffreEmploiCompétenceList).toEqual(résultatAttendu);
+
+describe('mapper', () => {
+  describe('mapCompétence', () => {
+    describe('quand la réponse des compétences est undefined', () => {
+      it('retourne undefined', () => {
+        //given
+        const compétenceResponse = undefined;
+        //when
+        const mappedOffreEmploiCompétenceList = mapCompétence(compétenceResponse);
+        const résultatAttendu = undefined;
+        //then
+        expect(mappedOffreEmploiCompétenceList).toEqual(résultatAttendu);
+      });
+    });
+
+    describe('quand on récupère une liste de compétences', () => {
+      it('retourne les compétences', () => {
+        //given
+        const compétenceResponse = anOffreEmploiResponseCompétenceList();
+        //when
+        const mappedOffreEmploiCompétenceList = mapCompétence(compétenceResponse);
+
+        const résultatAttendu: string[] = [
+          'Réaliser la prescription médicale',
+          'Déterminer les besoins thérapeutiques'];
+        //then
+        expect(mappedOffreEmploiCompétenceList).toEqual(résultatAttendu);
+      });
+
+      it('retourne seulement les libellés qui ne sont pas undefined', () => {
+        const compétenceResponse = anOffreEmploiResponseCompétenceListAvecCompétenceNonDéfinie();
+
+        const mappedOffreEmploiCompétenceList = mapCompétence(compétenceResponse);
+        const résultatAttendu: string[] = [
+          'Réaliser la prescription médicale',
+        ];
+        expect(mappedOffreEmploiCompétenceList).toEqual(résultatAttendu);
+      });
     });
   });
 
-  describe('quand on récupère une liste de compétences', () => {
-    it('retourne les compétences', () => {
-      //given
-      const compétenceResponse = anOffreEmploiResponseCompétenceList();
-      //when
-      const mappedOffreEmploiCompétenceList = mapCompétence(compétenceResponse);
-
-      const résultatAttendu: string[] = [
-        'Réaliser la prescription médicale',
-        'Déterminer les besoins thérapeutiques'];
-      //then
-      expect(mappedOffreEmploiCompétenceList).toEqual(résultatAttendu);
+  describe('mapQualitéeProfessionnelle', () => {
+    describe('quand la réponse des qualitées professionnelles est undefined', () => {
+      it('retourne undefined', () => {
+        //given
+        const qualitéesProfessionnelleResponse = undefined;
+        //when
+        const mappedOffreEmploiQualitéeProfessionnelleList = mapQualitéeProfessionnelle(qualitéesProfessionnelleResponse);
+        const résultatAttendu = undefined;
+        //then
+        expect(mappedOffreEmploiQualitéeProfessionnelleList).toEqual(résultatAttendu);
+      });
     });
 
-    it('retourne seulement les libellés qui ne sont pas undefined', () => {
-      const compétenceResponse = anOffreEmploiResponseCompétenceListAvecCompétenceNonDéfinie();
+    describe('quand on récupère une liste de qualitées Professionnelles', () => {
+      it('retourne les qualitées Professionnelles', () => {
+        //given
+        const qualitéesProfessionnelleResponse = anOffreEmploiResponseQualitéeProfessionnelleList();
+        //when
+        const mappedOffreEmploiQualitéeProfessionnelleList = mapQualitéeProfessionnelle(qualitéesProfessionnelleResponse);
 
-      const mappedOffreEmploiCompétenceList = mapCompétence(compétenceResponse);
-      const résultatAttendu: string[] = [
-        'Réaliser la prescription médicale',
-      ];
-      expect(mappedOffreEmploiCompétenceList).toEqual(résultatAttendu);
+        const résultatAttendu: string[] = [
+          'Capacité d\'adaptation',
+          'Capacité de décision'];
+        //then
+        expect(mappedOffreEmploiQualitéeProfessionnelleList).toEqual(résultatAttendu);
+      });
+
+      it('retourne seulement les libellés qui ne sont pas undefined', () => {
+        const qualitéesProfessionnelleResponse = anOffreEmploiResponseQualitéeProfessionnelleListAvecQualitéeNonDéfinie();
+
+        const mappedOffreEmploiQualitéeProfessionnelleList = mapQualitéeProfessionnelle(qualitéesProfessionnelleResponse);
+        const résultatAttendu: string[] = [
+          'Capacité d\'adaptation',
+        ];
+        expect(mappedOffreEmploiQualitéeProfessionnelleList).toEqual(résultatAttendu);
+      });
+    });
+
+    describe('mapFormation', () => {
+      describe('quand la réponse des formations est undefined', () => {
+        it('retourne undefined', () => {
+        //given
+          const formationResponse = undefined;
+          //when
+          const mappedOffreEmploiFormationList = mapFormation(formationResponse);
+          const résultatAttendu = undefined;
+          //then
+          expect(mappedOffreEmploiFormationList).toEqual(résultatAttendu);
+        });
+      });
+
+      describe('quand on récupère une liste de formations', () => {
+        it('retourne les formations', () => {
+        //given
+          const formationResponse = anOffreEmploiResponseFormationList();
+          //when
+          const mappedOffreEmploiFormationList = mapFormation(formationResponse);
+
+          const résultatAttendu: OffreEmploi.Formation[] = [
+            { commentaire: 'DE docteur en médecine', libellé: 'Bac+5 et plus ou équivalents' },
+            { commentaire: 'Licence pro commerce', libellé: 'Bac+3 et plus ou équivalents' },
+          ];
+          //then
+          expect(mappedOffreEmploiFormationList).toEqual(résultatAttendu);
+        });
+
+        it('retourne seulement les champs qui ne sont pas undefined', () => {
+          const formationResponse = anOffreEmploiResponseFormationListAvecFormationNonDéfinie();
+
+          const mappedOffreEmploiFormationList = mapFormation(formationResponse);
+          const résultatAttendu: OffreEmploi.Formation[] = [
+            { commentaire: 'DE docteur en médecine', libellé: 'Bac+5 et plus ou équivalents' },
+          ];
+          expect(mappedOffreEmploiFormationList).toEqual(résultatAttendu);
+        });
+      });
     });
   });
 });
-
-
 
 
 
