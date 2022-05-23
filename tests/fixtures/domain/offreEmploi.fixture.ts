af019f77d56052d9994184375f9f8f80debde33e
Ali El Amrani <alib.elamrani@gmail.com>
2022-05-23 17:22:35 +0200

    test : ajout formations, compétences, qualités lists dans offreEmploi.fixture

--- tests/fixtures/domain/offreEmploi.fixture.ts
+++ tests/fixtures/domain/offreEmploi.fixture.ts
@@ -18,6 +18,7 @@ export function aRésultatsRechercheOffreEmploi(override?: Partial<RésultatsRec
 
 export function aBarmanOffreEmploi(): OffreEmploi {
   return {
+    compétenceList:[],
     description: 'Nous recherchons pour la saison demi-mai à mi-octobre 2022 un(e) Barman h/f.\n\nVos missions principales: \n- Vous effectuez le service au comptoir, en salle, en terrasse, de boissons chaudes ou froides selon la législation relative à la consommation d\'alcools. \n- Vous entretenez la verrerie, les équipements du bar et les locaux selon les règles d\'hygiène et la réglementation  en vigueur.\n- Vous participez à la vie de la paillote. \n \nVous travaillez vendredi et samedi. \n\n\n',
     duréeTravail: OffreEmploi.DuréeTravail.TEMPS_PARTIEL,
     entreprise: {
@@ -25,9 +26,11 @@ export function aBarmanOffreEmploi(): OffreEmploi {
       nom: 'LE PLEIN AIR',
     },
     expérience: OffreEmploi.Expérience.DEBUTANT_ACCEPTE,
+    formationList: [],
     id: '132LKFB',
     intitulé: 'Barman / Barmaid (H/F)',
     lieuTravail: 'BOURG LES VALENCE (26)',
+    qualitéeProfessionnelleList: [],
     typeContrat: OffreEmploi.CONTRAT_SAISONNIER,
     urlOffreOrigine: 'https://candidat.pole-emploi.fr/offres/recherche/detail/132LKFB',
   };
@@ -35,6 +38,7 @@ export function aBarmanOffreEmploi(): OffreEmploi {
 
 export function aMaçonOffreEmploi(): OffreEmploi {
   return {
+    compétenceList:[],
     description: 'Vous recherchez un emploi ? Faites confiances à nos différences ! R.A.S Intérim, réseau d\'agences d\'emploi de 170 agences, propose des centaines d\'opportunités d\'emploi dans tous les secteurs d\'activité, en intérim, CDD et CDI.\n\nVotre Agence R.A.S Intérim de PORNIC, recherche un MACON dans pour un de ses clients spécialiste du BTP.\n\nVos missions:\n- Travaux de maçonnerie\n- Travaux sur différents matériaux (parpaings, brique...)\n- Lecture de plans\n\nVotre profil:\n- Titulaire d\'un CAP maçonnerie\n- Expérience sur un poste similaire\n- Rigueur/ Autonome\n\nDisponible? Envoyez nous votre CV !',
     duréeTravail: OffreEmploi.DuréeTravail.TEMPS_PARTIEL,
     entreprise: {
@@ -42,9 +46,11 @@ export function aMaçonOffreEmploi(): OffreEmploi {
       nom: 'RAS 1040',
     },
     expérience: OffreEmploi.Expérience.EXPERIENCE_EXIGEE,
+    formationList: [],
     id: '130WPHC',
     intitulé: 'Maçon / Maçonne',
     lieuTravail: undefined,
+    qualitéeProfessionnelleList: [],
     typeContrat: OffreEmploi.CONTRAT_INTÉRIMAIRE,
     urlOffreOrigine: 'https://candidat.pole-emploi.fr/offres/recherche/detail/130WPHC',
   };
@@ -52,6 +58,7 @@ export function aMaçonOffreEmploi(): OffreEmploi {
 
 export function aValetOffreEmploi(): OffreEmploi {
   return {
+    compétenceList:[],
     description: 'Vous interviendrez sur le nettoyage des chambres de l\'Hôtel.\nVous changerez les draps et serviettes, nettoierez la salle de bain et les sanitaires, effectuerez la poussière et passerez l\'aspirateur.  \n\nNous vous proposons un contrat en vacation, vous devez pouvoir être disponible les weekend. 3 à 4 vacations par semaine.\nLa durée du contrat et le nombre d\'heure varieront en fonction des nécessites du service; c\'est à dire de 20 h à 24h de travail par semaine.\n\nPrise de poste au plus tôt.\n',
     duréeTravail: OffreEmploi.DuréeTravail.TEMPS_PARTIEL,
     entreprise: {
@@ -59,9 +66,11 @@ export function aValetOffreEmploi(): OffreEmploi {
       nom: undefined,
     },
     expérience: OffreEmploi.Expérience.EXPERIENCE_SOUHAITEE,
+    formationList: [],
     id: '132MDKM',
     intitulé: 'Valet / Femme de chambre',
     lieuTravail: 'BALARUC LES BAINS (34)',
+    qualitéeProfessionnelleList: [],
     typeContrat: OffreEmploi.CONTRAT_CDD,
     urlOffreOrigine: 'https://candidat.pole-emploi.fr/offres/recherche/detail/132MDKM',
   };
