7c0202ec0829af797ca6bc5424ca4814b8f0af40
Ali <ali.el.amrani@octo.com>
2022-05-25 18:04:20 +0200

    feat: add img alt + fix ServiceCard desktop

--- src/client/components/ui/ServiceCard/ServiceCardComponent.tsx
+++ src/client/components/ui/ServiceCard/ServiceCardComponent.tsx
@@ -5,19 +5,20 @@ import React from 'react';
 import styles from '~/client/components/ui/ServiceCard/ServiceCardComponent.module.css';
 
 interface ServiceCardProps {
+  alt: string,
   logo: string,
 }
 
-export function ServiceCard({ logo, children }: React.PropsWithChildren<ServiceCardProps>) {
+export function ServiceCard({ logo, children, alt }: React.PropsWithChildren<ServiceCardProps>) {
   return (
     <div className={styles.card}>
       <div className={styles.cardHeader}>
-        <Image alt="" src={logo} width="140" height="80"/>
+        <Image alt={alt} src={logo} width='100%' height='100%'/>
       </div>
       <div className={styles.cardBody}>
         {children}
         <ButtonGroup className='fr-link' align="right">
-          <Icon name="ri-arrow-right-line" size="lg" iconPosition="right" />
+          <Icon name='ri-arrow-right-line' size="lg" iconPosition="right"/>
         </ButtonGroup>
       </div>
     </div>
