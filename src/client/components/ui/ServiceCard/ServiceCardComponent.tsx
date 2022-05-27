ac8cb412bf4c1a8431de301ed0bda3220919f8f0
Ali El Amrani <alib.elamrani@gmail.com>
2022-05-27 10:18:52 +0200

    feat: add links and fix width on very large screens

--- src/client/components/ui/ServiceCard/ServiceCardComponent.tsx
+++ src/client/components/ui/ServiceCard/ServiceCardComponent.tsx
@@ -1,5 +1,6 @@
 import { ButtonGroup, Icon } from '@dataesr/react-dsfr';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 import styles from '~/client/components/ui/ServiceCard/ServiceCardComponent.module.css';
@@ -7,20 +8,23 @@ import styles from '~/client/components/ui/ServiceCard/ServiceCardComponent.modu
 interface ServiceCardProps {
   alt: string,
   logo: string,
+  link: string,
 }
 
-export function ServiceCard({ logo, children, alt }: React.PropsWithChildren<ServiceCardProps>) {
+export function ServiceCard({ logo, children, alt, link }: React.PropsWithChildren<ServiceCardProps>) {
   return (
-    <div className={styles.card}>
-      <div className={styles.cardHeader}>
-        <Image alt={alt} src={logo} width='100%' height='100%'/>
+    <Link href={link}>
+      <div className={styles.card}>
+        <div className={styles.cardLogo}>
+          <Image alt={alt} src={logo} width='100%' height='100%'/>
+        </div>
+        <div className={styles.cardBody}>
+          {children}
+          <ButtonGroup className='fr-link' align="right">
+            <Icon name='ri-arrow-right-line' size="lg" iconPosition="right"/>
+          </ButtonGroup>
+        </div>
       </div>
-      <div className={styles.cardBody}>
-        {children}
-        <ButtonGroup className='fr-link' align="right">
-          <Icon name='ri-arrow-right-line' size="lg" iconPosition="right"/>
-        </ButtonGroup>
-      </div>
-    </div>
+    </Link>
   );
 }
