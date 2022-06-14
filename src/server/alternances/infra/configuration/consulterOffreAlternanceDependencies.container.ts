07534a87e76144d99552904b2a87d915ccba7873
Ali El Amrani <ali.el.amrani@octo.com>
2022-06-14 09:14:07 +0200

    fix: add configuration service in container & remove console log

--- src/server/alternances/infra/configuration/consulterOffreAlternanceDependencies.container.ts
+++ src/server/alternances/infra/configuration/consulterOffreAlternanceDependencies.container.ts
@@ -1,5 +1,6 @@
 import { ApiLaBonneAlternanceRepository } from '~/server/alternances/infra/repositories/apiLaBonneAlternance.repository';
 import { ConsulterOffreAlternanceUseCase } from '~/server/alternances/useCases/consulterOffreAlternance.useCase';
+import { ConfigurationService } from '~/server/services/configuration.service';
 import { LaBonneAlternanceHttpClientService } from '~/server/services/http/laBonneAlternanceHttpClient.service';
 
 export interface ConsulterOffreAlternanceDependenciesContainer {
@@ -8,8 +9,9 @@ export interface ConsulterOffreAlternanceDependenciesContainer {
 
 export const consulterOffreAlternanceDependenciesContainer = (
   laBonneAlternanceHttpClient: LaBonneAlternanceHttpClientService,
+  configurationService: ConfigurationService,
 ): ConsulterOffreAlternanceDependenciesContainer => {
-  const alternanceRepository = new ApiLaBonneAlternanceRepository(laBonneAlternanceHttpClient);
+  const alternanceRepository = new ApiLaBonneAlternanceRepository(laBonneAlternanceHttpClient, configurationService);
 
   return {
     consulterOffreAlternance: new ConsulterOffreAlternanceUseCase(alternanceRepository),
--- src/server/alternances/infra/configuration/rechercheAlternanceDependencies.container.ts
+++ src/server/alternances/infra/configuration/rechercheAlternanceDependencies.container.ts
@@ -1,13 +1,14 @@
 import { ApiLaBonneAlternanceRepository } from '~/server/alternances/infra/repositories/apiLaBonneAlternance.repository';
 import { RechercherAlternanceUseCase } from '~/server/alternances/useCases/rechercherAlternance.useCase';
+import { ConfigurationService } from '~/server/services/configuration.service';
 import { LaBonneAlternanceHttpClientService } from '~/server/services/http/laBonneAlternanceHttpClient.service';
 
 export interface RechercherAlternanceDependenciesContainer {
   rechercherAlternance: RechercherAlternanceUseCase;
 }
 
-export function rechercherAlternanceDependenciesContainer (laBonneAlternanceHttpClient: LaBonneAlternanceHttpClientService): RechercherAlternanceDependenciesContainer {
-  const apiLaBonneAlternanceRepository = new ApiLaBonneAlternanceRepository(laBonneAlternanceHttpClient);
+export function rechercherAlternanceDependenciesContainer (laBonneAlternanceHttpClient: LaBonneAlternanceHttpClientService, configurationService: ConfigurationService): RechercherAlternanceDependenciesContainer {
+  const apiLaBonneAlternanceRepository = new ApiLaBonneAlternanceRepository(laBonneAlternanceHttpClient, configurationService);
 
   return {
     rechercherAlternance: new RechercherAlternanceUseCase(apiLaBonneAlternanceRepository),
