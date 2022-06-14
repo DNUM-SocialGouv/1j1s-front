b1b3887f172bc643acc9f90760d230aa735b1283
olivocto <102825843+olivocto@users.noreply.github.com>
2022-06-14 18:09:53 +0200

    tech/ajout action lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  ci: {
+    collect: {
+      settings: {
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo',
+        ],
+      },
+      url: [
+        'https://1j1s-front.osc-fr1.scalingo.io/',
+        'https://1j1s-front.osc-fr1.scalingo.io/emplois',
+        'https://1j1s-front.osc-fr1.scalingo.io/apprentissage',
+        'https://1j1s-front.osc-fr1.scalingo.io/jobs-ete',
+      ],
+    },
+    upload: {
+      serverBaseUrl: 'https://mysterious-cove-34282.herokuapp.com',
+      target: 'lhci',
+      token: 'a6148b25-d912-4550-82b1-24eb96fe60ec',
+    },
+  },
+};
