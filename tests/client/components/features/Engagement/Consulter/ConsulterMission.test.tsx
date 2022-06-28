2c722b567d9f23a7a73d0ee2305431f174a53feb
Ali El Amrani <ali.el.amrani@octo.com>
2022-06-28 15:57:02 +0200

    feat: add consulterMission page & add new css for consulterMission

---
+++ tests/client/components/features/Engagement/Consulter/ConsulterMission.test.tsx
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen, within } from '@testing-library/react';
+import { mockSmallScreen } from '@tests/client/window.mock';
+import { aRésultatMission } from '@tests/fixtures/domain/missionEngagement.fixture';
+import React from 'react';
+
+import { ConsulterMissionEngagement } from '~/client/components/features/Engagement/Consulter/ConsulterMissionEngagement';
+
+describe('ConsulterMission', () => {
+  beforeEach(() => {
+    mockSmallScreen();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('quand on consulte une mission', () => {
+    it('affiche l\'offre de mission', async () => {
+      const offreMission = aRésultatMission();
+
+      render(<ConsulterMissionEngagement missionEngagement={offreMission} />);
+
+      const nomAssociation =  screen.getByText('Ebs le relais val de seine');
+      const tagList = screen.getByTestId('ÉtiquetteMissionEngagementList');
+
+      expect(nomAssociation).toBeInTheDocument();
+      expect(within(tagList).queryAllByRole('listitem')).toHaveLength(2);
+    });
+  });
+});
