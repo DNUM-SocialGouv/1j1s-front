8144cd8ccb2405de4b751e75f5cc2f8d4ad2ebda
Ali El Amrani <ali.el.amrani@octo.com>
2022-06-28 15:55:14 +0200

    feat: add & update missionEngagement & apiEngagement fixtures

--- tests/fixtures/domain/missionEngagement.fixture.ts
+++ tests/fixtures/domain/missionEngagement.fixture.ts
@@ -71,14 +71,21 @@ export function aRésultatRechercheMission(override?: Partial<RésultatsRecherch
   };
 }
 
+export function aRésultatMission(): Mission {
+  return anAmbassadeurDuDonDeVêtementMission();
+}
+
 function anAmbassadeurDuDonDeVêtementMission(): Mission {
   return {
     description: ' • Faire connaître les activités du Relais et inciter au don de textiles dans\n   votre quartier \n • Prévenir en cas de débordement ou de dégradation constatés d’une borne\n • Sensibiliser vos voisins \n • Participer à des évènements de collecte de collecte à proximité de son\n   domicile\n\n\\n\\nObjectifs: \\n\n\nNous désirons innover en développant un réseau d’ambassadeurs bénévoles autour\ndu geste du don dans la ville de Poissy, chaque ambassadeur se voyant assigné un\npoint de collecte - « une borne » - à proximité de son domicile et pouvant par\nla suite organiser des animations autour de la réduction des déchets et du tri\ndes textiles. ',
+    duréeContrat: 8,
     débutContrat: '09/05/2022',
     id: '6278e8ced7dda60703c3ca40',
+    localisation: 'Poissy (78 - Yvelines - Île-de-France)',
     logo: 'https://apicivique.s3.eu-west-3.amazonaws.com/app/publishers/5f5931496c7ea514150a818f/logo_JVA_gouv_carre_light.png',
     nomEntreprise: 'Ebs le relais val de seine',
     titre: 'Je deviens Ambassadeur du don des vêtements',
+    url: 'www.poissy.com',
     étiquetteList: [
       'Poissy (78300)',
       '09/05/2022',
@@ -89,11 +96,14 @@ function anAmbassadeurDuDonDeVêtementMission(): Mission {
 function aSoutienAuxEnfantsEttAuxJeunesMission(): Mission {
   return {
     description: 'Votre mission auprès de la personne accompagnée consiste à :\n\n • lui apporter une aide méthodologique dans ses devoirs  \n • la motiver, l’encourager à la persévérance scolaire \n • lui faire découvrir des ressources éducatives et culturelles \n • si l’enseignement est partiellement à distance, l’aider à accéder à ses\n   cours, à communiquer avec ses enseignants \n\n\\n\\nObjectifs: \\n\n\nCette mission de mentorat a pour but de  favoriser l’autonomie et le\ndéveloppement de la personne accompagnée en établissant des objectifs évolutifs\net adaptés à ses besoins spécifiques.',
+    duréeContrat: 5,
     débutContrat: '01/01/2022',
     id: '61aaf6d48028f7075b9dd472',
+    localisation: 'Rueil-Malmaison (92 - Haut-de-Seine - Île-de-France)',
     logo: 'https://apicivique.s3.eu-west-3.amazonaws.com/app/publishers/5f5931496c7ea514150a818f/logo_JVA_gouv_carre_light.png',
     nomEntreprise: 'Pass-Age',
     titre: 'J’apporte un soutien aux enfants et jeunes, notamment dans les quartiers populaires, les zones rurales et territoires fragiles',
+    url: 'www.rueil-malmaison.com',
     étiquetteList: [
       'Rueil-Malmaison (92500)',
       '01/01/2022',
