e4f41f27b64a55a0bd7f5797e09103b698d80689
Ali El Amrani <ali.el.amrani@octo.com>
2022-06-28 15:57:02 +0200

    feat: add consulterMission page & add new css for consulterMission

---
+++ src/pages/service-civique/[id].tsx
@@ -0,0 +1,54 @@
+import { GetStaticPathsResult, GetStaticPropsContext, GetStaticPropsResult } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import React from 'react';
+
+import { ConsulterMissionEngagement } from '~/client/components/features/Engagement/Consulter/ConsulterMissionEngagement';
+import { HeadTag } from '~/client/components/utils/HeaderTag';
+import { Mission, MissionId } from '~/server/engagement/domain/engagement';
+import { PageContextParamsException } from '~/server/exceptions/pageContextParams.exception';
+import { dependencies } from '~/server/start';
+
+interface ConsulterMissionEngagementPageProps {
+  missionEngagement: Mission;
+}
+
+export default function ConsulterMissionEngagementPage({ missionEngagement }: ConsulterMissionEngagementPageProps) {
+  if (!missionEngagement) return null;
+
+  return (
+    <>
+      <HeadTag title={`${missionEngagement.titre} | 1jeune1solution`} />
+      <ConsulterMissionEngagement missionEngagement={ missionEngagement } />
+    </>
+  );
+}
+
+interface MissionContext extends ParsedUrlQuery {
+  id: MissionId;
+}
+
+export async function getStaticProps(context: GetStaticPropsContext<MissionContext>): Promise<GetStaticPropsResult<ConsulterMissionEngagementPageProps>> {
+  if (!context.params) {
+    throw new PageContextParamsException();
+  }
+  const { id } = context.params;
+  const missionEngagement = await dependencies.engagementDependencies.consulterMissionEngagement.handle(id);
+
+  if (missionEngagement.instance === 'failure') {
+    return { notFound: true, revalidate: 1 };
+  }
+
+  return {
+    props: {
+      missionEngagement: JSON.parse(JSON.stringify(missionEngagement.result)),
+    },
+    revalidate: 86400,
+  };
+}
+
+export async function getStaticPaths(): Promise<GetStaticPathsResult> {
+  return {
+    fallback: true,
+    paths: [],
+  };
+}
