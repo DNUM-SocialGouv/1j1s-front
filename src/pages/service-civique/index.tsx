f42e949a8251ccb49ccc52071ca75cf54228ceaa
CLEM <43972380+OCTO-CLEM@users.noreply.github.com>
2022-07-05 16:32:25 +0200

    tech: refactor service civique component

--- src/pages/service-civique/index.tsx
+++ src/pages/service-civique/index.tsx
@@ -1,5 +1,5 @@
 import { RechercherMission } from '~/client/components/features/Engagement/Rechercher/RechercherMission';
 
 export default function RechercherMissionServiceCiviquePage() {
-  return ( <RechercherMission category='service-civique'/>);
+  return <RechercherMission category='service-civique'/>;
 }
