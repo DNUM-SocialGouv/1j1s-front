dd233d0640c5cdb544a094b9045aa605a316ee59
Florent Jaby <florent.jaby@gmail.com>
2022-07-11 16:52:14 +0200

    Bouge les styles dans la feuille de styles

--- src/pages/contrat-engagement-jeune/index.tsx
+++ src/pages/contrat-engagement-jeune/index.tsx
@@ -19,17 +19,11 @@ function Hero() {
   const titre = 'Je découvre le Contrat d\'Engagement Jeune';
   const accroche = 'Finie la galère, trouvez un métier qui va vous plaire.';
   const styles = heroStyles;
-  const styleTitre = {
-    color: 'white',
-  };
-  const styleAccroche = {
-    fontSize: '16px', fontWeight: '700', 
-  };
 
   const children = (
     <>
-      <h1 style={ styleTitre } >{ titre }</h1>
-      { !displayImage && (<p style={ styleAccroche }>{ accroche }</p>) }
+      <h1 className={ styles.titre } >{ titre }</h1>
+      { !displayImage && (<p className={ styles.heroAccroche}>{ accroche }</p>) }
       <a href="#" className={ styles.cta }>Je me lance &nbsp;<Icon name="ri-arrow-right-s-line" /></a>
     </>
   );
