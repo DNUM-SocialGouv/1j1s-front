9268ea297f7c5d2c3e62e77a6b4615bf40200b9c
Florent Jaby <florent.jaby@gmail.com>
2022-07-11 15:37:10 +0200

    Copie <Hero> pour en avoir un mieux pour CEJ

--- src/pages/contrat-engagement-jeune/index.tsx
+++ src/pages/contrat-engagement-jeune/index.tsx
@@ -1,14 +1,51 @@
+import { Icon } from '@dataesr/react-dsfr';
+import Image from 'next/image';
 import React from 'react';
 
-import { Hero } from '~/client/components/ui/Hero/Hero';
-/*import styles from '~/pages/contrat-engagement-jeune/index.module.css';*/
+import useBreakpoint from '~/client/hooks/useBreakpoint';
+import heroStyles from '~/pages/contrat-engagement-jeune/Hero.module.scss';
 
 
 export default function ContratEngagementJeune() {
-  const titre = 'Je découvre le Contrat d\'Engagement Jeune';
   return (
-    <Hero image="/images/banners/CEJ_banner_hero.jpg">
-      <h1>{ titre }</h1>
-    </Hero>
+    <Hero />
+  );
+}
+
+function Hero() {
+  const { isLargeScreen, isXLargeScreen } = useBreakpoint();
+  const displayImage = isLargeScreen || isXLargeScreen;
+  const image = '/images/banners/CEJ_banner_hero.jpg';
+  const titre = 'Je découvre le Contrat d\'Engagement Jeune';
+  const accroche = 'Finie la galère, trouvez un métier qui va vous plaire.';
+  const styles = heroStyles;
+  const styleTitre = {
+    color: 'white',
+  };
+  const styleAccroche = {
+    fontSize: '16px', fontWeight: '700', 
+  };
+
+  const children = (
+    <>
+      <h1 style={ styleTitre } >{ titre }</h1>
+      { !displayImage && (<p style={ styleAccroche }>{ accroche }</p>) }
+      <a href="#" className={ styles.cta }>Je me lance &nbsp;<Icon name="ri-arrow-right-s-line" /></a>
+    </>
+  );
+
+  return (
+    <div className={styles.hero}>
+      <div className={styles.heroContent}>
+        <span className={styles.heroTitle}>
+          {children}
+        </span>
+      </div>
+      {displayImage && (
+        <div className={styles.heroImage}>
+          <Image src={image} alt={ accroche } layout="fill" objectFit="contain" objectPosition="right"/>
+        </div>
+      )}
+    </div>
   );
 }
