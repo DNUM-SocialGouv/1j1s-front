bcc1006f38d3f6599954685e338877befb432928
Sergen <sergen.kovar@octo.com>
2022-07-13 18:18:21 +0200

    Ajout composant

--- src/pages/contrat-engagement-jeune/index.tsx
+++ src/pages/contrat-engagement-jeune/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import Bannière from '~/pages/contrat-engagement-jeune/Bannière';
+import Ckoi from '~/pages/contrat-engagement-jeune/Ckoi';
 
 
 export default function ContratEngagementJeune() {
   return (
-    <Bannière />
+    <>
+      <Bannière />
+      <Ckoi />
+    </>
   );
 }
