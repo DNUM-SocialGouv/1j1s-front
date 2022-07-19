4e1c6d432db299f6896a7e8f192c11aa35776ee3
thomas.benguigui <thomas.benguigui@octo.com>
2022-07-19 12:23:04 +0200

    Ajout du style du bloc mobile 'Le CEJ est fait pour moi si'

--- src/pages/contrat-engagement-jeune/PkCPourMoi.tsx
+++ src/pages/contrat-engagement-jeune/PkCPourMoi.tsx
@@ -12,11 +12,11 @@ export default function PkCPourMoi() {
       <div className={styles.container}>
         <h2 className={styles.titre}>Le Contrat d&apos;Engagement Jeune est fait pour moi si :</h2>
         <ul className={styles.liste}>
-          <li>{arrow} <strong>J&apos;ai entre 16 et 25 ans</strong> (moins de 30 si je suis en situation de handicap)</li>
-          <li>{arrow} <strong>Je suis sans emploi ni formation</strong></li>
-          <li>{arrow} <strong>Je n&apos;ai pas de projet professionnel défini</strong> et j&apos;ai perdu confiance en moi</li>
-          <li>{arrow} <strong>J&apos;ai fait face à des difficultés matérielles et financières</strong></li>
-          <li>{arrow} <strong>Je suis prêt(e) à m&apos;engager</strong> à suivre le programme</li>
+          <li>{arrow} <span><strong>J&apos;ai entre 16 et 25 ans</strong> (moins de 30 si je suis en situation de handicap)</span></li>
+          <li>{arrow} <span><strong>Je suis sans emploi ni formation</strong></span></li>
+          <li>{arrow} <span><strong>Je n&apos;ai pas de projet professionnel défini</strong> et j&apos;ai perdu confiance en moi</span></li>
+          <li>{arrow} <span><strong>J&apos;ai fait face à des difficultés matérielles et financières</strong></span></li>
+          <li>{arrow} <span><strong>Je suis prêt(e) à m&apos;engager</strong> à suivre le programme</span></li>
         </ul>
       </div>
     </section>
