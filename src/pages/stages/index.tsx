e2e5a4eb885990f2337542ec9c4a000a8662d3fe
Caroline Chuong - CACH <caroline.chuong@octo.com>
2022-07-19 18:16:15 +0200

    wip

--- src/pages/stages/index.tsx
+++ src/pages/stages/index.tsx
@@ -1,3 +1,5 @@
+import 'instantsearch.css/themes/satellite.css';
+
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
 import React from 'react';
 import { Hits,InstantSearch, RefinementList, SearchBox  } from 'react-instantsearch-hooks-web';
@@ -6,8 +8,8 @@ import styles from '~/client/components/layouts/RechercherSolution/RechercherSol
 import { HeadTag } from '~/client/components/utils/HeaderTag';
 
 const searchClient = instantMeiliSearch(
-  'https://integration-demos.meilisearch.com',
-  '99d1e034ed32eb569f9edc27962cccf90b736e4c5a70f7f5e76b9fab54d6a185',
+  'http://localhost:7700',
+  'masterKey',
 );
 
 export default function RechercherOffreStagePage() {
@@ -17,10 +19,10 @@ export default function RechercherOffreStagePage() {
       description="Plus de 400 000 offres d'emplois et d'alternances sélectionnées pour vous"/>
     <main id="contenu">
       <p className={styles.rechercheSolution}>coucou hibou</p>
-      <InstantSearch searchClient={searchClient} indexName="steam-video-games">
-        <SearchBox></SearchBox>
-        <RefinementList attribute={'genres'}/>
-        <Hits></Hits>
+      <InstantSearch searchClient={searchClient} indexName="stages">
+        <SearchBox/>
+        <RefinementList attribute={'domaines'} operator={'or'}/>
+        <Hits/>
       </InstantSearch>
     </main>
     </>
