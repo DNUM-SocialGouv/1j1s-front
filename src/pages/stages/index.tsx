1b0fb6ef6df2ed3fb1663d03333b854bda9fb237
Simon Belbeoch <sbelbeoch@octo.com>
2022-07-20 15:24:51 +0200

    WIP

--- src/pages/stages/index.tsx
+++ src/pages/stages/index.tsx
@@ -1,16 +1,30 @@
 import 'instantsearch.css/themes/satellite.css';
 
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
-import React from 'react';
-import { Hits,InstantSearch, RefinementList, SearchBox  } from 'react-instantsearch-hooks-web';
+import React, { ReactNode } from 'react';
+import { CurrentRefinements,Hits,InstantSearch, Pagination, RefinementList, SearchBox  } from 'react-instantsearch-hooks-web';
 
 import styles from '~/client/components/layouts/RechercherSolution/RechercherSolutionLayout.module.css';
+import {
+  RésultatRechercherSolution,
+} from '~/client/components/layouts/RechercherSolution/Résultat/RésultatRechercherSolution';
 import { HeadTag } from '~/client/components/utils/HeaderTag';
 
 const searchClient = instantMeiliSearch(
   'http://localhost:7700',
   'masterKey',
 );
+
+const Hit = (({ hit: résultat }): ReactNode => {
+  return <RésultatRechercherSolution
+    lienOffre={`/stages/${résultat.id}`}
+    intituléOffre={résultat.id}
+    logoEntreprise={'/images/logos/pole-emploi.svg'}
+    nomEntreprise={résultat.nomEmployeur}
+    descriptionOffre={résultat.description}
+    étiquetteOffreList={résultat.domaines}
+  />;
+});
 
 export default function RechercherOffreStagePage() {
   return (
@@ -19,10 +33,13 @@ export default function RechercherOffreStagePage() {
       description="Plus de 400 000 offres d'emplois et d'alternances sélectionnées pour vous"/>
     <main id="contenu">
       <p className={styles.rechercheSolution}>coucou hibou</p>
-      <InstantSearch searchClient={searchClient} indexName="stages">
-        <SearchBox/>
-        <RefinementList attribute={'domaines'} operator={'or'}/>
-        <Hits/>
+      <InstantSearch searchClient={searchClient} indexName="offre-de-stage">
+        <SearchBox className={'fr-input'}/>
+        <CurrentRefinements/>
+        <RefinementList attribute={'dureeEnJour'} operator={'or'} />
+        <RefinementList attribute={'domaines'}/>
+        <Hits hitComponent={Hit}  />
+        <Pagination/>
       </InstantSearch>
     </main>
     </>
