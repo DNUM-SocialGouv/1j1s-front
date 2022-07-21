00a4ee391897baff22c3e8f02712a4dbc2825597
CLEM <43972380+OCTO-CLEM@users.noreply.github.com>
2022-07-21 09:42:04 +0200

    feat: ajout du lien emplois/id pour la consultation d une offre alternance venant de pole emploi

---
+++ tests/client/utils/alternance.utils.test.ts
@@ -0,0 +1,48 @@
+import {
+  anApprentiBoucherOffreFromMatcha,
+  anApprentiBoucherOffreFromPoleEmploi,
+} from '@tests/fixtures/domain/alternance.fixture';
+
+import { mapAlternanceToLienSolution } from '~/client/utils/alternance.utils';
+
+describe('mapAlternanceToLienSolution', () => {
+  describe('quand l alternance provient d une offre pole emploi', () => {
+    it('retourne un lien solution pour pole emploi', () => {
+      const result = mapAlternanceToLienSolution(anApprentiBoucherOffreFromPoleEmploi());
+
+      expect(result).toEqual({
+        descriptionOffre: 'Vos missions principales :\n \n- Réaliser les opérations de préparation de viandes et de spécialités bouchères selon les règles d\'hygiène et de sécurité alimentaires. \n- Effectuer la vente de produits de boucherie.',
+        id: '134CMXJ',
+        intituléOffre: 'APPRENTI (E) BOUCHER (ERE) (H/F)',
+        lienOffre: '/emplois/134CMXJ',
+        logoEntreprise: 'https://entreprise.pole-emploi.fr/static/img/logos/Oukw265FRpXdejCSFnIkDoqQujqGiEt4.png',
+        nomEntreprise: 'AUCHAN SUPERMARCHE',
+        étiquetteOffreList: [
+          'AURILLAC (15)',
+          'Alternance',
+          'CDD',
+        ],
+      });
+    });
+  });
+
+  describe('quand l alternance provient d une offre matcha', () => {
+    it('retourne un lien solution pour matcha', () => {
+      const result = mapAlternanceToLienSolution(anApprentiBoucherOffreFromMatcha());
+
+      expect(result).toEqual({
+        descriptionOffre: 'Réalise les opérations de préparation de viandes et de spécialités bouchères selon les règles d\'hygiène et de sécurité alimentaires.\\nPeut effectuer la vente de produits de boucherie.\\nPeut gérer un commerce de détail alimentaire (boucherie, boucherie-charcuterie, ...).',
+        id: '628a65a72ff4860027ae1531',
+        intituléOffre: 'Boucherie',
+        lienOffre: '/apprentissage/matcha-628a65a72ff4860027ae1531',
+        logoEntreprise: '/images/logos/la-bonne-alternance.svg',
+        nomEntreprise: 'BOUCHERIE STEPHANE VEIT',
+        étiquetteOffreList: [
+          'Cap, autres formations niveau (Infrabac)',
+          'Apprentissage',
+          'Professionnalisation',
+        ],
+      });
+    });
+  });
+});
