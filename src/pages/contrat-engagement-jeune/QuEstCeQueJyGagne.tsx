94863ae3633e71aed6caf4cac4ad5c55caf1712d
thomas.benguigui <thomas.benguigui@octo.com>
2022-07-21 08:56:28 +0200

    Ajout de la version mobile du bloc Qu'est ce que j'y gagne à long terme

--- src/pages/contrat-engagement-jeune/QuEstCeQueJyGagne.tsx
+++ src/pages/contrat-engagement-jeune/QuEstCeQueJyGagne.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { AccordionComponent } from '~/client/components/ui/Accordion/AccordionComponent';
+import useBreakpoint from '~/client/hooks/useBreakpoint';
 import styles from '~/pages/contrat-engagement-jeune/QuEstCeQueJyGagne.module.scss';
 
 
 export default function QuEstCeQueJyGagne() {
+  const { isSmallScreen, isMediumScreen } = useBreakpoint();
+  const displayAccordion = isSmallScreen || isMediumScreen;
+  const contenu = <div className={styles.contenu}>
+    <strong>Quand je m&apos;engage, je prépare mon avenir et je mets toutes les chances de mon côté pour :</strong>
+    <br/><br/>
+    <ul>
+      <li>Définir et bâtir un <strong>projet professionnel durable</strong></li>
+      <li>Mettre en valeur <strong>mes talents et mes compétences</strong></li>
+      <li><strong>Découvrir le monde professionnel</strong> et comprendre son fonctionnement et ses codes</li>
+      <li><strong>Construire mon réseau</strong> pour trouver plus facilement et plus rapidement un emploi</li>
+    </ul>
+  </div>;
 
   return (
     <section className={styles.section}>
@@ -12,16 +26,8 @@ export default function QuEstCeQueJyGagne() {
           <h2>Mais en vrai, qu&apos;est-ce que j&apos;y gagne à long terme ?</h2>
           <strong>Le contrat d&apos;Engagement Jeune, c&apos;est tout bénéf&apos; pour moi !</strong>
         </div>
-        <div className={styles.contenu}>
-          <strong>Quand je m&apos;engage, je prépare mon avenir et je mets toutes les chances de mon côté pour :</strong>
-          <br/><br/>
-          <ul>
-            <li>Définir et bâtir un <strong>projet professionnel durable</strong></li>
-            <li>Mettre en valeur <strong>mes talents et mes compétences</strong></li>
-            <li><strong>Découvrir le monde professionnel</strong> et comprendre son fonctionnement et ses codes</li>
-            <li><strong>Construire mon réseau</strong> pour trouver plus facilement et plus rapidement un emploi</li>
-          </ul>
-        </div>
+        {!displayAccordion && contenu}
+        {displayAccordion && <span className={styles.accordion}><AccordionComponent>{contenu}</AccordionComponent></span>}
       </div>
     </section>
   );
