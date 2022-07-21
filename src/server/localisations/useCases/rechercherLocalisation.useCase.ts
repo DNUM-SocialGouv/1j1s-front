c816906d4f2f972f88e2ebd68b2ade155e9dd8d9
Paul Gallo <paul.gallo@octo.com>
2022-07-21 19:20:14 +0200

    feat: refacto + ajout dépendance à l'api adresse dans le use case rechercherLocalisation

--- src/server/localisations/useCases/rechercherLocalisation.useCase.ts
+++ src/server/localisations/useCases/rechercherLocalisation.useCase.ts
@@ -1,43 +1,81 @@
+import { createFailure, createSuccess, Either } from '~/server/errors/either';
+import { ErrorType } from '~/server/errors/error.types';
 import { RechercheLocalisation } from '~/server/localisations/domain/localisation';
 import { LocalisationRepository } from '~/server/localisations/domain/localisation.repository';
+import {
+  LocalisationAvecCoordonnéesRepository,
+} from '~/server/localisations/domain/localisationAvecCoordonnées.repository';
 
 export class RechercherLocalisationUseCase {
-  constructor(private localisationRepository: LocalisationRepository) {
-  }
+  constructor(private localisationRepository: LocalisationRepository, 
+              private localisationAvecCoordonnéesRepository: LocalisationAvecCoordonnéesRepository) {}
 
   private DEPARTEMENT_LENGTH = 2;
   private CODE_POSTAL_LENGTH = 5;
 
-  async handle(recherche: string): Promise<RechercheLocalisation> {
-    if(RechercherLocalisationUseCase.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTH, recherche)) {
-      return {
-        communeList : await this.localisationRepository.getCommuneListByNuméroDépartement(recherche),
-        départementList : await this.localisationRepository.getDépartementListByNuméroDépartement(recherche),
-        régionList : [],
-      };
+  async handle(recherche: string): Promise<Either<RechercheLocalisation>> {
+    if (this.isRechercheByNumeroDepartement(recherche)) {
+      return await this.getLocalisationByNumeroDepartement(recherche);
+    } else if (this.isRechercheByNumeroCodePostal(recherche)) {
+      return await this.getLocalisationByNumeroCodePostal(recherche);
+    } else {
+      return await this.getLocalisationByNom(recherche);
+    }
+  }
+
+  private isRechercheByNumeroCodePostal(recherche: string): boolean {
+    return this.checkRechercheOnlyNumber(this.CODE_POSTAL_LENGTH, recherche);
+  }
+
+  private isRechercheByNumeroDepartement(recherche: string): boolean {
+    return this.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTH, recherche);
+  }
+
+  private checkRechercheOnlyNumber(length: number, recherche: string): boolean {
+    return new RegExp(/^\d*$/).test(recherche) && recherche.length === length;
+  }
+
+  private async getLocalisationByNumeroDepartement(recherche: string): Promise<Either<RechercheLocalisation>> {
+    try {
+      return createSuccess({
+        communeList: [],
+        départementList: await this.localisationRepository.getDépartementListByNuméroDépartement(recherche),
+        régionList: [],
+      });
+    } catch {
+      return createFailure(ErrorType.ERREUR_INATTENDUE);
+    }
+  }
+
+  private async getLocalisationByNumeroCodePostal(recherche: string): Promise<Either<RechercheLocalisation>> {
+    const communeList = await this.localisationAvecCoordonnéesRepository.getCommuneList(recherche);
+
+    if (communeList.instance === 'success') {
+      return createSuccess({
+        communeList: communeList.result.résultats,
+        départementList: [],
+        régionList: [],
+      });
     }
 
-    if(RechercherLocalisationUseCase.checkRechercheOnlyNumber(this.CODE_POSTAL_LENGTH, recherche)) {
-      return  {
-        communeList : await this.localisationRepository.getCommuneListByCodePostal(recherche),
-        départementList : [],
-        régionList : [],
-      };
-    }
+    return createFailure(communeList.errorType);
+  }
 
+  private async getLocalisationByNom(recherche: string): Promise<Either<RechercheLocalisation>> {
     const [communeList, départementList, régionList] = await Promise.all([
-      this.localisationRepository.getCommuneListByNom(recherche),
+      this.localisationAvecCoordonnéesRepository.getCommuneList(recherche),
       this.localisationRepository.getDépartementListByNom(recherche),
       this.localisationRepository.getRégionListByNom(recherche),
     ]);
-    return {
-      communeList,
-      départementList,
-      régionList,
-    };
-  }
 
-  private static checkRechercheOnlyNumber(length: number, recherche: string) {
-    return new RegExp(/^\d*$/).test(recherche) && recherche.length === length;
+    if (communeList.instance === 'success') {
+      return createSuccess({
+        communeList: communeList.result.résultats,
+        départementList,
+        régionList,
+      });
+    }
+
+    return createFailure(communeList.errorType);
   }
 }
