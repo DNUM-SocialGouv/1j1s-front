3d3de2a50001f18c673661c7fa77d0b43edad94a
Sergen <sergen.kovar@octo.com>
2022-07-22 17:19:52 +0200

    Review : Modifiacation

--- src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
+++ src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
@@ -1,4 +1,4 @@
-import { Modal, ModalClose, ModalContent, ModalTitle, TextInput } from '@dataesr/react-dsfr';
+import { Modal, ModalClose, ModalContent, ModalTitle } from '@dataesr/react-dsfr';
 import React, { useState } from 'react';
 
 import styles from '~/client/components/features/ContratEngagementJeune/Rappel/Rappel.module.scss';
@@ -13,70 +13,74 @@ export default function Rappel() {
         <Marked markdown={'## J\'ai des questions sur le Contrat d\'Engagement Jeune'}/>
         <Button buttonType="primary">Je souhaite être contacté(e)</Button>
       </div>
+      <div className={ styles.rappelModal }>
       <Modal
         isOpen={isPopInOpen}
         hide={() => setIsPopInOpen(false)}
         data-testid="FormulaireRappel"
-        className={styles.modal}
+        className={ styles.rappelModal__Open }
       >
         <ModalClose hide={() => setIsPopInOpen(false)} title="Fermer les filtres"/>
-        <ModalTitle className={styles.modalTitle}>
+        <ModalTitle className={styles.rappelModal__Title}>
             J&apos;ai des questions sur le Contrat d&apos;Engagement Jeune et souhaite être rappelé
         </ModalTitle>
-        <ModalContent className={styles.modalContenu}>
-          <form className={styles.modalForm}>
-            <label htmlFor='firstName'>Prénom</label>
-            <TextInput
+        <ModalContent>
+          <form className={styles.rappelModal__Form}>
+            <label htmlFor='firstName'>Prénom
+            <input
               type='text'
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
               name="firstName"
-              placeholder="Exemple : Jean"/>
-            <label htmlFor='lastName'>Nom</label>
-            <TextInput
+              placeholder="Exemple : Jean"/></label>
+            <label htmlFor='lastName'>Nom
+            <input
               type='text'
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
               name="lastName"
-              placeholder="Exemple : Dupont"/>
-            <label htmlFor='email'>Adresse email</label>
-            <TextInput
+              placeholder="Exemple : Dupont"/></label>
+            <label htmlFor='email'>Adresse email
+            <input
               type='text'
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
               name="email"
-              placeholder="Exemple : jean.dupont@gmail.com"/>
-            <label htmlFor='phone'>Téléphone</label>
-            <TextInput
+              placeholder="Exemple : jean.dupont@gmail.com"/></label>
+            <label htmlFor='phone'>Téléphone
+            <input
               type='text'
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
               name="phone"
-              placeholder="Exemple : 0606060606"/>
-            <label htmlFor='age'>Age</label>
-            <TextInput
+              placeholder="Exemple : 0606060606"/></label>
+            <label htmlFor='age'>Age
+            <input
               type='text'
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
               name="age"
-              placeholder="Exemple : 21"/>
-            <label htmlFor='country'>Ville</label>
-            <TextInput
+              placeholder="Exemple : 21"/></label>
+            <label htmlFor='country'>Ville
+            <input
               type='text'
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
               name="country"
-              placeholder="Exemple : Paris"/>
-            J&apos;accepte de recevoir des informations de « 1 Jeune, 1 Solution »
+              placeholder="Exemple : Paris"/></label>
+            <p>J&apos;accepte de recevoir des informations de « 1 Jeune, 1 Solution »</p>
             <Button
               buttonType="primary"
             >
-          Envoyer la demande
+              Envoyer la demande
             </Button>
-        En cliquant sur &quot;Envoyer la demande&quot;, j&apos;accepte d&apos;être recontacté par Pôle Emploi ou la Mission Locale la plus proche de chez moi, dans le cadre du Contrat d&apos;Engagement Jeune
           </form>
+          <div className={styles.rappelModal__Open__Text}>
+        <p>En cliquant sur &quot;Envoyer la demande&quot;, j&apos;accepte d&apos;être recontacté par Pôle Emploi ou la Mission Locale la plus proche de chez moi, dans le cadre du Contrat d&apos;Engagement Jeune</p>
+        </div>
         </ModalContent>
       </Modal>
+      </div>
     </section>
   );
 }
