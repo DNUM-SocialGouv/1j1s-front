70d9f137cf8b313d65d6f4417de59d0ff738e9f3
Sergen <sergen.kovar@octo.com>
2022-07-22 14:12:43 +0200

    Update review

--- src/client/components/features/ContratEngagementJeune/Allocations/Allocations.tsx
+++ src/client/components/features/ContratEngagementJeune/Allocations/Allocations.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image';
+import illustration from 'public/images/CEJ/benefit-from-it.png';
 import React from 'react';
 
+import styles from '~/client/components/features/ContratEngagementJeune/Allocations/Allocations.module.scss';
 import { AccordionComponent } from '~/client/components/ui/Accordion/AccordionComponent';
 import Marked from '~/client/components/ui/Marked/Marked';
 import useBreakpoint from '~/client/hooks/useBreakpoint';
-import styles from '~/pages/contrat-engagement-jeune/Allocations.module.scss';
-
-import illustration from '../../../public/images/CEJ/benefit-from-it.png';
 
 const contenu = `
 Je perçois une allocation pouvant aller jusqu’à 500 euros par mois en fonction de : 
@@ -21,17 +20,18 @@ export default function Allocations() {
   const { isSmallScreen, isMediumScreen } = useBreakpoint();
   const displayAccordion = isSmallScreen || isMediumScreen;
   return (
-    <section className={ styles.section }>
-      <div className={ styles.container }>
-        <aside className= { styles.illustration }>
-          <Image src={ illustration } objectFit='cover' layout='intrinsic' alt='' />
+    <section className={ styles.allocations }>
+      <div className={ styles.allocationsContainer }>
+        <aside className= { styles.allocationsIllustration }>
+          <Image src={ illustration } objectFit='cover' alt='' />
         </aside>
-        <article className={ styles.article }>
-          <h2 className= { styles.titre }>Est-ce que je peux bénéficier de l&apos;allocation ?</h2>
+        <article className={ styles.allocationsArticle }>
+          <h2 className={ styles.allocationsArticle__Title }>Est-ce que je peux bénéficier de l&apos;allocation ?</h2>
           {!displayAccordion && <Marked markdown={ contenu }/>}
-          {displayAccordion && (<span className={ styles.accordion }><AccordionComponent>
-            <Marked markdown={ contenu }/>
-          </AccordionComponent></span>)}
+          {displayAccordion && (
+            <AccordionComponent>
+              <Marked markdown={ contenu }/>
+            </AccordionComponent>)}
         </article>
       </div>
     </section>
--- src/pages/contrat-engagement-jeune/index.tsx
+++ src/pages/contrat-engagement-jeune/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 
-import Allocations from '~/pages/contrat-engagement-jeune/Allocations';
-import Bannière from '~/pages/contrat-engagement-jeune/Bannière';
+import Allocations from '~/client/components/features/ContratEngagementJeune/Allocations/Allocations';
+import Bannière from '~/client/components/features/ContratEngagementJeune/Bannière/Bannière';
+import QuEstCeQueCEst from '~/client/components/features/ContratEngagementJeune/QuEstCeQueCest/QuEstCeQueCEst';
 import PourquoiCEstFaitPourMoi from '~/pages/contrat-engagement-jeune/PourquoiCEstFaitPourMoi';
-import QuEstCeQueCEst from '~/pages/contrat-engagement-jeune/QuEstCeQueCEst';
-
+import { HeadTag } from '~/client/components/utils/HeaderTag';
 
 export default function ContratEngagementJeune() {
   return (
     <>
-      <Bannière/>
-      <QuEstCeQueCEst />
-      <PourquoiCEstFaitPourMoi/>
-      <Allocations />
+      <HeadTag
+        title='Contrat Engagement Jeune'
+        description="Plus de 400 000 offres d'emplois et d'alternances sélectionnées pour vous"
+      />
+      <main id='contenu'>
+        <Bannière />
+        <QuEstCeQueCEst />
+        <PourquoiCEstFaitPourMoi/>
+        <Allocations />
+      </main>
     </>
   );
 }
