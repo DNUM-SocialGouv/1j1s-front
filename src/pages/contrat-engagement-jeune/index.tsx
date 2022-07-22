e708a81be161de3db8e5a1c237390ccfc1604003
toma-benguigui <108266581+toma-benguigui@users.noreply.github.com>
2022-07-22 10:07:33 +0200

    [319] Ajout du bloc 'Le CEJ est fait pour moi si' (#132)
    * Ajout du bloc desktop 'Le CEJ est fait pour moi si'
    
    * Ajout du style du bloc mobile 'Le CEJ est fait pour moi si'
    
    * Renommage de PkCPourMoi en PourquoiCEstFaitPourMoi
    
    * Retours de review de Florian et Olivia

--- src/pages/contrat-engagement-jeune/index.tsx
+++ src/pages/contrat-engagement-jeune/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import Bannière from '~/pages/contrat-engagement-jeune/Bannière';
+import PourquoiCEstFaitPourMoi from '~/pages/contrat-engagement-jeune/PourquoiCEstFaitPourMoi';
 import QuEstCeQueCEst from '~/pages/contrat-engagement-jeune/QuEstCeQueCEst';
 
 
 export default function ContratEngagementJeune() {
   return (
     <>
-      <Bannière />
+      <Bannière/>
       <QuEstCeQueCEst />
+      <PourquoiCEstFaitPourMoi/>
     </>
   );
 }
