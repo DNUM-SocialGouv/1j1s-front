79b3519960a68190167c79e72ac0a7df54c1f715
Florent Jaby <florent.jaby@gmail.com>
2022-07-22 18:27:23 +0200

    feat/322: Ajoute la compatibilité mobile

--- src/pages/contrat-engagement-jeune/Témoignages.tsx
+++ src/pages/contrat-engagement-jeune/Témoignages.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import React from 'react';
 
+import { AccordionComponent } from '~/client/components/ui/Accordion/AccordionComponent';
 import Marked from '~/client/components/ui/Marked/Marked';
+import useBreakpoint from '~/client/hooks/useBreakpoint';
 import styles from '~/pages/contrat-engagement-jeune/Témoignages.module.scss';
 
 import portraitKévin from '../../../public/images/CEJ/vignette-kevin.jpg';
@@ -21,18 +23,23 @@ const programme = `
 `;
 
 export function TémoignageKévin () {
+  const { isSmallScreen } = useBreakpoint();
+  let ContenuProgramme = (<Marked className={ styles.programme } markdown={ programme }/>);
+  if (isSmallScreen) {
+    ContenuProgramme = (<AccordionComponent>{ ContenuProgramme }</AccordionComponent>);
+  }
   return (
     <section className={ styles.témoignage}>
       <article>
         <h2>Ce que le Contrat d&apos;Engagement Jeune proposera à Kévin</h2>
         <div className={ styles.portrait }>
-          <Image src={ portraitKévin } layout='intrinsic' alt='' />
+          <Image src={ portraitKévin } alt="Portrait de Kévin" />
         </div>
         <div className={ styles.bio }>
           <h3>Kévin, 18 ans</h3>
           <p >Sans diplôme et sans aucune ressource financière, il pourra bénéficier du Contrat d&apos;Engagement Jeune.</p>
         </div>
-        <Marked className={ styles.programme } markdown={ programme } />
+        { ContenuProgramme }
       </article>
     </section>
   );
