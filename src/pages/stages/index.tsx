0f81ee749c506774e3141e783db02b913844b06f
Simon Belbeoch <sbelbeoch@octo.com>
2022-07-26 17:36:00 +0200

    feature[stage]: 1ere version du design de la liste des stages
    Redesign et ajout d'un feature flipping en dur pour enlever le lien vers la page

--- src/pages/stages/index.tsx
+++ src/pages/stages/index.tsx
@@ -1,40 +1,59 @@
-import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
+import { SearchClient } from 'algoliasearch-helper/types/algoliasearch';
+import { marked } from 'marked';
 import React from 'react';
-import { CurrentRefinements, Hits,InstantSearch, Pagination, SearchBox } from 'react-instantsearch-hooks-web';
+import { Configure,CurrentRefinements, Hits,InstantSearch, Pagination, SearchBox } from 'react-instantsearch-hooks-web';
 
 import { OffreDeStageIndexée } from '~/client/components/features/OffreDeStage/OffreDeStage.type';
 import {
   RésultatRechercherSolution,
 } from '~/client/components/layouts/RechercherSolution/Résultat/RésultatRechercherSolution';
+import { Hero } from '~/client/components/ui/Hero/Hero';
 import { HeadTag } from '~/client/components/utils/HeaderTag';
+import { useDependency } from '~/client/context/dependenciesContainer.context';
+import styles from '~/pages/stages/RechercherStagePage.module.css';
 
-const searchClient = instantMeiliSearch(
-  'http://localhost:7700',
-  'masterKey',
-);
 
 const IMAGE_FIXE = '/images/logos/fallback.svg';
 const Résultat = (({ hit: résultat }: { hit: OffreDeStageIndexée }) => {
   return <RésultatRechercherSolution
-    lienOffre={`/stages/${résultat.slug}`}
+    lienOffre={`/stages/${'idNonExistant' || résultat.slug}`}
     intituléOffre={résultat.titre}
     logoEntreprise={IMAGE_FIXE}
-    nomEntreprise={résultat.nomEmployeur}
-    descriptionOffre={résultat.description}
+    nomEntreprise={résultat.nomEmployeur || 'Employeur'}
+    descriptionOffre={marked.parse(résultat.description) }
     étiquetteOffreList = {résultat.domaines || [] }
+    key={résultat.slug}
   />;
 });
 
 export default function RechercherOffreStagePage() {
+  const searchClient = useDependency<SearchClient>('rechercheClientService');
   return (
     <><HeadTag
-      title={'Des milliers d\'offres de stages sélectionnés pour vous'}
+      title={'Des milliers d’offres de stages sélectionnés pour vous'}
       description="Des milliers d'offres de stages sélectionnées pour vous"/>
-    <main id="contenu">
+    <Hero>
+        Des milliers d’offres de stages sélectionnés pour vous
+    </Hero>
+    <main id="contenu" className={'fr-container'}>
       <InstantSearch searchClient={searchClient} indexName="offre-de-stage">
-        <SearchBox/>
+        <Configure hitsPerPage={15}/>
+        <SearchBox classNames={
+          {
+            form: [styles.stageFormElement].join(' '),
+            input: ['fr-input', styles.stageInputElement].join(' '),
+            root: styles.stageRootElement,
+            submitIcon: styles.stageSubmitIconElement,
+          }
+        }/>
         <CurrentRefinements/>
-        <Hits hitComponent={Résultat}  />
+        <Hits hitComponent={Résultat} classNames={
+          {
+            item: styles.stageItemElement,
+            list: styles.stageListeElement,
+            root: styles.stageListeRootElement,
+          }
+        }/>
         <Pagination/>
       </InstantSearch>
     </main>
