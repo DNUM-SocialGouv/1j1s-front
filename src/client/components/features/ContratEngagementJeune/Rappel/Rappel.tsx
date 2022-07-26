50fd06a872fa4dad9d2ac6c12a9e836c6c2be77d
Sergen <sergen.kovar@octo.com>
2022-07-26 18:04:02 +0200

    Mise en place formulaire

--- src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
+++ src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
@@ -1,4 +1,4 @@
-import { Modal, ModalClose, ModalContent, ModalFooter, ModalTitle } from '@dataesr/react-dsfr';
+import { Modal, ModalClose, ModalContent, ModalFooter, ModalTitle, TextInput } from '@dataesr/react-dsfr';
 import React, { useState } from 'react';
 
 import styles from '~/client/components/features/ContratEngagementJeune/Rappel/Rappel.module.scss';
@@ -6,7 +6,7 @@ import { Button } from '~/client/components/ui/Button/Button';
 import Marked from '~/client/components/ui/Marked/Marked';
 
 export default function Rappel() {
-  const [isFiltresAvancésMobileOpen, setIsFiltresAvancésMobileOpen] = useState(false);
+  const [isPopInOpen, setIsPopInOpen] = useState(false);
   return (
     <section className={styles.rappel}>
       <div className={styles.rappelContainer}>
@@ -14,35 +14,71 @@ export default function Rappel() {
         <Button buttonType="primary">Je souhaite être contacté(e)</Button>
       </div>
       <Modal
-        isOpen={isFiltresAvancésMobileOpen}
-        hide={() => setIsFiltresAvancésMobileOpen(false)}
-        data-testid="FiltreRechercheMobile"
+        isOpen={isPopInOpen}
+        hide={() => setIsPopInOpen(false)}
+        data-testid="FormulaireRappel"
       >
-        <ModalClose hide={() => setIsFiltresAvancésMobileOpen(false)} title="Fermer les filtres"/>
+        <ModalClose hide={() => setIsPopInOpen(false)} title="Fermer les filtres"/>
         <ModalTitle className={styles.filtresAvancésModalTitle}>
             J&apos;ai des questions sur le Contrat d&apos;Engagement Jeune et souhaite être rappelé
         </ModalTitle>
         <ModalContent className={styles.filtresAvancésModalContenu}>
           <form>
             <label>
-          Nom
-              <input
-                name="isGoing"
-                type="text" 
-                placeholder='Exemple : Dupont'/>
+              Prénom
+              <TextInput
+                type='text'
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                name="firstName"
+                placeholder="Exemple : Jean"/>
             </label>
-            <br />
             <label>
-          Prénom
-              <input
-                name="numberOfGuests"
-                type="text"
-                placeholder='Exemple : Jean' />
+              Nom
+              <TextInput
+                type='text'
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                name="lastName"
+                placeholder="Exemple : Dupont"/>
             </label>
-          </form>
-              
-              
-              
+            <label>
+              Adresse email
+              <TextInput
+                type='text'
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                name="email"
+                placeholder="Exemple : jean.dupont@gmail.com"/>
+            </label>
+            <label>
+              Téléphone
+              <TextInput
+                type='number'
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                name="phone"
+                placeholder="Exemple : 0606060606"/>
+            </label>
+            <label>
+              Age
+              <TextInput
+                type='text'
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                name="age"
+                placeholder="Exemple : 21 ans"/>
+            </label>
+            <label>
+              Ville
+              <TextInput
+                type='text'
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                name="ville"
+                placeholder="Exemple : Paris"/>
+            </label>
+          </form>    
         </ModalContent>
         <ModalFooter className={styles.filtresAvancésModalFooter}>
               
