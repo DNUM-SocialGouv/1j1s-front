d2cfcdce32253d1750cc8c839be8bcb1822d48ab
Sergen <sergen.kovar@octo.com>
2022-07-27 16:37:18 +0200

    Essai du formulaire

--- src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
+++ src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
@@ -1,4 +1,4 @@
-import { Modal, ModalClose, ModalContent, ModalFooter, ModalTitle, TextInput } from '@dataesr/react-dsfr';
+import { Modal, ModalClose, ModalContent, ModalTitle, TextInput } from '@dataesr/react-dsfr';
 import React, { useState } from 'react';
 
 import styles from '~/client/components/features/ContratEngagementJeune/Rappel/Rappel.module.scss';
@@ -17,72 +17,65 @@ export default function Rappel() {
         isOpen={isPopInOpen}
         hide={() => setIsPopInOpen(false)}
         data-testid="FormulaireRappel"
+        className={styles.modal}
       >
         <ModalClose hide={() => setIsPopInOpen(false)} title="Fermer les filtres"/>
-        <ModalTitle className={styles.filtresAvancésModalTitle}>
+        <ModalTitle className={styles.modalTitle}>
             J&apos;ai des questions sur le Contrat d&apos;Engagement Jeune et souhaite être rappelé
         </ModalTitle>
-        <ModalContent className={styles.filtresAvancésModalContenu}>
-          <form>
-            <label>
-              Prénom
-              <TextInput
-                type='text'
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                name="firstName"
-                placeholder="Exemple : Jean"/>
-            </label>
-            <label>
-              Nom
-              <TextInput
-                type='text'
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                name="lastName"
-                placeholder="Exemple : Dupont"/>
-            </label>
-            <label>
-              Adresse email
-              <TextInput
-                type='text'
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                name="email"
-                placeholder="Exemple : jean.dupont@gmail.com"/>
-            </label>
-            <label>
-              Téléphone
-              <TextInput
-                type='number'
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                name="phone"
-                placeholder="Exemple : 0606060606"/>
-            </label>
-            <label>
-              Age
-              <TextInput
-                type='text'
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                name="age"
-                placeholder="Exemple : 21 ans"/>
-            </label>
-            <label>
-              Ville
-              <TextInput
-                type='text'
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                name="ville"
-                placeholder="Exemple : Paris"/>
-            </label>
-          </form>    
+        <ModalContent className={styles.modalContenu}>
+          <form className={styles.modalForm}>
+            <label htmlFor='firstName'>Prénom</label>
+            <TextInput
+              type='text'
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              name="firstName"
+              placeholder="Exemple : Jean"/>
+            <label htmlFor='lastName'>Nom</label>
+            <TextInput
+              type='text'
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              name="lastName"
+              placeholder="Exemple : Dupont"/>
+            <label htmlFor='email'>Adresse email</label>
+            <TextInput
+              type='text'
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              name="email"
+              placeholder="Exemple : jean.dupont@gmail.com"/>
+            <label htmlFor='phone'>Téléphone</label>
+            <TextInput
+              type='text'
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              name="phone"
+              placeholder="Exemple : 0606060606"/>
+            <label htmlFor='age'>Age</label>
+            <TextInput
+              type='text'
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              name="age"
+              placeholder="Exemple : 21"/>
+            <label htmlFor='country'>Ville</label>
+            <TextInput
+              type='text'
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              name="country"
+              placeholder="Exemple : Paris"/>
+            J&apos;accepte de recevoir des informations de « 1 Jeune, 1 Solution »
+            <Button
+              buttonType="primary"
+            >
+          Envoyer la demande
+            </Button>
+        En cliquant sur &quot;Envoyer la demande&quot;, j&apos;accepte d&apos;être recontacté par Pôle Emploi ou la Mission Locale la plus proche de chez moi, dans le cadre du Contrat d&apos;Engagement Jeune
+          </form>
         </ModalContent>
-        <ModalFooter className={styles.filtresAvancésModalFooter}>
-              
-        </ModalFooter>
       </Modal>
     </section>
   );
