5a2b8daea1afdba156884a99abf222f1b85b7fb0
Caroline Chuong - CACH <caroline.chuong@octo.com>
2022-07-27 11:13:15 +0200

    feat[recherche]: ajout d'un placeholder dans la recherche de stage

--- src/pages/stages/index.tsx
+++ src/pages/stages/index.tsx
@@ -1,12 +1,10 @@
 import { SearchClient } from 'algoliasearch-helper/types/algoliasearch';
 import { marked } from 'marked';
 import React from 'react';
-import { Configure,CurrentRefinements, Hits,InstantSearch, Pagination, SearchBox } from 'react-instantsearch-hooks-web';
+import { Configure, CurrentRefinements, Hits, InstantSearch, Pagination, SearchBox } from 'react-instantsearch-hooks-web';
 
 import { OffreDeStageIndexée } from '~/client/components/features/OffreDeStage/OffreDeStage.type';
-import {
-  RésultatRechercherSolution,
-} from '~/client/components/layouts/RechercherSolution/Résultat/RésultatRechercherSolution';
+import { RésultatRechercherSolution } from '~/client/components/layouts/RechercherSolution/Résultat/RésultatRechercherSolution';
 import { Hero } from '~/client/components/ui/Hero/Hero';
 import { HeadTag } from '~/client/components/utils/HeaderTag';
 import { useDependency } from '~/client/context/dependenciesContainer.context';
@@ -20,8 +18,8 @@ const Résultat = (({ hit: résultat }: { hit: OffreDeStageIndexée }) => {
     intituléOffre={résultat.titre}
     logoEntreprise={IMAGE_FIXE}
     nomEntreprise={résultat.nomEmployeur || 'Employeur'}
-    descriptionOffre={marked.parse(résultat.description) }
-    étiquetteOffreList = {résultat.domaines || [] }
+    descriptionOffre={marked.parse(résultat.description)}
+    étiquetteOffreList={résultat.domaines || []}
     key={résultat.slug}
   />;
 });
@@ -33,7 +31,7 @@ export default function RechercherOffreStagePage() {
       title={'Des milliers d’offres de stages sélectionnés pour vous'}
       description="Des milliers d'offres de stages sélectionnées pour vous"/>
     <Hero>
-        Des milliers d’offres de stages sélectionnés pour vous
+                Des milliers d’offres de stages sélectionnés pour vous
     </Hero>
     <main id="contenu" className={'fr-container'}>
       <InstantSearch searchClient={searchClient} indexName="offre-de-stage">
@@ -45,7 +43,9 @@ export default function RechercherOffreStagePage() {
             root: styles.stageRootElement,
             submitIcon: styles.stageSubmitIconElement,
           }
-        }/>
+        }
+        placeholder="Métiers, mots clés, …"
+        />
         <CurrentRefinements/>
         <Hits hitComponent={Résultat} classNames={
           {
