844b0d3be1fa0c7fc667c4a53280488972a02b5d
Olivia Peny-Coblentz <oliv@octo.com>
2022-07-28 15:53:55 +0200

    add test

---
+++ tests/client/components/ui/Link/Link.test.tsx
@@ -0,0 +1,43 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom';
+
+import { render, screen } from '@testing-library/react';
+
+import { Link } from '~/client/components/ui/Link/Link';
+
+describe('Link', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('quand le lien est un lien externe', () => {
+    it('retourne le composant Link avec un *tag a* directement et les propriétés target et rel', () => {
+      const lienExterne = 'https://mon-lien-externe';
+      render(
+        <Link link={lienExterne} getLinkType={jest.fn()} />,
+      );
+
+      const linkComponent = screen.getByRole('link');
+
+      expect(linkComponent).toHaveAttribute('target');
+      expect(linkComponent).toHaveAttribute('rel');
+    });
+  });
+
+  describe('quand le lien est un lien interne', () => {
+    it('retourne le composant Link avec sans les propriétés de la redirection externe', () => {
+      const lienInterne = '/emplois';
+      render(
+        <Link link={lienInterne} getLinkType={jest.fn()} />,
+      );
+
+      const linkComponent = screen.getByRole('link');
+
+      expect(linkComponent).not.toHaveAttribute('target');
+      expect(linkComponent).not.toHaveAttribute('rel');
+    });
+  });
+  
+});
