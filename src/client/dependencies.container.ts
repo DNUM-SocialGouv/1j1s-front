6764fb05fdf97eb65e03375870cbadb95ccc23ad
Florian Leroy <florian.leroy@octo.com>
2022-07-28 09:14:59 +0200

    fix: linter

--- src/client/dependencies.container.ts
+++ src/client/dependencies.container.ts
@@ -19,6 +19,13 @@ export type Dependencies = {
   rechercheClientService: SearchClient
 }
 
+class DependencyInitException extends Error {
+  constructor(dependencyName: string, reason: string) {
+    super(`Cannot init ${dependencyName} dependency, reason: ${reason}`);
+    this.name = 'MatomoInitException';
+  }
+}
+
 export default function dependenciesContainer(sessionId: string): Dependencies {
   const loggerService = new LoggerService(sessionId);
   const httpClientService =  new HttpClientService(sessionId, loggerService);
@@ -27,9 +34,20 @@ export default function dependenciesContainer(sessionId: string): Dependencies {
   const alternanceService = new AlternanceService(httpClientService);
   const métierRecherchéService = new MétierRecherchéService(httpClientService);
   const missionEngagementService = new MissionEngagementService(httpClientService);
+
+  const meiliSearchBaseUrl = process.env.NEXT_PUBLIC_STAGE_SEARCH_ENGINE_BASE_URL;
+  const meiliSearchApiKey = process.env.NEXT_PUBLIC_STAGE_SEARCH_ENGINE_API_KEY;
+
+  if (!meiliSearchApiKey || !meiliSearchBaseUrl) {
+    throw new DependencyInitException(
+      'rechercheClientService',
+      'NEXT_PUBLIC_STAGE_SEARCH_ENGINE_BASE_URL or NEXT_PUBLIC_STAGE_SEARCH_ENGINE_API_KEY environment variable is missing',
+    );
+  }
+
   const rechercheClientService = instantMeiliSearch(
-    process.env.NEXT_PUBLIC_STAGE_SEARCH_ENGINE_BASE_URL!,
-    process.env.NEXT_PUBLIC_STAGE_SEARCH_ENGINE_API_KEY!,
+    meiliSearchBaseUrl,
+    meiliSearchApiKey,
   );
 
   return {
