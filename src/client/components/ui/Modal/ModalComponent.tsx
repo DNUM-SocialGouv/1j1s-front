28710da4ea18009503a434d243b183a5658d07f0
Paul Gallo <paul.gallo@octo.com>
2022-07-28 16:49:54 +0200

    feat: refacto types modal

--- src/client/components/ui/Modal/ModalComponent.tsx
+++ src/client/components/ui/Modal/ModalComponent.tsx
@@ -5,12 +5,7 @@ import { createPortal } from 'react-dom';
 import { CloseIcon } from '~/client/components/ui/Icon/close.icon';
 import styles from '~/client/components/ui/Modal/ModalComponent.module.scss';
 
-interface ModalProps {
-  className: string
-	isOpen: boolean
-}
-
-export function ModalComponent({ children,  className, isOpen, ...rest }: React.PropsWithChildren<ModalProps>) {
+export function ModalComponent({ children, className, isOpen, ...rest }: { isOpen: boolean } & React.HTMLAttributes<HTMLDialogElement>) {
   function disableDocumentBodyScroll() {
     isOpen ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'visible';
   }
@@ -35,9 +30,9 @@ interface ModalCloseProps {
   title?: string
 }
 
-function ModalClose({ close, label = 'Fermer', title = 'Fermer la modale' }: ModalCloseProps) {
+function ModalClose({ className, close, label = 'Fermer', title = 'Fermer la modale' }: ModalCloseProps & React.HTMLAttributes<HTMLDivElement>) {
   return ( 
-    <div className={styles.modalClose}>
+    <div className={classNames(className, styles.modalClose)}>
       <button type="button" title={title} onClick={() => close()}>
         <span className={styles.modalCloseLabel}>{label}</span>
         <CloseIcon className={styles.modalCloseIcon}/>
@@ -48,15 +43,15 @@ function ModalClose({ close, label = 'Fermer', title = 'Fermer la modale' }: Mod
 
 type TitleLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
-function ModalTitle({ titleLevel = 'h1', children, className }: React.PropsWithChildren<{ className: string, titleLevel?: TitleLevel }>) {
+function ModalTitle({ titleLevel = 'h1', children, className }: { titleLevel?: TitleLevel } & React.HTMLAttributes<HTMLTitleElement>) {
   return React.createElement(titleLevel, { className: classNames(className, styles.modalTitle) }, children);
 }
 
-function ModalContent({ children, className }: React.PropsWithChildren<{ className: string }>) {
+function ModalContent({ children, className }: React.HTMLAttributes<HTMLDivElement>) {
   return <div className={classNames(className, styles.modalContent)}>{children}</div>;
 }
 
-function ModalFooter({ children, className }: React.PropsWithChildren<{ className: string }>) {
+function ModalFooter({ children, className }: React.HTMLAttributes<HTMLDivElement>) {
   return <footer className={classNames(className, styles.modalFooter)}>{children}</footer>;
 }
 
