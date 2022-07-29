855bcbc1ef72bf20a1c2defe19595d8df62b30e2
Naorid <ashur4d@gmail.com>
2022-07-29 17:57:07 +0200

    Refacto : Skeleton scss

--- src/client/components/ui/Loader/Skeleton/Skeleton.tsx
+++ src/client/components/ui/Loader/Skeleton/Skeleton.tsx
@@ -2,27 +2,57 @@ import classNames from 'classnames';
 
 import styles from '~/client/components/ui/Loader/Skeleton/Skeleton.module.scss';
 
-export const Skeleton = () => {
+function card() {
   return (
-    <div className={styles.background}>
-      <div className={styles.header}>
-        <div className={classNames(styles.gradient, styles.image)}></div>
-        <div className={styles.companyName}>
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <div className={classNames(styles.gradient, styles.cardImage)}></div>
+        <div>
           <div className={classNames(styles.gradient, styles.textBold)}></div>
           <div className={classNames(styles.gradient, styles.text)}></div>
         </div>
       </div>
-      <div className={classNames(styles.gradient, styles.line)}></div>
-      <div className={styles.tags}>
-        <div className={classNames(styles.gradient, styles.tag)}></div>
-        <div className={classNames(styles.gradient, styles.tag)}></div>
-        <div className={classNames(styles.gradient, styles.tag)}></div>
-      </div>
-      <div className={classNames(styles.gradient, styles.body)}></div>
-      <div className={styles.links}>
-        <div className={classNames(styles.gradient, styles.link)}></div>
-        <div className={classNames(styles.gradient, styles.link2)}></div>
+      {tagList()}
+      <div className={classNames(styles.gradient, styles.cardContent)}></div>
+      <div className={styles.cardLink}>
+        <div className={classNames(styles.gradient, styles.cardLinkLabel)}></div>
+        <div className={classNames(styles.gradient, styles.cardLinkIcon)}></div>
       </div>
     </div>
+  );
+}
+
+function cardList() {
+  return (
+    <div>
+      {card()}
+      {card()}
+      {card()}
+    </div>
+  );
+}
+
+function tagList() {
+  return (
+    <ul className={styles.tagList}>
+      <li className={classNames(styles.gradient, styles.tag)}></li>
+      <li className={classNames(styles.gradient, styles.tag)}></li>
+      <li className={classNames(styles.gradient, styles.tag)}></li>
+    </ul>
+  );
+}
+
+function text() {
+  return (
+    <div className={classNames(styles.gradient, styles.text)}></div>
+  );
+}
+
+export const Skeleton = () => {
+  return (
+    <>
+      {cardList()}
+      {text()}
+    </>
   );
 };
