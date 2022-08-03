b551477ec63e29a535982e3c369e17baa66aec2d
Florent Jaby <florent.jaby@gmail.com>
2022-08-03 18:26:11 +0200

    bsr: utilitaires pour Either

--- src/server/errors/either.ts
+++ src/server/errors/either.ts
@@ -1,8 +1,19 @@
 import { ErreurMétier } from '~/server/errors/erreurMétier.types';
 
+export type Either<T> = Success<T> | Failure;
 export type Failure = { instance: 'failure', errorType: ErreurMétier };
 export type Success<T> = { instance: 'success', result: T };
-export type Either<T> = Success<T> | Failure;
+
+export function isSuccess<T> (e: Either<T>): e is Success<T> {
+  return e.instance === 'success';
+}
+
+export function isFailure<T> (e: Either<T>): e is Failure {
+  return e.instance === 'failure';
+}
+
+
+
 
 export const createFailure = (errorType: ErreurMétier): Failure => ({ errorType, instance: 'failure' });
 export const createSuccess = <T>(result: T): Success<T> => ({ instance: 'success', result });
