e78c4e24a6635da98473a3f19e20968ece1af9a3
thomas.benguigui <thomas.benguigui@octo.com>
2022-08-03 15:27:07 +0200

    [337] Modifications de l'UI et du bloc au dessus

--- src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
+++ src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
@@ -3,29 +3,13 @@ import React from 'react';
 import styles from '~/client/components/features/ContratEngagementJeune/Rappel/Rappel.module.scss';
 import { Button } from '~/client/components/ui/Button/Button';
 import Marked from '~/client/components/ui/Marked/Marked';
-import useBreakpoint from '~/client/hooks/useBreakpoint';
-
-const titre = `
-## J'ai des questions sur le Contrat d'Engagement Jeune et souhaite être rappelé(e)
-`;
-const titreMobile = `
-## J'ai des questions sur le Contrat d'Engagement Jeune
-`;
 
 export default function Rappel() {
-  const { isSmallScreen, isMediumScreen } = useBreakpoint();
-  const displayScreen = isSmallScreen || isMediumScreen;
-  const buttonMobile = 'Je me lance  >';
-  const buttonTitle = 'Je souhaite être contacté(e)';
   return (
-    <section className={ styles.rappel }>
-      <div className={ styles.rappelContainer }>
-        {displayScreen ? <Marked markdown={ titreMobile }/> : <Marked markdown={ titre }/> }
-        <Button
-          buttonType="primary"
-        >
-          {displayScreen ? buttonMobile : buttonTitle }
-        </Button>
+    <section className={styles.rappel}>
+      <div className={styles.rappelContainer}>
+        <Marked markdown={'## J\'ai des questions sur le Contrat d\'Engagement Jeune'}/>
+        <Button buttonType="primary">Je souhaite être contacté(e)</Button>
       </div>
     </section>
   );
