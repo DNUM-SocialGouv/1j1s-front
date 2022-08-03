8409f99682a6e323653b22cd24c667c89637b851
OCTO-CLEM <theo.clement@octo.com>
2022-08-03 12:10:43 +0200

    feat: desktop

--- src/client/components/layouts/Footer/Footer.tsx
+++ src/client/components/layouts/Footer/Footer.tsx
@@ -10,32 +10,34 @@ export function Footer() {
     <footer id="footer" className={styles.footer}>
       <Container>
         <div className={styles.footerHeader}>
-          <div className={styles.footerLogo}>
-            <img
-              src="/images/logos/mariane.svg"
-              alt="Mariane"
-              width="44"
-              height="17"
-            />
-            <div className={styles.footerProprietaire}>
-              Ministère du travail, de l&apos;emploi et de l&apos;insertion
+          <div className={styles.footerSlogan}>
+            <div className={styles.footerLogo}>
+              <img
+                src="/images/logos/mariane.svg"
+                alt="Mariane"
+                width="44"
+                height="17"
+              />
+              <div className={styles.footerProprietaire}>
+                Ministère du travail, de l&apos;emploi et de l&apos;insertion
+              </div>
+              <div className={styles.footerDevise}>
+                Liberté, <br /> égalité, <br /> fraternité
+              </div>
             </div>
-            <div className={styles.footerDevise}>
-              Liberté, <br /> égalité, <br /> fraternité
+            <div className={styles.footerLogoFranceRelance}>
+              <img
+                src="/images/logos/france-relance.svg"
+                alt="Mariane"
+                width="55"
+                height="56"
+              />
             </div>
           </div>
-          <div className={styles.footerLogoFranceRelance}>
-            <img
-              src="/images/logos/france-relance.svg"
-              alt="Mariane"
-              width="55"
-              height="56"
-            />
+          <div className={styles.footerMessage}>
+            <div>#1jeune1solution</div>
+            <div>Une initiative du Gouvernement pour accompagner, former, et faciliter l’entrée dans la vie professionnelle de tous les jeunes de 13 à 30 ans, sur tous les territoires.</div>
           </div>
-        </div>
-        <div className={styles.footerMessage}>
-          <div>#1jeune1solution</div>
-          <div>Une initiative du Gouvernement pour accompagner, former, et faciliter l’entrée dans la vie professionnelle de tous les jeunes de 13 à 30 ans, sur tous les territoires.</div>
         </div>
         <div className={styles.footerLienExterne}>
           <Link href="/">
@@ -56,13 +58,12 @@ export function Footer() {
           <Link href="/"><a>Accessibilité</a></Link>
           <Link href="/"><a>Mentions légales</a></Link>
           <Link href="/"><a>Données personnelles</a></Link>
-
-          <div className={styles.footerCopyRight}>
-            Sauf mention contraire, tous les contenus de ce site sont sous licence
-            <Link href="https://www.etalab.gouv.fr/wp-content/uploads/2017/04/ETALAB-Licence-Ouverte-v2.0.pdf">
-              <a> etalab-2.0 <ExternalRedirectionIcon color="#3A3A3A" /></a>
-            </Link>
-          </div>
+        </div>
+        <div className={styles.footerCopyRight}>
+          Sauf mention contraire, tous les contenus de ce site sont sous licence
+          <Link href="https://www.etalab.gouv.fr/wp-content/uploads/2017/04/ETALAB-Licence-Ouverte-v2.0.pdf">
+            <a> etalab-2.0 <ExternalRedirectionIcon color="#3A3A3A" /></a>
+          </Link>
         </div>
       </Container>
     </footer>
