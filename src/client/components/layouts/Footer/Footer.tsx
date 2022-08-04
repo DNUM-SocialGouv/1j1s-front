686ae3843a31e5bdd3544c68f2ffcda34430d9d0
theo.clement <theo.clement@accenture.com>
2022-08-04 15:36:25 +0200

    feat: retour PR LEF

--- src/client/components/layouts/Footer/Footer.tsx
+++ src/client/components/layouts/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 
 import { Container } from '~/client/components/layouts/Container/Container';
@@ -11,57 +11,48 @@ export function Footer() {
       <div className={styles.preFooter}>
         Une initiative du Gouvernement pour accompagner, former, et faciliter l’entrée dans la vie professionnelle de tous les jeunes de 13 à 30 ans, sur tous les territoires.
       </div>
-      <footer id="footer" className={styles.footer}>
+      <footer id="footer" className={styles.footer} role="contentinfo">
         <Container>
           <div className={styles.footerHeader}>
             <div className={styles.footerSlogan}>
               <div className={styles.footerLogo}>
-                <img src="/images/logos/mariane.svg" alt="Mariane" width="55" height="20" />
+                <Image src="/images/logos/mariane.svg" alt="Mariane" width="55" height="20" />
                 <div className={styles.footerProprietaire}>
-                  Ministère du travail, de l&apos;emploi et de l&apos;insertion
+                  Ministère du <br />travail, de<br /> l&apos;emploi et de<br /> l&apos;insertion
                 </div>
                 <div className={styles.footerDevise}>
-                  Liberté, <br /> égalité, <br /> fraternité
+                  <Image src="/images/logos/devise.svg" alt="Mariane" width="52" height="37" />
                 </div>
               </div>
               <div className={styles.footerLogoFranceRelance}>
-                <img src="/images/logos/france-relance.svg" alt="Mariane" width="65" height="65" />
+                <Image src="/images/logos/france-relance.svg" alt="Mariane" width="65" height="65" />
               </div>
             </div>
             <div className={styles.footerMessageWrapper}>
-              <div className={styles.footerMessage}>
-                <div>#1jeune1solution</div>
-                <div>Une initiative du Gouvernement pour accompagner, former, et faciliter l’entrée dans la vie
+              <p className={styles.footerMessage}>
+                #1jeune1solution <br /><br />
+                Une initiative du Gouvernement pour accompagner, former, et faciliter l’entrée dans la vie
                   professionnelle de tous les jeunes de 13 à 30 ans, sur tous les territoires.
-                </div>
-              </div>
+              </p>
               <div className={styles.footerLienExterne}>
-                <Link href="/">
-                  <a>legifrance.gouv.fr <ExternalRedirectionIcon color="#3A3A3A" /></a>
-                </Link>
-                <Link href="/">
-                  <a>gouvernement.fr <ExternalRedirectionIcon color="#3A3A3A" /></a>
-                </Link>
-                <Link href="/">
-                  <a>service-public.fr <ExternalRedirectionIcon color="#3A3A3A" /></a>
-                </Link>
-                <Link href="/">
-                  <a>data.gouv.fr <ExternalRedirectionIcon color="#3A3A3A" /></a>
-                </Link>
+                <a href="/" target="_blank" rel="noreferrer">legifrance.gouv.fr <ExternalRedirectionIcon /></a>
+                <a href="/" target="_blank" rel="noreferrer">gouvernement.fr <ExternalRedirectionIcon /></a>
+                <a href="/" target="_blank" rel="noreferrer">service-public.fr <ExternalRedirectionIcon /></a>
+                <a href="/" target="_blank" rel="noreferrer">data.gouv.fr <ExternalRedirectionIcon /></a>
               </div>
             </div>
           </div>
           <div className={styles.footerLienUtile}>
-            <Link href="/"><a>Plan du site</a></Link>
-            <Link href="/"><a>Accessibilité</a></Link>
-            <Link href="/"><a>Mentions légales</a></Link>
-            <Link href="/"><a>Données personnelles</a></Link>
+            <a>Plan du site</a>
+            <a>Accessibilité</a>
+            <a>Mentions légales</a>
+            <a>Données personnelles</a>
           </div>
           <div className={styles.footerCopyRight}>
-            Sauf mention contraire, tous les contenus de ce site sont sous licence
-            <Link href="https://www.etalab.gouv.fr/wp-content/uploads/2017/04/ETALAB-Licence-Ouverte-v2.0.pdf">
-              <a> etalab-2.0 <ExternalRedirectionIcon color="#3A3A3A" /></a>
-            </Link>
+            Sauf mention contraire, tous les contenus de ce site sont sous licence&nbsp;
+            <a href="https://www.etalab.gouv.fr/wp-content/uploads/2017/04/ETALAB-Licence-Ouverte-v2.0.pdf"
+              target="_blank" rel="noreferrer">etalab-2.0 <ExternalRedirectionIcon /></a>
+
           </div>
         </Container>
       </footer>
