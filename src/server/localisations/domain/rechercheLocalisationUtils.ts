cad6750de4afe0270f8d9db6c81ac3f00ded6c75
Naorid <ashur4d@gmail.com>
2022-08-04 16:00:49 +0200

    Rename departement name variables

--- src/server/localisations/domain/rechercheLocalisationUtils.ts
+++ src/server/localisations/domain/rechercheLocalisationUtils.ts
@@ -1,14 +1,15 @@
 export default class RechercheLocalisationUtils {
   private static CODE_POSTAL_LENGTH = 5;
-  private static DEPARTEMENT_LENGTH_LIST = [2, 3];
+  private static DEPARTEMENT_LENGTH_METROPOLE = 2;
+  private static DEPARTEMENT_LENGTH_DOMTOM = 3;
 
   static isRechercheByNumeroCodePostal(recherche: string): boolean {
     return this.checkRechercheOnlyNumber(this.CODE_POSTAL_LENGTH, recherche);
   }
 
   static isRechercheByNumeroDepartement(recherche: string): boolean {
-    return this.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTH_LIST[0], recherche) ||
-          this.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTH_LIST[1], recherche);
+    return this.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTH_METROPOLE, recherche) ||
+          this.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTH_DOMTOM, recherche);
   }
 
   static checkRechercheOnlyNumber(length: number, recherche: string): boolean {
