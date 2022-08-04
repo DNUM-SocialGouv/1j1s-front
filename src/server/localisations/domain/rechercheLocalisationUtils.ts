f2fb41fafcf93301ec2abd10246e721b817a4aaf
Naorid <ashur4d@gmail.com>
2022-08-04 11:30:20 +0200

    Resolve conversation

--- src/server/localisations/domain/rechercheLocalisationUtils.ts
+++ src/server/localisations/domain/rechercheLocalisationUtils.ts
@@ -1,14 +1,14 @@
 export default class RechercheLocalisationUtils {
   private static CODE_POSTAL_LENGTH = 5;
-  private static DEPARTEMENT_LENGTHS = [2, 3];
+  private static DEPARTEMENT_LENGTH_LIST = [2, 3];
 
   static isRechercheByNumeroCodePostal(recherche: string): boolean {
     return this.checkRechercheOnlyNumber(this.CODE_POSTAL_LENGTH, recherche);
   }
 
   static isRechercheByNumeroDepartement(recherche: string): boolean {
-    return this.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTHS[0], recherche) ||
-          this.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTHS[1], recherche);
+    return this.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTH_LIST[0], recherche) ||
+          this.checkRechercheOnlyNumber(this.DEPARTEMENT_LENGTH_LIST[1], recherche);
   }
 
   static checkRechercheOnlyNumber(length: number, recherche: string): boolean {
