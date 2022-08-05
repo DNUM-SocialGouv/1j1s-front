64bb5eb14446f76619b05b0efb5b2c8861a45102
Naorid <ashur4d@gmail.com>
2022-08-05 18:40:12 +0200

    Fix Description, transcription borders

--- src/pages/mes-aides/index.tsx
+++ src/pages/mes-aides/index.tsx
@@ -35,9 +35,9 @@ export default function MesAidesPage() {
 
         <Container className={styles.mesAidesVidéo}>
           <div className={styles.mesAidesVidéoDescription}>
-            <p>
+            <p className={styles.mesAidesVidéoDescriptionText}>
               <strong className={styles.mesAidesVidéoDescriptionBoldText}>Toutes vos aides sont maintenant à portée de clic !</strong>
-              <strong className={styles.mesAidesVidéoDescriptionText}>Découvrez comment faire en regardant la vidéo ci-dessous :</strong>
+              &nbsp;Découvrez comment faire en regardant la vidéo ci-dessous :
             </p>
           </div>
           <div className={styles.mesAidesVidéoPlayer}>
@@ -66,7 +66,6 @@ export default function MesAidesPage() {
 
 
             <div className={styles.mesAidesVidéoTranscription}>
-              <div className={styles.mesAidesVidéoAccordionContour}/>
               <Accordion
                 title='Lire la transcription'
                 className={styles.mesAidesVidéoAccordion}
@@ -75,7 +74,6 @@ export default function MesAidesPage() {
                   L’image d’une jeune femme tenant son téléphone sur un fond rose apparaît. Au centre, les phrases « Vous avez moins de 30 ans » et « Bonne Nouvelle ! » s’affichent successivement. Apparition d’un fond blanc avec au centre la phrase « Toutes vos aides sont maintenant à portée de clic ! » qui est ensuite remplacée par le #1jeune1solutionApparition d’un fond vert avec au centre la phrase « Parce qu’en 5 min ». Apparition d’un fond jaune où s’affiche au centre un pictogramme d’emballage de plat avec en dessous la phrase « on peut se commander à manger ». Apparition d’un fond rose où s’affiche au centre un pictogramme de tasse de café avec en dessous la phrase « se faire un café ». Apparition d’un fond orange où s’affiche au centre un pictogramme de main avec un pouce levé, avec en dessous la phrase « et désormais trouver toutes ses aides facilement ». Apparition d’un fond bleu où l’url « 1jeune1solution.gouv.fr » apparait au centre et défile en rétrécissant jusqu’en haut. En dessous de l’url, sept mots avec à chaque fois à leur gauche un pictogramme de côche verte apparaissent successivement : « Famille », « Logement », « Emploi et insertion », « Santé », « Culture et Loisirs », « Etudiants » et « Permis de conduire ». Apparition d’un fond jaune où s’affichent successivement, au centre, les phrases « plus de 600 aides », « 1 questionnaire », « 5minutes ». Apparition d’un fond blanc avec au centre la question « Vous êtes prêt(e) ? ». Apparition d’un fond bleu où s’affiche au centre l’url « 1jeune1solution.gouv.fr » avec en dessous la phrase « Toutes vos aides à portée de clic ! ». Apparition d’un fond blanc avec le logo du gouvernement.
                 </p>
               </Accordion>
-              <div className={styles.mesAidesVidéoAccordionContour}/>
             </div>
           </div>
         </Container>
