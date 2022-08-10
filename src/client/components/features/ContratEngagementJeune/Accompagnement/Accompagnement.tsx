57305cb71a2f94145b57f7abf403f74aad33c2b1
Sergen <sergen.kovar@octo.com>
2022-08-10 10:55:33 +0200

    Correction design

--- src/client/components/features/ContratEngagementJeune/Accompagnement/Accompagnement.tsx
+++ src/client/components/features/ContratEngagementJeune/Accompagnement/Accompagnement.tsx
@@ -6,7 +6,6 @@ import Démarrage from '~/client/components/features/ContratEngagementJeune/Acco
 import PasDAccompagnement from '~/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/PasDAccompagnement';
 import { Icon } from '~/client/components/ui/Icon/Icon';
 import { LinkAsButton } from '~/client/components/ui/Link/LinkAsButton';
-import useBreakpoint from '~/client/hooks/useBreakpoint';
 
 export type Formulaires = 'Démarrage' | 'PasDAccompagnement';
 
@@ -16,8 +15,6 @@ export interface FormulairesProps {
 }
 
 export default function Accompagnement() {
-  const { isSmallScreen, isMediumScreen } = useBreakpoint();
-  const isMobile = isSmallScreen || isMediumScreen;
   const [typeFormulaireAffiché, setTypeFormulaireAffiché] = useState<Formulaires>('Démarrage');
   const [isPôleEmploiModalOpen, setIsPôleEmploiModalOpen] = useState(false);
   const lienPôleEmploi = 'https://authentification-candidat.pole-emploi.fr/connexion/XUI/?realm=%2Findividu&goto=https%3A%2F%2Fauthentification-candidat.pole-emploi.fr%2Fconnexion%2Foauth2%2Frealms%2Froot%2Frealms%2Findividu%2Fauthorize%3Frealm%3D%252Findividu%26response_type%3Did_token%2520token%26scope%3Dopenid%2520compteUsager%2520profile%2520contexteAuthentification%2520email%2520courrier%2520notifications%2520etatcivil%2520logW%2520individu%2520pilote%2520nomenclature%2520coordonnees%2520navigation%2520reclamation%2520prdvl%2520idIdentiteExterne%2520pole_emploi%2520suggestions%2520actu%2520application_USG_PN073-tdbcandidat_6408B42F17FC872440D4FF01BA6BAB16999CD903772C528808D1E6FA2B585CF2%26client_id%3DUSG_PN073-tdbcandidat_6408B42F17FC872440D4FF01BA6BAB16999CD903772C528808D1E6FA2B585CF2%26state%3DOZ3c4XQiDGwEdFxx%26nonce%3DF54AlR39GLoLCIpT%26redirect_uri%3Dhttps%253A%252F%252Fcandidat.pole-emploi.fr%252Fespacepersonnel%252F#login/';
@@ -33,7 +30,7 @@ export default function Accompagnement() {
       <div className={styles.accompagnementContainer}>
         <div>
           <h2>Contrat d&apos;Engagement Jeune, je me lance !</h2>
-          {!isMobile && question}
+          {question}
         </div>
 
         <article className={styles.accompagnementArticle}>
@@ -50,8 +47,6 @@ export default function Accompagnement() {
             </ModalContent>
           </Modal>
         </article>
-
-        {isMobile && question}
       </div>
     </section>
   );
