aa8c6dfaf342a7e912b86bb608595b3b6b57a080
Florent Jaby <florent.jaby@gmail.com>
2022-08-11 14:41:22 +0200

    335: gère les cas d'erreur des POST vers strapi

--- tests/server/contrat-engagement-jeune/infra/strapiDemandeDeContact.repository.test.ts
+++ tests/server/contrat-engagement-jeune/infra/strapiDemandeDeContact.repository.test.ts
@@ -2,7 +2,8 @@ import { aStrapiHttpClientService } from '@tests/fixtures/services/strapiHttpCli
 
 import { DemandeDeContact } from '~/server/contrat-engagement-jeune/domain/DemandeDeContact';
 import { StrapiDemandeDeContactRepository } from '~/server/contrat-engagement-jeune/infra/strapiDemandeDeContact.repository';
-import { createSuccess } from '~/server/errors/either';
+import { createFailure,createSuccess } from '~/server/errors/either';
+import { ErreurMétier } from '~/server/errors/erreurMétier.types';
 
 describe('StrapiDemandeDeContactRepository', () => {
   const demandeDeContact: DemandeDeContact = { age: 18, email: 'test@test.com', nom: 'Test', prénom: 'TEST', téléphone: '0123456789', ville: 'Paris' };
@@ -25,6 +26,19 @@ describe('StrapiDemandeDeContactRepository', () => {
       const result = await repository.save(demandeDeContact);
       // Then
       expect(result).toEqual(createSuccess(undefined));
+    });
+  });
+
+  describe('Quand la requête HTTP échoue', () => {
+    it('Résoud une Failure', async () => {
+      // Given
+      const spy = aStrapiHttpClientService();
+      jest.spyOn(spy, 'post').mockRejectedValue(new Error('Erreur non gérée'));
+      const repository = new StrapiDemandeDeContactRepository(spy);
+      // When
+      const result = await repository.save(demandeDeContact);
+      // Then
+      expect(result).toEqual(createFailure(ErreurMétier.SERVICE_INDISPONIBLE));
     });
   });
 });
