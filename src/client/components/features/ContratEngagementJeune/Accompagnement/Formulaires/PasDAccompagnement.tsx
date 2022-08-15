8a4abc87455af56a92200fee96b77d70855d9fac
Sergen <sergen.kovar@octo.com>
2022-08-15 21:52:35 +0200

    Formulaires mission locale pour - de 18 ans

--- src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/PasDAccompagnement.tsx
+++ src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/PasDAccompagnement.tsx
@@ -5,7 +5,7 @@ import styles from '~/client/components/features/ContratEngagementJeune/Accompag
 import { AngleLeftIcon } from '~/client/components/ui/Icon/angle-left.icon';
 import useBreakpoint from '~/client/hooks/useBreakpoint';
 
-export default function PasDAccompagnement({ setTypeFormulaireAffiché }: FormulairesProps) {
+export default function PasDAccompagnement({ setTypeFormulaireAffiché, setIsMissionLocaleModalOpen }: FormulairesProps) {
   const { isSmallScreen, isMediumScreen } = useBreakpoint();
   const isMobile = isSmallScreen || isMediumScreen;
 
@@ -16,7 +16,7 @@ export default function PasDAccompagnement({ setTypeFormulaireAffiché }: Formul
     <p className={styles.accompagnementQuestion}>Quel âge avez-vous ?</p>
     <div>
       {isMobile && <span>Sélectionnez l&apos;option qui vous correspond :</span>}
-      <button className={styles.optionBouton}>Moins de 18 ans</button>
+      <button className={styles.optionBouton} onClick={() => setIsMissionLocaleModalOpen(true)}>Moins de 18 ans</button>
       <button className={styles.optionBouton}>Entre 18 et 25 ans</button>
       <button className={styles.optionBouton}>Plus de 26 ans</button>
     </div>
