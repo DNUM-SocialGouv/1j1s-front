305b2419cc5f245600d08b0eeae48767e7bcca15
Florent Jaby <florent.jaby@gmail.com>
2022-08-17 16:04:30 +0200

    injecte les services pour le formulaire contact

--- tests/client/components/features/ContratEngagementJeune/Rappel/Rappel.test.tsx
+++ tests/client/components/features/ContratEngagementJeune/Rappel/Rappel.test.tsx
@@ -7,12 +7,27 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Rappel from '~/client/components/features/ContratEngagementJeune/Rappel/Rappel';
+import { DependenciesProvider } from '~/client/context/dependenciesContainer.context';
+import { DemandeDeContactService } from '~/client/services/demandeDeContact.service';
+import { createSuccess } from '~/server/errors/either';
 
 describe('<Rappel />', () => {
+  function renderComponent () {
+    const demandeDeContactService  : DemandeDeContactService  = {
+      envoyer: jest.fn().mockResolvedValue(createSuccess(undefined)),
+    } as unknown as DemandeDeContactService;
+
+    render(
+      <DependenciesProvider demandeDeContactService={demandeDeContactService}>
+        <Rappel />
+      </DependenciesProvider>,
+    );
+
+  }
   it("le composant s'affiche correctement", () => {
     // Given
     // When
-    render(<Rappel />);
+    renderComponent();
     // Then
     expect(screen.getByText('Je souhaite être contacté(e)')).toBeInTheDocument();
   });
@@ -20,7 +35,7 @@ describe('<Rappel />', () => {
     const labels = ['Prénom', 'Nom', 'Adresse email', 'Téléphone', 'Age', 'Ville'];
     it('affiche un formulaire de rappel', async () => {
       // Given
-      render(<Rappel />);
+      renderComponent();
       // When
       await userEvent.click(screen.getByText('Je souhaite être contacté(e)'));
       // Then
