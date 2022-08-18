c8a83e798437e57b94fd6ced6c745f0e59dd2f7a
theo.clement <theo.clement@accenture.com>
2022-08-18 11:26:26 +0200

    fix: review FJA

--- src/pages/je-deviens-mentor/index.tsx
+++ src/pages/je-deviens-mentor/index.tsx
@@ -1,12 +1,48 @@
 import Image from 'next/image';
 import React from 'react';
 
-import { Icon } from '~/client/components/ui/Icon/Icon';
 import { LinkAsButton } from '~/client/components/ui/Link/LinkAsButton';
+import Marked from '~/client/components/ui/Marked/Marked';
 import { HeadTag } from '~/client/components/utils/HeaderTag';
 import useBreakpoint from '~/client/hooks/useBreakpoint';
 
 import styles from './JeDeviensMentor.module.scss';
+
+const contenuHeader = `
+1 jeune 1 mentor, accompagner un jeune pour l'aider à réussir
+=============================================================
+
+Faire la rencontre qui change tout !
+
+*   **Vous êtes employeur ou citoyen et souhaitez devenir mentor ?**  
+    Embarquer dans une aventure humaine hors du commun, pour partager votre expérience, favoriser l'égalité des chances et continuer à apprendre en accompagnant un jeune.
+
+*   **Votre entreprise recrute ou porte une initiative pour les jeunes ? Rejoignez la mobilisation !**  
+    Permettez à votre entreprise d’apporter des solutions pour les jeunes, rejoignez des milliers d’entreprises déjà engagées et bénéficiez de services inédits.
+`;
+
+const quEstCeQueLeMentorat = `
+Qu'est-ce que le mentorat ?
+===========================
+
+Le mentorat est un engagement personnel pour le mentor comme pour le jeune mentoré, basé sur le volontariat de chaque côté, la confiance, la bienveillance et le respect mutuel. Il s'inscrit dans la durée : le "binôme" que forment le mentor et le jeune se rencontre plusieurs heures par mois, pendant au moins six mois. Le binôme est encadré par une structure, le plus souvent une association, qui offre un cadre sécurisé pour chacun.
+`;
+
+const siVousÊtesEmployeur = `
+*   Pour offrir la possibilité à ses collaborateurs de former un “binôme” avec un jeune, encadré par une structure spécialisée dans le mentorat
+
+*   Cela permet de contribuer à la valorisation de vos collaborateurs, au développement de leurs compétences (ex : bienveillance, écoute, conseil) et à leur épanouissement personnel
+
+*   Le mentorat permettra également la mise en valeur de votre entreprise et de vos métiers
+`;
+
+const siVousÊtesCitoyen = `
+*   Pour offrir la possibilité à ses collaborateurs de former un “binôme” avec un jeune, encadré par une structure spécialisée dans le mentorat
+
+*   Cela permet de contribuer à la valorisation de vos collaborateurs, au développement de leurs compétences (ex : bienveillance, écoute, conseil) et à leur épanouissement personnel
+
+*   Le mentorat permettra également la mise en valeur de votre entreprise et de vos métiers
+`;
 
 export default function MentoratPage() {
   const { isLargeScreen } = useBreakpoint();
@@ -19,29 +55,7 @@ export default function MentoratPage() {
       />
       <main id="contenu">
         <div className={styles.heading}>
-          <h1>1 jeune 1 mentor, accompagner un jeune pour l&apos;aider à réussir</h1>
-          <p>
-            Faire la rencontre qui change tout !
-          </p>
-          <p>
-            <ul>
-              <li>
-                <strong>Vous êtes employeur ou citoyen et souhaitez devenir mentor ?</strong>
-                <br />
-                Embarquer dans une aventure humaine hors du commun, pour partager votre expérience, favoriser l&apos;égalité des chances et continuer à apprendre en accompagnant un jeune.
-              </li>
-            </ul>
-          </p>
-          <br />
-          <p>
-            <ul>
-              <li>
-                <strong>Votre entreprise recrute ou porte une initiative pour les jeunes ? Rejoignez la mobilisation !</strong>
-                <br />
-                Permettez à votre entreprise d’apporter des solutions pour les jeunes, rejoignez des milliers d’entreprises déjà engagées et bénéficiez de services inédits.
-              </li>
-            </ul>
-          </p>
+          <Marked markdown={contenuHeader} />
 
           <div className={styles.linkAsButtonWrapper}>
             <LinkAsButton
@@ -63,8 +77,7 @@ export default function MentoratPage() {
           isLargeScreen && 
           <article className={styles.quEstCeQueLeMentorat}>
             <Image src="/icons/community.svg" alt="" layout="fixed" width={120} height={120}/>
-            <h1>Qu&apos;est-ce que le mentorat ?</h1>
-            <p>Le mentorat est un engagement personnel pour le mentor comme pour le jeune mentoré, basé sur le volontariat de chaque côté, la confiance, la bienveillance et le respect mutuel. Il s&apos;inscrit dans la durée : le &quot;binôme&quot; que forment le mentor et le jeune se rencontre plusieurs heures par mois, pendant au moins six mois. Le binôme est encadré par une structure, le plus souvent une association, qui offre un cadre sécurisé pour chacun.</p>
+            <Marked markdown={quEstCeQueLeMentorat} />
           </article>
         }
 
@@ -83,50 +96,24 @@ export default function MentoratPage() {
 
 function displayRaisonParticipationsMentoratEmployeur(isLargeScreen: boolean) {
   return (
-    <section className={styles.RaisonParticipationMentoratContainer}>
+    <section>
       { isLargeScreen && <Image src="/images/employeurs/employeur.png" alt="" layout="fixed" width={500} height={250}/>}
       <div>Si vous êtes employeur :</div>
 
-      <div className={styles.RaisonParticipationMentoratElement}>
-        <Icon name='arrow-right'/>
-        <p>Pour offrir la possibilité à ses collaborateurs de former un “binôme” avec un jeune, encadré par une structure spécialisée dans le mentorat</p>
-      </div>
-
-      <div className={styles.RaisonParticipationMentoratElement}>
-        <Icon name='arrow-right'/>
-        <p>Cela permet de contribuer à la valorisation de vos collaborateurs, au développement de leurs compétences (ex : bienveillance, écoute, conseil) et à leur épanouissement personnel </p>
-      </div>
-
-      <div className={styles.RaisonParticipationMentoratElement}>
-        <Icon name='arrow-right'/>
-        <p>Le mentorat permettra également la mise en valeur de votre entreprise et de vos métiers</p>
-      </div>
+      <Marked markdown={siVousÊtesEmployeur} />
     </section>
   );
 }
 
 function displayRaisonParticipationsMentoratCitoyen(isLargeScreen: boolean) {
   return (
-    <section className={styles.RaisonParticipationMentoratContainer}>
+    <section>
       { isLargeScreen && <Image src="/images/employeurs/citoyen.png" alt="" layout="fixed" width={500} height={250}/>}
       <div className={styles.RaisonParticipationMentoratHeader}>
         Si vous êtes citoyen :
       </div>
 
-      <div className={styles.RaisonParticipationMentoratElement}>
-        <Icon name='arrow-right'/>
-        <p>Pour partager votre expérience. Vous contribuerez à la réussite de jeunes et les ferez bénéficier de votre propre expérience</p>
-      </div>
-
-      <div className={styles.RaisonParticipationMentoratElement}>
-        <Icon name='arrow-right'/>
-        <p>Pour favoriser l&apos;égalité des chances. Vous continuerez à servir la société et demeurez actif au sein d&apos;un réseau dynamique, même à la retraite</p>
-      </div>
-
-      <div className={styles.RaisonParticipationMentoratElement}>
-        <Icon name='arrow-right'/>
-        <p>Pour continuer à apprendre. Vous développerez votre réseau et vos compétences</p>
-      </div>
+      <Marked markdown={siVousÊtesCitoyen} />
     </section>
   );
 }
