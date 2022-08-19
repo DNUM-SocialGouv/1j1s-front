612b179b606beace160cf7e2f0fe58eb90d22755
Sergen <sergen.kovar@octo.com>
2022-08-19 18:12:02 +0200

    Ajout bouton valider et retour

--- src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/AutresBesoins.tsx
+++ src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/AutresBesoins.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import { FormulairesProps } from '~/client/components/features/ContratEngagementJeune/Accompagnement/Accompagnement';
 import styles from '~/client/components/features/ContratEngagementJeune/Accompagnement/Accompagnement.module.scss';
+import { Button } from '~/client/components/ui/Button/Button';
 import { AngleLeftIcon } from '~/client/components/ui/Icon/angle-left.icon';
 
 export default function AutresBesoins({ setTypeFormulaireAffiché }: FormulairesProps) {
 
   return <>
-    <button className={styles.boutonRetour} onClick={() => setTypeFormulaireAffiché('BesoinAide')}>
-      <AngleLeftIcon className={styles.iconeRetour}/> Retour
-    </button>
     <p className={styles.accompagnementQuestion}>Rencontrez-vous d’autres besoins ?</p>
     <div className={ styles.accompagnementBoutons}>
       <button>Logement</button>
@@ -19,5 +17,9 @@ export default function AutresBesoins({ setTypeFormulaireAffiché }: Formulaires
       <button>Maîtrise de français</button>
       <button>Contraintes familiales</button>
     </div>
+    <Button buttonType={'primary'}>Valider</Button>
+    <button className={styles.boutonRetour} onClick={() => setTypeFormulaireAffiché('BesoinAide2')}>
+      <AngleLeftIcon className={styles.iconeRetour}/> Retour
+    </button>
   </>;
 }
