31981cdae3b706926fb80cac86cf886ca62a4755
Sergen <sergen.kovar@octo.com>
2022-08-22 15:25:24 +0200

    Cas Oui entre 18 et 25 ans

--- src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/AutresBesoins.tsx
+++ src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/AutresBesoins.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FormulairesProps } from '~/client/components/features/ContratEngagementJeune/Accompagnement/Accompagnement';
 import styles from '~/client/components/features/ContratEngagementJeune/Accompagnement/Accompagnement.module.scss';
 import { Button } from '~/client/components/ui/Button/Button';
 import { AngleLeftIcon } from '~/client/components/ui/Icon/angle-left.icon';
 
+
 export default function AutresBesoins({ setTypeFormulaireAffiché }: FormulairesProps) {
+  const [isActive, setActive] = useState(false);
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
 
   return <>
     <p className={styles.accompagnementQuestion}>Rencontrez-vous d’autres besoins ?</p>
-    <div className={ styles.accompagnementBoutons}>
-      <button>Logement</button>
-      <button>Santé</button>
-      <button>Difficultés administratives ou juridiques</button>
-      <button>Problématique d’accès aux droits</button>
-      <button>Maîtrise de français</button>
-      <button>Contraintes familiales</button>
-    </div>
+    <div>
+      <button
+        onClick={toggleClass}>Logement</button></div>
+    <button onClick={toggleClass}>Santé</button>
+    <button >Difficultés administratives ou juridiques</button>
+    <button >Problématique d’accès aux droits</button>
+    <button>Maîtrise de français</button>
+    <button>Contraintes familiales</button>
     <Button buttonType={'primary'}>Valider</Button>
     <button className={styles.boutonRetour} onClick={() => setTypeFormulaireAffiché('BesoinAide2')}>
       <AngleLeftIcon className={styles.iconeRetour}/> Retour
