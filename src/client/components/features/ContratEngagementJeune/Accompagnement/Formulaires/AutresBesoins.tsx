77d4238ddab379dd6bd2a2220aec40b5739592ab
Sergen <sergen.kovar@octo.com>
2022-08-24 18:26:32 +0200

    Modification fonctionnalité

--- src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/AutresBesoins.tsx
+++ src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/AutresBesoins.tsx
@@ -5,25 +5,29 @@ import styles from '~/client/components/features/ContratEngagementJeune/Accompag
 import { Button } from '~/client/components/ui/Button/Button';
 import { AngleLeftIcon } from '~/client/components/ui/Icon/angle-left.icon';
 
-
-export default function AutresBesoins({ setTypeFormulaireAffiché }: FormulairesProps) {
+export default function AutresBesoins({ setTypeFormulaireAffiché, setIsMissionLocaleModalOpen }: FormulairesProps) {
   const [isActive, setActive] = useState(false);
   const toggleClass = () => {
     setActive(!isActive);
+    setActive(true);
   };
 
 
   return <>
     <p className={styles.accompagnementQuestion}>Rencontrez-vous d’autres besoins ?</p>
-    <div>
-      <button
-        onClick={toggleClass}>Logement</button></div>
-    <button onClick={toggleClass}>Santé</button>
-    <button >Difficultés administratives ou juridiques</button>
-    <button >Problématique d’accès aux droits</button>
-    <button>Maîtrise de français</button>
-    <button>Contraintes familiales</button>
-    <Button buttonType={'primary'}>Valider</Button>
+    <div className={styles.accompagnementBoutons}>
+      <button onClick={toggleClass}>Logement
+        {isActive ? 'black' : ''}</button>
+      <button onClick={toggleClass}>Santé</button>
+      <button onClick={toggleClass}>Difficultés administratives ou juridiques</button>
+      <button onClick={toggleClass}>Problématique d’accès aux droits</button>
+      <button onClick={toggleClass}>Maîtrise de français</button>
+      <button onClick={toggleClass}>Contraintes familiales</button>
+    </div>
+    {isActive ?
+      <Button buttonType={'primary'} onClick={() => setIsMissionLocaleModalOpen(true)}>Valider</Button> :
+      <Button buttonType={'primary'} onClick={() => setTypeFormulaireAffiché('Démarrage')}>Valider</Button>
+    }
     <button className={styles.boutonRetour} onClick={() => setTypeFormulaireAffiché('BesoinAide2')}>
       <AngleLeftIcon className={styles.iconeRetour}/> Retour
     </button>
