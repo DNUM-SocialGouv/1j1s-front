4a1e151ad52f0de9f6fb13b327aa573249305132
Ali <ali.el.amrani@octo.com>
2022-08-29 14:10:53 +0200

    fix : useIsInternalLink

--- src/client/hooks/useIsInternalLink.ts
+++ src/client/hooks/useIsInternalLink.ts
@@ -1,9 +1,8 @@
 import { useMemo } from 'react';
 
 export function useIsInternalLink(href: string) {
-  const getUrlProd = process.env['FRONT_URL'] ? process.env['FRONT_URL'] : '/';
   const INTERNAL_URL_PREFIX = '/';
-  const INTERNAL_URL_PROD = getUrlProd.toString();
+  const INTERNAL_URL_PROD = 'https://1j1s-front.osc-fr1.scalingo.io/';
   const isInternalLink = useMemo(function () {
     const isInternalProd = href.startsWith(INTERNAL_URL_PROD);
     const isInternalPrefix = href.startsWith(INTERNAL_URL_PREFIX);
