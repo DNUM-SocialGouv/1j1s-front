0e38b029d3d11b9f6af4e1bfc75d4c97b7460d7c
Ali <ali.el.amrani@octo.com>
2022-08-30 07:36:58 +0200

    fix: liens internes

--- src/client/hooks/useIsInternalLink.ts
+++ src/client/hooks/useIsInternalLink.ts
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 
 export function useIsInternalLink(href: string) {
   const INTERNAL_URL_PREFIX = '/';
-  const INTERNAL_URL_PROD = 'https://1j1s-front.osc-fr1.scalingo.io/';
+  const INTERNAL_URL_PROD = process.env['FRONT_URL'] ?? '/';
   const isInternalLink = useMemo(function () {
     const isInternalProd = href.startsWith(INTERNAL_URL_PROD);
     const isInternalPrefix = href.startsWith(INTERNAL_URL_PREFIX);
