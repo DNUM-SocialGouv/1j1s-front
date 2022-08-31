a088f9c61af6a316180e62bdf906e08b30e93144
Sergen <sergen.kovar@octo.com>
2022-08-31 17:52:44 +0200

    Mise en place contenu : design mobile v2

--- src/client/components/features/LesEntreprisesSEngagent/Avantages/AvantagesMobilisation.tsx
+++ src/client/components/features/LesEntreprisesSEngagent/Avantages/AvantagesMobilisation.tsx
@@ -5,16 +5,13 @@ import React from 'react';
 import styles from '~/client/components/features/LesEntreprisesSEngagent/Avantages/AvantagesMobilisation.module.scss';
 import Marked from '~/client/components/ui/Marked/Marked';
 
-const contenuText = `
+const contenu = `
 Quels avantages à rejoindre la mobilisation pour les jeunes ?
 ------------------------------------------------------------
-* Mettez en avant tous vos engagements pour les jeunes en créant votre page dédiée
-
-* Publiez vos offres d’emploi sur la plateforme
-
-* Bénéficiez d’un accompagnement par un Conseiller Pôle Emploi (rappel sous 72h)
-
-* Relayez vos engagements par intermédiaire d’un kit de communication #1jeune1solution
++ Mettez en avant tous vos engagements pour les jeunes en créant votre page dédiée
++ Publiez vos offres d’emploi sur la plateforme
++ Bénéficiez d’un accompagnement par un Conseiller Pôle Emploi (rappel sous 72h)
++ Relayez vos engagements par intermédiaire d’un kit de communication #1jeune1solution
 `;
 
 export default function AvantagesMobilisation() {
@@ -25,7 +22,7 @@ export default function AvantagesMobilisation() {
           <Image src={ illustration } objectFit='cover' layout='fill' alt='' />
         </aside>
         <article className={ styles.article }>
-          <Marked markdown={ contenuText }/>
+          <Marked markdown={ contenu }/>
         </article>
       </div>
     </section>
