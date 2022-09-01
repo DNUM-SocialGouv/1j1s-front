7a0e40a7e98668ef7020e8c908aa9ec15bd6ad6a
Sergen <sergen.kovar@octo.com>
2022-09-01 15:07:40 +0200

    Design

--- src/client/components/features/MesuresEmployeurs/Bannière/Bannière.tsx
+++ src/client/components/features/MesuresEmployeurs/Bannière/Bannière.tsx
@@ -13,14 +13,14 @@ export default function Bannière () {
   return (
     <div className={styles.bannière}>
       <div className={styles.bannièreContent}>
-        <span className={styles.bannièreTitle}>
-          <h1>Employeurs, découvrez toutes les mesures du plan 1 jeune 1 solution pour vous aider à
-            <span className={ styles.bannièreAccroche }> recruter plus facilement</span>
+        <span className={styles.bannièreContentTitle}>
+          <h1 className={ styles.bannièreContentTitleTitre }>Employeurs, découvrez toutes les mesures du plan 1 jeune 1 solution pour vous aider à
+            <span className={ styles.bannièreContentTitleTitreAccroche }> recruter plus facilement</span>
           </h1>
         </span>
       </div>
       {isLargeScreen && (
-        <div className={styles.bannièreImage}>
+        <div className={ styles.bannièreImage }>
           <Image
             priority
             src={bannièreImage}
