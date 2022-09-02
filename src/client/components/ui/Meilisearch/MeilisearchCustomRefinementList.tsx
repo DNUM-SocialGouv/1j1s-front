1cfbd8b89e5544319db5f1d835f5b49f5897b543
Caroline Chuong - CACH <caroline.chuong@octo.com>
2022-09-02 16:24:19 +0200

    feat(stage): ajout du comportement fermer les options au clic en dehors de la liste

--- src/client/components/ui/Meilisearch/MeilisearchCustomRefinementList.tsx
+++ src/client/components/ui/Meilisearch/MeilisearchCustomRefinementList.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 // eslint-disable-next-line import/named
 import { useRefinementList, UseRefinementListProps } from 'react-instantsearch-hooks-web';
 
 import { AngleDownIcon } from '~/client/components/ui/Icon/angle-down.icon';
 import { AngleUpIcon } from '~/client/components/ui/Icon/angle-up.icon';
 import styles from '~/client/components/ui/Meilisearch/MeilisearchRefinementSelect.module.scss';
+import { KeyBoard } from '~/client/utils/keyboard.util';
 
 
 export function MeilisearchCustomRefinementList(props: UseRefinementListProps & { label: string }) {
@@ -12,11 +13,35 @@ export function MeilisearchCustomRefinementList(props: UseRefinementListProps & 
   const { label } = props;
   const [isOptionsOpen, setIsOptionsOpen] = useState(false);
   const buttonLabel = 'Sélectionnez vos choix';
+  const optionsRef = useRef<HTMLDivElement>(null);
+
+  const closeOptionsOnClickOutside = useCallback((event: MouseEvent) => {
+    if (!(optionsRef.current)?.contains(event.target as Node)) {
+      setIsOptionsOpen(false);
+    }
+  }, []);
+
+  const closeOptionsOnEscape = useCallback((event: KeyboardEvent) => {
+    if (event.key === KeyBoard.ESCAPE) {
+      setIsOptionsOpen(false);
+    }
+  }, []);
+
+  useEffect(function setEventListenerOnMount() {
+    document.addEventListener('mousedown', closeOptionsOnClickOutside);
+    document.addEventListener('keyup', closeOptionsOnEscape);
+
+    return () => {
+      document.removeEventListener('mousedown', closeOptionsOnClickOutside);
+      document.removeEventListener('keyup', closeOptionsOnEscape);
+    };
+  }, [closeOptionsOnClickOutside, closeOptionsOnEscape]);
+
   return (
     <>
       <div className={styles.selectWrapper}>
         <label className={styles.selectLabel}>{label}</label>
-        <div className={styles.selectContainer}>
+        <div ref={optionsRef}  className={styles.selectContainer}>
           <button
             type="button"
             aria-haspopup="listbox"
