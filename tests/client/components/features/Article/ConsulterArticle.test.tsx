f8817752441a7c3ed24ff2a1d2cb03a736e71619
Paul Gallo <paul.gallo@octo.com>
2022-09-05 11:16:36 +0200

    fix: prise en compte retour recette + fix test consulter article

--- tests/client/components/features/Article/ConsulterArticle.test.tsx
+++ tests/client/components/features/Article/ConsulterArticle.test.tsx
@@ -10,16 +10,17 @@ import { ConsulterArticle } from '~/client/components/features/Article/Consulter
 const article = anArticle();
 
 describe('ConsulterArticle', () => {
-
   it('affiche le titre de l\'article', () => {
     render(<ConsulterArticle article={article} />);
-    const titre = screen.getByText('Mon article');
+    const titre = screen.getByRole('heading', { level: 1, name: 'Mon article' });
     expect(titre).toBeInTheDocument();
   });
 
   it('affiche le contenu de l\'article', () => {
     render(<ConsulterArticle article={article} />);
-    const contenu = screen.getByRole('heading', { name: 'Hic devia socero Latiaeque habe foedabis genetricis Lorem markdownum torumque sic latet' });
-    expect(contenu).toBeInTheDocument();
+    const contenuTitre = screen.getByRole('heading', { level: 2, name: 'Hic devia socero Latiaeque habe foedabis genetricis' });
+    const contenuParagraphe = screen.getByText('Lorem markdownum torumque sic latet');
+    expect(contenuTitre).toBeInTheDocument();
+    expect(contenuParagraphe).toBeInTheDocument();
   });
 });
