5194bf1b33b8a304085d9280eb1ca09df761fbe9
Caroline Chuong - CACH <caroline.chuong@octo.com>
2022-09-08 12:03:36 +0200

    fix(stage): ajout balise main page recherche et title detail d'une offre

--- src/pages/stages/index.tsx
+++ src/pages/stages/index.tsx
@@ -35,70 +35,75 @@ export default function RechercherOffreStagePage() {
       <HeadTag
         title={'Rechercher un stage | 1jeune1solution'}
         description="Des milliers d'offres de stages sélectionnées pour vous"/>
-      <Hero>
+      <main id="contenu">
+        <Hero>
                 Des milliers d’offres de stages sélectionnés pour vous
-      </Hero>
-      <Container className={[styles.stageContainer].join(' ')}>
-        <InstantSearch searchClient={searchClient} indexName={MEILISEARCH_INDEX}
-          routing={MEILISEARCH_QUERYPARAMS_ROUTING_ENABLED}>
-          <Configure hitsPerPage={HITS_PER_PAGE}/>
-          <form className={styles.stageForm}>
-            <label>Métiers, mots clés, …
-              <SearchBox
-                className='recherche-principale-stage'
-                placeholder="Métiers, mots clés, …"
+        </Hero>
+        <Container className={[styles.stageContainer].join(' ')}>
+          <InstantSearch searchClient={searchClient} indexName={MEILISEARCH_INDEX}
+            routing={MEILISEARCH_QUERYPARAMS_ROUTING_ENABLED}>
+            <Configure hitsPerPage={HITS_PER_PAGE}/>
+            <form className={styles.stageForm}>
+              <label>Métiers, mots clés, …
+                <SearchBox
+                  className='recherche-principale-stage'
+                  placeholder="Métiers, mots clés, …"
+                  classNames={
+                    {
+                      form: styles.stageFormElement,
+                      input: ['fr-input', styles.stageInputElement].join(' '),
+                      reset:styles.none,
+                      resetIcon:styles.none,
+                      root: styles.stageRootElement,
+                      submit: styles.none,
+                      submitIcon: styles.none,
+                    }
+                  }
+                />
+              </label>
+              <div className={styles.stageSelectWrapper}>
+                <MeilisearchCustomRefinementList
+                  attribute={'domaines'}
+                  limit={100}
+                  label={'Domaines'}
+                  sortBy={['name:asc']}/>
+                <MeilisearchCustomRefinementList
+                  attribute={'dureeCategorisee'}
+                  label={'Durée de stage'}
+                  sortBy={['name:asc']}/>
+              </div>
+            </form>
+            <div className={styles.informationRésultats}>
+              <CurrentRefinements
                 classNames={
                   {
-                    form: styles.stageFormElement,
-                    input: ['fr-input', styles.stageInputElement].join(' '),
-                    root: styles.stageRootElement,
-                    submitIcon: styles.stageSubmitIconElement,
+                    category: styles.stageTagCategoryElement,
+                    categoryLabel: styles.stageTagItem,
+                    item: styles.stageTagItem,
+                    label: styles.none,
+                    noRefinementList: styles.none,
+                    noRefinementRoot: styles.none,
                   }
                 }
               />
-            </label>
-            <div className={styles.stageSelectWrapper}>
-              <MeilisearchCustomRefinementList
-                attribute={'domaines'}
-                limit={100}
-                label={'Domaines'}
-                sortBy={['name:asc']}/>
-              <MeilisearchCustomRefinementList
-                attribute={'dureeCategorisee'}
-                label={'Durée de stage'}
-                sortBy={['name:asc']}/>
+              <MeilisearchStats labelSingulier='offre de stage' labelPluriel='offres de stage'/>
             </div>
-          </form>
-          <div className={styles.informationRésultats}>
-            <CurrentRefinements
+            <Hits
+              hitComponent={Résultat}
               classNames={
                 {
-                  category: styles.stageTagCategoryElement,
-                  categoryLabel: styles.stageTagItem,
-                  item: styles.stageTagItem,
-                  label: styles.stageTagLabel,
-                  list: styles.stageTagList,
+                  item: styles.stageItemElement,
+                  list: styles.stageListeElement,
+                  root: styles.stageListeRootElement,
                 }
-              }
+              }/>
+            <MeilsearchCustomPagination
+              padding={0}
+              numberOfResultPerPage={HITS_PER_PAGE}
             />
-            <MeilisearchStats labelSingulier='offre de stage' labelPluriel='offres de stage'/>
-          </div>
-
-          <Hits
-            hitComponent={Résultat}
-            classNames={
-              {
-                item: styles.stageItemElement,
-                list: styles.stageListeElement,
-                root: styles.stageListeRootElement,
-              }
-            }/>
-          <MeilsearchCustomPagination
-            padding={0}
-            numberOfResultPerPage={HITS_PER_PAGE}
-          />
-        </InstantSearch>
-      </Container>
+          </InstantSearch>
+        </Container>
+      </main>
     </>
   );
 }
