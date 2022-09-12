db300e236fd7924e8a36c9860701214c6b31b342
Florent Jaby <florent.jaby@gmail.com>
2022-09-12 17:19:19 +0200

    395: affichage des joulis blocs

--- src/client/components/features/MesuresEmployeurs/MesuresEmployeurs.tsx
+++ src/client/components/features/MesuresEmployeurs/MesuresEmployeurs.tsx
@@ -1,30 +1,36 @@
+import classNames from 'classnames';
+
 import Bannière from '~/client/components/features/MesuresEmployeurs/Bannière/Bannière';
+import styles from '~/client/components/features/MesuresEmployeurs/MesuresEmployeurs.module.scss';
+import useBreakpoint from '~/client/hooks/useBreakpoint';
 import useSanitize from '~/client/hooks/useSanitize';
 import { CarteMesuresEmployeurs, MesuresEmployeurs } from '~/server/cms/domain/mesuresEmployeurs';
 
-import { LinkCard } from '../../ui/Card/LinkCard';
+import { EmployeurLinkCard } from '../../ui/Card/EmployeurLinkCard';
 import Marked from '../../ui/Marked/Marked';
 import { HeadTag } from '../../utils/HeaderTag';
 
 export interface MesuresEmployeursProps {
-  mesuresEmployeurs : MesuresEmployeurs
+mesuresEmployeurs : MesuresEmployeurs
 }
 export function MesuresEmployeursComponent ({ mesuresEmployeurs }: MesuresEmployeursProps) {
   const { dispositifs } = mesuresEmployeurs;
+  const { isLargeScreen } = useBreakpoint();
+  const isMobile = !isLargeScreen;
   return (
     <>
       <HeadTag
         title="Mesures Employeurs | 1jeune1solution"
         description="Plus de 400 000 offres d'emplois et d'alternances sélectionnées pour vous"
       />
-      <main id="contenu">
+      <main id="contenu" className={ classNames({ [styles.mobile]: isMobile }) }>
         <Bannière />
-        <section>
+        <section className={ styles.dispositifs }>
           <h2 id="dispositifs">
-            Découvrir les dispositifs pour vous aider à recruter
+        Découvrir les dispositifs pour vous aider à recruter
           </h2>
-          <ul>
-            {dispositifs.map((carte) => (<li key={carte.url}><CarteMesureEmployeur carte={carte} /></li>))}
+          <ul className={ styles.cartes }>
+            {dispositifs.map((carte) => (<li className={styles.carte} key={carte.url}><CarteMesureEmployeur carte={carte} isMobile={isMobile} /></li>))}
           </ul>
         </section>
       </main>
@@ -32,19 +38,33 @@ export function MesuresEmployeursComponent ({ mesuresEmployeurs }: MesuresEmploy
   );
 }
 
-function CarteMesureEmployeur ({ carte }: { carte: CarteMesuresEmployeurs }) {
+interface CarteMesureEmployeurProps {
+    carte: CarteMesuresEmployeurs;
+    isMobile: boolean;
+}
+
+function CarteMesureEmployeur ({ carte, isMobile=false }: CarteMesureEmployeurProps) {
   const titre = useSanitize(carte.titre);
   const bannière = carte.bannière?.url || '';
   const url = useSanitize(carte.url);
   const contenu = useSanitize(carte.contenu);
+  const brief = extrait(contenu, 110);
 
-  return <LinkCard
+  return <EmployeurLinkCard
     imageUrl={bannière}
+    isMobile={isMobile}
     link={url}
     linkLabel="En savoir plus"
     title={titre}
   >
-    <Marked markdown={contenu} />
-  </LinkCard>;
-  
+    <Marked markdown={brief} />
+  </EmployeurLinkCard>;
+
+}
+
+function extrait(contenu: string, size = 120): string {
+  const end = contenu.substring(size);
+  const charactersLeft = end.indexOf(' ');
+  const brief = contenu.substring(0, size+charactersLeft);
+  return `${brief} [...]`;
 }
---
+++ src/client/components/ui/Card/EmployeurLinkCard.tsx
@@ -0,0 +1,47 @@
+import classNames from 'classnames';
+import Image from 'next/image';
+import React from 'react';
+
+import { HtmlHeadingTag } from '~/client/components/props';
+import styles from '~/client/components/ui/Card/EmployeurLinkCard.module.scss';
+import { AngleRightIcon } from '~/client/components/ui/Icon/angle-right.icon';
+import { Link } from '~/client/components/ui/Link/Link';
+
+import { ArrowRightIcon } from '../Icon/arrow-right.icon';
+
+interface EmployeurLinkCardProps {
+	imageUrl: string
+	link: string
+	linkLabel?: string
+	title: string
+  titleLevel?: HtmlHeadingTag
+  isMobile?: boolean
+}
+
+export function EmployeurLinkCard({ children, imageUrl, isMobile=false, link, linkLabel, title, titleLevel }: React.PropsWithChildren<EmployeurLinkCardProps>)  {
+
+  function LinkCardTitle({ children, className }: { titleLevel?: HtmlHeadingTag } & React.HTMLAttributes<HTMLTitleElement>) {
+    return React.createElement(titleLevel || 'h3', { className: className }, children);
+  }
+
+  return (
+    <Link href={link} className={classNames(styles.card, { [styles.mobile]: isMobile })}>
+      <article>
+        <div className={styles.image}>
+          <Image src={imageUrl} alt="" layout="fill" objectFit="cover" objectPosition="top"/>
+        </div>
+        <LinkCardTitle className={styles.title}>{title}</LinkCardTitle>
+        <div className={styles.description}>{children}</div>
+        <Link href={link} className={styles.link}>
+          {linkLabel}
+          { isMobile
+            ? <ArrowRightIcon />
+            : <AngleRightIcon />
+          }
+        </Link>
+      </article>
+    </Link>
+  );
+};
+
+
