508b7b20a2d985f6fe0cf2d32e093302b88c32fe
CLEM <43972380+OCTO-CLEM@users.noreply.github.com>
2022-09-12 14:39:10 +0200

    chore(deps): update last version

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,7 @@ const config: Config.InitialOptions = {
     '\\.(png|jpg|jpeg)$': '<rootDir>/tests/image.mock.ts',
     '^@tests/(.*)$': '<rootDir>/tests/$1',
     '^~/(.*)$': '<rootDir>/src/$1',
+    uuid: require.resolve('uuid'),
   },
   setupFilesAfterEnv: ['./react-testing-library.setup.ts'],
 };
