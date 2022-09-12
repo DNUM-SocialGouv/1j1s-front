06b8efc0b8aa2387fbb5649339224e1055589f41
Florent Jaby <florent.jaby@gmail.com>
2022-09-12 12:12:46 +0200

    bsr: remove useless assertions on mapping function

--- tests/server/cms/infra/repositories/strapiCms.repository.test.ts
+++ tests/server/cms/infra/repositories/strapiCms.repository.test.ts
@@ -9,9 +9,7 @@ import { MentionsObligatoires } from '~/server/cms/domain/mentionsObligatoires';
 import { MesuresEmployeurs } from '~/server/cms/domain/mesuresEmployeurs';
 import { MesuresJeunes } from '~/server/cms/domain/mesuresJeunes';
 import {
-  mapArticle,
   mapFicheMetier,
-  mapMentionObligatoire,
 } from '~/server/cms/infra/repositories/strapi.mapper';
 import { StrapiCmsRepository } from '~/server/cms/infra/repositories/strapiCms.repository';
 import { createFailure, createSuccess, Failure, Success } from '~/server/errors/either';
@@ -36,7 +34,7 @@ describe('strapi cms repository', () => {
         const result = await strapiCmsRepository.getArticleBySlug(slug) as Success<Article>;
 
         expect(result.result).toEqual(expectedArticle);
-        expect(httpClientService.get).toHaveBeenCalledWith(`articles?filters[slug][$eq]=${slug}&populate[0]=banniere`, mapArticle);
+        expect(httpClientService.get).toHaveBeenCalledWith(`articles?filters[slug][$eq]=${slug}&populate[0]=banniere`, expect.any(Function));
       });
     });
   });
@@ -95,11 +93,11 @@ describe('strapi cms repository', () => {
         contenu: 'La présente politique de confidentialité définit et vous informe de la manière dont le Ministère du Travail utilise les données à caractère personnel en conformité à le Règlement t européen (UE) 2016/679 du Parlement européen et du Conseil du 27 avril 2016 et la loi nᵒ 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés. \n\n[...]\n\n**Gestion des cookies**\n\n**<u>Cookie présent sur le Site</u>**\n| Type de cookie | Nom | Finalité | Durée de conservation |\n| - | - | - | - |\n| ... | Cookie chocolat blanc | Être mangé | Courte |\n| ... | Cookie myrtille | Être mangé, normal | Extrèmement courte |\n\n[...]\n\n<u>**Comment paramétrer les cookies ?**</u>\n\n[...]\n\nLors de votre utilisation du Site, il vous est possible de configurer vos préférences sur les cookies à tout moment en vous rendant sur l’onglet « Gestion des cookies » disponible en bas de la page d’accueil du Site. \n',
         titre: 'Politique de confidentialité',
       });
-      expect(httpClientService.get).toHaveBeenCalledWith('politique-de-confidentialite?populate=*', mapMentionObligatoire);
+      expect(httpClientService.get).toHaveBeenCalledWith('politique-de-confidentialite?populate=*', expect.any(Function));
     });
   });
 
-  describe('getCartesMesuresJeunes', () => {
+  describe('getMesuresJeunes', () => {
     describe('Si les cartes mesures jeunes sont trouvés', () => {
       it('récupère les cartes jeunes', async () => {
         httpClientService = aStrapiHttpClientService();
