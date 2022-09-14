23f099b64045de7877aaebcf8d6a835a7c4b9223
Florent Jaby <florent.jaby@gmail.com>
2022-09-14 15:37:48 +0200

    split in components

--- src/client/components/features/JeDeviensMentor/JeDeviensMentor.tsx
+++ src/client/components/features/JeDeviensMentor/JeDeviensMentor.tsx
@@ -9,6 +9,26 @@ import Marked from '~/client/components/ui/Marked/Marked';
 import { HeadTag } from '~/client/components/utils/HeaderTag';
 import useBreakpoint from '~/client/hooks/useBreakpoint';
 
+import { Container } from '../../layouts/Container/Container';
+import { SectionLayout } from '../../layouts/Section/SectionLayout';
+
+export default function JeDeviensMentor() {
+  return (
+    <>
+      <HeadTag
+        title="Je deviens mentor | 1jeune1solution"
+        description="1 jeune 1 mentor, accompagner un jeune pour l’aider à réussir"
+      />
+      <main>
+        <PourQui />
+        <QuEstCeQueLeMentorat />
+        <Pourquoi />
+        <AidesExceptionnelles />
+      </main>
+    </>
+  );
+}
+
 const contenuHeader = `
 1 jeune 1 mentor, accompagner un jeune pour l'aider à réussir
 =============================================================
@@ -30,14 +50,18 @@ Le mentorat est un engagement personnel pour le mentor comme pour le jeune mento
 `;
 
 const siVousÊtesEmployeur = `
-*   Pour offrir la possibilité à ses collaborateurs de former un “binôme” avec un jeune, encadré par une structure spécialisée dans le mentorat
+#### Si vous êtes employeur :
 
-*   Cela permet de contribuer à la valorisation de vos collaborateurs, au développement de leurs compétences (ex : bienveillance, écoute, conseil) et à leur épanouissement personnel
+* Pour offrir la possibilité à ses collaborateurs de former un “binôme” avec un jeune, encadré par une structure spécialisée dans le mentorat
 
-*   Le mentorat permettra également la mise en valeur de votre entreprise et de vos métiers
+* Cela permet de contribuer à la valorisation de vos collaborateurs, au développement de leurs compétences (ex : bienveillance, écoute, conseil) et à leur épanouissement personnel
+
+* Le mentorat permettra également la mise en valeur de votre entreprise et de vos métiers
 `;
 
 const siVousÊtesCitoyen = `
+#### Si vous êtes citoyen :
+
 *   Pour offrir la possibilité à ses collaborateurs de former un “binôme” avec un jeune, encadré par une structure spécialisée dans le mentorat
 
 *   Cela permet de contribuer à la valorisation de vos collaborateurs, au développement de leurs compétences (ex : bienveillance, écoute, conseil) et à leur épanouissement personnel
@@ -45,77 +69,58 @@ const siVousÊtesCitoyen = `
 *   Le mentorat permettra également la mise en valeur de votre entreprise et de vos métiers
 `;
 
-export default function JeDeviensMentor() {
+
+function QuEstCeQueLeMentorat() {
   const { isLargeScreen } = useBreakpoint();
-
+  if (!isLargeScreen) {
+    return (<></>);
+  }
   return (
-    <>
-      <HeadTag
-        title="Je deviens mentor | 1jeune1solution"
-        description="1 jeune 1 mentor, accompagner un jeune pour l’aider à réussir"
-      />
-      <main id="contenu">
-        <div className={styles.heading}>
-          <Marked markdown={contenuHeader} />
+    <article className={styles.quEstCeQueLeMentorat}>
+      <Image src="/icons/community.svg" alt="" layout="fixed" width={120} height={120} />
+      <Marked markdown={quEstCeQueLeMentorat} />
+    </article>
+  );
+}
 
-          <div className={styles.linkAsButtonWrapper}>
-            <LinkAsButton
-              href="https://www.1jeune1mentor.fr/formulaire-mentor?1jeune1solution"
-              target="_blank"
-            >
-              Je deviens mentor
-            </LinkAsButton>
-            <LinkAsButton
-              href="/les-entreprises-s-engagent"
-              className={styles.linkAsButtonMentorat}
-            >
-              J&apos;engage mon entreprise
-            </LinkAsButton>
-          </div>
+function PourQui() {
+  return (
+    <SectionLayout className={styles.heading}>
+      <Container>
+        <Marked markdown={contenuHeader} />
+
+        <div className={styles.linkAsButtonWrapper}>
+          <LinkAsButton
+            href="https://www.1jeune1mentor.fr/formulaire-mentor?1jeune1solution"
+            target="_blank"
+          >
+                    Je deviens mentor
+          </LinkAsButton>
+          <LinkAsButton
+            href="/les-entreprises-s-engagent"
+            className={styles.linkAsButtonMentorat}
+          >
+                    J&apos;engage mon entreprise
+          </LinkAsButton>
         </div>
-
-        {
-          isLargeScreen && 
-          <article className={styles.quEstCeQueLeMentorat}>
-            <Image src="/icons/community.svg" alt="" layout="fixed" width={120} height={120}/>
-            <Marked markdown={quEstCeQueLeMentorat} />
-          </article>
-        }
-
-        <div className={styles.body}>
-          <h1>Pourquoi participer à l&apos;aventure du mentorat ?</h1>
-          <article>
-            {displayRaisonParticipationsMentoratEmployeur(isLargeScreen)}
-            {displayRaisonParticipationsMentoratCitoyen(isLargeScreen)}
-          </article>
-        </div>
-        <AidesExceptionnelles />
-      </main>
-    </>
-  );
+      </Container>
+    </SectionLayout>);
 }
 
-function displayRaisonParticipationsMentoratEmployeur(isLargeScreen: boolean) {
+function Pourquoi() {
+  const { isLargeScreen } = useBreakpoint();
   return (
-    <section>
-      { isLargeScreen && <Image src="/images/employeurs/employeur.png" alt="" layout="fixed" width={500} height={250}/> }
-      <div className={styles.RaisonParticipationMentoratHeader}>Si vous êtes employeur :</div>
-
-      <Marked markdown={siVousÊtesEmployeur} />
-    </section>
-  );
+    <div className={styles.pourquoi}>
+      <h1>Pourquoi participer à l&apos;aventure du mentorat ?</h1>
+      <article>
+        <section>
+          { isLargeScreen && <Image src="/images/employeurs/employeur.png" alt="" layout="fixed" width={500} height={250}/> }
+          <Marked markdown={siVousÊtesEmployeur} />
+        </section>
+        <section>
+          { isLargeScreen && <Image src="/images/employeurs/citoyen.png" alt="" layout="fixed" width={500} height={250}/>}
+          <Marked markdown={siVousÊtesCitoyen} />
+        </section>
+      </article>
+    </div>);
 }
-
-function displayRaisonParticipationsMentoratCitoyen(isLargeScreen: boolean) {
-  return (
-    <section>
-      { isLargeScreen && <Image src="/images/employeurs/citoyen.png" alt="" layout="fixed" width={500} height={250}/>}
-      <div className={styles.RaisonParticipationMentoratHeader}>
-        Si vous êtes citoyen :
-      </div>
-
-      <Marked markdown={siVousÊtesCitoyen} />
-    </section>
-  );
-}
-
