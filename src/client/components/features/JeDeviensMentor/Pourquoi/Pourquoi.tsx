3f31ef217d62573bc41cc2e2028a7ca5cfd634a0
Sergen <sergen.kovar@octo.com>
2022-09-19 15:06:26 +0200

    Modifications bloc Pourquoi participer

--- src/client/components/features/JeDeviensMentor/Pourquoi/Pourquoi.tsx
+++ src/client/components/features/JeDeviensMentor/Pourquoi/Pourquoi.tsx
@@ -2,40 +2,49 @@ import Image from 'next/image';
 
 import styles from '~/client/components/features/JeDeviensMentor/Pourquoi/Pourquoi.module.scss';
 import Marked from '~/client/components/ui/Marked/Marked';
+import { SeeMore } from '~/client/components/ui/SeeMore/SeeMore';
 import useBreakpoint from '~/client/hooks/useBreakpoint';
 
 export function Pourquoi() {
-  const { isLargeScreen } = useBreakpoint();
+  const { isLargeScreen, isSmallScreen, isMediumScreen } = useBreakpoint();
+  const displayAccordion = isSmallScreen || isMediumScreen;
   return (
     <div className={styles.pourquoi}>
-      <h1>Pourquoi participer à l&apos;aventure du mentorat ?</h1>
       <article>
         <section>
           { isLargeScreen && <Image src="/images/employeurs/employeur.png" alt="" layout="fixed" width={500} height={250}/> }
-          <Marked markdown={siVousÊtesEmployeur} />
+          <h2>Pourquoi participer à l’aventure du mentorat en tant qu’employeur ?</h2>
+          {!displayAccordion && <Marked markdown={siVousÊtesEmployeur}/>}
+          {displayAccordion && (
+            <SeeMore>
+              <Marked markdown={siVousÊtesEmployeur}/>
+            </SeeMore>)}
         </section>
         <section>
           { isLargeScreen && <Image src="/images/employeurs/citoyen.png" alt="" layout="fixed" width={500} height={250}/>}
-          <Marked markdown={siVousÊtesCitoyen} />
+          <h2>Vous êtes citoyen : vous pouvez devenir mentor !</h2>
+          {!displayAccordion && <Marked markdown={siVousÊtesCitoyen}/>}
+          {displayAccordion && (
+            <SeeMore>
+              <Marked markdown={siVousÊtesCitoyen}/>
+            </SeeMore>)}
         </section>
       </article>
     </div>);
 }
 
 const siVousÊtesEmployeur = `
-#### Si vous êtes employeur :
 
 * Pour offrir la possibilité à ses collaborateurs de former un “binôme” avec un jeune, encadré par une structure spécialisée dans le mentorat
-* Cela permet de contribuer à la valorisation de vos collaborateurs, au développement de leurs compétences (ex : bienveillance, écoute, conseil) et à leur épanouissement personnel
-* Le mentorat permettra également la mise en valeur de votre entreprise et de vos métiers
+* Pour contribuer à la valorisation de vos collaborateurs, au développement de leurs compétences (ex : bienveillance, écoute, conseil) et à leur épanouissement personnel
+* Pour permettre la mise en valeur de votre entreprise et de vos métiers 
 `;
 
 const siVousÊtesCitoyen = `
-#### Si vous êtes citoyen :
 
-*   Pour offrir la possibilité à ses collaborateurs de former un “binôme” avec un jeune, encadré par une structure spécialisée dans le mentorat
-*   Cela permet de contribuer à la valorisation de vos collaborateurs, au développement de leurs compétences (ex : bienveillance, écoute, conseil) et à leur épanouissement personnel
-*   Le mentorat permettra également la mise en valeur de votre entreprise et de vos métiers
+*  Pour partager votre expérience. Vous contribuerez à la réussite de jeunes et les ferez bénéficier de votre propre expérience
+*  Pour favoriser l’égalité des chances. Vous continuerez à servir la société et demeurrez actif au sein d’un réseau dynamique, même à la retraite
+*  Pour continuer à apprendre. Vous développerez votre réseau et vos compétences
 `;
 
 
