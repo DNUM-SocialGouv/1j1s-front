1fbbb3fdee270ede31fc40a3e4d2a8a93f80e576
Sergen <sergen.kovar@octo.com>
2022-09-23 16:08:41 +0200

    Review Tech

--- src/client/components/features/Immersions/ReferencesEntreprises/RéférencerEntreprises.tsx
+++ src/client/components/features/Immersions/ReferencesEntreprises/RéférencerEntreprises.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 
 import styles from '~/client/components/features/Immersions/ReferencesEntreprises/RéférencerEntreprises.module.scss';
+import { Container } from '~/client/components/layouts/Container/Container';
+import { SectionLayout } from '~/client/components/layouts/Section/SectionLayout';
 import { LinkAsButton } from '~/client/components/ui/Link/LinkAsButton';
 
 
 export function RéférencerEntreprises () {
   return (
-    <section className={styles.referencer}>
-      <div className={styles.referencerContainer}>
-        <h2 className={styles.referencerContainerTitre}>Référencer votre entreprise afin de proposer des immersions au sein de votre entreprise</h2>
+    <SectionLayout className={styles.referencer}>
+      <Container>
+        <h2 className={styles.referencerTitre}>Référencer votre entreprise afin de proposer des immersions au sein de votre entreprise</h2>
         <p>Proposer une immersion c’est permettre à des jeunes :</p>
         <ul>
           <li>De découvrir un métier</li>
           <li>De les aider à confirmer un projet professionnel en situation réelle de travail</li>
           <li>D’initier un parcours d’embauche pour leur permettre d’accéder à un emploi ou dans le cadre d’une reconversion</li>
         </ul>
-        <div className={styles.referencerContainerBouton}>
+        <div className={styles.referencerBouton}>
           <LinkAsButton href={'https://immersion-facile.beta.gouv.fr/etablissement/lesentreprises-sengagent'}>
             Référencer mon entreprise
           </LinkAsButton>
         </div>
-      </div>
-    </section>
+      </Container>
+    </SectionLayout>
 
   );
 }
