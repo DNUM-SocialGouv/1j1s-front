f35760c42556c416083edbf838be6a8a881a47e1
Florent Jaby <florent.jaby@gmail.com>
2022-09-26 18:31:35 +0200

    720: ajoute l'iframe des immersions faciles

---
+++ src/client/components/features/Immersions/ReferencesEntreprises/RéférencerImmersion.tsx
@@ -0,0 +1,20 @@
+import styles from '~/client/components/features/Immersions/ReferencesEntreprises/RéférencerImmersion.module.scss';
+import { Hero } from '~/client/components/ui/Hero/Hero';
+import { HeadTag } from '~/client/components/utils/HeaderTag';
+
+const subtitle = 'Ce formulaire vous permet d\'indiquer les métiers de votre établissement ouverts aux immersions. Si votre entreprise comprend plusieurs établissements, il convient de renseigner un formulaire pour chaque établissement (SIRET différent).';
+
+export function RéférencerImmersion () {
+  return (
+    <>
+      <HeadTag title="je référence mon entreprise pour des immersions | 1jeune1solution" />
+      <Hero className={ styles.hero }>
+        <p><b>Je référence mon entreprise</b></p>
+        <p className={styles.heroSubtitle}>{ subtitle }</p>
+      </Hero>
+      <div className={ styles.section }>
+        <iframe className={styles.iframe} src="https://immersion-facile.beta.gouv.fr/etablissement/lesentreprises-sengagent" />
+      </div>
+    </>
+  );
+}
