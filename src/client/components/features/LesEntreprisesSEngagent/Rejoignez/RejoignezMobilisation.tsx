099a225bd4bf0d93b2e42d634a863d8172262f90
Sergen <sergen.kovar@octo.com>
2022-09-26 17:30:27 +0200

    Ajustements: les container

--- src/client/components/features/LesEntreprisesSEngagent/Rejoignez/RejoignezMobilisation.tsx
+++ src/client/components/features/LesEntreprisesSEngagent/Rejoignez/RejoignezMobilisation.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 
 import styles from '~/client/components/features/LesEntreprisesSEngagent/Rejoignez/RejoignezMobilisation.module.scss';
+import { Container } from '~/client/components/layouts/Container/Container';
 import { LinkAsButton } from '~/client/components/ui/Link/LinkAsButton';
 import Marked from '~/client/components/ui/Marked/Marked';
 
@@ -16,23 +17,25 @@ Rejoignez la mobilisation !
 
 export default function RejoignezMobilisation() {
   return <div className={styles.content}>
-    <div className={styles.lesEntreprisesSEngagent}>
-      <Image src="/icons/les-entreprises-s-engagent.svg" alt="" width="65" height="65"/>
-      <span>Les entreprises s&apos;engagent</span>
-    </div>
-    <Marked markdown={contenuHeader}/>
+    <Container className={styles.container} >
+      <div className={styles.lesEntreprisesSEngagent}>
+        <Image src="/icons/les-entreprises-s-engagent.svg" alt="" width="65" height="65"/>
+        <span>Les entreprises s&apos;engagent</span>
+      </div>
+      <Marked markdown={contenuHeader}/>
 
-    <div className={styles.linkAsButtonWrapper}>
-      <LinkAsButton href="/les-entreprises-s-engagent/inscription">
+      <div className={styles.linkAsButtonWrapper}>
+        <LinkAsButton href="/les-entreprises-s-engagent/inscription">
         Rejoindre la mobilisation
-      </LinkAsButton>
-      <LinkAsButton
-        className={styles.linkAsButtonMentorat}
-        href="https://lesentreprises-sengagent.gouv.fr/les-entreprises-engagees"
-        target="_blank"
-      >
+        </LinkAsButton>
+        <LinkAsButton
+          className={styles.linkAsButtonMentorat}
+          href="https://lesentreprises-sengagent.gouv.fr/les-entreprises-engagees"
+          target="_blank"
+        >
         Découvrir les entreprises engagées
-      </LinkAsButton>
-    </div>
+        </LinkAsButton>
+      </div>
+    </Container>
   </div>;
 }
