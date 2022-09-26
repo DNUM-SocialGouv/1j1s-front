0a9cfdd671ed32f543ff89c054b09d8f57a832a9
Florent Jaby <florent.jaby@gmail.com>
2022-09-26 12:28:08 +0200

    719: Intégrer iframe offre d'emploi (#325)

---
+++ src/pages/je-recrute/deposer-une-offre-d-emploi/index.tsx
@@ -0,0 +1,6 @@
+import { DéposerOffreEmploi } from '~/client/components/features/OffreEmploi/Déposer/DéposerOffreEmploi';
+
+export default function DéposerUneOffreDEmploi () {
+  return (<DéposerOffreEmploi />);
+}
+
