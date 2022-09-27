b8dcda5e4334b3d8c18333b109275e20a0c10bfb
Sergen <sergen.kovar@octo.com>
2022-09-27 15:47:00 +0200

    update file: finalisation retour

--- src/client/components/features/JeDeviensMentor/PourQui/PourQui.tsx
+++ src/client/components/features/JeDeviensMentor/PourQui/PourQui.tsx
@@ -31,7 +31,7 @@ const contenuHeader = `
 1 jeune 1 mentor, accompagner un jeune pour l'aider à réussir
 =============================================================
 
-Faire la rencontre qui change tout !
+Faites la rencontre qui change tout !
 
 *   **Vous êtes employeur ou citoyen et souhaitez devenir mentor ?**  
     Embarquer dans une aventure humaine hors du commun, pour partager votre expérience, favoriser l'égalité des chances et continuer à apprendre en accompagnant un jeune.
