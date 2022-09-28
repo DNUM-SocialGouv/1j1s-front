bb25313e47ec50c52112e5a9a7d38a770a15e2c2
OCTO-CLEM <theo.clement@octo.com>
2022-09-28 16:01:24 +0200

    feat: ajout d'un mail to pour déposer un stage

--- src/client/components/features/JeRecrute/DecouvrirDispositifs/DecouvrirDispositifs.tsx
+++ src/client/components/features/JeRecrute/DecouvrirDispositifs/DecouvrirDispositifs.tsx
@@ -4,6 +4,12 @@ import { LinkAsButton } from '~/client/components/ui/Link/LinkAsButton';
 
 
 export function DécouvrirDispositifs () {
+
+  const body = 'Afin de recevoir des candidatures correspondant au besoin de la mission proposée, nous vous conseillons de bien détailler votre offre, en n’oubliant pas de préciser : <br><br>• Un titre pour votre offre de stage ; <br>• La description des missions (n’hésitez pas à faire une liste) ; <br>• Le lieu du stage (ville, code postal, département, région, pays) ; <br>• Votre secteur d’activité ; <br>• Les dates de début et de fin du stage, ainsi que la rémunération prévue ;<br>• Les coordonnées et le SIRET de votre entreprise ;<br>• Vos coordonnées ;<br>• L’URL ou le mail pour envoyer sa candidature. <br><br><br>Votre offre sera visible sur la plateforme après modération.';
+  const subject = '[Déposer une offre de stage]';
+  const mail = 'contact-1J1S@sg.social.gouv.fr';
+  const MAIL_TO = `mailto:${mail}?subject=${subject}&body=${body.replaceAll('<br>', '%0D%0A').replaceAll('•', '%E2%80%A2')}`;
+
   return (
     <div>
       <Container className={styles.recruter}>
@@ -14,7 +20,7 @@ export function DécouvrirDispositifs () {
             <LinkAsButton href="/je-recrute/deposer-une-offre-d-emploi">Déposer une offre d&apos;emploi ou d&apos;alternance</LinkAsButton>
           </li>
           <li>
-            <LinkAsButton disabled href="/">Déposer une offre de stage</LinkAsButton>
+            <LinkAsButton href={MAIL_TO}>Déposer une offre de stage</LinkAsButton>
           </li>
         </ul>
       </Container>
