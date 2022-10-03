3b8f797a1c3588d7958997d153ccd5ae7136aba2
Florian Leroy <Drastal@users.noreply.github.com>
2022-10-03 14:18:41 +0200

    chore: configure security headers

--- next.config.js
+++ next.config.js
@@ -16,6 +16,47 @@ const CMS_HOST = getHostName(process.env.STRAPI_URL_API);
 const API_POLE_EMPLOI_HOST = getHostName(process.env.POLE_EMPLOI_CONNECT_URL);
 const STRAPI_MEDIA_URL = getHostName(process.env.STRAPI_MEDIA_URL);
 const BUCKET_S3_URL = process.env.BUCKET_S3_URL;
+const TRUSTED_SOURCES = '*.fabrique.social.gouv.fr';
+const ANALYTICS_SOURCES = '*.xiti.com';
+
+const contentSecurityPolicy = `
+  default-src 'self' ${TRUSTED_SOURCES};
+  script-src 'self' ${ANALYTICS_SOURCES};
+  img-src 'self' data: ${BUCKET_S3_URL} ${STRAPI_MEDIA_URL} ${ANALYTICS_SOURCES};
+  style-src 'self' 'unsafe-inline';
+  frame-ancestors 'none';
+  frame-src *.apprentissage.beta.gouv.fr immersion-facile.beta.gouv.fr deposer-offre.www.1jeune1solution.gouv.fr *.youtube.com;
+  form-action 'self';
+  base-uri 'none';
+`;
+
+const DEV_ENVIRONMENTS = ['development', 'local'];
+
+async function headers() {
+  if (DEV_ENVIRONMENTS.includes(process.env.NODE_ENV)) {
+    return [];
+  } else {
+    return [{
+      headers: [{
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+      }, {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload',
+      }, {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      }, {
+        key: 'Referrer-Policy',
+        value: 'no-referrer, strict-origin-when-cross-origin',
+      }, {
+        key: 'Content-Security-Policy',
+        value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+      }],
+      source: '/:path*',
+    }];
+  }
+}
 
 const moduleExports = {
   compress: true,
@@ -23,9 +64,11 @@ const moduleExports = {
     npm_package_name: process.env.npm_package_name,
     npm_package_version: process.env.npm_package_version,
   },
+  headers,
   images: {
     domains: [CMS_HOST, API_POLE_EMPLOI_HOST, BUCKET_S3_URL, STRAPI_MEDIA_URL],
   },
+  poweredByHeader: false,
   reactStrictMode: true,
   sentry: {
     disableClientWebpackPlugin: true,
