c2b6e1e52d2e30feb16e40b1df56fa4f2b1a7aaf
Florent Jaby <florent.jaby@gmail.com>
2022-10-04 17:41:26 +0200

    animation

--- src/client/components/layouts/Header/NavEmployeurs.tsx
+++ src/client/components/layouts/Header/NavEmployeurs.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { useLayoutEffect, useRef, useState } from 'react';
 
 import styles from '~/client/components/layouts/Header/NavEmployeurs.module.scss';
+import { useExitModal } from '~/client/hooks/useExitModal';
 
 import { Icon } from '../../ui/Icon/Icon';
 import { isNavigationItem, NavigationItem, NavigationItemWithChildren } from './NavigationStructure';
@@ -28,29 +29,34 @@ export function NavEmployeurs ({ item: root }: NavEmployeursProps) {
     return () => window.removeEventListener('resize', onResize);
   }, [wrapper, content]);
 
+  
+  useExitModal(wrapper, isExpanded, () => isExpanded && setIsExpanded(false));
+
 
   return (
     <li className={styles.navItem}>
       <button className={styles.navItemButton}
-        onClick={() => setIsExpanded(!isExpanded)}
+        onClick={(e) => {e.stopPropagation(); setIsExpanded(!isExpanded); }}
       >
         <span className={styles.navItemLabel}>{root.label}</span>
         <Icon name="angle-down" className={classNames(styles.icon, { [styles.expanded]: isExpanded })} />
       </button>
       <div ref={wrapper} className={classNames(styles.navWrapper, { [styles.expanded]: isExpanded })}>
         <ul ref={content} className={styles.navDetail}>
-          { listsFromChildren(root) }
+          { listsFromChildren(root, () => setIsExpanded(false)) }
         </ul>
       </div>
     </li>
   );
 }
 
-function listsFromChildren(item: NavigationItemWithChildren | NavigationItem) {
+function listsFromChildren(item: NavigationItemWithChildren | NavigationItem, onItemChosen: () => void) {
   if (isNavigationItem(item)) {
     return (
       <li className={styles.navLeaf}>
-        <Link href={item.link}>{item.label}</Link>
+        <span onClick={onItemChosen}>
+          <Link href={item.link}>{item.label}</Link>
+        </span>
       </li>
     );
   }
@@ -61,7 +67,7 @@ function listsFromChildren(item: NavigationItemWithChildren | NavigationItem) {
         { item.legend ? <em>{item.legend}</em> : ''}
       </span>
       <ul className={styles.navSectionItems}>
-        {item.children.map((i) => listsFromChildren(i))}
+        {item.children.map((i) => listsFromChildren(i, onItemChosen))}
       </ul>
     </li>
   );
---
+++ src/client/hooks/useExitModal.ts
@@ -0,0 +1,25 @@
+import { RefObject, useEffect } from 'react';
+
+import { KeyBoard } from '../components/keyboard/keyboard.enum';
+
+export function useExitModal<E extends HTMLElement> (wrapperElement: RefObject<E>, isOpen: boolean, onExit: () => void) {
+  useEffect(() => {
+    function onGlobalClick (e: MouseEvent) {
+      if (isOpen && wrapperElement.current && !(wrapperElement.current)?.contains(e.target as Node)) {
+        onExit();
+      }
+    }
+    function onTypeEscape (e: KeyboardEvent) {
+      if (isOpen && e.key === KeyBoard.ESCAPE) {
+        onExit();
+      }
+    }
+
+    document.addEventListener('click', onGlobalClick);
+    document.addEventListener('keydown', onTypeEscape);
+    return () => {
+      document.removeEventListener('click', onGlobalClick);
+      document.addEventListener('keydown', onTypeEscape);
+    };
+  }, [wrapperElement, isOpen]);
+}
