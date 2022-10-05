f50cbeee7c1b24a9eacff8f814921a1b236ce63e
Sergen <sergen.kovar@octo.com>
2022-10-05 18:19:54 +0200

    Mise en place du contenu

---
+++ src/pages/maintenance-pole-emploi/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import MaintenancePoleEmploi from '~/client/components/features/MaintenancePoleEmploi/MaintenancePoleEmploi';
+
+export default function MaintenancePoleEmploiPage() {
+  return (
+    <MaintenancePoleEmploi />
+  );
+}
