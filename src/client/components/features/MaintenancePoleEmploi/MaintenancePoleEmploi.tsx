21dcbd84e208dc4d75805a17fcad7de248c56c2f
Sergen <sergen.kovar@octo.com>
2022-10-07 16:29:36 +0200

    updates files: review UX

--- src/client/components/features/MaintenancePoleEmploi/MaintenancePoleEmploi.tsx
+++ src/client/components/features/MaintenancePoleEmploi/MaintenancePoleEmploi.tsx
@@ -10,6 +10,9 @@ export default function MaintenancePoleEmploi() {
   return (
     <section className={ styles.section }>
       <Container className={ styles.sectionContainer }>
+        <aside className= { styles.sectionContainerIllustration }>
+          <Image src="/illustrations/maintenance.svg" objectFit='cover' layout='fill' alt='' />
+        </aside>
         <article className={ styles.sectionContainerText}>
           <h1 className={styles.sectionContainerTextTitre}>Le formulaire pour déposer une offre d&apos;emploi est actuellement en maintenance. Merci de réessayer plus tard.</h1>
           <p>Pour patienter, découvrez l&apos;ensemble des dispositifs du plan jeune pour les employeurs.</p>
@@ -19,9 +22,6 @@ export default function MaintenancePoleEmploi() {
             Je découvre les dispositifs
           </LinkAsButton>
         </article>
-        <aside className= { styles.sectionContainerIllustration }>
-          <Image src="/illustrations/maintenance.svg" objectFit='cover' layout='fill' alt='' />
-        </aside>
       </Container>
     </section>
   );
