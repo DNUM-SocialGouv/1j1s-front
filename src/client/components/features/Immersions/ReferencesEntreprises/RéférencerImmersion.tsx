0cbe9edad164c587dd5760595b1919e9db128d97
toma-benguigui <108266581+toma-benguigui@users.noreply.github.com>
2022-10-10 11:28:47 +0200

    [764] Ajout du bandeau RGPD sous l'iframe immersions (#385)

--- src/client/components/features/Immersions/ReferencesEntreprises/RéférencerImmersion.tsx
+++ src/client/components/features/Immersions/ReferencesEntreprises/RéférencerImmersion.tsx
@@ -5,16 +5,24 @@ import { HeadTag } from '~/client/components/utils/HeaderTag';
 const subtitle = 'Ce formulaire vous permet d\'indiquer les métiers de votre établissement ouverts aux immersions. Si votre entreprise comprend plusieurs établissements, il convient de renseigner un formulaire pour chaque établissement (SIRET différent).';
 const immersionFacileUrl = 'https://immersion-facile.beta.gouv.fr/etablissement/unJeuneUneSolution';
 
-export function RéférencerImmersion () {
+export function RéférencerImmersion() {
   return (
     <>
-      <HeadTag title="je référence mon entreprise pour des immersions | 1jeune1solution" />
-      <Hero className={ styles.hero }>
+      <HeadTag title="je référence mon entreprise pour des immersions | 1jeune1solution"/>
+      <Hero className={styles.hero}>
         <p><b>Je référence mon entreprise</b></p>
-        <p className={styles.heroSubtitle}>{ subtitle }</p>
+        <p className={styles.heroSubtitle}>{subtitle}</p>
       </Hero>
-      <div className={ styles.section }>
-        <iframe className={styles.iframe} src={ immersionFacileUrl } />
+      <div className={styles.section}>
+        <iframe className={styles.iframe} src={immersionFacileUrl}/>
+        <div className={styles.rgpd}>
+          Vous êtes informé que vos données sont collectées et traitées par le Groupement d&apos;intérêt public de plateforme de l&apos;inclusion pour
+          traiter votre demande de référencement de votre entreprise. Pour en savoir plus sur vos droits consultez la <a
+            href={'https://immersion-facile-1.gitbook.io/mentions-legales/politique-de-confidentialite'}>Politique de Confidentialité</a> du Groupement
+          d&apos;intérêt public de plateforme de l&apos;inclusion. En cliquant sur « confirmer » vous reconnaissez avoir pris connaissance et accepter
+          les <a href={'https://immersion-facile-1.gitbook.io/mentions-legales/conditions-generales-dutilisation'}>Conditions Générales
+          d&apos;Utilisation</a> du Groupement d&apos;intérêt public de plateforme de l&apos;inclusion.
+        </div>
       </div>
     </>
   );
