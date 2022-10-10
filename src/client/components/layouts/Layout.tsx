a90afac6d02ca271401aa735811fdcc6bc82c362
Paul Gallo <paul.gallo@octo.com>
2022-10-10 12:35:00 +0200

    fix(webpack): ajout chunkname header footer

--- src/client/components/layouts/Layout.tsx
+++ src/client/components/layouts/Layout.tsx
@@ -9,8 +9,8 @@ import { Header } from '~/client/components/layouts/Header/Header';
 import { useDependency } from '~/client/context/dependenciesContainer.context';
 import { AnalyticsService } from '~/client/services/analyticsService';
 
-const Footer = dynamic(() => import('~/client/components/layouts/Footer/Footer'), { ssr: false });
-const SkipLink = dynamic(() => import('~/client/components/ui/SkipLink/SkipLink'), { ssr: false });
+const Footer = dynamic(() => import(/* webpackChunkName: 'footer' */ '~/client/components/layouts/Footer/Footer'), { ssr: false });
+const SkipLink = dynamic(() => import(/* webpackChunkName: 'header' */ '~/client/components/ui/SkipLink/SkipLink'), { ssr: false });
 
 export function Layout({ children }: React.PropsWithChildren) {
   const router = useRouter();
