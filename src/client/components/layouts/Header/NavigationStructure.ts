1ce8581524a7d76a25a1f4ab46cc23e75cd4686c
Sergen <sergen.kovar@octo.com>
2022-10-24 16:53:12 +0200

    feat(POE): masquer du menu

--- src/client/components/layouts/Header/NavigationStructure.ts
+++ src/client/components/layouts/Header/NavigationStructure.ts
@@ -64,7 +64,6 @@ const employeurNav: NavigationItemWithChildren = {
         { label: 'Je recrute', link: '/je-recrute' },
         { label: 'Je deviens mentor', link: '/je-deviens-mentor' },
         { label: 'Je propose des immersions', link: '/immersions' },
-        { label: 'Je forme les jeunes grâce à l\'emploi', link: '/rejoindre-mobilisation-poe' },
       ], label: 'Recruter et agir pour les jeunes',
     },
     { label: 'Découvrir les mesures employeurs', link: '/mesures-employeurs' },
