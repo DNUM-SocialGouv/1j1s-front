aef0ba432260a2708b37a7a5be14d8da18ffdbe9
Caroline Chuong - CACH <caroline.chuong@octo.com>
2022-10-25 16:12:44 +0200

    feat(Stage): ajout de la description de l'employeur dans le détail d'une offre

--- src/client/components/features/OffreDeStage/Consulter/ConsulterOffreDeStage.tsx
+++ src/client/components/features/OffreDeStage/Consulter/ConsulterOffreDeStage.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 
 import commonStyles from '~/client/components/features/ConsulterOffre.module.scss';
 import { dureeCategorisee } from '~/client/components/features/OffreDeStage/Consulter/getDureeCategorisee';
-import {
-  Domaines,
-  OffreDeStageDétail,
-} from '~/client/components/features/OffreDeStage/OffreDeStage.type';
+import { Domaines, OffreDeStageDétail } from '~/client/components/features/OffreDeStage/OffreDeStage.type';
 import { ConsulterOffreLayout } from '~/client/components/layouts/ConsulterOffre/ConsulterOffreLayout';
 import { Link } from '~/client/components/ui/Link/Link';
 import { TagList } from '~/client/components/ui/Tag/TagList';
@@ -20,7 +17,7 @@ export function ConsulterOffreDeStage({ offreDeStage }: ConsulterOffreDeStagePro
   const listeEtiquettes: Array<string> = offreDeStage.domaines
     ? offreDeStage.domaines
       .filter((domaine) => domaine.nom !== Domaines.NON_RENSEIGNE)
-      .map((domaine)=>domaine.nom)
+      .map((domaine) => domaine.nom)
     : [];
   listeEtiquettes.push(
     offreDeStage.localisation?.ville || offreDeStage.localisation?.departement || offreDeStage.localisation?.region as string,
@@ -33,15 +30,21 @@ export function ConsulterOffreDeStage({ offreDeStage }: ConsulterOffreDeStagePro
     <ConsulterOffreLayout>
       <header className={commonStyles.titre}>
         <h1>{offreDeStage.titre}</h1>
-        { offreDeStage.employeur?.nom && <h2>{offreDeStage.employeur?.nom}</h2>}
+        {offreDeStage.employeur?.nom && <h2>{offreDeStage.employeur?.nom}</h2>}
         <TagList list={listeEtiquettes} aria-label="Caractéristiques de l'offre de stage"/>
         <div className={commonStyles.buttonAsLinkWrapper}>
           <div className={commonStyles.buttonAsLink}>
-            {offreDeStage.urlDeCandidature && <Link href={offreDeStage.urlDeCandidature} appearance="asPrimaryButton">Postuler</Link>}
+            {offreDeStage.urlDeCandidature &&
+                        <Link href={offreDeStage.urlDeCandidature} appearance="asPrimaryButton">Postuler</Link>}
           </div>
         </div>
       </header>
       <section className={commonStyles.contenu}>
+        {offreDeStage.employeur?.description &&
+                <div>
+                  <h3>Description de l&apos;employeur :</h3>
+                  <p dangerouslySetInnerHTML={{ __html: marked.parse(offreDeStage.employeur.description) }}/>
+                </div>}
         {offreDeStage.description &&
                 <div>
                   <h3>Description du poste :</h3>
