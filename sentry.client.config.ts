a0ba02acf46c3478df8816a809daca9f075bd9b4
Florian Leroy <florian.leroy@octo.com>
2022-11-03 07:57:41 +0100

    chore(logs): filtre des events selon une blacklist de user agent

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,15 +6,23 @@ import * as Sentry from '@sentry/nextjs';
 
 const DEFAULT_SENTRY_CLIENT_ENVIRONMENT = 'local';
 
+const userAgentBlacklist = process.env.NEXT_PUBLIC_SENTRY_USER_AGENT_BLACKLIST?.split(' ');
+
 Sentry.init({
+  beforeSend(event) {
+    if(!event.request?.headers) {
+      return event;
+    }
+    const userAgent: string | undefined = event.request.headers['user-agent'];
+    const userAgentDuBot = userAgentBlacklist?.find((botUserAgent) => userAgent?.includes(botUserAgent));
+    if (userAgentDuBot !== undefined) {
+      return null; // Don't send this event to Sentry
+    }
+    return event;
+  },
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   enabled: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === 'production' || process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === 'integration',
   environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || DEFAULT_SENTRY_CLIENT_ENVIRONMENT,
-  // Adjust this value in production, or use tracesSampler for greater control
   release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
-  tracesSampleRate: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === 'production' ? 0.1 : 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+  tracesSampleRate: Number(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE),
 });
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,15 +6,23 @@ import * as Sentry from '@sentry/nextjs';
 
 const DEFAULT_SENTRY_SERVER_ENVIRONMENT = 'local';
 
+const userAgentBlacklist = process.env.SENTRY_USER_AGENT_BLACKLIST?.split(' ');
+
 Sentry.init({
+  beforeSend(event) {
+    if(!event.request?.headers) {
+      return event;
+    }
+    const userAgent: string | undefined = event.request.headers['user-agent'];
+    const userAgentDuBot = userAgentBlacklist?.find((botUserAgent) => userAgent?.includes(botUserAgent));
+    if (userAgentDuBot !== undefined) {
+      return null; // Don't send this event to Sentry
+    }
+    return event;
+  },
   dsn: process.env.SENTRY_DSN,
   enabled: process.env.SENTRY_ENVIRONMENT === 'production' || process.env.SENTRY_ENVIRONMENT === 'integration',
   environment: process.env.SENTRY_ENVIRONMENT || DEFAULT_SENTRY_SERVER_ENVIRONMENT,
-  // Adjust this value in production, or use tracesSampler for greater control
   release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
-  tracesSampleRate: process.env.SENTRY_ENVIRONMENT === 'production' ? 0.1 : 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+  tracesSampleRate: Number(process.env.SENTRY_TRACES_SAMPLE_RATE),
 });
