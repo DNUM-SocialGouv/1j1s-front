cb7e22a6c89413472ab53011043d92c1a48283e9
Sergen <sergen.kovar@octo.com>
2022-11-03 12:05:12 +0100

    feat(Page POE): mise en place dependencies

---
+++ src/server/contact-poe/configuration/contactpoeDependencies.ts
@@ -0,0 +1,15 @@
+import { FormerPoleEmploiRepository } from '~/server/contact-poe/domain/FormerPoleEmploi.repository';
+import { JeRecruteAfprPoeiUseCase } from '~/server/contact-poe/usecase/jeRecruteAfprPoeiUseCase';
+
+export interface ContactpoeDependencies {
+  jeRecruteAfprPoeiUseCase: JeRecruteAfprPoeiUseCase
+}
+
+export const contactpoeDependenciesContainer = (
+  jeRecruteAfprPoei: FormerPoleEmploiRepository,
+  sauvegarde: FormerPoleEmploiRepository,
+): ContactpoeDependencies => {
+  return {
+    jeRecruteAfprPoeiUseCase: new JeRecruteAfprPoeiUseCase(jeRecruteAfprPoei, sauvegarde),
+  };
+};
