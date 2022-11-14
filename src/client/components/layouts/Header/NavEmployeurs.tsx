4d612e7b3b6615ab85af5e25b396a93b2e585a5e
Sergen <sergen.kovar@octo.com>
2022-11-14 12:07:00 +0100

    fix(header): lien active non cliquable

--- src/client/components/layouts/Header/NavEmployeurs.tsx
+++ src/client/components/layouts/Header/NavEmployeurs.tsx
@@ -69,7 +69,7 @@ function listsFromChildren(path: string, item: NavigationItemWithChildren | Navi
   if (isNavigationItem(item)) {
     return (
       <li key={item.link} className={styles.navLeaf}>
-        <span aria-current={isActive} onClick={onItemChosen} className={styles.employeursLien}>
+        <span aria-current={isActive} onClick={onItemChosen} className={styles.employeursLien} style={{ pointerEvents: isActive ? 'none' : 'auto' }}>
           <Link href={item.link}>
             {item.label}
           </Link>
