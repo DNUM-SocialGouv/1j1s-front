96af960fdb765e3ff58456833c5f707436686c91
Paul Gallo <paul.gallo@octo.com>
2022-11-16 14:01:57 +0100

    refactor(button): suppression composant déprécié

--- src/client/components/ui/Button/button.props.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export interface ButtonProps extends React.ButtonHTMLAttributes<unknown> {
-  buttonType: 'primary' | 'link' | 'linkWithRightIcon' | 'withLeftIcon' | 'withRightIcon' | 'withTopIcon' | 'secondary'
-  icon?: React.ReactNode
-  buttonOnDarkBackground?: boolean
-}
