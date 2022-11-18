0f036c53f1fae6cbc65d38c555435f7a02cdcc4f
Olivia Peny-Coblentz <oliv@octo.com>
2022-11-18 09:47:18 +0100

    refactor(employeur): modification structure section

--- src/client/components/features/JeRecrute/DecouvrirMesuresEmployeurs/DecouvrirMesuresEmployeurs.tsx
+++ src/client/components/features/JeRecrute/DecouvrirMesuresEmployeurs/DecouvrirMesuresEmployeurs.tsx
@@ -9,15 +9,15 @@ export default function DecouvrirMesuresEmployeurs() {
   return (
     <section className={ styles.section }>
       <Container className={ styles.sectionContainer }>
-        <aside className= { styles.sectionContainerIllustration }>
+        <span className= { styles.sectionContainerIllustration }>
           <Image src="/icons/decouverteMesuresEmployeurs.svg" objectFit='cover' layout='fill' alt='' />
-        </aside>
-        <article className={ styles.sectionContainerText }>
+        </span>
+        <span className={ styles.sectionContainerText }>
           <h2 className={styles.sectionContainerTextTitre}>Découvrez les mesures du plan 1jeune1solution pour vous aider
             <span className={styles.sectionContainerTextTitreAccroche}> à recruter plus facilement</span>
           </h2>
           <Link href="/mesures-employeurs" appearance="asSecondaryButton">Découvrir toutes les mesures employeurs</Link>
-        </article>
+        </span>
       </Container>
     </section>
   );
