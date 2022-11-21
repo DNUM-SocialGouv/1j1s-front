9a2bfb433f34e8fd2736300905e73243c56a7a22
Florian Leroy <florian.leroy@octo.com>
2022-11-21 18:44:35 +0100

    fix(document): correction chemin du favicon 128

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,7 +21,7 @@ export default function Document() {
         <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16" />
-        <link rel="icon" type="image/png" href="/favicons/favicon-128.png" sizes="128x128" />
+        <link rel="icon" type="image/png" href="/favicons/favicon-128x128.png" sizes="128x128" />
         <meta name="application-name" content="1jeune1solution"/>
         <meta name="msapplication-TileColor" content="#FFFFFF" />
         <meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png" />
