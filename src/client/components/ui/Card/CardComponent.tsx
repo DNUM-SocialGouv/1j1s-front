75c4578c39a44257fc1c8dd9c6bfb26669130766
Paul Gallo <paul.gallo@octo.com>
2022-11-22 17:11:31 +0100

    refactor(cartes): creation composant carte générique + refacto carte découvrir les métiers

---
+++ src/client/components/ui/Card/CardComponent.tsx
@@ -0,0 +1,68 @@
+import classNames from 'classnames';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useMemo } from 'react';
+
+import { HtmlHeadingTag } from '../../props';
+import styles from './CardComponent.module.scss';
+
+interface CardComponentProps {
+  layout: 'horizontal' | 'vertical'
+  link: string
+}
+
+export function CardComponent({ children, className, layout, link }: CardComponentProps & React.HTMLAttributes<HTMLLinkElement>) {
+  const layoutClass = useMemo(() => {
+    switch (layout) {
+      case 'horizontal': return styles.cardComponentHorizontal;
+      case 'vertical': return styles.cardComponentVertical;
+    }
+  }, [layout]);
+
+  return (
+    <>
+      <Link className={classNames(styles.cardComponent, layoutClass, className)} href={link}>
+        {children}
+      </Link>
+    </>
+  );
+}
+
+function CardContent({ children, className, rest }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={className} {...rest}>{children}</div>;
+}
+
+function CardButton({ appearance = 'tertiary', className, icon, label }: { appearance: 'primary' | 'secondary' | 'tertiary', icon?: React.ReactNode, label?: string } & React.HTMLAttributes<HTMLButtonElement>) {
+  const appearanceClass = useMemo(() => {
+    switch (appearance) {
+      case 'primary': return styles.cardComponentButtonPrimary;
+      case 'secondary': return styles.cardComponentButtonSecondary;
+      case 'tertiary': return styles.cardComponentButtonTertiary;
+    }
+  }, [appearance]);
+
+  return (
+    <span className={classNames(className, appearanceClass, styles.cardComponentButton)}>
+      <span>{label}</span>
+      {icon}
+    </span>
+  );
+}
+
+function CardImage({ className, objectFit = 'contain', src }: { objectFit?: 'cover' | 'contain', src: string } & React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={classNames(styles.cardImageWrapper, className)}>
+      <Image src={src} alt={''} layout={'fill'} objectFit={objectFit} />
+    </div>
+  );
+}
+
+function CardTitle({ children, className, id, titleAs = 'h2' }: { titleAs?: HtmlHeadingTag } & React.HTMLAttributes<HTMLTitleElement>) {
+  return React.createElement(titleAs, { className: classNames(styles.cardTitle, className), id }, children );
+}
+
+CardComponent.Button = CardButton;
+CardComponent.Content = CardContent;
+CardComponent.Image = CardImage;
+CardComponent.Title = CardTitle;
+
