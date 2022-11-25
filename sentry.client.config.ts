eb33a68f9eacd2ecc3c076851f99d12e0f4a4d33
Simon Belbeoch <39310468+LiquidITGuy@users.noreply.github.com>
2022-11-25 17:28:16 +0100

    chore(Sentry): utilisation des bonnes variables d'environnements pour sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,35 +1,43 @@
-// This file configures the initialization of Sentry on the browser.
-// The config you add here will be used whenever a page is visited.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from '@sentry/nextjs';
+import { SeverityLevel } from '@sentry/nextjs';
 
-const DEFAULT_SENTRY_CLIENT_ENVIRONMENT = 'local';
+const DEFAULT_SENTRY_ENVIRONMENT = 'local';
+const USER_AGENT_BLACKLIST = process.env.NEXT_PUBLIC_SENTRY_USER_AGENT_BLACKLIST?.split(',');
+const SENTRY_ENVIRONMENTS_ENABLE_SEND_DATA = ['integration', 'production', 'review_app'];
+const SENTRY_ENVIRONMENTS_ENABLE_DEBUG = ['local', 'review_app'];
+const SEND_DATA = SENTRY_ENVIRONMENTS_ENABLE_SEND_DATA.includes(process!.env!.NEXT_PUBLIC_SENTRY_ENVIRONMENT!);
+const DEBUG_DATA = SENTRY_ENVIRONMENTS_ENABLE_DEBUG.includes(process!.env!.NEXT_PUBLIC_SENTRY_ENVIRONMENT!);
 
-const userAgentBlacklist = process.env.NEXT_PUBLIC_SENTRY_USER_AGENT_BLACKLIST?.split(',');
+
+const releaseName = (environnement = process.env) => {
+  if(environnement.NEXT_PUBLIC_SENTRY_ENVIRONMENT === 'review_app') {
+    return 'reviewApp';
+  }
+  return `${environnement.npm_package_name}@${environnement.npm_package_version}`;
+};
 
 Sentry.init({
   beforeSend(event) {
+    if(!SEND_DATA) {
+      return null;
+    }
     if (!event.request?.headers) {
       return event;
     }
     const userAgent: string | undefined = event.request.headers['user-agent'];
-    const userAgentDuBot = userAgentBlacklist?.find((botUserAgent) => userAgent?.includes(botUserAgent));
+    const userAgentDuBot = USER_AGENT_BLACKLIST?.find((botUserAgent) => userAgent?.includes(botUserAgent));
     if (userAgentDuBot !== undefined) {
       return null; // Don't send this event to Sentry
     }
     return event;
   },
+  debug: DEBUG_DATA,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  enabled: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === 'production' || process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === 'integration',
-  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || DEFAULT_SENTRY_CLIENT_ENVIRONMENT,
+  enabled: true,
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || DEFAULT_SENTRY_ENVIRONMENT,
   initialScope: {
-    level: 'error',
+    level: process.env.NEXT_PUBLIC_SENTRY_LOG_LEVEL as SeverityLevel,
   },
-  release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
+  release: releaseName(),
   tracesSampleRate: Number(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE),
-});
-
-Sentry.configureScope((scope) => {
-  scope.setLevel(process.env.NEXT_PUBLIC_SENTRY_LOG_LEVEL);
 });
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,26 +1,26 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from '@sentry/nextjs';
+import { SeverityLevel } from '@sentry/nextjs';
 
-const DEFAULT_SENTRY_SERVER_ENVIRONMENT = 'local';
+const DEFAULT_SENTRY_ENVIRONMENT = 'local';
 const SENTRY_ENVIRONMENTS_ENABLE_SEND_DATA = ['integration', 'production', 'review_app'];
 const SENTRY_ENVIRONMENTS_ENABLE_DEBUG = ['local', 'review_app'];
 
-const SEND_DATA = SENTRY_ENVIRONMENTS_ENABLE_SEND_DATA.includes(process.env.SENTRY_ENVIRONMENT!);
-const DEBUG_DATA = SENTRY_ENVIRONMENTS_ENABLE_DEBUG.includes(process.env.SENTRY_ENVIRONMENT!);
+const SEND_DATA = SENTRY_ENVIRONMENTS_ENABLE_SEND_DATA.includes(process!.env!.NEXT_PUBLIC_SENTRY_ENVIRONMENT!);
+const DEBUG_DATA = SENTRY_ENVIRONMENTS_ENABLE_DEBUG.includes(process!.env!.NEXT_PUBLIC_SENTRY_ENVIRONMENT!);
 
 const releaseName = () => {
-  if(process.env.SENTRY_ENVIRONMENT === 'review_app') {
+  if(process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === 'review_app') {
     return 'reviewApp';
   }
   return `${process.env.npm_package_name}@${process.env.npm_package_version}`;
 };
-const userAgentBlacklist = process.env.SENTRY_USER_AGENT_BLACKLIST?.split(',');
+const userAgentBlacklist = process.env.NEXT_PUBLIC_SENTRY_USER_AGENT_BLACKLIST?.split(',');
 
 Sentry.init({
   beforeSend(event) {
+    if(!SEND_DATA) {
+      return null;
+    }
     if (!event.request?.headers) {
       return event;
     }
@@ -32,13 +32,12 @@ Sentry.init({
     return event;
   },
   debug: DEBUG_DATA,
-  dsn: process.env.SENTRY_DSN,
-  enabled: SEND_DATA,// vérifier
-  environment: process.env.SENTRY_ENVIRONMENT || DEFAULT_SENTRY_SERVER_ENVIRONMENT,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: true,
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || DEFAULT_SENTRY_ENVIRONMENT,
+  initialScope: {
+    level: process.env.NEXT_PUBLIC_SENTRY_LOG_LEVEL as SeverityLevel,
+  },
   release: releaseName(),
-  tracesSampleRate: Number(process.env.SENTRY_TRACES_SAMPLE_RATE),
-});
-
-Sentry.configureScope((scope) => {
-  scope.setLevel(process.env.SENTRY_LOG_LEVEL);
+  tracesSampleRate: Number(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE),
 });
