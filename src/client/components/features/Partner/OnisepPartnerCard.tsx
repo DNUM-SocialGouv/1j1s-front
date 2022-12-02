724f936946c9b5f316dc37ae10b4f2b4747a43c6
Sergen <sergen.kovar@octo.com>
2022-12-02 11:09:12 +0100

    feat(accompagnement): wording couleurs des cards partenaires

--- src/client/components/features/Partner/OnisepPartnerCard.tsx
+++ src/client/components/features/Partner/OnisepPartnerCard.tsx
@@ -7,7 +7,7 @@ export function OnisepPartnerCard() {
     <PartnerCard
       description="Trouvez un métier qui vous correspond parmi plus de 700 fiches."
       headline="Renseignez-vous sur les différents métiers avec l'ONISEP."
-      headlineColor="asOnisepColor"
+      headlineColor="red"
       linkLabel="Je découvre les métiers"
       link="/decouvrir-les-metiers"
       logo="/images/logos/onisep.svg"
