aac5c725a764e1bf09dcd61506f624575986206a
Florian Leroy <florian.leroy@octo.com>
2022-12-06 23:20:29 +0100

    refactor(évènement): afficher date avec Date natif

--- src/client/utils/formatEventDateTime.util.ts
+++ src/client/utils/formatEventDateTime.util.ts
@@ -1,8 +1,6 @@
-import { DateTime } from 'luxon';
-
 export function formatEventDateTime(dateDebut: string, dateFin: string) {
-  const dateTimeDebut = DateTime.fromISO(dateDebut);
-  const dateTimeFin = DateTime.fromISO(dateFin);
+  const dateTimeDebut = new Date(dateDebut);
+  const dateTimeFin = new Date(dateFin);
 
   if (isSameDate(dateTimeDebut, dateTimeFin)) {
     return `${getLongDate(dateTimeDebut)} de ${getTime(dateTimeDebut)} à ${getTime(dateTimeFin)}`;
@@ -12,35 +10,27 @@ export function formatEventDateTime(dateDebut: string, dateFin: string) {
     return `${getLongDate(dateTimeDebut)} - ${getLongDate(dateTimeFin)}`;
   }
 
-  return `${getLongDateTime(dateTimeDebut)} - ${getLongDateTime(dateTimeFin)}`;
+  return `${getLongDate(dateTimeDebut)} à ${getTime(dateTimeDebut)} - ${getLongDate(dateTimeFin)} à ${getTime(dateTimeFin)}`;
 }
 
-export function getLongDate(date: DateTime) {
-  return date.toLocaleString({
+export function getLongDate(date: Date) {
+  return date.toLocaleString(undefined, {
     day: 'numeric',
     month: 'long',
     weekday: 'short',
   });
 }
 
-export function getLongDateTime(date: DateTime) {
-  return date.toLocaleString({
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    month: 'long',
-    weekday: 'short',
-  });
+export function getTime(date: Date) {
+  return date.toLocaleTimeString(undefined, { hour: '2-digit', minute:'2-digit' });
 }
 
-export function getTime(date: DateTime) {
-  return date.toLocaleString(DateTime.TIME_SIMPLE);
+function isSameDate(dateDebut: Date, dateFin: Date) {
+  return dateDebut.getFullYear() === dateFin.getFullYear() &&
+    dateDebut.getMonth() === dateFin.getMonth() &&
+    dateDebut.getDate() === dateFin.getDate();
 }
 
-function isSameDate(dateDebut: DateTime, dateFin: DateTime) {
-  return dateDebut.toLocaleString() === dateFin.toLocaleString();
-}
-
-function timeIsZero(dateDebut: DateTime) {
-  return dateDebut.hour === 0 && dateDebut.minute === 0;
+function timeIsZero(dateDebut: Date) {
+  return dateDebut.getHours() === 0 && dateDebut.getMinutes() === 0;
 }
