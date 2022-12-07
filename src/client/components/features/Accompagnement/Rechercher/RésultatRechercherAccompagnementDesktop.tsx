720776e400f879239107c18d3a64e0f4d2465ea0
Dorian De Rosa <dorian.de.rosa@octo.com>
2022-12-07 13:31:21 +0100

    fix(accompagnement): ne pas afficher bouton contact si email non renseigné

--- src/client/components/features/Accompagnement/Rechercher/RésultatRechercherAccompagnementDesktop.tsx
+++ src/client/components/features/Accompagnement/Rechercher/RésultatRechercherAccompagnementDesktop.tsx
@@ -24,12 +24,15 @@ export function RésultatRechercherAccompagnementDesktop(props: Omit<LienSolutio
             {nomEntreprise && <span className={styles.address}>{nomEntreprise}</span>}
             <TagList list={étiquetteOffreList} className={styles.tags}/>
           </div>
-          <a href={lienOffre} className={styles.button}>
-            Contacter l‘agence
-            <svg className={styles.buttonIcon} width="15" height="20" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M1 0H19C19.5523 0 20 0.447715 20 1V17C20 17.5523 19.5523 18 19 18H1C0.447715 18 0 17.5523 0 17V1C0 0.447715 0.447715 0 1 0ZM18 4.238L10.072 11.338L2 4.216V16H18V4.238ZM2.511 2L10.061 8.662L17.502 2H2.511Z" fill="currentColor"/>
-            </svg>
-          </a>
+          {
+            lienOffre &&
+            <a href={lienOffre} className={styles.button}>
+              Contacter l‘agence
+              <svg className={styles.buttonIcon} width="15" height="20" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M1 0H19C19.5523 0 20 0.447715 20 1V17C20 17.5523 19.5523 18 19 18H1C0.447715 18 0 17.5523 0 17V1C0 0.447715 0.447715 0 1 0ZM18 4.238L10.072 11.338L2 4.216V16H18V4.238ZM2.511 2L10.061 8.662L17.502 2H2.511Z" fill="currentColor"/>
+              </svg>
+            </a>
+          }
         </div>
         <details className={styles.details}>
           <summary className={styles.summary}>Voir les horaires d‘ouverture</summary>
