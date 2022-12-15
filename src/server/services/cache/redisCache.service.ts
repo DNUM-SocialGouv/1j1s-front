b0cecfe4ea33162eaf9e0769e396f6578f86b1c7
Paul Gallo <paul.gallo@octo.com>
2022-12-15 16:11:04 +0100

    fix(cache-redis): ajout try/catch autours des appels à redis

--- src/server/services/cache/redisCache.service.ts
+++ src/server/services/cache/redisCache.service.ts
@@ -1,7 +1,9 @@
 import Redis from 'ioredis';
 
+import { SentryException } from '~/server/exceptions/sentryException';
 import { CacheService } from '~/server/services/cache/cache.service';
 import { ConfigurationService } from '~/server/services/configuration.service';
+import { LoggerService } from '~/server/services/logger.service';
 
 export class RedisCacheService implements CacheService {
   private client: Redis;
@@ -17,12 +19,33 @@ export class RedisCacheService implements CacheService {
   }
 
   async get<T>(key: string): Promise<T | null> {
-    const value = await this.client.get(key);
-    return value ? JSON.parse(value) as T : null;
+    try {
+      const value = await this.client.get(key);
+      return value ? JSON.parse(value) as T : null;
+    } catch (error) {
+      LoggerService.warnWithExtra(
+        new SentryException(
+          '[Cache Redis] Erreur de récupération du cache',
+          { context: JSON.stringify(error), source: 'Cache Redis' },
+          { errorDetail: `${key}` },
+        ),
+      );
+      return Promise.reject();
+    }
   }
 
   async set(key: string, value: unknown, expiresInHours: number) {
-    await this.client.set(key, JSON.stringify(value));
-    this.client.expire(key, 3600 *  expiresInHours);
+    try {
+      await this.client.set(key, JSON.stringify(value));
+      this.client.expire(key, 3600 *  expiresInHours);
+    } catch (error) {
+      LoggerService.warnWithExtra(
+        new SentryException(
+          '[Cache Redis] Erreur de mise en cache',
+          { context: JSON.stringify(error), source: 'Cache Redis' },
+          { errorDetail: `${key} : ${value} - Expires in ${expiresInHours} hours` },
+        ),
+      );
+    }
   }
 }
