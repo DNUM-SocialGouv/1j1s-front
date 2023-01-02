46dbc3139e334784c8f9a601f6cc3f5eb2879475
Sergen <sergen.kovar@octo.com>
2023-01-02 17:52:18 +0100

    fix(cej): refacto formulaire

---
+++ src/client/components/features/ContratEngagementJeune/Rappel/FormulaireDeContact/FormulaireDeContactCEJ.tsx
@@ -0,0 +1,114 @@
+import React, { FormEvent, PropsWithChildren, useState } from 'react';
+
+import { useDependency } from '~/client/context/dependenciesContainer.context';
+import { ageOptions } from '~/client/domain/selectAgeData';
+import { DemandeDeContactService } from '~/client/services/demandeDeContact/demandeDeContact.service';
+import { isSuccess } from '~/server/errors/either';
+
+import { ButtonComponent } from '../../../../ui/Button/ButtonComponent';
+import InputAutocomplétionCommune from '../../../../ui/Form/InputAutocomplétion/InputAutocomplétionCommune';
+import { InputText } from '../../../../ui/Form/InputText/InputText';
+import { SpinnerIcon } from '../../../../ui/Icon/spinner.icon';
+import { Select } from '../../../../ui/Select/Select';
+import { DéchargeRGPD } from '../../../LesEntreprisesSEngagent/DéchargeRGPD/DéchargeRGPD';
+import styles from './FormulaireDeContactCEJ.module.scss';
+
+interface FormulaireDeContactCEJProps {
+  onSuccess?: () => void;
+}
+
+export default function FormulaireDeContactCEJ({ onSuccess }: PropsWithChildren<FormulaireDeContactCEJProps>) {
+	const [inputAge, setInputAge] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const demandeDeContactService = useDependency<DemandeDeContactService>('demandeDeContactService');
+	const [inputVille, setInputVille] = useState('');
+	const [inputCodePostal, setInputCodePostal] = useState('');
+
+	async function envoyerFormulaireDeContact(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		const form: HTMLFormElement = event.currentTarget;
+		const data = new FormData(form);
+		setIsLoading(true);
+		const response = await demandeDeContactService.envoyerPourLeCEJ({
+			age: Number(data.get('age')),
+			codePostal: inputCodePostal,
+			email: String(data.get('mail')),
+			nom: String(data.get('lastname')),
+			prénom: String(data.get('firstname')),
+			téléphone: String(data.get('phone')),
+			ville: inputVille,
+		});
+		setIsLoading(false);
+
+		if (isSuccess(response)) {
+			if (onSuccess) {
+				onSuccess();
+			}
+		} else {
+			alert('Erreur dans l‘envoi du formulaire :' + response.errorType);
+		}
+	}
+
+	return (
+		<form
+			className={styles.formulaire}
+			onSubmit={envoyerFormulaireDeContact}
+		>
+			<InputText
+				label="Prénom"
+				name="firstname"
+				autoFocus
+				placeholder="Exemple : Jean"
+				required
+			/>
+			<InputText
+				label="Nom"
+				name="lastname"
+				placeholder="Exemple : Dupont"
+				required
+			/>
+			<InputText
+				type="email"
+				label="Adresse email"
+				name="mail"
+				placeholder="Exemple : jean.dupont@gmail.com"
+				required
+			/>
+			<InputText
+				type="tel"
+				pattern="^(\+33|0|0033)[1-9]\d{8}$"
+				label="Téléphone"
+				name="phone"
+				placeholder="Exemple : 0606060606"
+				required
+			/>
+			<Select
+				required
+				label="Age"
+				name="age"
+				optionList={ageOptions}
+				onChange={setInputAge}
+				value={inputAge}
+			/>
+			<InputAutocomplétionCommune
+				required
+				id="autocomplete-commune"
+				label="Ville"
+				name="ville"
+				placeholder="Exemple: Paris, Béziers..."
+				onSuggestionSelected={(event, suggestion) => {
+					setInputCodePostal(suggestion.codePostal);
+					setInputVille(suggestion.ville);
+				}}
+			/>
+			{isLoading
+				? (<ButtonComponent disabled icon={<SpinnerIcon />} iconPosition='left' label='Envoi en cours' />)
+				: (<ButtonComponent className={styles.formulaireButton} label="Envoyer la demande" />)
+			}
+
+			<div className={styles.formulaireDécharge}>
+				<DéchargeRGPD />
+			</div>
+		</form>
+	);
+}
--- src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
+++ src/client/components/features/ContratEngagementJeune/Rappel/Rappel.tsx
@@ -1,29 +1,17 @@
 import classNames from 'classnames';
-import React, {
-	useEffect,
-	useState,
-} from 'react';
+import React, { useState } from 'react';
 
-import FormulaireDeContactCEJ from '~/client/components/features/ContratEngagementJeune/FormulaireDeContact/FormulaireDeContactCEJ';
+import {
+	ModalDemandeDeContactCEJ,
+} from '~/client/components/features/ContratEngagementJeune/Rappel/ModalDemandeDeContactCEJ/ModalDemandeDeContactCEJ';
 import styles from '~/client/components/features/ContratEngagementJeune/Rappel/Rappel.module.scss';
 import { Container } from '~/client/components/layouts/Container/Container';
 import { ButtonComponent } from '~/client/components/ui/Button/ButtonComponent';
 import { Icon } from '~/client/components/ui/Icon/Icon';
 import Marked from '~/client/components/ui/Marked/Marked';
-import { ModalComponent } from '~/client/components/ui/Modal/ModalComponent';
-
 
 export default function Rappel() {
 	const [isPopInOpen, setIsPopInOpen] = useState(false);
-	const [isSuccess, setIsSuccess] = useState(false);
-
-	useEffect(() => {
-		if (!isPopInOpen) setIsSuccess(false);
-	}, [isPopInOpen]);
-
-	function onFormulaireEnvoyé() {
-		setIsSuccess(true);
-	}
 
 	return (
 		<div className={classNames(styles.rappel, 'background-white-lilac')}>
@@ -31,23 +19,10 @@ export default function Rappel() {
 				<Marked markdown={'## J‘ai des questions sur le Contrat d‘Engagement Jeune'}/>
 				<ButtonComponent label='Je souhaite être contacté(e)' icon={<Icon name={'angle-right'}/>} onClick={() => setIsPopInOpen(true)} />
 			</Container>
-			<ModalComponent
-				className={styles.rappelModal}
+			<ModalDemandeDeContactCEJ
 				isOpen={isPopInOpen}
-				close={() => setIsPopInOpen(false)}
-				aria-labelledby={ !isSuccess ? 'dialog_label' : 'dialog_label_success'}
-			>
-				{ !isSuccess && <ModalComponent.Title className={styles.rappelTitle} id="dialog_label">
-        J‘ai des questions sur le Contrat d‘Engagement Jeune et souhaite être rappelé
-				</ModalComponent.Title>
-				}
-				<ModalComponent.Content className={!isSuccess ? styles.rappelContent : styles.rappelContentSuccess}>
-					{ !isSuccess && <small>(Tous les champs sont obligatoires)</small> }
-					<FormulaireDeContactCEJ onSuccess={() => onFormulaireEnvoyé() }>
-						<ButtonComponent label='Fermer' onClick={ () => setIsPopInOpen(false)} title="Fermer, Revenir à la page" />
-					</FormulaireDeContactCEJ>
-				</ModalComponent.Content>
-			</ModalComponent>
+				setIsOpen={setIsPopInOpen}
+			/>
 		</div>
 	);
 }
