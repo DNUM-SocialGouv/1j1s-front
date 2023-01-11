9bdf3ee166385072311ffd24e7cca84b21099d04
Sergen <sergen.kovar@octo.com>
2023-01-11 15:31:29 +0100

    feat(stages): test et design

--- src/client/components/features/JeRecrute/DecouvrirDispositifs/DecouvrirDispositifs.tsx
+++ src/client/components/features/JeRecrute/DecouvrirDispositifs/DecouvrirDispositifs.tsx
@@ -6,19 +6,14 @@ import { Link } from '~/client/components/ui/Link/Link';
 
 export function DécouvrirDispositifs () {
 
-	if (process.env.NEXT_PUBLIC_DEPOT_STAGE_FEATURE === '1') {
-		return <Container className={styles.recruter}>
-			<h1>Vous cherchez à recruter ?</h1>
-			<p>Dans le cadre du plan 1 jeune, 1 solution, nous vous accompagnons dans la recherche de vos futurs collaborateurs.</p>
-			<Link href="/je-recrute/deposer-une-offre-d-emploi" appearance='asPrimaryButton'>Déposer une offre d‘emploi ou d‘alternance</Link>
-			<Link href="/stages/deposer-offre" appearance='asPrimaryButton'>Déposer une offre de stage</Link>
-		</Container>;
-	}
-
 	const body = 'Afin de recevoir des candidatures correspondant au besoin de la mission proposée, nous vous conseillons de bien détailler votre offre, en n’oubliant pas de préciser : <br><br>• Un titre pour votre offre de stage ; <br>• La description des missions (n’hésitez pas à faire une liste) ; <br>• Le lieu du stage (ville, code postal, département, région, pays) ; <br>• Votre secteur d’activité ; <br>• Les dates de début et de fin du stage, ainsi que la rémunération prévue ;<br>• Les coordonnées et le SIRET de votre entreprise ;<br>• Vos coordonnées ;<br>• L’URL ou le mail pour envoyer sa candidature. <br><br><br>Votre offre sera visible sur la plateforme après modération.';
 	const subject = '[Déposer une offre de stage]';
 	const mail = 'contact-1j1s@sg.social.gouv.fr';
-	const MAIL_TO = `mailto:${mail}?subject=${subject}&body=${body.replaceAll('<br>', '%0D%0A').replaceAll('•', '%E2%80%A2')}`;
+	let MAIL_TO = `mailto:${mail}?subject=${subject}&body=${body.replaceAll('<br>', '%0D%0A').replaceAll('•', '%E2%80%A2')}`;
+
+	if (process.env.NEXT_PUBLIC_DEPOT_STAGE_FEATURE === '1') {
+		MAIL_TO = '/stages/deposer-offre';
+	}
 
 	return (
 		<section>
--- src/pages/stages/deposer-offre/index.page.tsx
+++ src/pages/stages/deposer-offre/index.page.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
+import { Hero } from '~/client/components/ui/Hero/Hero';
 import { HeadTag } from '~/client/components/utils/HeaderTag';
 import useReferrer from '~/client/hooks/useReferrer';
-
-import { Hero } from '../../../client/components/ui/Hero/Hero';
-import styles from './DeposerOffreStage.module.scss';
 
 export default function DeposerOffreStagePage() {
 	useReferrer();
@@ -12,12 +10,13 @@ export default function DeposerOffreStagePage() {
 	return (
 		<>
 			<HeadTag
-				title={'Rechercher un stage | 1jeune1solution'}
-				description="Des milliers d‘offres de stages sélectionnées pour vous"/>
-			<Hero className={styles.bannière}>
-				<span>Déposez votre offre de stage </span>
-				<span>sur 1jeune1solution</span>
-			</Hero>
+				title={'Dépôt d’offre de stage - Etape 1 sur 3 : Votre entreprise | 1jeune1solution '}
+			/>
+			<main id="contenu">
+				<Hero>
+					<p><b>Déposez votre offre de stage sur 1jeune1solution</b></p>
+				</Hero>
+			</main>
 		</>
 	);
 }
