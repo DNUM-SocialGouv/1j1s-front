6539dd40f035846baefec3e07d65e5cc4cbb8024
Olivia Peny-Coblentz <oliv@octo.com>
2023-01-19 12:05:49 +0100

    add supportFile in cypress

--- next.config.js
+++ next.config.js
@@ -235,4 +235,4 @@ const moduleExports = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports);
+module.exports = (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'integration') ? withSentryConfig(moduleExports, sentryModuleExports) : moduleExports;
