23a6e7496057f3d8e88033b3996810f55fdad370
Caroline Chuong - CACH <caroline.chuong@octo.com>
2023-01-23 15:58:13 +0100

    feat(logement): ajout du bilan énergétique sur le détail d'une annonce

--- src/server/cms/domain/annonceDeLogement.type.ts
+++ src/server/cms/domain/annonceDeLogement.type.ts
@@ -1,85 +1,98 @@
 import { Image } from '~/client/components/props';
 
-export interface AnnonceDeLogementIndexee  {
-  titre: string
-  slug: string
-  dateDeDisponibilite: string
-  prix: number
-  prixHT?: number
-  surfaceAAfficher: string
-  type: string
-  typeBien: string
-  url: string
-  dateDeMiseAJour: string
-  localisationAAfficher: string
-  devise: string
-  imagesUrl: Array<string>
+export interface AnnonceDeLogementIndexee {
+    titre: string
+    slug: string
+    dateDeDisponibilite: string
+    prix: number
+    prixHT?: number
+    surfaceAAfficher: string
+    type: string
+    typeBien: string
+    url: string
+    dateDeMiseAJour: string
+    localisationAAfficher: string
+    devise: string
+    imagesUrl: Array<string>
 }
 
 export namespace AnnonceDeLogementResponse {
-  export interface Localisation {
-    ville?: string
-    adresse?: string
-    département?: string
-    codePostal?: string
-    région?: string
-    pays?: string
-  }
+    export interface Localisation {
+        ville?: string
+        adresse?: string
+        département?: string
+        codePostal?: string
+        région?: string
+        pays?: string
+    }
+
+    export interface BilanEnergetique {
+        consommationEnergetique?: CategorieEnergetique,
+        emissionDeGaz?: CategorieEnergetique
+    }
 }
 
 export interface AnnonceDeLogementResponse {
-  titre: string
-  slug: string
-  dateDeDisponibilite: string
-  nombreDePieces: number
-  surface: number
-  surfaceMax?: number
-  etage?: number
-  prix: number
-  prixHT?: number
-  charge?: number
-  garantie?: number
-  type: string
-  typeBien: string
-  meuble: boolean
-  url: string
-  sourceUpdatedAt: Date
-  sourceCreatedAt: Date
-  devise: string
-  description: string
-  localisation: AnnonceDeLogementResponse.Localisation
-  imagesUrl?: Array<{ value: string }>
+    titre: string
+    slug: string
+    dateDeDisponibilite: string
+    nombreDePieces: number
+    surface: number
+    surfaceMax?: number
+    etage?: number
+    prix: number
+    prixHT?: number
+    charge?: number
+    garantie?: number
+    type: string
+    typeBien: string
+    meuble: boolean
+    url: string
+    sourceUpdatedAt: Date
+    sourceCreatedAt: Date
+    devise: string
+    description: string
+    localisation: AnnonceDeLogementResponse.Localisation
+    bilanEnergetique: AnnonceDeLogementResponse.BilanEnergetique
+    imagesUrl?: Array<{ value: string }>
 }
-
 
 export namespace AnnonceDeLogement {
-  export interface Localisation {
-    ville?: string
-    adresse?: string
-    département?: string
-    codePostal?: string
-    région?: string
-    pays?: string
-  }
+    export interface Localisation {
+        ville?: string
+        adresse?: string
+        département?: string
+        codePostal?: string
+        région?: string
+        pays?: string
+    }
+
+    export interface BilanEnergetique {
+        consommationEnergetique?: CategorieEnergetique,
+        emissionDeGaz?: CategorieEnergetique
+    }
 }
 
+export type CategorieEnergetique = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G'
+
 export interface AnnonceDeLogement {
-  titre: string
-  type: string
-  typeBien: string
-  dateDeMiseAJour: string
-  surface: number
-  surfaceMax?: number
-  nombreDePièces: number
-  étage?: number
-  prix: number
-  prixHT?: number
-  charge?: number
-  garantie?: number
-  dateDeDisponibilité: string
-  meublé: boolean
-  localisation: AnnonceDeLogement.Localisation
-  description: string
-  devise: string
-  imageUrlList: Array<Image>
+    titre: string
+    type: string
+    typeBien: string
+    dateDeMiseAJour: string
+    surface: number
+    surfaceMax?: number
+    nombreDePièces: number
+    étage?: number
+    prix: number
+    prixHT?: number
+    charge?: number
+    garantie?: number
+    dateDeDisponibilité: string
+    meublé: boolean
+    localisation: AnnonceDeLogement.Localisation
+    description: string
+    devise: string
+    imageUrlList: Array<Image>
+    bilanEnergetique: AnnonceDeLogement.BilanEnergetique
 }
