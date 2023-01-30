fea036357f5e53e7c528122b65d578af549ba177
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-01-30 17:06:19 +0100

    feat(logement/details): Ajoute les icônes

--- src/client/components/features/Logement/Consulter/Services.tsx
+++ src/client/components/features/Logement/Consulter/Services.tsx
@@ -4,28 +4,41 @@ import React from 'react';
 import styles from '~/client/components/features/Logement/Consulter/ConsulterAnnonce.module.scss';
 import { AnnonceDeLogement } from '~/server/cms/domain/annonceDeLogement.type';
 import ServiceInclus = AnnonceDeLogement.ServiceInclus;
+import { Icon, IconName } from '~/client/components/ui/Icon/Icon';
 
 interface ServicesProps {
 	inclus: Array<AnnonceDeLogement.ServiceInclus>;
 	optionnels: Array<AnnonceDeLogement.ServiceOptionnel>;
 }
 
-// const ServiceElements: Record<
-// 	ServiceInclus,
-// 	string
-// > = {
-// 	[ServiceInclus.BIKE_STORAGE]: 'Parking à vélo',
-// 	[ServiceInclus.CLEANING_TOOLS]: 'Nécessaire de nettoyage',
-// 	[ServiceInclus.FITNESS_ROOM]: 'Salle de sport',
-// 	[ServiceInclus.INTERNET]: 'Wi-Fi',
-// 	[ServiceInclus.IRON]: 'Fer à repasser',
-// 	[ServiceInclus.MICROWAVE]: 'Micro-onde',
-// 	[ServiceInclus.PARKING]: 'Parking voiture',
-// 	[ServiceInclus.PRIVATE_BATHROOM]: 'Salle de bain privative',
-// 	[ServiceInclus.TV]: 'Télévision',
-// 	[ServiceInclus.VACUUM]: 'Aspirateur',
-// 	[ServiceInclus.WASHING_MACHINE]: 'Machine à laver',
-// };
+const Icônes: Record<
+	ServiceInclus,
+	IconName
+> = {
+	[ServiceInclus.ASCENSEUR]: 'bike',
+	[ServiceInclus.ASPIRATEUR]: 'vacuum',
+	[ServiceInclus.CAVE]: 'error',
+	[ServiceInclus.FER_A_REPASSER]: 'iron',
+	[ServiceInclus.FIBRE_OPTIQUE]: 'error',
+	[ServiceInclus.FOUR]: 'error',
+	[ServiceInclus.GARAGE]: 'error',
+	[ServiceInclus.GARDIEN_RESIDENCE]: 'error',
+	[ServiceInclus.INTERNET]: 'wifi',
+	[ServiceInclus.LAVE_LINGE]: 'washing-machine',
+	[ServiceInclus.LAVE_VAISSELLE]: 'error',
+	[ServiceInclus.LOCAL_A_VELO]: 'bike',
+	[ServiceInclus.MICRO_ONDE]: 'microwave',
+	[ServiceInclus.NECESSAIRE_DE_NETTOYAGE]: 'clean-hands',
+	[ServiceInclus.PARKING]: 'car',
+	[ServiceInclus.PISCINE]: 'error',
+	[ServiceInclus.REFRIGERATEUR]: 'error',
+	[ServiceInclus.SALLE_DE_BAIN_PRIVATIVE]: 'lock',
+	[ServiceInclus.SALLE_DE_SPORT]: 'sport',
+	[ServiceInclus.SECHE_LINGE]: 'error',
+	[ServiceInclus.TERRACE]: 'error',
+	[ServiceInclus.TV]: 'TV',
+	[ServiceInclus.NON_RENSEIGNE]: 'error',
+};
 
 export function Services({ inclus, optionnels }: ServicesProps) {
 	return (
@@ -33,13 +46,19 @@ export function Services({ inclus, optionnels }: ServicesProps) {
 			<h2>Équipements et services inclus</h2>
 			<ul>
 				{inclus.map((service) => (
-					<li key={service}>{service}</li>
+					<li key={service}>
+						<Icon name={Icônes[service]} />
+						{service}
+					</li>
 				))}
 			</ul>
 			<h2>Équipements et services optionnels</h2>
 			<ul>
 				{optionnels.map((service) => (
-					<li key={service}>{service}</li>
+					<li key={service}>
+						<Icon name={Icônes[service]} />
+						{service}
+					</li>
 				))}
 			</ul>
 		</section>
