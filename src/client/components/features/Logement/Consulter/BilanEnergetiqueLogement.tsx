e2977ec064ceb5b8546ee035bb5d7401a8a2264e
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-02-06 14:47:58 +0100

    refactor(logement/details): Update le composant SquareMeter dans les pages

--- src/client/components/features/Logement/Consulter/BilanEnergetiqueLogement.tsx
+++ src/client/components/features/Logement/Consulter/BilanEnergetiqueLogement.tsx
@@ -3,32 +3,32 @@ import React from 'react';
 
 import styles from '~/client/components/features/Logement/Consulter/BilanEnergetiqueLogement.module.scss';
 import cardStyles from '~/client/components/features/Logement/Consulter/ConsulterAnnonce.module.scss';
+import { SquareMeter } from '~/client/components/ui/SquareMeter/SquareMeter';
 import { CategorieEnergetique } from '~/server/cms/domain/annonceDeLogement.type';
 
 const LessOrEqual = () => <>{'\u{02A7D}'}</>;
 const MoreOrEqual = () => <>{'\u{02A7E}'}</>;
 const LessThan = () => <>{'\u{0003C}'}</>;
 const MoreThan = () => <>{'\u{0003E}'}</>;
-const MettreCarré = () => <abbr title="mètre carré">m<sup>2</sup></abbr>;
 
 const CONSOMMATION_ENERGETIQUE: Record<CategorieEnergetique, React.ReactNode> = {
-	A: <>Excellente performance énergétique (consommation <LessThan /> 50 kWh/<MettreCarré />/an)</>,
-	B: <>Très bonne performance énergétique (consommation de 51 à 90 kWh/<MettreCarré />/an)</>,
-	C: <>Bonne performance énergétique (consommation de 91 à 150 kWh/<MettreCarré />/an)</>,
-	D: <>Assez bonne performance énergétique (consommation de 151 à 230 kWh/<MettreCarré />/an)</>,
-	E: <>Performance énergétique moyenne (consommation de 231 à 330 kWh/<MettreCarré />/an)</>,
-	F: <>Mauvaise performance énergétique (consommation de 331 à 450 kWh/<MettreCarré />/an)</>,
-	G: <>Très mauvaise performance énergétique (consommation <MoreOrEqual /> 451 kWh/<MettreCarré />/an)</>,
+	A: <>Excellente performance énergétique (consommation <LessThan /> 50 kWh/<SquareMeter />/an)</>,
+	B: <>Très bonne performance énergétique (consommation de 51 à 90 kWh/<SquareMeter />/an)</>,
+	C: <>Bonne performance énergétique (consommation de 91 à 150 kWh/<SquareMeter />/an)</>,
+	D: <>Assez bonne performance énergétique (consommation de 151 à 230 kWh/<SquareMeter />/an)</>,
+	E: <>Performance énergétique moyenne (consommation de 231 à 330 kWh/<SquareMeter />/an)</>,
+	F: <>Mauvaise performance énergétique (consommation de 331 à 450 kWh/<SquareMeter />/an)</>,
+	G: <>Très mauvaise performance énergétique (consommation <MoreOrEqual /> 451 kWh/<SquareMeter />/an)</>,
 };
 
 const EMISSION_DE_GAZ: Record<CategorieEnergetique, React.ReactNode> = {
-	A: <>Très peu d’émission de gaz à effet de serre (émission de gaz <LessOrEqual /> 5 kg/<MettreCarré />/an)</>,
-	B: <>Peu d’émission de gaz à effet de serre (émission de gaz de 6 à 10 kg/<MettreCarré />/an)</>,
-	C: <>Émission de gaz à effet de serre correcte (émission de gaz de 11 à 20 kg/<MettreCarré />/an) </>,
-	D: <>Émission de gaz à effet de serre notable (émission de gaz de 21 à 35 kg/<MettreCarré />/an)</>,
-	E: <>Émission assez importante de gaz à effet de serre (émission de gaz de 36 à 55 kg/<MettreCarré />/an)</>,
-	F: <>Importante émission de gaz à effet de serre (émission de gaz de 56 à 80 kg/<MettreCarré />/an)</>,
-	G: <>Très importante émission de gaz à effet de serre (émission de gaz <MoreThan /> 80 kg/<MettreCarré />/an)</>,
+	A: <>Très peu d’émission de gaz à effet de serre (émission de gaz <LessOrEqual /> 5 kg/<SquareMeter />/an)</>,
+	B: <>Peu d’émission de gaz à effet de serre (émission de gaz de 6 à 10 kg/<SquareMeter />/an)</>,
+	C: <>Émission de gaz à effet de serre correcte (émission de gaz de 11 à 20 kg/<SquareMeter />/an) </>,
+	D: <>Émission de gaz à effet de serre notable (émission de gaz de 21 à 35 kg/<SquareMeter />/an)</>,
+	E: <>Émission assez importante de gaz à effet de serre (émission de gaz de 36 à 55 kg/<SquareMeter />/an)</>,
+	F: <>Importante émission de gaz à effet de serre (émission de gaz de 56 à 80 kg/<SquareMeter />/an)</>,
+	G: <>Très importante émission de gaz à effet de serre (émission de gaz <MoreThan /> 80 kg/<SquareMeter />/an)</>,
 };
 
 interface BilanEnergetiqueLogementProps {
