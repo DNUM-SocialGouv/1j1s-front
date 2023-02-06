24eeeb47682e72c52b901cec3965f13cb8881421
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-02-06 16:15:49 +0100

    refactor(logement/details): Supprime un commentaire confusant

--- src/client/components/ui/SquareMeter/SquareMeter.test.tsx
+++ src/client/components/ui/SquareMeter/SquareMeter.test.tsx
@@ -10,7 +10,6 @@ describe('<SquareMeter />', () => {
 	it('affiche une abréviation', async () => {
 		render(<SquareMeter />);
 
-		// NOTE (GAFI 06-02-2023): nécessaire puisque texte splitté dans plusieurs balises sans rôle associé
 		const abbreviation = screen.getByText((content, element) => element?.tagName === 'ABBR' && element?.textContent === 'm2');
 		expect(abbreviation).toHaveAttribute('title', 'mètre carré');
 	});
