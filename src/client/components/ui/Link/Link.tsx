2acfe653e72bca9c79080a5090e5ecd3a11b9d59
Florian Leroy <florian.leroy@octo.com>
2023-02-09 23:10:39 +0100

    feat(multiple): mise à niveau du composant Link vers celui de NextJS 13

--- src/client/components/ui/Link/Link.tsx
+++ src/client/components/ui/Link/Link.tsx
@@ -9,37 +9,46 @@ import { useIsInternalLink } from '~/client/hooks/useIsInternalLink';
 type ButtonAppearance = 'default' | 'asPrimaryButton' | 'asSecondaryButton' | 'asBackButton';
 
 interface LinkProps extends React.AnchorHTMLAttributes<unknown> {
-  appearance?: ButtonAppearance
-  href: string
-  prefetch?: boolean
+	appearance?: ButtonAppearance
+	href: string
+	prefetch?: boolean
 }
 
-export function Link({ appearance = 'default', className, children, href, prefetch = false, ...rest }: LinkProps & React.HTMLAttributes<HTMLLinkElement>) {
+export function Link(props: LinkProps & React.HTMLAttributes<HTMLLinkElement>) {
+	const {
+		appearance = 'default',
+		className,
+		children,
+		href,
+		prefetch = false,
+		...rest
+	} = props;
 	const isInternalLink = useIsInternalLink(href);
 	const hasIcon = useMemo(() => appearance === 'asPrimaryButton' || appearance === 'asSecondaryButton', [appearance]);
 	const hasBackIcon = useMemo(() => appearance === 'asBackButton', [appearance]);
 	const appearanceClass = useMemo(() => {
 		switch (appearance) {
-			case 'asPrimaryButton': return styles.primaryButton;
-			case 'asSecondaryButton': return styles.secondaryButton;
-			case 'asBackButton': return styles.backButton;
-			default: return styles.link;
+			case 'asPrimaryButton':
+				return styles.primaryButton;
+			case 'asSecondaryButton':
+				return styles.secondaryButton;
+			case 'asBackButton':
+				return styles.backButton;
+			default:
+				return styles.link;
 		}
 	}, [appearance]);
 
-	// Must use legacyBehavior because of [this issue](https://github.com/vercel/next.js/issues/41962)
 	return isInternalLink ? (
-		<LinkNext legacyBehavior href={href} prefetch={prefetch}>
-			<a className={classNames(className, appearanceClass)} {...rest}>
-				{hasBackIcon && <Icon name="angle-left" />}
-				{children}
-				{hasIcon && <Icon name="arrow-right" />}
-			</a>
+		<LinkNext href={href} prefetch={prefetch} className={classNames(className, appearanceClass)} {...rest}>
+			{hasBackIcon && <Icon name="angle-left"/>}
+			{children}
+			{hasIcon && <Icon name="arrow-right"/>}
 		</LinkNext>
 	) : (
 		<a href={href} target="_blank" rel="noreferrer" className={classNames(className, appearanceClass)} {...rest}>
 			{children}
-			{hasIcon && <Icon name="external-redirection" />}
+			{hasIcon && <Icon name="external-redirection"/>}
 		</a>
 	);
 }
