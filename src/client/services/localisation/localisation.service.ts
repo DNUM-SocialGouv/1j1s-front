84e346cd737fe64af813c74bae3c559b8c6f771b
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-02-09 12:14:05 +0100

    feat(multiple): Ajoute le check de taille de query pour l'API localisation

--- src/client/services/localisation/localisation.service.ts
+++ src/client/services/localisation/localisation.service.ts
@@ -14,23 +14,20 @@ export class LocalisationService {
 
 	async rechercherLocalisation(recherche: string): Promise<Either<RechercheLocalisationApiResponse> | null> {
 		const localisationsLength = recherche.length;
-		const queryTooShort = localisationsLength === 1;
+		const queryTooShort = localisationsLength < 3;
 		const containsInvalidSymbols = !this.REGEX_ALL_LETTRES_AVEC_ACCENTS_TIRET_ESPACE_AND_DIGITS.test(recherche);
 		const allDigits = this.REGEX_ALL_DIGITS.test(recherche);
 		const isDepartement = allDigits && recherche.length === this.DEPARTEMENT_LENGTH;
 		const isCodePostal = allDigits && recherche.length === this.CODE_POSTAL_LENGTH;
 
-		if(queryTooShort) {
+		if(containsInvalidSymbols)
 			return null;
-		}
-		if(containsInvalidSymbols) {
-			return null;
-		}
 		if(allDigits
 			&& !isDepartement
-			&& !isCodePostal) {
+			&& !isCodePostal)
 			return null;
-		}
+		if (!allDigits && queryTooShort)
+			return null;
 
 		return this.httpClientService.get<RechercheLocalisationApiResponse>(`localisations?recherche=${recherche}`);
 	}
