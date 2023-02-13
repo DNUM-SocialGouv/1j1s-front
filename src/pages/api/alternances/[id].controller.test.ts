6252d9be26cc126fc506b7b743c0eb77460d473b
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-02-13 18:35:12 +0100

    feat(alternance/details): Ajoute la validation des queries

--- src/pages/api/alternances/[id].controller.test.ts
+++ src/pages/api/alternances/[id].controller.test.ts
@@ -20,10 +20,29 @@ describe('rechercher alternance', () => {
 			handler: (req, res) => getAlternanceHandler(req, res),
 			test: async ({ fetch }) => {
 				const res = await fetch({ method: 'GET' });
-				const json = await res.json();
-				expect(json).toEqual(uneAlternance());
+				const body = await res.json();
+				expect(body).toEqual(uneAlternance());
 			},
 			url: `/alternances/${alternanceId}?rome=${rome}`,
+		});
+	});
+	it('retourne un erreur quand la rome est manquante', async () => {
+		const alternanceId = 'g8_DWUYBnYooOG9KzQM_y-0';
+		const rome = undefined;
+		nock('https://labonnealternance-recette.apprentissage.beta.gouv.fr/api/v1/').get(
+			`/jobs?caller=1jeune1solution&romes=${rome}&sources=matcha`,
+		).reply(200, aListeLaBonneAlternanceApiResponse());
+
+		await testApiHandler<Alternance | ErrorHttpResponse>({
+			handler: (req, res) => getAlternanceHandler(req, res),
+			test: async ({ fetch }) => {
+				const res = await fetch({ method: 'GET' });
+				const body = await res.json();
+				const error = res.status;
+				expect(error).toEqual(400);
+				expect(body).toEqual({ error: 'les paramètres dans l‘url ne respectent pas le schema de validation' });
+			},
+			url: `/alternances/${alternanceId}`,
 		});
 	});
 });
--- src/pages/api/alternances/[id].controller.ts
+++ src/pages/api/alternances/[id].controller.ts
@@ -1,7 +1,16 @@
+import Joi from 'joi';
+
+import { withValidation } from '~/pages/api/middlewares/validation/validation.middleware';
 import { handleResponse } from '~/pages/api/utils/response/response.util';
 import { uneAlternance } from '~/server/alternances/domain/alternance.fixture';
 import { createSuccess } from '~/server/errors/either';
 
-export default async function getAlternanceHandler(req, res) {
+const validation = Joi.object({
+	rome: Joi.string().required().not().empty(),
+});
+
+const getAlternanceHandler = withValidation({ query: validation }, async function(req, res) {
 	return handleResponse(createSuccess(uneAlternance()), res);
-}
+});
+
+export default  getAlternanceHandler;
