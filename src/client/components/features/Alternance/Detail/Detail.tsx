1c4ed61d97271b9e05d5392cf23192122611aa17
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-03-02 14:22:39 +0100

    feat(alternance): Ajoute le style

--- src/client/components/features/Alternance/Detail/Detail.tsx
+++ src/client/components/features/Alternance/Detail/Detail.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { DetailAlternance } from '~/client/components/features/Alternance/Detail/DetailAlternance.type';
-import styles from '~/client/components/features/ConsulterOffre.module.scss';
-import { Tag } from '~/client/components/ui/Tag/Tag';
+import { ConsulterOffreLayout } from '~/client/components/layouts/ConsulterOffre/ConsulterOffreLayout';
 import { TagList } from '~/client/components/ui/Tag/TagList';
 import { useLocale } from '~/client/context/locale.context';
+
+import styles from './Detail.module.scss';
 
 function toISODuration(duration: number) {
 	return `P${duration}Y`;
@@ -17,20 +18,20 @@ function toISODate(date: Date) {
 export function Detail({ annonce }: { annonce: DetailAlternance }) {
 	const locale = useLocale();
 	return (
-		<>
-			<header className={styles.titre}>
+		<ConsulterOffreLayout>
+			<header className={styles.entete}>
 				<h1>{annonce.titre}</h1>
 				{annonce.entreprise.nom && <p className={styles.sousTitre}>{annonce.entreprise.nom}</p>}
-				<TagList list={[annonce.localisation, annonce.typeDeContrat, annonce.niveauRequis]} />
+				<TagList className={styles.tags} list={[annonce.localisation, annonce.typeDeContrat, annonce.niveauRequis]} />
 			</header>
 			<dl className={styles.contenu}>
 				{annonce.description && (
-					<>
+					<div className={styles.description}>
 						<dt>Description du contrat</dt>
 						<dd>{annonce.description}</dd>
-					</>)}
+					</div>)}
 				{annonce.compétences && annonce.compétences.length > 0 && (
-					<>
+					<div>
 						<dt>Connaissances et compétences requises</dt>
 						<dd>
 							<ul>
@@ -39,62 +40,64 @@ export function Detail({ annonce }: { annonce: DetailAlternance }) {
 								))}
 							</ul>
 						</dd>
-					</>
+					</div>
 				)}
-				{annonce.niveauRequis && (
-					<>
-						<dt>Niveau requis</dt>
-						<dd>{annonce.niveauRequis}</dd>
-					</>
-				)}
-				{annonce.dateDébut && (
-					<>
-						<dt>Début du contrat</dt>
-						<dd>
-							<time dateTime={toISODate(annonce.dateDébut)}>{annonce.dateDébut.toLocaleDateString(locale, { dateStyle: 'long' })}</time></dd>
-					</>
-				)}
-				{annonce.typeDeContrat && (
-					<>
-						<dt>Type de contrat</dt>
-						<dd>{annonce.typeDeContrat}</dd>
-					</>
-				)}
-				{annonce.durée && (
-					<>
-						<dt>Durée du contrat</dt>
-						<dd>
-							<time dateTime={toISODuration(annonce.durée)}>{annonce.durée} {annonce.durée > 1 ? 'ans' : 'an'}</time></dd>
-					</>
-				)}
-				{annonce.rythmeAlternance && (
-					<>
-						<dt>Rythme de l’alternance</dt>
-						<dd>{annonce.rythmeAlternance}</dd>
-					</>
-				)}
+				<div>
+					{annonce.niveauRequis && (
+						<div className={styles.niveauRequis}>
+							<dt>Niveau requis</dt>
+							<dd>{annonce.niveauRequis}</dd>
+						</div>
+					)}
+					{annonce.dateDébut && (
+						<div className={styles.dateDebut}>
+							<dt>Début du contrat</dt>
+							<dd>
+								<time dateTime={toISODate(annonce.dateDébut)}>{annonce.dateDébut.toLocaleDateString(locale, { dateStyle: 'long' })}</time></dd>
+						</div>
+					)}
+					{annonce.typeDeContrat && (
+						<div className={styles.typeContrat}>
+							<dt>Type de contrat</dt>
+							<dd>{annonce.typeDeContrat}</dd>
+						</div>
+					)}
+					{annonce.durée && (
+						<div className={styles.duree}>
+							<dt>Durée du contrat</dt>
+							<dd>
+								<time dateTime={toISODuration(annonce.durée)}>{annonce.durée} {annonce.durée > 1 ? 'ans' : 'an'}</time></dd>
+						</div>
+					)}
+					{annonce.rythmeAlternance && (
+						<div className={styles.rythme}>
+							<dt>Rythme de l’alternance</dt>
+							<dd>{annonce.rythmeAlternance}</dd>
+						</div>
+					)}
+				</div>
 				{(annonce.entreprise.téléphone || annonce.entreprise.localisation) && (
-					<>
+					<div>
 						<dt>Informations sur l’entreprise</dt>
 						<dd>
 							<dl>
 								{annonce.entreprise.localisation && (
-									<>
+									<div className={styles.adresse}>
 										<dt>Adresse</dt>
 										<dd>{annonce.entreprise.localisation}</dd>
-									</>
+									</div>
 								)}
 								{annonce.entreprise.téléphone && (
-									<>
+									<div className={styles.telephone}>
 										<dt>Contact</dt>
 										<dd>{annonce.entreprise.téléphone}</dd>
-									</>
+									</div>
 								)}
 							</dl>
 						</dd>
-					</>
+					</div>
 				)}
 			</dl>
-		</>
+		</ConsulterOffreLayout>
 	);
 }
