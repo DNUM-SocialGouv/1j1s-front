76fbaa1424b7975132474ba97ed4ad58e1c35039
Julie Brunetto <julie.brunetto@octo.com>
2023-03-09 16:55:29 +0100

    feat(accompagnement): mise à jour du nom de l'aide de pôle emploi

--- src/client/components/features/Partner/PoleEmploiPartner.tsx
+++ src/client/components/features/Partner/PoleEmploiPartner.tsx
@@ -5,12 +5,12 @@ import { PartnerCard } from './Card/PartnerCard';
 export function PoleEmploiPartner() {
 	return (
 		<PartnerCard
-			title="Pôle Emploi propose un accompagnement intensif pour les jeunes de 16 à 30 ans"
+			title="Pôle Emploi propose un accompagnement individualisé pour les jeunes de 16 à 30 ans"
 			logo="/images/logos/pole-emploi.svg"
 			link="/articles/pole-emploi"
 			linkLabel="En savoir plus"
 		>
-			Avec son dispositif d’accompagnement intensif des jeunes (AIJ), Pôle
+			Avec son dispositif d’accompagnement individualisé des jeunes (AIJ), Pôle
 			emploi propose à tous les jeunes demandeurs d’emploi de 16 à 30 ans, un
 			accompagnement personnalisé intensif d’une durée de 3 à 6 mois pour les
 			aider à trouver ou retrouver plus rapidement un emploi.
