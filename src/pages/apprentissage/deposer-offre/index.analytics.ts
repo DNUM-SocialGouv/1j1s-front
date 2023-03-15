3389885e909983682ec221a4a89bd33c803842d3
Julie Brunetto <julie.brunetto@octo.com>
2023-03-15 15:34:58 +0100

    feat(alternance): ajout du formulaire de dépôt d'alternance La bonne alternance

---
+++ src/pages/apprentissage/deposer-offre/index.analytics.ts
@@ -0,0 +1,10 @@
+import { PageTags } from '~/client/services/analytics/analytics';
+
+const analyticsPageConfig: PageTags = {
+	page_template: 'deposer_offre_emploi_etape_1',
+	pagegroup: 'deposer_offre_emploi',
+	pagelabel: 'deposer_offre_emploi_etape_1',
+	'segment-site': 'funnel_etape_1',
+};
+
+export default analyticsPageConfig;
