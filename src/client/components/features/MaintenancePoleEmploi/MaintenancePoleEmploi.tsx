34b546e8ebe9edce88076801458e23bf3b0484f7
Olivia Peny-Coblentz <oliv@octo.com>
2023-03-24 10:28:00 +0100

    update after review

--- src/client/components/features/MaintenancePoleEmploi/MaintenancePoleEmploi.tsx
+++ src/client/components/features/MaintenancePoleEmploi/MaintenancePoleEmploi.tsx
@@ -12,7 +12,7 @@ export default function MaintenancePoleEmploi() {
 		<div className={ styles.container }>
 			{ !isSmallScreen && <Image src="/illustrations/maintenance.svg" width={480} height={480} alt='' /> }
 			<div className={ styles.containerText}>
-				<h1 className={styles.containerTextTitre}>Le formulaire pour déposer une offre d’emploi est actuellement en maintenance. Merci de réessayer plus tard.</h1>
+				<h1 className={styles.containerTextTitle}>Le formulaire pour déposer une offre d’emploi est actuellement en maintenance. Merci de réessayer plus tard.</h1>
 				<p>Pour patienter, découvrez l‘ensemble des dispositifs du plan jeune pour les employeurs.</p>
 				<Link href="/mesures-employeurs" appearance="asPrimaryButton">Je découvre les dispositifs</Link>
 			</div>
