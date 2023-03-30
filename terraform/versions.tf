c243d6d13754252827fc2b837b0b653ca798da8e
Joseph Page <josephpage@users.noreply.github.com>
2023-03-30 17:12:23 +0200

    ci: utilisation d'un workflow réutilisable pour Terraform

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,14 +2,16 @@ terraform {
   required_version = ">= 1.4.2, < 2.0.0"
 
   backend "s3" {
-    key    = "state/scalingo.tfstate"
+    key    = "state/front.tfstate"
     bucket = "1j1s-terraform-remote-state-applications"
+    region = "lorem-ipsum" # fake region because we use Minio instead of AWS
 
     # Ces options sont nécessaires pour utiliser le backend S3 Minio
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     force_path_style            = true
+
   }
 
   required_providers {
