92c85f313f4ca22e70956d7eabd7516fb97db684
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-04-05 15:59:47 +0200

    feat(apprentissage): Ajoute le feature flip sur la page

---
+++ src/pages/campagne-apprentissage-jeunes/index.page.test.tsx
@@ -0,0 +1,20 @@
+import * as process from 'process';
+
+import { getServerSideProps } from './index.page';
+
+describe('<CampagneApprentissageJeunes />', () => {
+	it('renvoie 404 quand la page est feature flippé off', async () => {
+		process.env.NEXT_PUBLIC_CAMPAGNE_APPRENTISSAGE_FEATURE = '0';
+
+		const result = await getServerSideProps();
+
+		expect(result).toMatchObject({ notFound: true });
+	});
+	it('renvoie les props quand la page est feature flippé on', async () => {
+		process.env.NEXT_PUBLIC_CAMPAGNE_APPRENTISSAGE_FEATURE = '1';
+
+		const result = await getServerSideProps();
+
+		expect(result).toMatchObject({ props: {} });
+	});
+});
--- src/pages/campagne-apprentissage-jeunes/index.page.tsx
+++ src/pages/campagne-apprentissage-jeunes/index.page.tsx
@@ -1,5 +1,18 @@
+import { GetServerSidePropsResult } from 'next';
+
 export default function CampagneApprentissageJeunes () {
 
 	return <h1>Campagne Apprentissage Jeunes</h1>;
 }
 
+export async function getServerSideProps(): Promise<GetServerSidePropsResult<Record<never, never>>> {
+	const featureFlip = process.env.NEXT_PUBLIC_CAMPAGNE_APPRENTISSAGE_FEATURE === '1';
+	if (!featureFlip) {
+		return {
+			notFound: true,
+		};
+	}
+	return {
+		props: {},
+	};
+}
