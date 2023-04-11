fdf26bba7a2b6f7fa93591a37fc1c6b94654f06e
Suxue LI <suxue.li@octo.com>
2023-04-11 11:11:51 +0200

    feat(apprentissage): gestion espacement ui

--- src/client/components/features/CampagneApprentissage/CampagneApprentissage.tsx
+++ src/client/components/features/CampagneApprentissage/CampagneApprentissage.tsx
@@ -9,9 +9,11 @@ export function CampagneApprentissage() {
 	return (
 		<main id="contenu" className={styles.simulateur}>
 			<HeroWithIllustration image={'/images/campagne-apprentissage.webp'} className={styles.hero}>
-				<h1>L’apprentissage : <small>pour moi c’est le bon choix</small></h1>
-				<p>Avant de démarrer la simulation de vos aides, pensez à vous munir de vos ressources et de celles de vos parents si vous êtes encore à leur charge.</p>
-				<Link href={'/apprentissage/simulation'} appearance={'asPrimaryButton'}>Simuler ma rémunération</Link>
+				<div className={styles.textContent}>
+					<h1>L’apprentissage : <small>pour moi c’est le bon choix</small></h1>
+					<p>Avant de démarrer la simulation de vos aides, pensez à vous munir de vos ressources et de celles de vos parents si vous êtes encore à leur charge.</p>
+					<Link href={'/apprentissage/simulation'} appearance={'asPrimaryButton'} className={styles.cta}>Simuler ma rémunération</Link>
+				</div>
 			</HeroWithIllustration>
 		</main>
 	);
