65064f63ba3e2027710259e079523534e84d243b
Suxue LI <suxue.li@octo.com>
2023-04-18 11:02:02 +0200

    feat(apprentissage): entreprise - WIP

--- src/client/components/features/CampagneApprentissage/CampagneApprentissageEntreprises/ToutSavoirSurApprentissage/ToutSavoirSurApprentissage.tsx
+++ src/client/components/features/CampagneApprentissage/CampagneApprentissageEntreprises/ToutSavoirSurApprentissage/ToutSavoirSurApprentissage.tsx
@@ -6,17 +6,19 @@ import { Container } from '~/client/components/layouts/Container/Container';
 import { Link } from '../../../../ui/Link/Link';
 
 export function ToutSavoirSurApprentissage() {
+	const embaucherUnApprentiLink = 'https://travail-emploi.gouv.fr/formation-professionnelle/formation-en-alternance-10751/apprentissage/embaucher-un-apprenti/';
+
 	return (
 		<section aria-labelledby={'titre-section-tout-savoir'} className={styles.toutSavoir}>
 			<Container className={styles.contenu}>
 				<h2 id={'titre-section-tout-savoir'}>
-            Tout savoir sur l’embauche d’un apprenti
+					Tout savoir sur l’embauche d’un apprenti
 				</h2>
 				<div>
-            Des conseils pour bien recruter votre apprenti, le point sur les aides à l’embauche d’un apprenti… Le site “Embaucher un apprenti” met à disposition un ensemble de conseils pratiques à destination des employeurs
+					Des conseils pour bien recruter votre apprenti, le point sur les aides à l’embauche d’un apprenti… Le site “Embaucher un apprenti” met à disposition un ensemble de conseils pratiques à destination des employeurs
 				</div>
-				<Link href={'https://travail-emploi.gouv.fr/formation-professionnelle/formation-en-alternance-10751/apprentissage/embaucher-un-apprenti/'} appearance={'asSecondaryButton'}>
-            Découvrir les conseils pratiques
+				<Link href={embaucherUnApprentiLink} appearance={'asSecondaryButton'}>
+					Découvrir les conseils pratiques
 				</Link>
 			</Container>
 		</section>
