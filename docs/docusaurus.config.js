104d7379d84fc0a6fce6e95c6227c857d4e0b605
Laurent Igout <laurent.igout@octo.com>
2023-04-19 12:28:42 +0200

    chore: docusaurus fix lint erreurs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,160 +6,283 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  // Titre de votre site. Sera utilisé dans les métadonnées et comme titre dans l'onglet du navigateur.
-  title: 'Documentation Technique 1jeune1solution',
-  customFields: {
-    shortTitle: '1jeune1solution',
-  },
+	
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: '1j1s-front/',
+	
+	
+	
 
-  // Le slogan de votre site web.
-  tagline: 'Le code source de 1jeune1solution est ouvert pour que chacun puisse contribuer à l\'avenir des jeunes !',
+	customFields: {
+		shortTitle: '1jeune1solution',
+	},
 
-  // Le chemin d'accès au favicon de votre site, il doit s'agir d'une URL qui peut être utilisée dans le href du lien.
-  favicon: 'img/favicon.ico',
+	
+	
+	
+	
 
-  // URL de votre site web. Cela peut également être considéré comme le nom d'hôte de premier niveau.
-  url: 'https://dnum-socialgouv.github.io/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '1j1s-front/',
+	// Le chemin d'accès au favicon de votre site, il doit s'agir d'une URL qui peut être utilisée dans le href du lien.
+	favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'DNUM-SocialGouv', // Usually your GitHub org/user name.
-  projectName: '1j1s-front', // Usually your repo name.
+	
+	
+	
+	
+	
+	
+	
+	
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'fr',
+		locales: ['fr'],
+	},
 
-  // Cette option ajoute <meta name="robots" content="noindex, nofollow"> à chaque page,
-  // pour dire aux moteurs de recherche de ne pas indexer votre site 
-  noIndex: true, 
+	
+	
 
-  // Le comportement de Docusaurus lorsqu'il détecte un lien défectueux.
-  onBrokenLinks: 'throw',
+	
+	
 
-  // Le comportement de Docusaurus lorsqu'il détecte un lien défectueux du Markdown.
-  onBrokenMarkdownLinks: 'throw',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr'],
-  },
+	
+	
+	
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
+
+	// Usually your repo name.
+	// Cette option ajoute <meta name="robots" content="noindex, nofollow"> à chaque page,
+	// pour dire aux moteurs de recherche de ne pas indexer votre site 
+	noIndex: true,
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+	// Le comportement de Docusaurus lorsqu'il détecte un lien défectueux.
+	onBrokenLinks: 'throw',
+
+	
+	
+	
+
+
+	
+	
+	
+	
+
+
+	
+	// Le comportement de Docusaurus lorsqu'il détecte un lien défectueux du Markdown.
+	onBrokenMarkdownLinks: 'throw', 
+  
+
+
+	
+	
+	
+	
+	
+
+
+	
+	
+	
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'DNUM-SocialGouv', 
+
+  
+  
+	
+	
+	
+	
+	
+
+
+	
+	
+	
+	
+
+	
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
             'https://github.com/DNUM-SocialGouv/1j1s-front/tree/main/docs/docs/',
+					
+					
+					lastVersion: 'current',
 
-          lastVersion: 'current',
-          versions: {
-            current: {
-              label: `Future version 🚧`,
-            },
-          },
-        },
-        pages: {
-          path: 'pages',
-          mdxPageComponent: '@theme/MDXPage',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+					sidebarPath: require.resolve('./sidebars.js'),
+					versions: {
+						current: {
+							label: 'Future version 🚧',
+						},
+					},
+				},
+				pages: {
+					mdxPageComponent: '@theme/MDXPage',
+					path: 'pages',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	], 
 
-  themeConfig:
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	// Usually your GitHub org/user name.
+	projectName: '1j1s-front',
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// Le slogan de votre site web.
+	tagline: 'Le code source de 1jeune1solution est ouvert pour que chacun puisse contribuer à l\'avenir des jeunes !',
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Documentation technique 1jeune1solution',
-        logo: {
-          alt: '#1jeune1solution',
-          src: '/img/mariane.svg'
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'architecture/architecture',
-            position: 'left',
-            label: 'Front',
-          },
-          {
-            position: 'left',
-            label: 'ETL',
-            href: 'https://dnum-socialgouv.github.io/1j1s-etl/docs/architecture',
-          },
-          {
-            type: 'docsVersionDropdown',
-            position: 'right',
-          },
-          {
-            href: 'https://github.com/DNUM-SocialGouv/1j1s-front',
-            label: 'Code source',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Composants techniques',
-            items: [
-              {
-                label: 'Front (sur Github)',
-                href: 'https://github.com/DNUM-SocialGouv/1j1s-front',
-              },
-              {
-                label: 'CMS (sur Github)',
-                href: 'https://github.com/DNUM-SocialGouv/1j1s-main-cms',
-              },
-              {
-                label: 'ETL (sur Github)',
-                href: 'https://github.com/DNUM-SocialGouv/1j1s-etl',
-              },
-            ],
-          },
-          {
-            title: 'Outils',
-            items: [
-              {
-                label: 'Figma',
-                href: 'https://www.figma.com/file/AYdRLod5b9ZMKVoBU2hczj/Prototypes_1jeune1solution?node-id=4%3A2&t=62l0sMZ6lFPWxcxS-0',
-              },
-              {
-                label: 'JIRA',
-                href: 'https://jira-mcas.atlassian.net/browse/UNJ1S',
-              },
-            ],
-          },
-          {
-            title: '#1jeune1solution',
-            items: [
-              {
-                html: 'Une initiative du Gouvernement pour accompagner, former, et faciliter l’entrée dans la vie professionnelle de tous les jeunes de 15 à 30 ans, sur tous les territoires.'
-              }
-            ],
-          },
-        ],
-        copyright: `Sauf mention contraire, tous les contenus de ce site sont sous licence <a href="https://github.com/etalab/licence-ouverte/blob/master/LO.md">etalab-2.0</a>`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
+    	
+    	footer: {
+    		copyright: 'Sauf mention contraire, tous les contenus de ce site sont sous licence <a href="https://github.com/etalab/licence-ouverte/blob/master/LO.md">etalab-2.0</a>',
+    		links: [
+    			{
+    				items: [
+    					{
+    						href: 'https://github.com/DNUM-SocialGouv/1j1s-front',
+    						label: 'Front (sur Github)',
+    					},
+    					{
+    						href: 'https://github.com/DNUM-SocialGouv/1j1s-main-cms',
+    						label: 'CMS (sur Github)',
+    					},
+    					{
+    						href: 'https://github.com/DNUM-SocialGouv/1j1s-etl',
+    						label: 'ETL (sur Github)',
+    					},
+    				],
+    				title: 'Composants techniques',
+    			},
+    			{
+    				items: [
+    					{
+    						href: 'https://www.figma.com/file/AYdRLod5b9ZMKVoBU2hczj/Prototypes_1jeune1solution?node-id=4%3A2&t=62l0sMZ6lFPWxcxS-0',
+    						label: 'Figma',
+    					},
+    					{
+    						href: 'https://jira-mcas.atlassian.net/browse/UNJ1S',
+    						label: 'JIRA',
+    					},
+    				],
+    				title: 'Outils',
+    			},
+    			{
+    				items: [
+    					{
+    						html: 'Une initiative du Gouvernement pour accompagner, former, et faciliter l’entrée dans la vie professionnelle de tous les jeunes de 15 à 30 ans, sur tous les territoires.',
+    					},
+    				],
+    				title: '#1jeune1solution',
+    			},
+    		],
+    		style: 'dark',
+    	},
+    	// Replace with your project's social card
+    	image: 'img/docusaurus-social-card.jpg',
+    	navbar: {
+    		items: [
+    			{
+    				docId: 'architecture/architecture',
+    				label: 'Front',
+    				position: 'left',
+    				type: 'doc',
+    			},
+    			{
+    				href: 'https://dnum-socialgouv.github.io/1j1s-etl/docs/architecture',
+    				label: 'ETL',
+    				position: 'left',
+    			},
+    			{
+    				position: 'right',
+    				type: 'docsVersionDropdown',
+    			},
+    			{
+    				href: 'https://github.com/DNUM-SocialGouv/1j1s-front',
+    				label: 'Code source',
+    				position: 'right',
+    			},
+    		],
+    		logo: {
+    			alt: '#1jeune1solution',
+    			src: '/img/mariane.svg',
+    		},
+    		title: 'Documentation technique 1jeune1solution',
+    	},
+    	prism: {
+    		darkTheme: darkCodeTheme,
+    		theme: lightCodeTheme,
+    	},
     }),
+
+	
+	
+	// Titre de votre site. Sera utilisé dans les métadonnées et comme titre dans l'onglet du navigateur.
+	title: 'Documentation Technique 1jeune1solution',
+
+	// URL de votre site web. Cela peut également être considéré comme le nom d'hôte de premier niveau.
+	url: 'https://dnum-socialgouv.github.io/',
 };
 
 module.exports = config;
--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import clsx from 'clsx';
+import React from 'react';
+
 import styles from './styles.module.css';
 
 type FeatureItem = {
@@ -9,64 +10,64 @@ type FeatureItem = {
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Facile d\'utilisation',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
+	{
+		Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+		description: (
+			<>
         Une documentation complète dans une mise en page moderne et facile d'accès,
         voilà les objectifs de la documentation 1jeune1solution. <br /> Avez-vous déjà essayé la recherche ?
-      </>
-    ),
-  },
-  {
-    title: 'Facile de contribuer',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
+			</>
+		),
+		title: 'Facile d\'utilisation',
+	},
+	{
+		Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+		description: (
+			<>
         Il manque quelque chose ? <br />
         vous avez trouvé une erreur ? <br />
         A vous de le corriger
         en proposant un ajout ou un correctif dans le dossier <code>docs</code> du projet.
-      </>
-    ),
-  },
-  {
-    title: 'Extensible par nature',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
+			</>
+		),
+		title: 'Facile de contribuer',
+	},
+	{
+		Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+		description: (
+			<>
         Grâce aux plugins, les documentations de tous les dépôts de code source 1jeune1solution
         sont réunis, mais en plus les pages sont illustrées et riche de multiples intégrations.
-      </>
-    ),
-  },
+			</>
+		),
+		title: 'Extensible par nature',
+	},
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ title, Svg, description }: FeatureItem) {
+	return (
+		<div className={clsx('col col--4')}>
+			<div className="text--center">
+				<Svg className={styles.featureSvg} role="img" />
+			</div>
+			<div className="text--center padding-horiz--md">
+				<h3>{title}</h3>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.features}>
+			<div className="container">
+				<div className="row">
+					{FeatureList.map((props, idx) => (
+						<Feature key={idx} {...props} />
+					))}
+				</div>
+			</div>
+		</section>
+	);
 }
