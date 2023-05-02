1cdbb520e377af6a7447adcfbacf2be4dc7b5435
Suxue LI <suxue.li@octo.com>
2023-05-02 15:38:27 +0200

    test(apprentissage): fixer les tests de la page jeune

--- src/pages/apprentissage-jeunes/index.page.test.tsx
+++ src/pages/apprentissage-jeunes/index.page.test.tsx
@@ -8,6 +8,9 @@ import * as process from 'process';
 import { HeadMock } from '~/client/components/head.mock';
 import { mockUseRouter } from '~/client/components/useRouter.mock';
 import { mockSmallScreen } from '~/client/components/window.mock';
+import { DependenciesProvider } from '~/client/context/dependenciesContainer.context';
+import { AnalyticsService } from '~/client/services/analytics/analytics.service';
+import { anAnalyticsService } from '~/client/services/analytics/analytics.service.fixture';
 import { aVideoCampagneApprentissageList } from '~/server/cms/domain/videoCampagneApprentissage.fixture';
 import { createFailure, createSuccess } from '~/server/errors/either';
 import { ErreurMétier } from '~/server/errors/erreurMétier.types';
@@ -28,15 +31,17 @@ jest.mock('~/server/start', () => ({
 }));
 
 describe('Page Apprentissage Jeunes', () => {
+	let analyticsService: AnalyticsService;
 	beforeEach(() => {
 		mockSmallScreen();
 		mockUseRouter({});
+		analyticsService = anAnalyticsService();
 	});
 
 	afterEach(() => {
 		jest.clearAllMocks();
 	});
-	
+
 	describe('getServerSideProps', () => {
 		describe('quand le feature flip n‘est pas actif', () => {
 			it('renvoie une 404', async () => {
@@ -47,63 +52,74 @@ describe('Page Apprentissage Jeunes', () => {
 				expect(result).toMatchObject({ notFound: true });
 			});
 		});
-		
+
 		describe('quand le feature flip est actif', () => {
 			describe('quand les vidéos ne sont pas récupérées', () => {
 				it('renvoie une liste vide pour les vidéos', async () => {
 					process.env.NEXT_PUBLIC_CAMPAGNE_APPRENTISSAGE_FEATURE = '1';
 					(dependencies.cmsDependencies.recupererVideosCampagneApprentissage.handle as jest.Mock).mockReturnValue(createFailure(ErreurMétier.SERVICE_INDISPONIBLE));
-					
+
 					const result = await getServerSideProps();
-					
+
 					expect(result).toMatchObject({ props: {
 						videos: [],
 					} });
 				});
 			});
-			
+
 			describe('quand les vidéos sont récupérées', () => {
 				it('renvoie les props', async () => {
 					process.env.NEXT_PUBLIC_CAMPAGNE_APPRENTISSAGE_FEATURE = '1';
 					(dependencies.cmsDependencies.recupererVideosCampagneApprentissage.handle as jest.Mock).mockReturnValue(createSuccess(aVideoCampagneApprentissageList()));
-	
+
 					const result = await getServerSideProps();
-			
+
 					expect(result).toMatchObject({ props: {
 						videos: aVideoCampagneApprentissageList(),
 					} });
 				});
 			});
 		});
-		
+
 	});
 
-	describe('<ApprentissageJeunes />', () => {
-		it('affiche une section principale avec ancre pour le lien d‘évitement', () => {
-			// WHEN
-			render(<ApprentissageJeunes videos={[]} />);
-	
-			// THEN
-			const main = screen.getByRole('main');
-			expect(main).toBeVisible();
-			expect(main).toHaveAttribute('id', 'contenu');
-		});
-	
-		it('affiche la bonne page', () => {
-			render(<ApprentissageJeunes videos={[]}/>);
-	
-			const titrePage = screen.getByRole('heading', { level: 1, name: /L’apprentissage : pour moi c’est le bon choix/i });
-	
-			expect(titrePage).toBeVisible();
-		});
-	
-		it('affiche le titre du document', () => {
-			render(<ApprentissageJeunes videos={[]}/> );
-	
-			const titre = document.title;
-	
-			expect(titre).toEqual('Découvrir et trouver sa voie avec l’apprentissage | 1jeune1solution');
-		});
+	describe('<ApprentissageJeunes />', () => {it('affiche une section principale avec ancre pour le lien d‘évitement', () => {
+		// WHEN
+		render(
+			<DependenciesProvider analyticsService={analyticsService}>
+				<ApprentissageJeunes videos={[]}/>
+			</DependenciesProvider>,
+		);
+
+		// THEN
+		const main = screen.getByRole('main');
+		expect(main).toBeVisible();
+		expect(main).toHaveAttribute('id', 'contenu');
+	});
+
+	it('affiche la bonne page', () => {
+		render(
+			<DependenciesProvider analyticsService={analyticsService}>
+				<ApprentissageJeunes videos={[]}/>
+			</DependenciesProvider>,
+		);
+
+		const titrePage = screen.getByRole('heading', { level: 1, name: /L’apprentissage : pour moi c’est le bon choix/i });
+
+		expect(titrePage).toBeVisible();
+	});
+
+	it('affiche le titre du document', () => {
+		render(
+			<DependenciesProvider analyticsService={analyticsService}>
+				<ApprentissageJeunes videos={[]}/>
+			</DependenciesProvider>,
+		);
+
+		const titre = document.title;
+
+		expect(titre).toEqual('Découvrir et trouver sa voie avec l’apprentissage | 1jeune1solution');
+	});
 	});
 
 });
