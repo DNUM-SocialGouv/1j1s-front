4f83d37c0f2535a32510ff8d819ddfb524eb22fe
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-05-04 15:39:41 +0200

    fix: Fix typage

--- src/client/components/layouts/Header/HeaderNavMobile.tsx
+++ src/client/components/layouts/Header/HeaderNavMobile.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import { Container } from '~/client/components/layouts/Container/Container';
 import styles from '~/client/components/layouts/Header/Header.module.scss';
@@ -19,26 +19,19 @@ export function HeaderNavMobile({ toggleModal }: { toggleModal: () => void }) {
 		logementsNav,
 	} = navigationItemList();
 	const router = useRouter();
-	const [path, setPath] = useState(() => router.pathname || '');
-
-	useEffect(() => {
-		if (path !== router.pathname) {
-			setPath(router.pathname);
-		}
-	}, [path, setPath, router]);
 
 	return (
 		<Container className={styles.headerModalContainer}>
 			<nav role="navigation">
 				<ul className={styles.headerModalNavigationList}>
-					<NavItem className={styles.navItem} label={accueil.label} link={accueil.link} isActive={path === accueil.link} onClick={toggleModal}/>
-					<NavItemWithSubItems className={styles.navItem} item={offresNav} path={path} onClick={toggleModal}/>
-					<NavItemWithSubItems className={styles.navItem} item={orientationNav} path={path} onClick={toggleModal}/>
-					<NavItemWithSubItems className={styles.navItem} item={engagementNav} path={path} onClick={toggleModal}/>
-					<NavItemWithSubItems className={styles.navItem} item={logementsNav} path={path} onClick={toggleModal}/>
-					<NavItemWithSubItems className={styles.navItem} item={accompagnementNav} path={path} onClick={toggleModal}/>
-					<NavItemWithSubItems className={styles.navItem} item={aidesEtOutilsNav} path={path} onClick={toggleModal}/>
-					<NavItemWithSubItems className={styles.navItem} item={employeurNav} path={path} onClick={toggleModal}/>
+					<NavItem className={styles.navItem} label={accueil.label} link={accueil.link} isActive={router.pathname === accueil.link} onClick={toggleModal}/>
+					<NavItemWithSubItems className={styles.navItem} item={offresNav} onClick={toggleModal}/>
+					<NavItemWithSubItems className={styles.navItem} item={orientationNav} onClick={toggleModal}/>
+					<NavItemWithSubItems className={styles.navItem} item={engagementNav} onClick={toggleModal}/>
+					<NavItemWithSubItems className={styles.navItem} item={logementsNav} onClick={toggleModal}/>
+					<NavItemWithSubItems className={styles.navItem} item={accompagnementNav} onClick={toggleModal}/>
+					<NavItemWithSubItems className={styles.navItem} item={aidesEtOutilsNav} onClick={toggleModal}/>
+					<NavItemWithSubItems className={styles.navItem} item={employeurNav} onClick={toggleModal}/>
 				</ul>
 			</nav>
 		</Container>
