54f794fdde9169e29ea405ece83de7ec163fcbef
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-05-04 15:34:39 +0200

    style: Fix linter

--- src/client/components/layouts/Header/HeaderBody.tsx
+++ src/client/components/layouts/Header/HeaderBody.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { Container } from '~/client/components/layouts/Container/Container';
 import styles from '~/client/components/layouts/Header/Header.module.scss';
