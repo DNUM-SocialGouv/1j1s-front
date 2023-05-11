9fcbffa0d1a6d2dff65b0ed35ac5880c33dd8cf9
gmoizan <guillaume.moizan@octo.com>
2023-05-11 17:43:28 +0200

    feat: ajout d'un encart informatif avant l'iframe de depot d'offre alternance

--- src/pages/apprentissage/deposer-offre/index.page.tsx
+++ src/pages/apprentissage/deposer-offre/index.page.tsx
@@ -11,6 +11,7 @@ import styles from '~/pages/apprentissage/deposer-offre/index.module.scss';
 const SOURCE_LBA_IFRAME = `${process.env.NEXT_PUBLIC_LA_BONNE_ALTERNANCE_URL}espace-pro/widget/1J1S`;
 const POLITIQUE_CONFIDENTIALITÉ_URL = `${process.env.NEXT_PUBLIC_LA_BONNE_ALTERNANCE_URL}politique-de-confidentialite`;
 const CONDITIONS_GÉNÉRALES_UTILISATION_URL = `${process.env.NEXT_PUBLIC_LA_BONNE_ALTERNANCE_URL}cgu`;
+const FORMULAIRE_DEPOT_OFFRE_LBA_URL = `${process.env.NEXT_PUBLIC_LA_BONNE_ALTERNANCE_URL}espace-pro/creation/entreprise/redirec_from_widget_1j1s`;
 
 export default function DeposerOffrePage() {
 	useAnalytics(analytics);
@@ -32,12 +33,17 @@ export default function DeposerOffrePage() {
 				</HeroSecondaryText>
 			</Hero>
 			<Container className={styles.formulaire}>
+				<p className={styles.cadreInformation}>
+					Si le formulaire de dépôt d’offre n’est pas visible ci-dessous, cliquez <Link href={FORMULAIRE_DEPOT_OFFRE_LBA_URL} aria-label="Formulaire de dépôt d'offre sur La Bonne Alternance">ici</Link> pour y accéder.
+
+					A noter : si le message suivant apparaît <q>L’adresse email est déjà associée à un compte</q>, nous vous invitons à saisir directement votre email sur la page d’authentification. Vous recevrez alors un message avec le lien de connexion pour déposer votre offre.
+				</p>
 				<iframe
 					className={styles.iframe}
 					src={SOURCE_LBA_IFRAME}
 					title="Formulaire de dépôt d’offre d’alternance en partenariat avec La bonne alternance"
 				/>
-				<p className={styles.rgpd}>
+				<p className={styles.cadreInformation}>
 					Vous êtes informé que vos données sont collectées et traitées par La bonne alternance pour traiter votre
 					demande de dépôt d‘offre d’alternace. Pour en savoir plus sur vos droits, consultez la <Link
 						href={POLITIQUE_CONFIDENTIALITÉ_URL}>Politique de Confidentialité</Link> de La bonne Alternance.
