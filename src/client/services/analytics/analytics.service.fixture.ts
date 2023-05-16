201a32e893aa6f3a779d34edaa767ce2804dd310
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-05-16 12:50:52 +0200

    feat: Ajoute l'update de l'iframe avec l'update des cookies

--- src/client/services/analytics/analytics.service.fixture.ts
+++ src/client/services/analytics/analytics.service.fixture.ts
@@ -4,5 +4,6 @@ export function anAnalyticsService(): AnalyticsService {
 	return {
 		envoyerAnalyticsPageVue: jest.fn(),
 		isAnalyticsAutorisé: jest.fn(() => false),
+		isConsentementCookieAutorisé: jest.fn(() => false),
 	} as unknown as AnalyticsService;
 }
