b219831dea6e85f2ce41e0102897bea70a4e77f5
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-05-16 12:50:52 +0200

    feat: Ajoute l'update de l'iframe avec l'update des cookies

--- src/client/components/features/CampagneApprentissage/VideosCampagneApprentissage/VideosCampagneApprentissage.tsx
+++ src/client/components/features/CampagneApprentissage/VideosCampagneApprentissage/VideosCampagneApprentissage.tsx
@@ -1,12 +1,14 @@
 import classNames from 'classnames';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container } from '~/client/components/layouts/Container/Container';
 import { ButtonComponent } from '~/client/components/ui/Button/ButtonComponent';
 import { Icon } from '~/client/components/ui/Icon/Icon';
 import { Link } from '~/client/components/ui/Link/Link';
 import { TextIcon } from '~/client/components/ui/TextIcon/TextIcon';
+import { useDependency } from '~/client/context/dependenciesContainer.context';
+import { AnalyticsService } from '~/client/services/analytics/analytics.service';
 import { VideoCampagneApprentissage } from '~/server/cms/domain/videoCampagneApprentissage.type';
 
 import styles from './VideosCampagneApprentissage.module.scss';
@@ -20,8 +22,28 @@ interface VideosCampagneApprentissageProps extends React.HTMLAttributes<HTMLDivE
 const YOUTUBE_THUMBNAIL_URL = 'https://img.youtube.com/vi/';
 
 export default function VideosCampagneApprentissage({ videos, titre, description, className }: VideosCampagneApprentissageProps) {
+	const analyticsService = useDependency<AnalyticsService>('analyticsService');
 	const [videoToDisplay, setVideoToDisplay] = useState(videos[0]);
-	const [areCookiesAccepted, setAreCookiesAccepted] = useState(false);
+	const [areCookiesAccepted, setAreCookiesAccepted] = useState(analyticsService.isConsentementCookieAutorisé('youtube'));
+
+	useEffect(function listenToCookieConsentChanges() {
+		// FIXME (GAFI 16-05-2023): Dirty implementation, to rework ASAP
+		function updateCookieSettings() {
+			setAreCookiesAccepted(analyticsService.isConsentementCookieAutorisé('youtube'));
+		}
+
+		document.addEventListener('youtube_loaded', updateCookieSettings);
+		document.addEventListener('youtube_added', updateCookieSettings);
+		document.addEventListener('youtube_allowed', () => setAreCookiesAccepted(true));
+		document.addEventListener('youtube_disallowed', () => setAreCookiesAccepted(false));
+
+		return () => {
+			document.removeEventListener('youtube_loaded', updateCookieSettings);
+			document.removeEventListener('youtube_added', updateCookieSettings);
+			document.removeEventListener('youtube_allowed', () => setAreCookiesAccepted(true));
+			document.removeEventListener('youtube_disallowed', () => setAreCookiesAccepted(false));
+		};
+	}, [analyticsService]);
 
 	function selectVideo(video: VideoCampagneApprentissage) {
 		setVideoToDisplay(video);
--- src/client/services/analytics/analytics.service.fixture.ts
+++ src/client/services/analytics/analytics.service.fixture.ts
@@ -3,5 +3,6 @@ import { AnalyticsService } from './analytics.service';
 export function anAnalyticsService(): AnalyticsService {
 	return {
 		envoyerAnalyticsPageVue: jest.fn(),
+		isConsentementCookieAutorisé: jest.fn(() => false),
 	} as unknown as AnalyticsService;
 }
