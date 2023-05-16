d8c77d6711cad3b6fbfd7e618dd4365ad954ee52
Suxue LI <suxue.li@octo.com>
2023-05-16 16:10:00 +0200

    feat(apprentissage): rebase with cookies from GAFI

--- src/client/components/features/CampagneApprentissage/VideosCampagneApprentissage/VideoFrame.tsx
+++ src/client/components/features/CampagneApprentissage/VideosCampagneApprentissage/VideoFrame.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles
 	from '~/client/components/features/CampagneApprentissage/VideosCampagneApprentissage/VideoFrame.module.scss';
@@ -8,12 +8,14 @@ import { ButtonComponent } from '~/client/components/ui/Button/ButtonComponent';
 import { Icon } from '~/client/components/ui/Icon/Icon';
 import { Link } from '~/client/components/ui/Link/Link';
 import { TextIcon } from '~/client/components/ui/TextIcon/TextIcon';
+import { useDependency } from '~/client/context/dependenciesContainer.context';
+import { AnalyticsService } from '~/client/services/analytics/analytics.service';
 import { VideoCampagneApprentissage } from '~/server/cms/domain/videoCampagneApprentissage.type';
 
 const YOUTUBE_THUMBNAIL_URL = 'https://img.youtube.com/vi/';
 
-function openCookiesPanel() {
-	window.tarteaucitron.userInterface.openPanel();
+function acceptYoutubeCookies() {
+	window.tarteaucitron.userInterface.respond(document.getElementById('youtubeAllowed'), true);
 }
 
 interface VideoFrameProps extends React.ComponentPropsWithoutRef<'div'> {
@@ -21,19 +23,43 @@ interface VideoFrameProps extends React.ComponentPropsWithoutRef<'div'> {
 }
 
 export function VideoFrame({ videoToDisplay, className }: VideoFrameProps) {
+	const analyticsService = useDependency<AnalyticsService>('analyticsService');
 	const [areCookiesAccepted, setAreCookiesAccepted] = useState(false);
+
+	useEffect(function listenToCookieConsentChanges() {
+		// FIXME (GAFI 16-05-2023): Dirty implementation, to rework ASAP
+		function updateCookieSettings() {
+			setAreCookiesAccepted(analyticsService.isConsentementCookieAutorisé('youtube'));
+		}
+
+		document.addEventListener('youtube_loaded', updateCookieSettings);
+		document.addEventListener('youtube_added', updateCookieSettings);
+		document.addEventListener('youtube_allowed', () => setAreCookiesAccepted(true));
+		document.addEventListener('youtube_disallowed', () => setAreCookiesAccepted(false));
+
+		return () => {
+			document.removeEventListener('youtube_loaded', updateCookieSettings);
+			document.removeEventListener('youtube_added', updateCookieSettings);
+			document.removeEventListener('youtube_allowed', () => setAreCookiesAccepted(true));
+			document.removeEventListener('youtube_disallowed', () => setAreCookiesAccepted(false));
+		};
+	}, [analyticsService]);
+
 
 	return <div className={classNames(styles.video, className)}>
 		{areCookiesAccepted ? (
-			<iframe
-				width="326"
-				height="180"
-				src={`https://www.youtube-nocookie.com/embed/${videoToDisplay.videoId}`}
-				title={videoToDisplay.titre}
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				allowFullScreen
-				className={styles.iframe}
-			/>
+		// <iframe
+		// 	width="326"
+		// 	height="180"
+		// 	src={`https://www.youtube-nocookie.com/embed/${videoToDisplay.videoId}`}
+		// 	title={videoToDisplay.titre}
+		// 	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		// 	allowFullScreen
+		// 	className={styles.iframe}
+		// />
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			<div videoID={videoToDisplay.videoId} width="326" height="180" className= 'youtube_player' theme="theme (dark | light)" rel="rel (1 | 0)" controls="controls (1 | 0)" showinfo="showinfo (1 | 0)" autoplay="autoplay (0 | 1)" mute="mute (0 | 1)" srcdoc="srcdoc" loop="loop (0 | 1)" loading="loading (0 | 1)" data-start="start" data-end="end"></div>
 		) : (
 			<>
 				<Image
@@ -62,10 +88,10 @@ export function VideoFrame({ videoToDisplay, className }: VideoFrameProps) {
 						</p>
 						<ButtonComponent
 							label={'Accepter les cookies'}
-							onClick={openCookiesPanel}
+							onClick={acceptYoutubeCookies}
 							appearance={'tertiary'}
 							className={styles.buttonAcceptCookies}
-							icon={<Icon className={styles.icon} name="mark-pen"/>}
+							icon={<Icon className={styles.icon} name="check-line"/>}
 							iconPosition="right"
 						/>
 					</div>
