0101c580b45ef0ccf9c4e3b86be4d949b0e192d0
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-05-23 18:15:05 +0200

    chore: Migre la page d'annonces de logement

--- cypress/e2e/annonces-de-logement.cy.ts
+++ cypress/e2e/annonces-de-logement.cy.ts
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+/// <reference types="@testing-library/cypress" />
 
 import emptyLogementResponse from '../fixture/annonces/emptyLogementMeilisearchResponse.fixture.json';
 import logementFiltreeResponse from '../fixture/annonces/logementFiltreeMeilisearchResponse.fixture.json';
@@ -15,12 +16,13 @@ describe('Parcours logement', () => {
 	  it("affiche le résultat du nombre d'annonces de logements", () => {
 				cy.intercept({ method: 'POST', pathname: '/multi-search' }, logementNonFiltreeResponse);
 
-				cy.contains('h2', '4 annonces pour étudiants');
+				cy.findByRole('heading', { level: 2, name: /4 annonces pour étudiants/ });
 	  });
 
 	  it("affiche une liste d'annonces de logement par défaut", () => {
 				cy.intercept({ method: 'POST', pathname: '/multi-search' }, logementNonFiltreeResponse);
 
+				// FIXME (GAFI 23-05-2023): Manque un label sur la liste de résultats
 				cy.get('ol > li').should('have.length', 4);
 	  });
 		});
@@ -30,7 +32,7 @@ describe('Parcours logement', () => {
 				cy.intercept({ pathname: '/multi-search' }, logementNonFiltreeResponse);
 				cy.intercept({ pathname: '/multi-search' }, logementFiltreeResponse);
 
-				cy.get('input[name="ville"]').type('par');
+				cy.findByRole('textbox', { name: 'Ville' }).type('par');
 
 				cy.get('ol > li').should('have.length', 2);
 	  });
@@ -41,7 +43,7 @@ describe('Parcours logement', () => {
 				cy.intercept({ pathname: '/multi-search' }, emptyLogementResponse).as('searchLogements');
 				cy.wait('@searchLogements');
 
-				cy.contains('p', '0 résultat');
+				cy.findByText('0 résultat').should('be.visible');
 	  });
   	});
 	});
