c5bfec4bfbcdb7520520747dacc42dd975a68629
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-05-24 16:43:27 +0200

    fix: Fix les analytics qui s'envoient quand on change ses préférences

--- src/client/hooks/useAnalytics.test.tsx
+++ src/client/hooks/useAnalytics.test.tsx
@@ -1,11 +1,12 @@
 /**
  * @jest-environment jsdom
  */
-import { fireEvent, render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 
 import { DependenciesProvider } from '~/client/context/dependenciesContainer.context';
 import useAnalytics from '~/client/hooks/useAnalytics';
 import { PageTags } from '~/client/services/analytics/analytics';
+import { AnalyticsService } from '~/client/services/analytics/analytics.service';
 import { anAnalyticsService, aPageTags } from '~/client/services/analytics/analytics.service.fixture';
 
 function TestComponent({ pageTags }: { pageTags: PageTags }) {
@@ -13,10 +14,28 @@ function TestComponent({ pageTags }: { pageTags: PageTags }) {
 
 	return <></>;
 }
+async function allowAnalytics(service: AnalyticsService) {
+	return act(() => {
+		service.isAllowed = () => true;
+		document.dispatchEvent(new Event('eulerian_allowed'));
+	});
+}
+async function disallowAnalytics(service: AnalyticsService) {
+	return act(() => {
+		service.isAllowed = () => false;
+		document.dispatchEvent(new Event('eulerian_disallowed'));
+	});
+}
+async function loadAllowedAnalytics(service: AnalyticsService) {
+	return act(() => {
+		service.isAllowed = () => true;
+		document.dispatchEvent(new Event('eulerian_loaded'));
+	});
+}
 
 describe('useAnalytics()', () => {
-	it("envoie les analytics de la page quand l'utilisateur accepte les cookies", () => {
-		const analyticsService = anAnalyticsService();
+	it("envoie les analytics de la page quand l'utilisateur accepte les cookies", async () => {
+		const analyticsService = anAnalyticsService({ isAllowed: () => false });
 		const pageTags = aPageTags();
 		render(
 			<DependenciesProvider analyticsService={analyticsService}>
@@ -24,13 +43,13 @@ describe('useAnalytics()', () => {
 			</DependenciesProvider>,
 		);
 
-		document.dispatchEvent(new Event('eulerian_allowed'));
+		await allowAnalytics(analyticsService);
 
 		expect(analyticsService.envoyerAnalyticsPageVue).toHaveBeenCalledTimes(1);
 		expect(analyticsService.envoyerAnalyticsPageVue).toHaveBeenCalledWith(pageTags);
 	});
-	it('envoie les analytics au chargement de la page quand les cookies sont déjà acceptés', () => {
-		const analyticsService = anAnalyticsService();
+	it('envoie les analytics au chargement de la page quand les cookies sont déjà acceptés', async () => {
+		const analyticsService = anAnalyticsService({ isAllowed: () => false });
 		const pageTags = aPageTags();
 		render(
 			<DependenciesProvider analyticsService={analyticsService}>
@@ -38,9 +57,41 @@ describe('useAnalytics()', () => {
 			</DependenciesProvider>,
 		);
 
-		document.dispatchEvent(new Event('eulerian_loaded'));
+		await loadAllowedAnalytics(analyticsService);
 
 		expect(analyticsService.envoyerAnalyticsPageVue).toHaveBeenCalledTimes(1);
 		expect(analyticsService.envoyerAnalyticsPageVue).toHaveBeenCalledWith(pageTags);
+	});
+	it('n’envoie pas les analytics passé la première fois que les cookies sont acceptés', async () => {
+		const analyticsService = anAnalyticsService({ isAllowed: () => false });
+		render(
+			<DependenciesProvider analyticsService={analyticsService}>
+				<TestComponent pageTags={aPageTags()}/>
+			</DependenciesProvider>,
+		);
+
+		await allowAnalytics(analyticsService);
+		await disallowAnalytics(analyticsService);
+		await allowAnalytics(analyticsService);
+		await disallowAnalytics(analyticsService);
+		await allowAnalytics(analyticsService);
+
+		expect(analyticsService.envoyerAnalyticsPageVue).toHaveBeenCalledTimes(1);
+	});
+	it('n’envoie pas les analytics passé la première fois que les cookies sont acceptés quand la page charge avec les cookies acceptés', async () => {
+		const analyticsService = anAnalyticsService({ isAllowed: () => false });
+		render(
+			<DependenciesProvider analyticsService={analyticsService}>
+				<TestComponent pageTags={aPageTags()}/>
+			</DependenciesProvider>,
+		);
+		await loadAllowedAnalytics(analyticsService);
+
+		await disallowAnalytics(analyticsService);
+		await allowAnalytics(analyticsService);
+		await disallowAnalytics(analyticsService);
+		await allowAnalytics(analyticsService);
+
+		expect(analyticsService.envoyerAnalyticsPageVue).toHaveBeenCalledTimes(1);
 	});
 });
