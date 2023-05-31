fd0978871632db185ab9f6b5345ca579d0f5ab2f
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-05-31 15:27:45 +0200

    refactor: Supprime la gestion de la page du constructor du service de marketing

--- src/client/services/marketing/marketing.service.ts
+++ src/client/services/marketing/marketing.service.ts
@@ -12,22 +12,10 @@ export class AdformService implements MarketingService {
 
 	constructor(cookiesService: CookiesService) {
 		this.cookiesService = cookiesService;
-		this.initialiserAnalyticsCampagneDeCommunication();
-	}
-
-	private initialiserAnalyticsCampagneDeCommunication(): void {
 		this.cookiesService.addUser('adformpm', AdformService.CLIENT_TRACKING_ID);
-		// FIXME (GAFI 19-05-2023): plutôt dans la page que dans le service
-		if (window.location.pathname === '/choisir-apprentissage') {
-			const pagename = '2023-04-1jeune1solution.gouv.fr-PageArrivee-ChoisirApprentissage';
-			this.cookiesService.addUser('adformpagename', pagename);
-		} else {
-			this.cookiesService.addUser('adformpagename', undefined);
-		}
-
+		this.cookiesService.addUser('adformpagename', undefined);
 		this.cookiesService.addService(AdformService.ADFORM_SERVICE);
 	}
-
 	trackPage(pagename: string): void {
 		this.cookiesService.addUser('adformpagename', pagename);
 	}
