a20326538d5275bbb2b037602ab6b7b4b682a221
gmoizan <guillaume.moizan@octo.com>
2023-06-02 11:57:23 +0200

    docs: ajout config eslint pour ignorer storybook

--- src/client/components/ui/SeeMore/MobileOnly/SeeMoreMobileOnly.stories.tsx
+++ src/client/components/ui/SeeMore/MobileOnly/SeeMoreMobileOnly.stories.tsx
@@ -10,14 +10,17 @@ const meta: Meta<typeof SeeMoreMobileOnly> = {
 export default meta;
 type Story = StoryObj<typeof SeeMoreMobileOnly>;
 
+const items = [
+	<p key={1}> Je suis visible ! </p>,
+	<p key={2}> Moi aussi, je suis visible ! </p>,
+	<p key={3}> Je suis caché en mobile uniquement ! </p>,
+	<p key={4}> Moi aussi, je suis caché en mobile uniquement ! ! </p>,
+];
+
 export const Example: Story = {
 	args: {
-		itemList: [
-			<p key={1}> Je suis visible ! </p>,
-			<p key={2}> Moi aussi, je suis visible ! </p>,
-			<p key={3}> Je suis caché en mobile uniquement ! </p>,
-			<p key={4}> Moi aussi, je suis caché en mobile uniquement ! ! </p>,
-		],
+		children: items,
+		itemList: items,
 		numberOfVisibleItems: 2,
 		seeLessAriaLabel: 'Voir moins de texte',
 		seeMoreAriaLabel: 'Voir plus de texte',
