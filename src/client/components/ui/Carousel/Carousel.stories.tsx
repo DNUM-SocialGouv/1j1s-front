479bb0f4f30ab2cc6c7d295c5e26586339b476f8
Fred-18 <fred.nobre@octo.com>
2023-06-06 16:56:05 +0200

    feat(storybook): Ajout d'un Carousel.stories

---
+++ src/client/components/ui/Carousel/Carousel.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Carousel } from './Carousel';
+
+const meta: Meta<typeof Carousel> = {
+	component: Carousel,
+	title: 'Components/Carousel',
+};
+
+export default meta;
+type Story = StoryObj<typeof Carousel>;
+
+export const Example: Story = {
+	args: {
+		hideIndicators: true,
+		imageList: [],
+		imageListLabel: 'Ceci est une image',
+		imagesSize: { height: 0, width:0 },
+		
+	},
+};
