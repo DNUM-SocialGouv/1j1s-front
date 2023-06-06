dee80440e704be1070ad7e04e9cb768334c6ebd8
Fred-18 <fred.nobre@octo.com>
2023-06-06 10:51:54 +0200

    feat(storybook): Ajout du composant Accordion.stories

---
+++ src/client/components/ui/Accordion/Accordion.Stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Accordion } from './Accordion';
+
+const meta: Meta<typeof Accordion> = {
+	component: Accordion,
+	title: 'Components/Accordion',
+};
+
+export default meta;
+ type Story = StoryObj<typeof Accordion>;
+ 
+export const Example:Story={
+	args:{
+		open:true,
+		summary:'titre',
+		summaryAs:'h2',
+		
+	},
+};
