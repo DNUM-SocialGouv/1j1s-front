ae541bd2f5e2209bcdefb4f7845cd25f79df9374
Fred-18 <fred.nobre@octo.com>
2023-06-07 17:30:57 +0200

    feat(storybook): Typo FilterAccordion.Stories

--- src/client/components/ui/FilterAccordion/FilterAccordion.Stories.tsx
+++ src/client/components/ui/FilterAccordion/FilterAccordion.Stories.tsx
@@ -1,16 +1,16 @@
-import type { Meta,StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import { FilterAccordion } from './FilterAccordion';
 
-const meta:Meta<typeof FilterAccordion>={
-	component:FilterAccordion,
-	title:'Components/FilterAccordion',
+const meta: Meta<typeof FilterAccordion> = {
+	component: FilterAccordion,
+	title: 'Components/FilterAccordion',
 };
 export default meta;
-type Strory=StoryObj<typeof FilterAccordion>;
-export const Example:Strory={
-	args:{
-		open:true,
-		title:'titre',
+type Strory = StoryObj<typeof FilterAccordion>;
+export const Example: Strory = {
+	args: {
+		open: true,
+		title: 'titre',
 	},
 };
