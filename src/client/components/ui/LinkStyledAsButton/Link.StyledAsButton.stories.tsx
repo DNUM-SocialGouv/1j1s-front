94bd0125a0ca1eaa86fbc4b0509de5c86fdf95aa
Fred-18 <fred.nobre@octo.com>
2023-06-08 17:39:45 +0200

    feat(storybook): refacto LinkStyledAsButton.Stories

--- src/client/components/ui/LinkStyledAsButton/Link.StyledAsButton.stories.tsx
+++ src/client/components/ui/LinkStyledAsButton/Link.StyledAsButton.stories.tsx
@@ -3,6 +3,11 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { LinkStyledAsButton } from './LinkStyledAsButton';
 
 const meta: Meta<typeof LinkStyledAsButton> = {
+	args:{
+		children:'Cliquez ici',
+		href:'https://www.1jeune1solution.gouv.fr/',
+		prefetch:true,
+	},
 	component: LinkStyledAsButton,
 	title: 'Components/LinkStyledAsButton',
 };
@@ -12,33 +17,22 @@ type Story = StoryObj<typeof LinkStyledAsButton>;
 export const asPrimaryButton: Story = {
 	args: {
 		appearance: 'asPrimaryButton',
-		href: 'https://octo.com/',
-		prefetch: true,
 	},
 };
 
 export const asSecondaryButton: Story = {
 	args: {
 		appearance: 'asSecondaryButton',
-		href: 'https://octo.com/',
-		prefetch: true,
-
 	},
 };
 export const asTertiaryButton: Story = {
 	args: {
 		appearance: 'asTertiaryButton',
-		href: 'https://octo.com/',
-		prefetch: true,
-
 	},
 };
 export const asQuaternayButton: Story = {
 	args: {
 		appearance: 'asQuaternayButton',
-		href: 'https://octo.com/',
-		prefetch: true,
-		
 	},
 };
 
