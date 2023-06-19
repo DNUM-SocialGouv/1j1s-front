f1d40e2c4a61197b4c2ff48b3a8f91b387fbfa94
gmoizan <guillaume.moizan@octo.com>
2023-06-19 11:44:43 +0200

    feat: update CSS modale

--- src/client/components/features/LesEntreprisesSEngagent/ModalLEEErreur/ModalLEEErreur.tsx
+++ src/client/components/features/LesEntreprisesSEngagent/ModalLEEErreur/ModalLEEErreur.tsx
@@ -1,7 +1,7 @@
-import { ModalComponent } from '~/client/components/ui/Modal/ModalComponent';
-import classNames from 'classnames';
-import { isSuccess } from '~/server/errors/either';
 import React from 'react';
+
+import { ModalComponent } from '~/client/components/ui/Modal/ModalComponent';
+
 import styles from './ModalLEEErreur.module.scss';
 
 export function ModalLEEErreur(props: { open: boolean, close: () => void }) {
@@ -9,14 +9,10 @@ export function ModalLEEErreur(props: { open: boolean, close: () => void }) {
 		isOpen={props.open}
 		close={props.close}
 	>
-		<ModalComponent.Title className={styles.modalTitle} id="dialog_label">
-			Une erreur est survenue
-		</ModalComponent.Title>
-		<ModalComponent.Content
-			className={classNames({ [styles.rappelContent]: !isSuccess, [styles.rappelContentSuccess]: isSuccess })}
-		>
-			<small className={styles.modalSubTitle}>Pour plus d'informations, rendez-vous sur le site des <a
-				href="https://lesentreprises-sengagent.gouv.fr/">entreprises s'engagent</a></small>
+		<ModalComponent.Content className={styles.content}>
+			<h1 className={styles.modalHeading}>Une erreur est survenue</h1>
+			<small className={styles.modalSubHeading}>Pour plus d‘informations, rendez-vous sur <a
+				href="https://lesentreprises-sengagent.gouv.fr/">le site des entreprises s‘engagent</a></small>
 		</ModalComponent.Content>
 	</ModalComponent>;
 }
--- src/server/entreprises/usecase/lesEntreprisesSEngagentUseCase.ts
+++ src/server/entreprises/usecase/lesEntreprisesSEngagentUseCase.ts
@@ -1,8 +1,7 @@
 import Joi from 'joi';
 import phone from 'phone';
 
-import { StrapiRepository } from '~/server/cms/infra/repositories/strapi.repository';
-import { createFailure, createSuccess, Either, isFailure, isSuccess } from '~/server/errors/either';
+import { createFailure, Either, isFailure } from '~/server/errors/either';
 import { ErreurMétier } from '~/server/errors/erreurMétier.types';
 
 import { Entreprise, SecteurDActivité, TailleDEntreprise } from '../domain/Entreprise';
@@ -11,7 +10,6 @@ import { RejoindreLaMobilisationRepository } from '../domain/RejoindreLaMobilisa
 export class LesEntreprisesSEngagentUseCase {
 	constructor(
     private primaryRepository: RejoindreLaMobilisationRepository,
-    private secondaryRepository: StrapiRepository,
 	) {}
 
 	async rejoindreLaMobilisation(command: RejoindreLaMobilisation): Promise<Either<void>> {
