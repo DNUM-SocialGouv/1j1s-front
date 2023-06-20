31d3536bfe3947f308c485138d7dbc47f4788d2b
suli-octo <129731991+suli-octo@users.noreply.github.com>
2023-06-20 17:47:38 +0200

    tech: utiliser les librairies séparées pour les options de marked (#1506)

--- src/client/components/ui/Marked/Marked.tsx
+++ src/client/components/ui/Marked/Marked.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
-import { marked } from 'marked';
 import React, { useMemo } from 'react';
 
+import { getHtmlFromMd } from '~/client/components/ui/Marked/getHtmlFromMd';
 import styles from '~/client/components/ui/Marked/Marked.module.scss';
 
 interface MarkedProps extends React.ComponentPropsWithoutRef<'div'> {
@@ -9,6 +9,6 @@ interface MarkedProps extends React.ComponentPropsWithoutRef<'div'> {
 }
 
 export default function Marked({ markdown, className, ...rest }: MarkedProps) {
-	const html = useMemo(() => ({ __html: marked.parse(markdown) }), [markdown]);
+	const html = useMemo(() => ({ __html: getHtmlFromMd(markdown) }), [markdown]);
 	return (<div dangerouslySetInnerHTML={html} className={classNames(styles.markdown, className)} {...rest} />);
 }
