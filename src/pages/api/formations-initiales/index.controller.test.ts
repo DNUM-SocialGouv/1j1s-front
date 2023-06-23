911cad2530bf3501168048560b5744578c3e85ca
Suxue LI <suxue.li@octo.com>
2023-06-23 15:04:07 +0200

    feat(formation-init): controller formation initiale disponible

--- src/pages/api/formations-initiales/index.controller.test.ts
+++ src/pages/api/formations-initiales/index.controller.test.ts
@@ -1,6 +1,7 @@
 import { testApiHandler } from 'next-test-api-route-handler';
 import nock from 'nock';
 
+import { rechercherFormationsInitialesHandler } from '~/pages/api/formations-initiales/index.controller';
 import { ErrorHttpResponse } from '~/pages/api/utils/response/response.type';
 import { FormationInitiale } from '~/server/formations-initiales/domain/formationInitiale';
 import { aFormationInitiale } from '~/server/formations-initiales/domain/formationInitiale.fixture';
@@ -9,22 +10,28 @@ import { aFormationInitialeResponse } from '~/server/formations-initiales/infra/
 describe('lorsque je veux faire une recherche de formations initiales', () => {
 	it('doit récupérer des formations initiales', async () => {
 		// GIVEN
+		const token = 'some-token';
 		const onisepLoginResponse = {
-			token: 'some-token',
+			token,
 		};
 		const apiBaseUrl = 'https://api.opendata.onisep.fr/api/1.0';
 		const apiAuthenticationUrl = `${apiBaseUrl}/login`;
 		const apiSearchUrl = `${apiBaseUrl}/dataset/5fa591127f501/search`;
-		const email = 'email@example.com';
-		const password = 'some-password';
+		const email = 'fake@example.com';
+		const password = 'password-bidon';
 		const getTokenRequestBody = { body: { email, password }, headers: { 'Content-Type':'application/x-www-form-urlencoded' } };
-		// const searchRequestBody = { headers: { 'Application-Id':'', Authentication:  } };
+		const requestOptions = {
+			reqheaders: {
+				applicationid: '123456789',
+				authorization: `Bearer ${token}`,
+			},
+		};
 		nock(apiAuthenticationUrl)
-			.post('', getTokenRequestBody )
+			.post('', getTokenRequestBody)
 			.reply(200, onisepLoginResponse);
 		nock(apiSearchUrl)
-			.get('')
-			.reply(200, aFormationInitialeResponse());
+			.get('', undefined, requestOptions)
+			.reply(200, [aFormationInitialeResponse()]);
 
 		// WHEN
 		await testApiHandler<Array<FormationInitiale> | ErrorHttpResponse>({
--- src/pages/api/formations-initiales/index.controller.ts
+++ src/pages/api/formations-initiales/index.controller.ts
@@ -1,3 +1,11 @@
-export function rechercherFormationsInitialesHandler() {
-	// tobecontinued
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { ErrorHttpResponse } from '~/pages/api/utils/response/response.type';
+import { handleResponse } from '~/pages/api/utils/response/response.util';
+import { FormationInitiale } from '~/server/formations-initiales/domain/formationInitiale';
+import { dependencies } from '~/server/start';
+
+export async function rechercherFormationsInitialesHandler(req: NextApiRequest, res: NextApiResponse<Array<FormationInitiale> | ErrorHttpResponse>) {
+	const resultatFormationsInitiales = await dependencies.formationInitialeDependencies.rechercherFormationsInitiales.handle();
+	return handleResponse(resultatFormationsInitiales, res);
 }
