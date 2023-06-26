100aa4677843b44fa4488f06bd4c956bf6d34854
julie BRUNETTO <julie.brunetto@octo.com>
2023-06-26 10:26:30 +0200

    feat(formations init): ajout du test appel du service

--- src/client/components/features/FormationInitiale/Rechercher/RechercherFormationInitiale.test.tsx
+++ src/client/components/features/FormationInitiale/Rechercher/RechercherFormationInitiale.test.tsx
@@ -20,13 +20,30 @@ describe('RechercherFormationInitiale', () => {
 	beforeEach(() => {
 		mockSmallScreen();
 	});
-	describe('le nombre de résultat', () => {
-		it('lorsqu‘il y a plusieurs résultats je vois le nombre de résultats affiché', async () => {
+	describe('Lorsque je fais une recherche de formation iniitale', () => {
+		beforeEach(() => {
 			mockUseRouter({
 				query: {
 					domaine: 'boulanger',
 				},
 			});
+		});
+		it('appelle le service concerné', async () => {
+			const aFormationService = aFormationInitialeService();
+
+			const resultRechercheFormation = createSuccess([aResultatListFormationInitiale({ libelle: 'boulanger' }),
+				aResultatListFormationInitiale({ libelle: 'patissier' })]);
+			jest.spyOn(aFormationService, 'rechercherFormationInitiale').mockResolvedValue(resultRechercheFormation);
+
+			render(<DependenciesProvider formationInitialeService={aFormationService}>
+				<RechercherFormationInitiale/>
+			</DependenciesProvider>);
+
+			await screen.findByText(/[0-9]+ formations/);
+			expect(aFormationService.rechercherFormationInitiale).toHaveBeenCalledTimes(1);
+			expect(aFormationService.rechercherFormationInitiale).toHaveBeenCalledWith({ domaine: 'boulanger' });
+		});
+		it('lorsqu‘il y a plusieurs résultats je vois le nombre de résultats affiché', async () => {
 			const aFormationService = aFormationInitialeService();
 
 			const resultRechercheFormation = createSuccess([aResultatListFormationInitiale({ libelle: 'boulanger' }),
@@ -38,17 +55,9 @@ describe('RechercherFormationInitiale', () => {
 			</DependenciesProvider>,
 			);
 
-			expect(aFormationService.rechercherFormationInitiale).toHaveBeenCalledTimes(1);
-
-			expect(screen.getByText(/[0-9]+ formations/)).toBeVisible();
+			expect(await screen.findByText(/[0-9]+ formations/)).toBeVisible();
 		});
 		it('lorsqu‘il n‘y a pas de résultat je ne vois le nombre de résultats affiché', async () => {
-			mockUseRouter({
-				query: {
-					domaine: 'boulanger',
-				},
-			});
-
 			const aFormationService = aFormationInitialeService();
 			const resultRechercheFormation = createSuccess([]);
 			jest.spyOn(aFormationService, 'rechercherFormationInitiale').mockResolvedValue(resultRechercheFormation);
@@ -61,11 +70,6 @@ describe('RechercherFormationInitiale', () => {
 			expect(screen.queryByText(/[0-9]+ formation(s)?/)).not.toBeInTheDocument();
 		});
 		it('lorsqu‘il y a un résultat je vois le nombre de résultats affiché au singulier', async () => {
-			mockUseRouter({
-				query: {
-					domaine: 'boulanger',
-				},
-			});
 			const aFormationService = aFormationInitialeService();
 			const resultRechercheFormation = createSuccess([aResultatListFormationInitiale({ libelle: 'boulanger' })]);
 			jest.spyOn(aFormationService, 'rechercherFormationInitiale').mockResolvedValue(resultRechercheFormation);
@@ -75,7 +79,22 @@ describe('RechercherFormationInitiale', () => {
 			</DependenciesProvider>,
 			);
 
-			expect(screen.getByText(/1 formation/)).toBeVisible();
+			expect(await screen.findByText(/1 formation/)).toBeVisible();
+		});
+	});
+	describe('footnote', () => {
+		it('affiche la footnote des partenaires', () => {
+			mockUseRouter({});
+			const aFormationService = aFormationInitialeService();
+
+			render(<DependenciesProvider formationInitialeService={aFormationService}>
+				<RechercherFormationInitiale/>
+			</DependenciesProvider>,
+			);
+
+			const CGULink = screen.getByRole('link', { name: 'liste disponible dans les CGU' });
+			expect(CGULink).toBeVisible();
+			expect(CGULink).toHaveAttribute('href','/cgu#3-services');
 		});
 	});
 });
