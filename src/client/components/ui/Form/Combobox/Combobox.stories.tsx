5080500106a633ac1b6595f3021f3dacff2ea058
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-06-27 22:11:20 +0200

    doc: Ajoute des stories

--- src/client/components/ui/Form/Combobox/Combobox.stories.tsx
+++ src/client/components/ui/Form/Combobox/Combobox.stories.tsx
@@ -1,5 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react';
 
+import { ButtonComponent } from '../../Button/ButtonComponent';
+import { Icon } from '../../Icon/Icon';
+import { InputText } from '../InputText/InputText';
 import { Combobox } from './Combobox';
 
 const meta: Meta<typeof Combobox> = {
@@ -20,11 +23,51 @@ const meta: Meta<typeof Combobox> = {
 
 export default meta;
 type Story = StoryObj<typeof Combobox>;
-export const story: Story = {
+export const exemple: Story = {
 	args: {},
 	render: ({ children, ...args }) => (
 		<Combobox {...args}>
 			{children.map((child, index) => <Combobox.Option key={index}>{child}</Combobox.Option>)}
 		</Combobox>
+	),
+};
+export const disabled: Story = {
+	args: {
+		disabled: true,
+	},
+	render: ({ children, ...args }) => (
+		<Combobox {...args}>
+			{children.map((child, index) => <Combobox.Option key={index}>{child}</Combobox.Option>)}
+		</Combobox>
+	),
+};
+
+export const intégrationDansUnFormulaire: Story = {
+	args: {},
+	render: ({ children, ...args }) => (
+		<form
+			onSubmit={(event) => { event.preventDefault(); alert('form submitted'); }}
+			style={{ display: 'grid', gap: '2ch', gridTemplateColumns: '1fr 1fr auto' }}
+		>
+			<label>
+				Mot clé
+				<InputText value="Informatique" />
+			</label>
+			<label htmlFor="localisation">
+				Localisation
+				<Combobox id="localisation" name="localisation" {...args}>
+					{children.map((child, index) => <Combobox.Option key={index}>{child}</Combobox.Option>)}
+				</Combobox>
+			</label>
+			<ButtonComponent label="Rechercher" icon={<Icon name="magnifying-glass" />} iconPosition="left">Rechercher</ButtonComponent>
+			<label htmlFor="domaine">
+				Domaine
+				<Combobox id="domaine" name="domaine" readOnly value="Informatique" />
+			</label>
+			<label htmlFor="domaine">
+				Durée
+				<Combobox id="domaine" name="domaine" readOnly value="6 Mois" />
+			</label>
+		</form>
 	),
 };
