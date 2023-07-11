e2d77394ed5aa9d73a53726b40330f8bde6fbc48
julie BRUNETTO <julie.brunetto@octo.com>
2023-07-11 17:43:18 +0200

    feat(formations init): linter

--- src/client/components/features/FormationInitiale/Rechercher/RechercherFormationInitiale.test.tsx
+++ src/client/components/features/FormationInitiale/Rechercher/RechercherFormationInitiale.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { render, screen, waitFor, within } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 
 import {
 	RechercherFormationInitiale,
