68a301347b6f3607b550c39c9b58a8fc5b62b5c5
Fred-18 <fred.nobre@octo.com>
2023-07-19 14:27:05 +0200

    feat(Entreprise): Modification du type Link en LinkStyledButton dans StageDeposerOffreFormulaireEnvoye

--- src/client/components/features/OffreDeStage/Déposer/Confirmation/StageDeposerOffreFormulaireEnvoye.tsx
+++ src/client/components/features/OffreDeStage/Déposer/Confirmation/StageDeposerOffreFormulaireEnvoye.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import { Container } from '~/client/components/layouts/Container/Container';
-import { Link } from '~/client/components/ui/Link/Link';
 import useReferrer from '~/client/hooks/useReferrer';
 import { URL_DEPOSER_OFFRE } from '~/pages/stages/deposer-offre/index.page';
 
+import { LinkStyledAsButton } from '../../../../ui/LinkStyledAsButton/LinkStyledAsButton';
 import styles from './StageDeposerOffreFormulaireEnvoye.module.scss';
 
 export default function StageDeposerOffreFormulaireEnvoye() {
@@ -14,18 +14,18 @@ export default function StageDeposerOffreFormulaireEnvoye() {
 		<Container className={styles.container}>
 			<div className={styles.texteValidation}>Cette offre est soumise à une validation avant sa mise en ligne.</div>
 			<div className={styles.boutonsConfirmationEnvoi}>
-				<Link
+				<LinkStyledAsButton
 					href={'/'}
 					appearance={'asPrimaryButton'}
 				>
 					Retourner à l’accueil
-				</Link>
-				<Link
+				</LinkStyledAsButton>
+				<LinkStyledAsButton
 					href={URL_DEPOSER_OFFRE}
 					appearance={'asSecondaryButton'}
 				>
 					Déposer une offre de stage
-				</Link>
+				</LinkStyledAsButton>
 			</div>
 		</Container>
 	);}
