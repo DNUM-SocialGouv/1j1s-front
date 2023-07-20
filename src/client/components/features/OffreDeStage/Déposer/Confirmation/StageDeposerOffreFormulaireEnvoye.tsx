4dc0d32e920abc63cbc04d22776b0090a5f542a8
Fred-18 <fred.nobre@octo.com>
2023-07-20 14:14:53 +0200

    feat(Entreprise): Modification suite au retour de PR

--- src/client/components/features/OffreDeStage/Déposer/Confirmation/StageDeposerOffreFormulaireEnvoye.tsx
+++ src/client/components/features/OffreDeStage/Déposer/Confirmation/StageDeposerOffreFormulaireEnvoye.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
+import { LinkStyledAsButton } from '~/../../../../ui/LinkStyledAsButton/LinkStyledAsButton';
 import { Container } from '~/client/components/layouts/Container/Container';
 import useReferrer from '~/client/hooks/useReferrer';
 import { URL_DEPOSER_OFFRE } from '~/pages/stages/deposer-offre/index.page';
 
-import { LinkStyledAsButton } from '../../../../ui/LinkStyledAsButton/LinkStyledAsButton';
 import styles from './StageDeposerOffreFormulaireEnvoye.module.scss';
 
 export default function StageDeposerOffreFormulaireEnvoye() {
@@ -12,20 +12,22 @@ export default function StageDeposerOffreFormulaireEnvoye() {
 
 	return (
 		<Container className={styles.container}>
-			<div className={styles.texteValidation}>Cette offre est soumise à une validation avant sa mise en ligne.</div>
+			<div className={styles.texteValidation}>Cette offre est soumise à une validation avant sa mise en ligne.
+			</div>
 			<div className={styles.boutonsConfirmationEnvoi}>
 				<LinkStyledAsButton
 					href={'/'}
 					appearance={'asPrimaryButton'}
 				>
-					Retourner à l’accueil
+                    Retourner à l’accueil
 				</LinkStyledAsButton>
 				<LinkStyledAsButton
 					href={URL_DEPOSER_OFFRE}
 					appearance={'asSecondaryButton'}
 				>
-					Déposer une offre de stage
+                    Déposer une offre de stage
 				</LinkStyledAsButton>
 			</div>
 		</Container>
-	);}
+	);
+}
