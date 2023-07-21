00f31e7f984a9c78f0a8877cb81932ba7b3ef7c0
julie BRUNETTO <julie.brunetto@octo.com>
2023-07-21 16:13:26 +0200

    feat(jobs d'été): modification du taggage analytic

--- src/pages/jobs-ete/[id].page.tsx
+++ src/pages/jobs-ete/[id].page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { ConsulterOffreEmploi } from '~/client/components/features/OffreEmploi/Consulter/ConsulterOffreEmploi';
 import { Head } from '~/client/components/head/Head';
 import useAnalytics from '~/client/hooks/useAnalytics';
-import analytics from '~/pages/jobs-etudiants/[id].analytics';
+import analytics from '~/pages/jobs-ete/[id].analytics';
 import { PageContextParamsException } from '~/server/exceptions/pageContextParams.exception';
 import { Offre, OffreId } from '~/server/offres/domain/offre';
 import { dependencies } from '~/server/start';
