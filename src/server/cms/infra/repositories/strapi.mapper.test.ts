da7aa19cd5070add180b293cf0c1eea47410836c
Suxue LI <suxue.li@octo.com>
2023-07-31 18:18:10 +0200

    feat(formation-init): fix test strapi repo

--- src/server/cms/infra/repositories/strapi.mapper.test.ts
+++ src/server/cms/infra/repositories/strapi.mapper.test.ts
@@ -1,3 +1,4 @@
+import { aFormationInitialeDetailCMS } from '~/server/cms/domain/formationInitiale.fixture';
 import { FormationInitialeDetailCMS } from '~/server/cms/domain/formationInitiale.type';
 import { Domaines, OffreDeStage, SourceDesDonnées } from '~/server/cms/domain/offreDeStage.type';
 import { anOffreDeStageResponse } from '~/server/cms/infra/repositories/strapi.fixture';
@@ -89,8 +90,8 @@ describe('mapOffreDeStage', () => {
 			updatedAt: '2023-01-06T07:49:10.773Z',
 			urlDeCandidature: 'https://www.jobteaser.com/en/job-offers/10067252',
 		};
-		
-		
+
+
 		const result: OffreDeStage = mapOffreStage(offreDeStageResponse);
 		const expectedResult: OffreDeStage = {
 			dateDeDebutMax: '2024-09-01',
@@ -142,13 +143,14 @@ describe('mapFormationInitialeDetail', () => {
 			intitule: 'BM boulanger',
 			niveauEtudesVise: '5',
 			poursuiteEtudes: 'Le BTS est un diplôme conçu pour une insertion professionnelle',
+			updatedAt: '2023-05-15T09:37:44.283Z',
 		};
-		const formationExpected: FormationInitialeDetailCMS = {
+		const formationExpected: FormationInitialeDetailCMS = aFormationInitialeDetailCMS({
 			attendusParcoursup: 'L‘option managament d‘unité de production culinaire vise à maîtriser des techniques culinaires propres aux différents types de restauration',
 			conditionsAcces: 'Le diplomé peut débuter comme chef de partie, second de cuisine, avant d‘accéder à des postes d‘encadrement ou de direction.',
 			description: 'Je suis une description de formation initiale',
 			poursuiteEtudes: 'Le BTS est un diplôme conçu pour une insertion professionnelle',
-		};
+		});
 
 
 		const formationInitialeMapped = mapFormationInitiale(formationInitialeStrapiReponse);
