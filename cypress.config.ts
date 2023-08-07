e64c9d63e24cd97d58a4d779bdb301896cb71cea
Dorian De Rosa <dorian.de.rosa@octo.com>
2023-08-07 17:53:01 +0200

    ci(stage): fix test flacky depot de stage timeout v2

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	e2e: {
 		baseUrl: 'http://127.0.0.1:3000',
+		defaultCommandTimeout: 10000,
 		pageLoadTimeout: 100000,
 		requestTimeout: 20000,
 		screenshotOnRunFailure: false,
