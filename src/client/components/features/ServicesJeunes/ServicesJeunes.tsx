90c4c399dd93bf30994615dac7f000fd747e9ca6
Suxue LI <suxue.li@octo.com>
2023-08-23 11:23:34 +0200

    feat(espace-jeune): voir seulement 6 cartes de services jeunes initialement

--- src/client/components/features/ServicesJeunes/ServicesJeunes.tsx
+++ src/client/components/features/ServicesJeunes/ServicesJeunes.tsx
@@ -11,7 +11,7 @@ interface ServicesJeunesProps {
 	cardList: Array<ServiceJeune>
 }
 
-const NUMBER_OF_VISIBLE_ITEMS = 9;
+const NUMBER_OF_VISIBLE_ITEMS = 6;
 
 export function ServicesJeunes(props: ServicesJeunesProps) {
 	const { cardList } = props;
@@ -43,16 +43,16 @@ interface ServiceJeuneCardProps {
 function ServiceJeuneCard({ serviceJeune }: ServiceJeuneCardProps) {
 	const titre = useSanitize(serviceJeune.titre);
 	const categorie = serviceJeune.categorie;
-	const bannière = serviceJeune.bannière?.src || '';
+	const banniere = serviceJeune.bannière?.src || '';
 	const link = serviceJeune.link;
-	const concerné = serviceJeune.concerné || '';
+	const concerne = serviceJeune.concerné || '';
 
 	return <FlippingCard
 		className={styles.card}
 		category={categorie}
-		imageUrl={bannière}
+		imageUrl={banniere}
 		link={link}
 		title={titre}
-		flippingCardContent={concerné}
+		flippingCardContent={concerne}
 	/>;
 }
