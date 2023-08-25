400c5229e81bbc35f1649b97dab41156b0737f1c
Nofe18 <121492696+Nofe18@users.noreply.github.com>
2023-08-25 11:17:56 +0200

    feat(engagement): modifier le wording des cta d engagement  (#1864)
    * feat(Engagement): modification des tests
    
    * feat(Engagement): modification du wording
    
    * feat(Engagement): modification suite au retour
    
    * feat(Engagement): modification du wording

--- src/client/components/features/Engagement/Consulter/ConsulterMissionEngagement.tsx
+++ src/client/components/features/Engagement/Consulter/ConsulterMissionEngagement.tsx
@@ -9,41 +9,73 @@ import { formatCarriageReturnToHtml } from '~/client/utils/formatCarriageReturnT
 import { Mission } from '~/server/engagement/domain/engagement';
 
 interface ConsulterMissionEngagementProps {
-  missionEngagement: Mission
+  missionEngagement: Mission;
 }
 
-export function ConsulterMissionEngagement({ missionEngagement }: ConsulterMissionEngagementProps) {
-	const descriptionMissionEngagement = useSanitize(formatCarriageReturnToHtml(missionEngagement.description));
-	const localisationMissionEngagement = useSanitize(formatCarriageReturnToHtml(missionEngagement.localisation));
+export function ConsulterMissionEngagement({
+	missionEngagement,
+}: ConsulterMissionEngagementProps) {
+	const descriptionMissionEngagement = useSanitize(
+		formatCarriageReturnToHtml(missionEngagement.description),
+	);
+	const localisationMissionEngagement = useSanitize(
+		formatCarriageReturnToHtml(missionEngagement.localisation),
+	);
 
 	return (
 		<ConsulterOffreLayout>
 			<header className={commonStyles.titre}>
 				<h1>{missionEngagement.titre}</h1>
-				{missionEngagement.nomEntreprise && <h2>{missionEngagement.nomEntreprise}</h2>}
-				<TagList list={missionEngagement.étiquetteList} aria-label="Caractéristiques de la mission" />
+				{missionEngagement.nomEntreprise && (
+					<h2>{missionEngagement.nomEntreprise}</h2>
+				)}
+				<TagList
+					list={missionEngagement.étiquetteList}
+					aria-label="Caractéristiques de la mission"
+				/>
 				<div className={commonStyles.buttonAsLinkWrapper}>
 					<div className={commonStyles.buttonAsLink}>
-						{missionEngagement.url && <LinkStyledAsButton href={missionEngagement.url} appearance="asPrimaryButton">Postuler</LinkStyledAsButton>}
+						{missionEngagement.url && (
+							<LinkStyledAsButton
+								href={missionEngagement.url}
+								appearance="asPrimaryButton"
+							>
+								S’engager
+							</LinkStyledAsButton>
+						)}
 					</div>
 				</div>
 			</header>
 			<section className={commonStyles.contenu}>
-				{missionEngagement.localisation &&
-          <div className={commonStyles.contenuAdaptatif}>
-          	<h3>Où ? </h3><p dangerouslySetInnerHTML={{ __html: localisationMissionEngagement }}/>
-          </div>
-				}
-				{missionEngagement.description &&
-          <div className={commonStyles.contenuAdaptatif}>
-          	<h3>Quoi ? </h3><p dangerouslySetInnerHTML={{ __html: descriptionMissionEngagement }}/>
-          </div>
-				}
-				{missionEngagement.débutContrat &&
-          <div className={commonStyles.contenuAdaptatif}>
-          	<h3>Quand ? </h3><p>À partir du {missionEngagement.débutContrat} {missionEngagement.duréeContrat ? `(${missionEngagement.duréeContrat} Mois)`: ''}</p>
-          </div>
-				}
+				{missionEngagement.localisation && (
+					<div className={commonStyles.contenuAdaptatif}>
+						<h3>Où ? </h3>
+						<p
+							dangerouslySetInnerHTML={{
+								__html: localisationMissionEngagement,
+							}}
+						/>
+					</div>
+				)}
+				{missionEngagement.description && (
+					<div className={commonStyles.contenuAdaptatif}>
+						<h3>Quoi ? </h3>
+						<p
+							dangerouslySetInnerHTML={{ __html: descriptionMissionEngagement }}
+						/>
+					</div>
+				)}
+				{missionEngagement.débutContrat && (
+					<div className={commonStyles.contenuAdaptatif}>
+						<h3>Quand ? </h3>
+						<p>
+              À partir du {missionEngagement.débutContrat}{' '}
+							{missionEngagement.duréeContrat
+								? `(${missionEngagement.duréeContrat} Mois)`
+								: ''}
+						</p>
+					</div>
+				)}
 			</section>
 		</ConsulterOffreLayout>
 	);
