3c0704202e79b5ff9a954e7c1000d0156e36e644
Fred-18 <fred.nobre@octo.com>
2023-08-25 10:40:58 +0200

    feat(Engagement): modification suite au retour

--- src/client/components/features/Engagement/Consulter/ConsulterMissionEngagement.test.tsx
+++ src/client/components/features/Engagement/Consulter/ConsulterMissionEngagement.test.tsx
@@ -1,48 +1,51 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen, within } from '@testing-library/react';
-import React from 'react';
+import { render, screen, within } from "@testing-library/react";
+import React from "react";
 
-import { ConsulterMissionEngagement } from '~/client/components/features/Engagement/Consulter/ConsulterMissionEngagement';
-import { mockUseRouter } from '~/client/components/useRouter.mock';
-import { mockSmallScreen } from '~/client/components/window.mock';
-import {
-	anAmbassadeurDuDonDeVêtementMission,
-} from '~/server/engagement/domain/missionEngagement.fixture';
+import { ConsulterMissionEngagement } from "~/client/components/features/Engagement/Consulter/ConsulterMissionEngagement";
+import { mockUseRouter } from "~/client/components/useRouter.mock";
+import { mockSmallScreen } from "~/client/components/window.mock";
+import { anAmbassadeurDuDonDeVêtementMission } from "~/server/engagement/domain/missionEngagement.fixture";
 
-describe('ConsulterMission', () => {
-	beforeEach(() => {
-		mockSmallScreen();
-		mockUseRouter({});
-	});
+describe("ConsulterMission", () => {
+  beforeEach(() => {
+    mockSmallScreen();
+    mockUseRouter({});
+  });
 
-	afterEach(() => {
-		jest.clearAllMocks();
-	});
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-	describe('quand on consulte une mission', () => {
-		it('affiche l‘offre de mission', async () => {
-			const offreMission = anAmbassadeurDuDonDeVêtementMission();
+  describe("quand on consulte une mission", () => {
+    it("affiche l‘offre de mission", async () => {
+      const offreMission = anAmbassadeurDuDonDeVêtementMission();
 
-			render(<ConsulterMissionEngagement missionEngagement={offreMission} />);
+      render(<ConsulterMissionEngagement missionEngagement={offreMission} />);
 
-			const nomAssociation = screen.getByText('Ebs le relais val de seine');
-			const tagList = screen.getByRole('list', { name: 'Caractéristiques de la mission' });
+      const nomAssociation = screen.getByText("Ebs le relais val de seine");
+      const tagList = screen.getByRole("list", {
+        name: "Caractéristiques de la mission",
+      });
 
-			expect(nomAssociation).toBeVisible();
-			expect(within(tagList).queryAllByRole('listitem')).toHaveLength(2);
-		});
+      expect(nomAssociation).toBeVisible();
+      expect(within(tagList).queryAllByRole("listitem")).toHaveLength(2);
+    });
 
-		it('affiche un lien pour s’engager à la mission', async () => {
-			const offreMission = anAmbassadeurDuDonDeVêtementMission();
+    it("affiche un lien pour s’engager sur la mission", async () => {
+      const offreMission = anAmbassadeurDuDonDeVêtementMission();
 
-			render(<ConsulterMissionEngagement missionEngagement={offreMission} />);
+      render(<ConsulterMissionEngagement missionEngagement={offreMission} />);
 
-			const lienPostuler = screen.getByRole('link', { name: 'S’engager' });
-			expect(lienPostuler).toBeVisible();
-			expect(lienPostuler).toHaveAttribute('href', offreMission.url);
-			expect(lienPostuler).toHaveAttribute('title', 'S’engager - nouvelle fenêtre');
-		});
-	});
+      const lienPostuler = screen.getByRole("link", { name: "S’engager" });
+      expect(lienPostuler).toBeVisible();
+      expect(lienPostuler).toHaveAttribute("href", offreMission.url);
+      expect(lienPostuler).toHaveAttribute(
+        "title",
+        "S’engager - nouvelle fenêtre"
+      );
+    });
+  });
 });
