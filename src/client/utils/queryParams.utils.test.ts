1eb6c98edade73eed718276e6c848d6aef1c73bd
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-09-08 14:07:37 +0200

    refactor: Renomme des tests

--- src/client/utils/queryParams.utils.test.ts
+++ src/client/utils/queryParams.utils.test.ts
@@ -1,21 +1,21 @@
 import { getArrayQueryParam, getSingleQueryParam } from '~/client/utils/queryParams.utils';
 
 describe('getSingleQueryParam', () => {
-	it('renvoie le param quand donné un param unique', () => {
+	it('renvoie le param lui-même quand on donne un param unique', () => {
 		const query = 'test';
 
 		const result = getSingleQueryParam(query);
 
 		expect(result).toEqual(query);
 	});
-	it('renvoie undefined quand donné un tableau de params', () => {
+	it('renvoie undefined quand on donne un tableau de params', () => {
 		const query = ['toto', 'tata'];
 
 		const result = getSingleQueryParam(query);
 
 		expect(result).toEqual(undefined);
 	});
-	it('renvoie undefined quand donné undefined', () => {
+	it('renvoie undefined quand on donne undefined', () => {
 		const query = undefined;
 
 		const result = getSingleQueryParam(query);
@@ -25,21 +25,21 @@ describe('getSingleQueryParam', () => {
 });
 
 describe('getArrayQueryParam', () => {
-	it('renvoie un tableau de params quand donné un tableau', () => {
+	it('renvoie un tableau de params quand on donne un tableau', () => {
 		const queries = ['toto', 'tata'];
 
 		const result = getArrayQueryParam(queries);
 
 		expect(result).toEqual(queries);
 	});
-	it('renvoie undefined quand donné undefined', () => {
+	it('renvoie undefined quand on donne undefined', () => {
 		const queries = undefined;
 
 		const result = getArrayQueryParam(queries);
 
 		expect(result).toEqual(undefined);
 	});
-	it('renvoie un tableau de params quand donné un tableau transformé en string', () => {
+	it('renvoie un tableau de params quand on donne un tableau transformé en string', () => {
 		const queries = ['toto', 'tata'];
 
 		const result = getArrayQueryParam(queries.toString());
