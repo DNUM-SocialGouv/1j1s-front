b4857520ae78241bff73516db7429abb6a81ce37
julie BRUNETTO <julie.brunetto@octo.com>
2023-09-20 11:04:14 +0200

    feat(stages): afficher non renseigné à la fin de l'input domaine

--- src/client/components/features/OffreDeStage/FormulaireRecherche/FormulaireRechercheOffreStage.tsx
+++ src/client/components/features/OffreDeStage/FormulaireRecherche/FormulaireRechercheOffreStage.tsx
@@ -1,37 +1,50 @@
+import type { SearchResults } from 'algoliasearch-helper';
 import React from 'react';
 
-import styles from '~/client/components/features/OffreDeStage/FormulaireRecherche/FormulaireRechercheOffreStage.module.scss';
+import styles
+	from '~/client/components/features/OffreDeStage/FormulaireRecherche/FormulaireRechercheOffreStage.module.scss';
 import { MeilisearchCustomRefinementList } from '~/client/components/ui/Meilisearch/MeilisearchCustomRefinementList';
 import { MeilisearchCustomSearchBox } from '~/client/components/ui/Meilisearch/MeilisearchCustomSearchBox';
 import { MeilisearchInputRefinement } from '~/client/components/ui/Meilisearch/MeilisearchInputRefinement';
+import { Domaines } from '~/server/cms/domain/offreDeStage.type';
 
 const LIMIT_MAX_FACETS = 100000;
 const LIMIT_MAX_DOMAINS = 100;
-const MEILISEARCH_SORT_BY_LABEL_ASC = ['name:asc'];
+const MEILISEARCH_SORT_BY_LABEL_ASC = 'name:asc';
+
+export function sortWithNonRenseigneInTheEnd(a: SearchResults.FacetValue, b: SearchResults.FacetValue) {
+	if (a.name === Domaines.NON_RENSEIGNE && b.name !== Domaines.NON_RENSEIGNE) {
+		return 1;
+	} else if (a.name !== Domaines.NON_RENSEIGNE && b.name === Domaines.NON_RENSEIGNE) {
+		return -1;
+	} else {
+		return a.name.localeCompare(b.name);
+	}
+}
 
 export function FormulaireRechercheOffreStage() {
 	return (
 		<form className={styles.RechercherStageForm} onSubmit={(event) => event.preventDefault()}>
-	  <MeilisearchCustomSearchBox
+			<MeilisearchCustomSearchBox
 				label="Métiers, mots clés, …"
 				name="motCle"
 				placeholder="Exemples : designer, juriste…"
-	  />
-	  <MeilisearchInputRefinement
+			/>
+			<MeilisearchInputRefinement
 				attribute="localisationFiltree"
 				limit={LIMIT_MAX_FACETS}
-	  />
-	  <MeilisearchCustomRefinementList
+			/>
+			<MeilisearchCustomRefinementList
 				attribute="domaines"
 				limit={LIMIT_MAX_DOMAINS}
 				label="Domaines"
-				sortBy={MEILISEARCH_SORT_BY_LABEL_ASC}
-	  />
-	  <MeilisearchCustomRefinementList
+				sortBy={sortWithNonRenseigneInTheEnd}
+			/>
+			<MeilisearchCustomRefinementList
 				attribute="dureeCategorisee"
 				label="Durée de stage"
-				sortBy={MEILISEARCH_SORT_BY_LABEL_ASC}
-	  />
+				sortBy={[MEILISEARCH_SORT_BY_LABEL_ASC]}
+			/>
 		</form>
 	);
 }
