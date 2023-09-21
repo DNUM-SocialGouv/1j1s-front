e2659cf22253fc4c1da872bba9b32dfd0f5320cb
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-09-21 17:42:53 +0200

    refactor: Inline les liens

--- src/client/components/features/Europe/Sections/LiensUtiles.tsx
+++ src/client/components/features/Europe/Sections/LiensUtiles.tsx
@@ -1,43 +1,26 @@
-import Image from 'next/image';
-
-import useBreakpoint from '../../../../hooks/useBreakpoint';
-import { Container } from '../../../layouts/Container/Container';
 import { LinkStyledAsButton } from '../../../ui/LinkStyledAsButton/LinkStyledAsButton';
 import styles from '../EmploiEnEuropeContent.module.scss';
-
-interface LienEmploiEurope {
-  url: string;
-  title: string;
-}
-
-const linkList: Array<LienEmploiEurope> = [
-	{
-		title: 'Vous souhaitez faire une partie de votre apprentissage en Europe',
-		url: 'https://www.euroappmobility.eu/fr/',
-	},
-	{
-		title: 'Vous cherchez un Volontariat International (V.I.E / V.I.A)',
-		url: 'https://mon-vie-via.businessfrance.fr/',
-	},
-	{
-		title: 'Vous souhaitez vous engager dans une mission de solidarité en Europe',
-		url: 'https://europa.eu/youth/solidarity/young-people/volunteering_fr',
-	},
-];
 
 export function LiensUtiles() {
 	return (
 		<section className={styles.liensUtiles}>
 			<h2>Découvrez d’autres services destinés à vous aider à trouver l’expérience en Europe faite pour vous :</h2>
 			<ul aria-label="liens utiles">
-				{/* FIXME (GAFI 18-09-2023): WTF, no!  */}
-				{linkList.map((link: LienEmploiEurope) => (
-					<li key={link.title}>
-						<LinkStyledAsButton appearance={'asQuaternayButton'} href={link.url}>
-							{link.title}
-						</LinkStyledAsButton>
-					</li>
-				))}
+				<li>
+					<LinkStyledAsButton appearance={'asQuaternayButton'} href="https://www.euroappmobility.eu/fr/">
+						Vous souhaitez faire une partie de votre apprentissage en Europe
+					</LinkStyledAsButton>
+				</li>
+				<li>
+					<LinkStyledAsButton appearance={'asQuaternayButton'} href="https://mon-vie-via.businessfrance.fr/">
+						Vous cherchez un Volontariat International (V.I.E / V.I.A)
+					</LinkStyledAsButton>
+				</li>
+				<li>
+					<LinkStyledAsButton appearance={'asQuaternayButton'} href="https://europa.eu/youth/solidarity/young-people/volunteering_fr">
+						Vous souhaitez vous engager dans une mission de solidarité en Europe
+					</LinkStyledAsButton>
+				</li>
 			</ul>
 		</section>
 	);
