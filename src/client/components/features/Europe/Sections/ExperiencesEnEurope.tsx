80bb4d0125864bbea75a90f4f551978e531483fe
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-09-21 17:18:36 +0200

    fix: Fix les couleurs de background des sections

--- src/client/components/features/Europe/Sections/ExperiencesEnEurope.tsx
+++ src/client/components/features/Europe/Sections/ExperiencesEnEurope.tsx
@@ -7,28 +7,26 @@ import styles from '../EmploiEnEuropeContent.module.scss';
 export function ExperiencesEnEurope() {
 	return (
 		// FIXME (GAFI 21-09-2023): Virer la div et mettre le container sur la section
-		<div className={'background-white-lilac'}>
-			<section className={styles.experience}>
-				<LightHero className={styles.hero}>
-					<h1>
-						<LightHeroPrimaryText>Je cherche une expérience </LightHeroPrimaryText>
-						<LightHeroSecondaryText>en Europe</LightHeroSecondaryText>
-					</h1>
-				</LightHero>
-				<p className={styles.description}>
+		<section className={styles.experience}>
+			<LightHero className={styles.hero}>
+				<h1>
+					<LightHeroPrimaryText>Je cherche une expérience </LightHeroPrimaryText>
+					<LightHeroSecondaryText>en Europe</LightHeroSecondaryText>
+				</h1>
+			</LightHero>
+			<p className={styles.description}>
 					Trouvez des offres d’emploi, de stage et des volontariats internationaux au sein de pays Européens ainsi que
 					des aides financières afin de partir à la découverte de nouvelles opportunités et de nouveaux pays !
-				</p>
-				<div className={styles.information}>
-					<Icon name="information"/>
-					<p>
+			</p>
+			<div className={styles.information}>
+				<Icon name="information"/>
+				<p>
 						Si vous êtes accompagné·e en mission locale, rapprochez-vous de votre conseiller pour en savoir plus sur les mobilités courtes
-					</p>
-				</div>
-				<LinkStyledAsButton href="https://ec.europa.eu/eures/portal/jv-se/home" appearance="asPrimaryButton">
+				</p>
+			</div>
+			<LinkStyledAsButton href="https://ec.europa.eu/eures/portal/jv-se/home" appearance="asPrimaryButton">
 					Trouver une offre d’emploi en Europe
-				</LinkStyledAsButton>
-			</section>
-		</div>
+			</LinkStyledAsButton>
+		</section>
 	);
 }
