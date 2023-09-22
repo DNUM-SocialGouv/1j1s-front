32a9c646684215870cfa8265b9e6fc9873521d10
Dorian De Rosa <dorian.de.rosa@octo.com>
2023-09-22 17:59:44 +0200

    feat(lee): modifier la pop in d'erreur d'inscription

--- src/client/components/ui/LinkStyledAsButton/LinkStyledAsButton.tsx
+++ src/client/components/ui/LinkStyledAsButton/LinkStyledAsButton.tsx
@@ -9,11 +9,14 @@ import styles from './LinkStyledAsButton.module.scss';
 
 type ButtonAppearance = 'asPrimaryButton' | 'asSecondaryButton' | 'asTertiaryButton' | 'asQuaternayButton';
 
-type IconPosition = 'top' | 'left' | 'right';
+type IconPosition = 'top' | 'left' | 'right' | 'none';
 
 type IconProps = {
 	icon: React.ReactNode;
 	iconPosition: IconPosition;
+} | {
+	icon?: never;
+	iconPosition?: IconPosition;
 } | {
 	icon?: never;
 	iconPosition?: never;
@@ -89,6 +92,8 @@ export function LinkStyledAsButtonWithIcon(props: PropsWithChildren<LinkStyledAs
 				return <>{icon}<span className={styles.linkLabel}>{children}</span></>;
 			case 'right':
 				return <><span className={styles.linkLabel}>{children}</span>{icon}</>;
+			case 'none':
+				return <span className={styles.linkLabel}>{children}</span>;
 			default:
 				return <>
 					<span className={styles.linkLabel}>{children}</span>
