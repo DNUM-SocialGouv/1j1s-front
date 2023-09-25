081dbe3863d017a5d489de37a00bb39353cd4a16
Guillaume Moizan <guillaume.moizan@octo.com>
2023-09-25 17:56:31 +0200

    fix: retours MR

--- src/client/components/features/FicheMétier/Rechercher/RésultatRechercherMétier.tsx
+++ src/client/components/features/FicheMétier/Rechercher/RésultatRechercherMétier.tsx
@@ -2,12 +2,11 @@ import React, { useMemo } from 'react';
 
 import styles from '~/client/components/features/FicheMétier/Rechercher/RésultatRechercherMétier.module.scss';
 import { HitProps } from '~/client/components/layouts/InstantSearch/InstantSearchLayout';
+import { RésultatRechercherSolution } from '~/client/components/layouts/RechercherSolution/Résultat/RésultatRechercherSolution';
 import useSanitize from '~/client/hooks/useSanitize';
 import { formatCarriageReturnToHtml } from '~/client/utils/formatCarriageReturnToHtml';
 import { Strapi } from '~/server/cms/infra/repositories/strapi.response';
 import { mapFicheMetier } from '~/server/fiche-metier/domain/ficheMetierHttp';
-
-import { RésultatRechercherSolution } from '../../../layouts/RechercherSolution/Résultat/RésultatRechercherSolution';
 
 
 export function RésultatRechercherMétier(props: HitProps<Partial<Strapi.CollectionType.FicheMétier>>) {
