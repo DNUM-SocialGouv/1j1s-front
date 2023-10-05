70e2c87361d54a6b072b547d067166228022a444
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-10-05 15:58:51 +0200

    fix: Fix le type de validation dans la story

--- src/client/components/ui/Form/InputText/Input.stories.tsx
+++ src/client/components/ui/Form/InputText/Input.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { ComponentPropsWithoutRef } from 'react';
 
 import { Input } from './Input';
 
@@ -33,11 +34,12 @@ export const disabled: Story = {
 
 export const withValidation: Story = {
 	args: {
-		validation: (value: string) => value.length >= 4 ? '' : 'Saisir au moins 4 caractères',
+		validation: (value: ComponentPropsWithoutRef<typeof Input>['value']) =>
+			(typeof value === 'string' && value === 'France') ? '' : 'Entrer "France"',
 	},
 	render: (args) => (
 		<>
-			<label htmlFor="pays">Pays(Tape au moins 4 caractères)</label>
+			<label htmlFor="pays">Pays (Entrer France)</label>
 			<Input id="pays" {...args}/>
 		</>
 	),
