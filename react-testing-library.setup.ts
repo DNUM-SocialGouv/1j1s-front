06778201b8a85c273706030a7548b7f8a38b6969
Dorian De Rosa <57262105+Naorid@users.noreply.github.com>
2023-10-10 11:46:48 +0200

    test: jest turn console error and console warn to errors (#2129)

--- react-testing-library.setup.ts
+++ react-testing-library.setup.ts
@@ -1,2 +1,10 @@
 import '@testing-library/jest-dom/jest-globals';
 
+beforeEach(() => {
+	jest.spyOn(console, 'error').mockImplementation((...args) => {
+		throw new Error(`console.error called with : ${args}`);
+	});
+	jest.spyOn(console, 'warn').mockImplementation((...args) => {
+		throw new Error(`console.warn called with : ${args}`);
+	});
+});
