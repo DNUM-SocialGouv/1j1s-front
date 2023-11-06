e079d7f0d47e47ef8195ab363db83f1afc59a150
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-11-06 19:15:45 +0100

    refactor: Cypress testing library page stages

--- cypress/e2e/stage.cy.ts
+++ cypress/e2e/stage.cy.ts
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 /// <reference types="@testing-library/cypress" />
+
 import stageNonFiltreeResponse from '../fixture/annonces/stageNonFiltreeMeilisearcheResponse.fixture.json';
 
 describe('Parcours logement', () => {
@@ -11,14 +12,16 @@ describe('Parcours logement', () => {
 	context('quand on arrive sur la page de recherche de stages', () => {
 		describe('quand on fait une recherche par domaines', () => {
 			it('affiche les domaines par ordre alphabétique avec non renseigné en dernier element', () => {
-				cy.intercept({ pathname: '/multi-search' }, stageNonFiltreeResponse);
+				cy.intercept({ pathname: '/multi-search' }, stageNonFiltreeResponse).as('facets');
+				cy.wait('@facets');
 
-				cy.findByRole('button', { name: 'Domaines' }).click();
+				// FIXME (GAFI 06-11-2023): Devrait être role combobox
+				cy.findByRole('button', { name: /Domaines/i }).click();
 
-				cy.get('ul > li[role="option"]').first().should('contain.text', 'Achats');
-				cy.get('ul > li[role="option"]').eq(1).should('contain.text', 'Production');
-				cy.get('ul > li[role="option"]').eq(2).should('contain.text', 'Travaux');
-				cy.get('ul > li[role="option"]').last().should('contain.text', 'Non renseigné');
+				cy.findAllByRole('option').first().should('contain.text', 'Achats');
+				cy.findAllByRole('option').eq(1).should('contain.text', 'Production');
+				cy.findAllByRole('option').eq(2).should('contain.text', 'Travaux');
+				cy.findAllByRole('option').last().should('contain.text', 'Non renseigné');
 			});
 		});
 	});
