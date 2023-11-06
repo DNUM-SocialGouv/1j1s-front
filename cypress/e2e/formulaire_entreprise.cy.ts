29a7b8236f6c6068559ee714f4c8b3a2d7fc649f
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-11-06 18:11:28 +0100

    refactor: Cypress testing library formulaire entreprise

--- cypress/e2e/formulaire_entreprise.cy.ts
+++ cypress/e2e/formulaire_entreprise.cy.ts
@@ -1,15 +1,9 @@
 /// <reference types="cypress" />
+/// <reference types="@testing-library/cypress" />
 
 import { interceptGet } from '../interceptGet';
 import { interceptPost } from '../interceptPost';
 
-/***
- * DEVNOTE
- * il faut configurer votre .env avec
- * STRAPI_BASE_URL=http://127.0.0.1:1337/
- * STRAPI_URL_API=http://127.0.0.1:1337/api/
- * car cypress n'a pas accès au localhost:1337
- */
 describe('Parcours formulaire entreprise', () => {
 	beforeEach(() => {
 		cy.viewport('iphone-x');
@@ -18,11 +12,12 @@ describe('Parcours formulaire entreprise', () => {
 	context('quand l’utilisateur remplit correctement le formulaire', () => {
 		it('affiche un message de succès', () => {
 
-			cy.get('input[name="companyName"]').type('octo', { force: true });
+			cy.findByRole('textbox', { name: /Nom de l’entreprise/i }).type('OCTO Technology');
 
 			interceptGet(
 				{
-					actionBeforeWaitTheCall: () => cy.get('input[name="companyPostalCode"]').type('paris', { force: true }),
+					// FIXME (GAFI 06-11-2023): Devrait être role combobox
+					actionBeforeWaitTheCall: () => cy.findByRole('textbox', { name: /Ville du siège social de l’entreprise/i }).type('paris'),
 					alias: 'recherche-communes',
 					path: '/api/communes*',
 					response: JSON.stringify({ résultats: [
@@ -39,27 +34,30 @@ describe('Parcours formulaire entreprise', () => {
 					] } ),
 				},
 			);
-			cy.get('ul[role="listbox"] > li').first().click();
+			cy.findByRole('option', { name: /Paris \(75006\)/i }).first().click();
 
-			cy.get('input[name="companySiret"]').type('41816609600069');
-			cy.get('input[name="companySector"]').type('admin');
-			cy.get('ul[role="listbox"] > li').first().click();
-			cy.get('button').contains('Exemple : 250 à 499 salariés').click();
-			cy.get('ul[role="listbox"] > li').first().click();
+			cy.findByRole('textbox', { name: /SIRET/i }).type('41816609600069');
+			// FIXME (GAFI 06-11-2023): Devrait être role combobox
+			cy.findByRole('textbox', { name: /Secteur d’activité de l’entreprise/i }).type('admin');
+			cy.findByRole('option', { name: /Administration publique/i }).click();
+			// FIXME (GAFI 06-11-2023): Devrait être role combobox
+			cy.findByRole('button', { name: /Taille de l’entreprise/i }).click();
+			cy.findByRole('option', { name: /0 à 19 salariés/i }).click();
 
-			cy.get('button').contains('Suivant').click();
+			cy.findByRole('button', { name: /Suivant/i }).click();
 
-			cy.contains('Etape 2 sur 2').should('be.visible');
+			// FIXME (GAFI 06-11-2023): Manque un accent sur le "E"
+			cy.findByText('Etape 2 sur 2').should('be.visible');
 
-			cy.get('input[name="firstName"]').type('jean');
-			cy.get('input[name="lastName"]').type('dupont');
-			cy.get('input[name="email"]').type('jean.dupont@gmail.com');
-			cy.get('input[name="job"]').type('rh');
-			cy.get('input[name="phone"]').type('0199999999');
+			cy.findByRole('textbox', { name: /Prénom/i }).type('Jean');
+			cy.findByRole('textbox', { name: /^Nom/i }).type('Dupont');
+			cy.findByRole('textbox', { name: /Fonction au sein de l’entreprise/i }).type('RH');
+			cy.findByRole('textbox', { name: /Adresse e-mail de contact/i }).type('jean.dupont@gmail.com');
+			cy.findByRole('textbox', { name: /Numéro de téléphone de contact/i }).type('0199999999');
 
 			interceptPost(
 				{
-					actionBeforeWaitTheCall: () => cy.get('button').contains('Envoyer le formulaire').click(),
+					actionBeforeWaitTheCall: () => cy.findByRole('button', { name: /Envoyer le formulaire/i }).click(),
 					alias: 'submit-form',
 					path: '/api/entreprises',
 					response: JSON.stringify({
@@ -68,20 +66,20 @@ describe('Parcours formulaire entreprise', () => {
 					responseBodyToCheck: {
 						codePostal: '75006',
 						email: 'jean.dupont@gmail.com',
-						nom: 'dupont',
-						nomSociété: 'octo',
-						prénom: 'jean',
+						nom: 'Dupont',
+						nomSociété: 'OCTO Technology',
+						prénom: 'Jean',
 						secteur: 'public-administration',
 						siret: '41816609600069',
 						taille: 'xxsmall',
-						travail: 'rh',
+						travail: 'RH',
 						téléphone: '0199999999',
 						ville: 'Paris',
 					},
 				},
 			);
 
-			cy.contains('Félicitations, votre formulaire a bien été envoyé !').should('be.visible');
+			cy.findByRole('heading', { level: 1 }).should('have.text', 'Félicitations, votre formulaire a bien été envoyé !');
 		});
 	});
 });
