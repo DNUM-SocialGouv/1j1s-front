699d33272bd936f3d65514c635843efbaafb3bae
Guillaume Moizan <guillaume.moizan@octo.com>
2023-11-10 10:10:11 +0100

    fix: add CSP config and fix CSS

--- config/headers.js
+++ config/headers.js
@@ -2,7 +2,7 @@ const LOCAL_MODE_HEADERS = [];
 
 const STRAPI_MEDIA_HOST = new URL(process.env.STRAPI_MEDIA_URL).hostname;
 const TRUSTED_SOURCES = '*.fabrique.social.gouv.fr *.meilisearch.io/multi-search 1j1s-front.osc-fr1.scalingo.io *.1jeune1solution.gouv.fr';
-const ANALYTICS_SOURCES = `${process.env.NEXT_PUBLIC_ANALYTICS_DOMAIN}`;
+const ANALYTICS_SOURCES = `${process.env.NEXT_PUBLIC_ANALYTICS_DOMAIN} ${process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_HOST}`;
 const contentSecurityPolicy = `
   default-src 'self' ${TRUSTED_SOURCES};
   script-src 'self' ${ANALYTICS_SOURCES} https://*.adform.net;
