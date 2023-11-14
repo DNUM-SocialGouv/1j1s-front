10f6f6b711cd45077a3c9d9d7663ad0712ae0a33
Suxue LI <suxue.li@octo.com>
2023-11-14 15:28:12 +0100

    feat(stages3eme): ajout de la page avec le titre

---
+++ src/pages/stages-3eme/index.page.test.tsx
@@ -0,0 +1,80 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '~/test-utils';
+
+import { render, screen } from '@testing-library/react';
+
+import { mockUseRouter } from '~/client/components/useRouter.mock';
+import { mockSmallScreen } from '~/client/components/window.mock';
+import { DependenciesProvider } from '~/client/context/dependenciesContainer.context';
+import { anAnalyticsService } from '~/client/services/analytics/analytics.service.fixture';
+
+import Stages3emePage, { getServerSideProps } from './index.page';
+
+describe('Page stages de 3ème', () => {
+	beforeEach(() => {
+		mockSmallScreen();
+		mockUseRouter({});
+	});
+
+	describe('lorsque la feature n‘est pas activée', () => {
+		it('retourne une page 404', async () => {
+			process.env.NEXT_PUBLIC_STAGES_3EME_FEATURE = '0';
+
+			const result = await getServerSideProps();
+			expect(result).toMatchObject({ notFound: true });
+		});
+	});
+
+	describe('lorsque la feature est activée', () => {
+		beforeEach(() => {
+			process.env.NEXT_PUBLIC_STAGES_3EME_FEATURE = '1';
+			mockUseRouter({});
+			mockSmallScreen();
+		});
+		it('n‘a pas de défaut d‘accessibilité', async () => {
+			const { container } = render(
+				<DependenciesProvider
+					analyticsService={anAnalyticsService()}
+				>
+					<Stages3emePage/>
+				</DependenciesProvider>);
+			await expect(container).toBeAccessible();
+		});
+
+		it('je vois le titre', async () => {
+			render(
+				<DependenciesProvider
+					analyticsService={anAnalyticsService()}
+				>
+					<Stages3emePage/>
+				</DependenciesProvider>);
+
+			const pageHeading = screen.getByRole('heading', {
+				level: 1,
+				name: 'Des milliers d’entreprises prêtes à vous accueillir pour votre stage de 3ème',
+			});
+			expect(pageHeading).toBeVisible();
+		});
+
+		it('envoie les analytics', () => {
+			const analyticsService = anAnalyticsService();
+			render(
+				<DependenciesProvider
+					analyticsService={analyticsService}
+				>
+					<Stages3emePage/>
+				</DependenciesProvider>,
+			);
+
+			expect(analyticsService.envoyerAnalyticsPageVue).toHaveBeenCalledWith({
+				page_template: 'contenu_liste_niv_1',
+				pagegroup: 'stages_3e_liste',
+				pagelabel: 'contenu_liste_niv_1',
+				'segment-site': 'contenu_liste',
+			});
+		});
+	});
+});
