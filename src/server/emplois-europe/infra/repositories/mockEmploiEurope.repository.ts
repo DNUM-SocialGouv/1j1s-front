0fd7b21cbd0695e8919f36df995c928e3be4b8c0
Suxue LI <suxue.li@octo.com>
2023-11-16 15:07:43 +0100

    feat(emploi-europe): fix test à cause du changement de la donnée

--- src/server/emplois-europe/infra/repositories/mockEmploiEurope.repository.ts
+++ src/server/emplois-europe/infra/repositories/mockEmploiEurope.repository.ts
@@ -1027,7 +1027,7 @@ export function mockResultatRechercheDetailApiEuresEmploiEurope(override?: Parti
 							dataSourceName: 'NL TEST',
 							handle: 'ZmY5ZDUwZWMtNjlkNy02Zjg1LWUwNTMtOGU5MmIyMGE4NzEzIDI2MQ',
 						},
-						hrxml: anApiEuresEmploiEuropeDetailXMLResponse('Offre avec un type de contrat', 'Nom Entreprise', 'FR', 'Paris', 'directhire'),
+						hrxml: anApiEuresEmploiEuropeDetailXMLResponse('Nom Offre', 'Nom Entreprise', 'FR', 'Paris'),
 					},
 					related: {
 						urls: [
@@ -1142,7 +1142,7 @@ export function mockResultatRechercheDetailApiEuresEmploiEurope(override?: Parti
 							dataSourceName: 'NL TEST',
 							handle: 'ZmY5ZDUwZTctZTMyYy02Zjg1LWUwNTMtOGU5MmIyMGE4NzEzIDI2MQ',
 						},
-						hrxml: anApiEuresEmploiEuropeDetailXMLResponse('Nom Offre'),
+						hrxml: anApiEuresEmploiEuropeDetailXMLResponse('Offre avec un type de contrat', undefined, undefined, undefined, 'directhire'),
 					},
 					related: {
 						urls: [
