af16ad4dafde254a8e2a25bbf39e798187ec03fc
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-11-20 16:54:58 +0100

    refactor: Découple les fixtures pour le formulaire des objets métier

--- src/client/components/features/OffreDeStage/Déposer/StageDeposerOffre.fixture.ts
+++ src/client/components/features/OffreDeStage/Déposer/StageDeposerOffre.fixture.ts
@@ -1,8 +1,6 @@
 import { Domaines } from '~/server/cms/domain/offreDeStage.type';
 
-import { OffreDeStageDeposee } from './StageDeposerOffre';
-
-export const aFormulaireEtapeEntreprise = (): OffreDeStageDeposee.Entreprise => {
+export const aFormulaireEtapeEntreprise = () => {
 	return {
 		descriptionEmployeur: 'test description',
 		emailEmployeur: 'oui@non.com',
@@ -12,18 +10,18 @@ export const aFormulaireEtapeEntreprise = (): OffreDeStageDeposee.Entreprise => 
 	};
 };
 
-export const aFormulaireEtapeStage = (): OffreDeStageDeposee.Stage => {
+export const aFormulaireEtapeStage = () => {
 	return {
 		dateDeDebutMax: '2123-02-20',
 		dateDeDebutMin: '2123-01-20',
 		descriptionOffre: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor eu augue ut lectus arcu bibendum. Curabitur gravida arcu ac tortor dignissim convallis aenean. Adipiscing commodo elit at imperdiet. Ipsum a arcu cursus vitae congue. Sed euismod nisi porta lorem. Facilisis gravida neque convallis a cras semper auctor neque vitae. Ante in nibh mauris cursus. In iaculis nunc sed augue lacus. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum.',
-		domaineStage: 'achats' as Domaines,
+		domaineStage: Domaines.ACHAT,
 		dureeStage: '30',
-		isDateDeDebutPrecise: 'true',
+		isDateDeDebutPrecise: 'true' as const,
 		lienCandidature: 'oui@non.com',
 		nomOffre: 'Nom Offre',
 		remunerationStage: '1',
-		teletravail: 'true',
+		teletravail: 'true' as const,
 	};
 };
 
