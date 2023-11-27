5e950dd855875d4bbb71eb22e4ef0391dcdb77bd
gmoocto <128365508+gmoocto@users.noreply.github.com>
2023-11-27 11:53:12 +0100

    feat: redimensionnement de l'iframe 1jeune1permis via postMessage (#2318)

--- src/pages/1jeune1permis/index.page.tsx
+++ src/pages/1jeune1permis/index.page.tsx
@@ -1,12 +1,15 @@
 import { GetServerSidePropsResult } from 'next';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Head } from '~/client/components/head/Head';
 import { Container } from '~/client/components/layouts/Container/Container';
 
 import styles from './index.module.scss';
 
-export async function getServerSideProps(): Promise<GetServerSidePropsResult<Record<never, never>>> {
+const URL_IFRAME_1JEUNE_1PERMIS = 'https://mes-aides.pole-emploi.fr/export/1-jeune-1-permis';
+const DOMAINE_1JEUNE_1PERMIS = 'https://mes-aides.pole-emploi.fr';
+
+export async function getStaticProps(): Promise<GetServerSidePropsResult<Record<never, never>>> {
 	const isFeatureActive = process.env.NEXT_PUBLIC_1JEUNE1PERMIS_FEATURE === '1';
 
 	if (!isFeatureActive) {
@@ -20,6 +23,28 @@ export async function getServerSideProps(): Promise<GetServerSidePropsResult<Rec
 
 export default function UnJeuneUnPermis() {
 
+	const [iframeHeight, setIframeHeight] = useState<number | undefined>(undefined);
+
+	const onMessage = (event: MessageEvent<{ type: string; height: number }>) => {
+		if (event.origin !== DOMAINE_1JEUNE_1PERMIS || typeof event.data !== 'object' || !event.data.type) {
+			return;
+		}
+		if (event.data.type === 'resize-iframe') {
+			setIframeHeight(event.data.height);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener(
+			'message',
+			onMessage,
+		);
+
+		return () => {
+			window.removeEventListener('message', onMessage);
+		};
+	});
+
 	return (
 		<main id="contenu">
 			<Head
@@ -29,7 +54,8 @@ export default function UnJeuneUnPermis() {
 			<Container>
 				<iframe className={styles.iframe}
 					title="Informations sur le dispositif 1 jeune 1 permis"
-					src={'https://mes-aides.pole-emploi.fr/export/1-jeune-1-permis'}/>
+					src={URL_IFRAME_1JEUNE_1PERMIS}
+					height={iframeHeight}/>
 			</Container>
 		</main>
 	);
