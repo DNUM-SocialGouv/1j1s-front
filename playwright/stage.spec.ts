4290a63a3b7b8a2766436cef0fbbdd8f19e4e0b8
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2023-12-18 15:18:41 +0100

    fix: Fix la race condition sur le test playwright

--- playwright/stage.spec.ts
+++ playwright/stage.spec.ts
@@ -1,4 +1,4 @@
-import { devices, expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 import stageNonFiltreeResponse from '../cypress/fixture/annonces/stageNonFiltreeMeilisearcheResponse.fixture.json';
 
@@ -9,22 +9,22 @@ test.beforeEach(async ({ page }) => {
 	await page.goto('/stages');
 });
 
-test.use({
-	...devices['iPhone X'],
-});
-
 test.describe('Stages', () => {
 	test.describe('quand on arrive sur la page de recherche de stages', () => {
 		test.describe('quand on fait une recherche par domaines', () => {
-			test('affiche les domaines par ordre alphabétique avec non renseigné en dernier element', ({ page }) => {
+			test('affiche les domaines par ordre alphabétique avec non renseigné en dernier element', async ({ page }) => {
 				// FIXME (GAFI 06-11-2023): Devrait être role combobox
-				page.getByRole('button', { name: /Domaines/i }).click();
+				await page.getByRole('button', { name: /Domaines/i }).click();
 
 				const options = page.getByRole('option');
-				expect(options.first()).toHaveText('Achats');
-				expect(options.nth(1)).toHaveText('Production');
-				expect(options.nth(2)).toHaveText('Travaux');
-				expect(options.last()).toHaveText('Non renseigné');
+				await expect(options.first()).toHaveText('Achats');
+				await expect(options.nth(1)).toHaveText('Production');
+				await expect(options.nth(2)).toHaveText('Travaux');
+				await expect(options.last()).toHaveText('Non renseigné');
+				await expect(options.first()).toBeVisible();
+
+				const premierResultat = page.getByRole('link', { name: /Développeur/i });
+				await expect(premierResultat).toBeVisible();
 			});
 		});
 	});
