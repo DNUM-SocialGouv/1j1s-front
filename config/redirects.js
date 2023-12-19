8e6c8c59e4b009f62f562a931fdaf1d0d2b703de
juliebrunetto83 <122871677+juliebrunetto83@users.noreply.github.com>
2023-12-19 09:42:24 +0100

    feat(transverse): ajout de redirection pour les pages 404 consultées (#2465)

--- config/redirects.js
+++ config/redirects.js
@@ -1,8 +1,63 @@
 const ALL_MODE_REDIRECT = [
 	{
-		destination: '/decouvrir-les-metiers',
+		destination: '/apprentissage-entreprises',
 		permanent: true,
-		source: '/trouver-mon-metier',
+		source: '/article/dynamisez-votre-entreprise-grace-au-recrutement-d-un-jeune-salarie-en-apprentissage',
+	},
+	{
+		destination: '/articles/grace-au-dispositif-sesame-accedez-aux-metiers-du-sport-de-l-animation',
+		permanent: true,
+		source: '/article/grace-au-dispositif-sesame-accedez-aux-metiers-du-sport-de-l-animation',
+	},
+	{
+		destination: '/articles/boostez-votre-recherche-d-emploi-grace-a-l-accompagnement-intensif-jeunes',
+		permanent: true,
+		source: '/article/boostez-votre-recherche-d-emploi-grace-a-l-accompagnement-intensif-jeunes',
+	},
+	{
+		destination: '/articles/les-garants-a-quoi-ca-sert-et-vers-qui-me-tourner',
+		permanent: true,
+		source: '/logements/articles/les-garants-a-quoi-ca-sert-et-vers-qui-me-tourner',
+	},
+	{
+		destination: '/articles/aide-a-l-embauche-d-un-jeune-en-parcours-emploi-competences-pec-jeunes-dans-le-secteur-non-marchand',
+		permanent: true,
+		source: '/article/aide-a-l-embauche-d-un-jeune-en-parcours-emploi-competences-jeunes-pec-jeunes',
+	},
+	{
+		destination: '/articles/aide-a-l-embauche-d-un-jeune-en-contrat-initiative-emploi-jeunes-cie-jeunes-dans-le-secteur-marchand',
+		permanent: true,
+		source: '/article/employeurs-du-secteur-marchand-beneficiez-d-une-aide-financiere-pour-recruter-des-jeunes-talents-de-moins-de-26-ans-jusqu-a-30-ans-inclus-pour-les-jeunes-reconnus-travailleurs-handicapes-meme-en-temps-de-crise',
+	},
+	{
+		destination: '/service-civique',
+		permanent: true,
+		source: '/engagement',
+	},
+	{
+		destination: '/',
+		permanent: true,
+		source: '/home',
+	},
+	{
+		destination: '/articles/la-promo-16-18-afpa',
+		permanent: true,
+		source: '/16-18-obligation-formation',
+	},
+	{
+		destination: '/creer-mon-cv',
+		permanent: true,
+		source: '/ma-video-de-motivation',
+	},
+	{
+		destination: '/',
+		permanent: true,
+		source: '/accueil',
+	},
+	{
+		destination: '/formations-initiales',
+		permanent: true,
+		source: '/formations',
 	},
 	{
 		destination: '/decouvrir-les-metiers',
