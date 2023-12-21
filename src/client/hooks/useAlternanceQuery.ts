f1135be6a25abd516d6543b47486625c1b8796ba
Suxue LI <suxue.li@octo.com>
2023-12-21 16:50:34 +0100

    feat(recherche-alt): utilisation du commune query

--- src/client/hooks/useAlternanceQuery.ts
+++ src/client/hooks/useAlternanceQuery.ts
@@ -1,28 +1,23 @@
 import { useRouter } from 'next/router';
 import { useMemo } from 'react';
 
+import { CommuneQueryParams, useCommuneQuery } from '~/client/hooks/useCommuneQuery';
 import { getArrayQueryParam, getSingleQueryParam } from '~/client/utils/queryParams.utils';
 
 export type AlternanceQueryParams = {
-	codeRomes?: string[]
-	libelleMetier?: string
-	codeCommune?: string
-	distanceCommune?: string
-	libelleCommune?: string
-	longitudeCommune?: string
-	latitudeCommune?: string
-}
+	codeRomes: string[] | undefined
+	libelleMetier: string | undefined
+	distanceCommune: string | undefined
+} & CommuneQueryParams
 
 export function useAlternanceQuery(): AlternanceQueryParams {
 	const { query } = useRouter();
+	const communeQuery = useCommuneQuery();
 
 	return useMemo(() => ({
-		codeCommune: getSingleQueryParam(query.codeCommune),
 		codeRomes: getArrayQueryParam(query.codeRomes),
 		distanceCommune: getSingleQueryParam(query.distanceCommune),
-		latitudeCommune: getSingleQueryParam(query.latitudeCommune),
-		libelleCommune: getSingleQueryParam(query.libelleCommune),
 		libelleMetier: getSingleQueryParam(query.libelleMetier),
-		longitudeCommune: getSingleQueryParam(query.longitudeCommune),
-	}), [query]);
+		...communeQuery,
+	}), [query, communeQuery]);
 }
