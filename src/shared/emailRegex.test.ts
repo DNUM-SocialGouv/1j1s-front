6f0259268ad96e3b58c84ada43fbb41844227149
Suxue LI <suxue.li@octo.com>
2023-12-22 13:46:29 +0100

    fix(regex-email): ajout des tests et du fix

---
+++ src/shared/emailRegex.test.ts
@@ -0,0 +1,52 @@
+import { emailRegex } from '~/shared/emailRegex';
+
+describe('E-mail regex', () => {
+	it.each([
+		'email@example.com',
+		'firstname.lastname@example.com',
+		'email@subdomain.example.com',
+		'firstname+lastname@example.com',
+		'email@123.123.123.123',
+		'1234567890@example.com',
+		'email@example-one.com',
+		'_______@example.com',
+		'email@example.name',
+		'email@example.museum',
+		'email@example.co.jp',
+		'firstname-lastname@example.com',
+	])('%j est une adresse e-mail valide', (validEmail) => {
+		// WHEN
+		const isMatching = new RegExp(emailRegex).test(validEmail);
+
+		// THEN
+		expect(isMatching).toBe(true);
+	});
+
+	it.each([
+		'example@gmail.com@gmail.com',
+		'jean@gmailcom',
+		'plainaddress',
+		'#@%^%#$@#$@#.com',
+		'@example.com',
+		'Joe Smith <email@example.com>',
+		'email.example.com',
+		'email@example@example.com',
+		'.email@example.com',
+		'email.@example.com',
+		'email..email@example.com',
+		'あいうえお@example.com',
+		'email@example.com (Joe Smith)',
+		'email@example',
+		'email@-example.com',
+		'email@example.web',
+		'email@111.222.333.44444',
+		'email@example..com',
+		'Abc..123@example.com',
+	])('%j est invalide', () => {
+		// WHEN
+		const isMatching = new RegExp(emailRegex).test('jean@gmailcom');
+
+		// THEN
+		expect(isMatching).toBe(false);
+	});
+});
