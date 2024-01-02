cad4dbba2f31aa69d74fe0dcb25884bb3bf2bcf4
Suxue LI <suxue.li@octo.com>
2024-01-02 16:24:10 +0100

    test(logement): ajout du test du mapper

--- src/server/logements/infra/strapiAnnonceDeLogement.mapper.test.ts
+++ src/server/logements/infra/strapiAnnonceDeLogement.mapper.test.ts
@@ -1,5 +1,110 @@
-describe('TO DO tests du mapper', () => {
-	it('todo', () => {
-		expect(true).toBe(false);
+import { AnnonceDeLogement } from '~/server/logements/domain/annonceDeLogement';
+import { anAnnonceDeLogement } from '~/server/logements/domain/annonceDeLogement.fixture';
+import { aStrapiAnnonceDeLogement } from '~/server/logements/infra/strapiAnnonceDeLogement.fixture';
+import { mapAnnonceLogement } from '~/server/logements/infra/strapiAnnonceDeLogement.mapper';
+
+describe('Strapi annonce de logement mapper', () => {
+	it('map vers une annonce de logement', () => {
+		// GIVEN
+		const strapiAnnonceDeLogement = aStrapiAnnonceDeLogement({
+			bilanEnergetique: {
+				consommationEnergetique: 'A',
+				emissionDeGaz: 'B',
+			},
+			charge: 100,
+			dateDeDisponibilite: '11/11/11',
+			description: 'appart à saisir',
+			devise: '€',
+			etage: 1,
+			garantie: 50,
+			localisation: { ville: 'paris' },
+			meuble: true,
+			nombreDePieces: 1,
+			prix: 1000,
+			prixHT: 980,
+			servicesInclus: [
+				{ nom: AnnonceDeLogement.Service.INTERNET },
+				{ nom: AnnonceDeLogement.Service.ASPIRATEUR },
+			],
+			servicesOptionnels: [
+				{ nom: AnnonceDeLogement.Service.TV },
+				{ nom: AnnonceDeLogement.Service.LOCAL_A_VELO },
+			],
+			slug: 'logement-slug',
+			source: 'immojeune',
+			sourceCreatedAt: new Date('2022-01-01T00:00:00.000Z'),
+			sourceUpdatedAt: new Date('2022-01-01T00:00:00.000Z'),
+			surface: 10,
+			surfaceMax: 12,
+			titre: 'mon titre',
+			type: 'Location',
+			typeBien: 'Appartement',
+			url: 'lien-immo-jeune.com',
+		});
+
+		// WHEN
+		const resultAnnonceDeLogement = mapAnnonceLogement(strapiAnnonceDeLogement);
+
+		// THEN
+		expect(resultAnnonceDeLogement).toStrictEqual(anAnnonceDeLogement({
+			bilanEnergetique: {
+				consommationEnergetique: 'A',
+				emissionDeGaz: 'B',
+			},
+			charge: 100,
+			dateDeDisponibilité: '11/11/11',
+			dateDeMiseAJour: '1/1/2022',
+			description: 'appart à saisir',
+			devise: '€',
+			garantie: 50,
+			imageList: [],
+			localisation: { ville: 'paris' },
+			meublé: true,
+			nombreDePièces: 1,
+			prix: 1000,
+			prixHT: 980,
+			servicesInclus: [
+				AnnonceDeLogement.Service.INTERNET,
+				AnnonceDeLogement.Service.ASPIRATEUR,
+			],
+			servicesOptionnels: [
+				AnnonceDeLogement.Service.TV,
+				AnnonceDeLogement.Service.LOCAL_A_VELO,
+			],
+			source: 'immojeune',
+			surface: 10,
+			surfaceMax: 12,
+			titre: 'mon titre',
+			type: 'Location',
+			typeBien: 'Appartement',
+			urlDeCandidature: 'lien-immo-jeune.com',
+			étage: 1,
+		}));
+	});
+	it('la liste d’images est correctement mappée', () => {
+		// GIVEN
+		const strapiAnnonceDeLogement = aStrapiAnnonceDeLogement({
+			imagesUrl: [
+				{ value: 'image/1' },
+				{ value: 'image/2' },
+			],
+		});
+
+		// WHEN
+		const resultAnnonceDeLogement = mapAnnonceLogement(strapiAnnonceDeLogement);
+
+		// THEN
+		expect(resultAnnonceDeLogement).toStrictEqual(anAnnonceDeLogement({
+			imageList: [
+				{
+					alt: '',
+					src: 'image/1',
+				},
+				{
+					alt: '',
+					src: 'image/2',
+				},
+			],
+		}));
 	});
 });
