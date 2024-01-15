2510c3d9affe0d676795880cbd4c5f678f1e8e89
Suxue LI <suxue.li@octo.com>
2024-01-15 10:56:32 +0100

    feat(stage3e2de): récupérer les appelations metiers par appelations code - usecase

--- src/server/stage-3e-et-2de/domain/metierStage3eEt2de.repository.ts
+++ src/server/stage-3e-et-2de/domain/metierStage3eEt2de.repository.ts
@@ -4,4 +4,5 @@ import { MetierStage3eEt2de } from './metierStage3eEt2de';
 
 export interface MetierStage3eEt2deRepository {
 	search(motCle?: string): Promise<Either<MetierStage3eEt2de[]>>;
+	getMetiersByAppellationCodes(appellationCodes: string[]): Promise<Either<MetierStage3eEt2de[]>>
 }
--- src/server/stage-3e-et-2de/useCase/recupererAppellationMetiersParAppellationCodes.useCase.ts
+++ src/server/stage-3e-et-2de/useCase/recupererAppellationMetiersParAppellationCodes.useCase.ts
@@ -1,9 +1,9 @@
 import { MetierStage3eEt2deRepository } from '../domain/metierStage3eEt2de.repository';
 
-export class RechercherAppellationMetierUseCase {
+export class RecupererAppellationMetiersParAppellationCodesUseCase {
 	constructor(private repository: MetierStage3eEt2deRepository) {}
 
 	async handle(appellationCodes: string[]) {
-		return this.repository.getMetiersByAppellationCodes(appellationCodes);
+		return this.repository.(motCle);
 	}
 }
