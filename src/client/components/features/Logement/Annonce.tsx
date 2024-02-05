18f1712aac9b3b0e96dbf8b40261cd83751ebe66
Suxue LI <suxue.li@octo.com>
2024-02-05 17:19:41 +0100

    fix(logement): remonter le titre en premier élément de la carte

--- src/client/components/features/Logement/Annonce.tsx
+++ src/client/components/features/Logement/Annonce.tsx
@@ -19,17 +19,16 @@ export function AnnonceDeLogement(props: HitProps<AnnonceDeLogementIndexee>) {
 
 	return (
 		<Card layout="vertical" className={styles.Card}>
+			<Card.Title titleAs="h3" className={styles.cardTextContent}>{annonce.titre}</Card.Title>
+
 			<CardImage imageSrcList={annonce.imagesUrl}/>
 
-			<Card.Content className={styles.CardContenu}>
+			<span className={classNames(styles.CardEnTete, styles.cardTextContent)}>
+				<span className={styles.CardEnTeteType}>{typeDeLogement}</span>
+				<span className={styles.CardEnTeteDate}>&nbsp;postée le {dateDeLAnnonce}</span>
+			</span>
 
-				<Card.Title titleAs="h3">{annonce.titre}</Card.Title>
-
-				<span className={styles.CardContenuEnTete}>
-					<span className={styles.CardContenuEnTeteType}>{typeDeLogement}</span>
-					<span className={styles.CardContenuEnTeteDate}>&nbsp;postée le {dateDeLAnnonce}</span>
-				</span>
-
+			<Card.Content className={classNames(styles.CardContenu, styles.cardTextContent)}>
 				<dl className={styles.CardDescription}>
 					<dt>Surface</dt>
 					<dd>{annonce.surfaceAAfficher}</dd>
@@ -38,7 +37,7 @@ export function AnnonceDeLogement(props: HitProps<AnnonceDeLogementIndexee>) {
 				</dl>
 			</Card.Content>
 
-			<span className={styles.CardFooter}>
+			<span className={classNames(styles.CardFooter, styles.cardTextContent)}>
 				<TextIcon
 					icon="map-pin"
 					iconPosition="left"
