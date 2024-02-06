db8a8eceb319177612b5c47e382a059f0b13b3e1
juliebrunetto83 <122871677+juliebrunetto83@users.noreply.github.com>
2024-02-06 14:23:28 +0100

    fix(stage3e2de): ajoute la distance depuis les query params (#2646)

--- src/client/components/features/Stages3eEt2de/Rechercher/FormulaireRecherche/FormulaireRechercheStages3eEt2de.test.tsx
+++ src/client/components/features/Stages3eEt2de/Rechercher/FormulaireRecherche/FormulaireRechercheStages3eEt2de.test.tsx
@@ -39,7 +39,7 @@ describe('FormulaireRechercheStages3eEt2de', () => {
 					longitudeCommune: '2.347',
 					ville: 'Paris',
 				}),
-				distanceCommune: '10',
+				distanceCommune: '100',
 			},
 		});
 		render(
@@ -58,6 +58,7 @@ describe('FormulaireRechercheStages3eEt2de', () => {
 		expect(screen.getByDisplayValue('2.347')).toBeInTheDocument();
 		expect(screen.getByDisplayValue('48.859')).toBeInTheDocument();
 		expect(screen.getByDisplayValue('75056')).toBeInTheDocument();
+		expect(screen.getByRole('button', { name: 'Rayon' })).toHaveTextContent('100 km');
 	});
 
 	it('quand on recherche par métier, ajoute le métier recherché aux query params', async () => {
