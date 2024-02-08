386b52aa3dc5e38886cdc5fcd9214b07cde4c793
julie BRUNETTO <julie.brunetto@octo.com>
2024-02-08 17:49:14 +0100

    feat(transverse): initialise le nouveau composant Link

--- src/client/components/ui/Link/Link.test.tsx
+++ src/client/components/ui/Link/Link.test.tsx
@@ -8,28 +8,22 @@ import { render, screen } from '@testing-library/react';
 import { Link } from '~/client/components/ui/Link/Link';
 
 describe('Link', () => {
-	afterEach(() => {
-		jest.clearAllMocks();
+	it('quand le lien est un lien externe, retourne un *tag a* avec les propriétés target et rel', () => {
+		const lienExterne = 'https://mon-lien-externe';
+
+		render(
+			<Link href={lienExterne} />,
+		);
+
+		const linkComponent = screen.getByRole('link');
+
+		expect(linkComponent.getAttribute('href')).toEqual('https://mon-lien-externe');
+		expect(linkComponent).toHaveAttribute('target');
+		expect(linkComponent).toHaveAttribute('rel');
 	});
 
-	describe('quand le lien est un lien externe', () => {
-		it('retourne le composant Link avec un *tag a* directement et les propriétés target et rel', () => {
-			const lienExterne = 'https://mon-lien-externe';
-
-			render(
-				<Link href={lienExterne} />,
-			);
-
-			const linkComponent = screen.getByRole('link');
-
-			expect(linkComponent.getAttribute('href')).toEqual('https://mon-lien-externe');
-			expect(linkComponent).toHaveAttribute('target');
-			expect(linkComponent).toHaveAttribute('rel');
-		});
-	});
-
-	describe('quand le lien est un lien interne avec href relatif', () => {
-		it('retourne le composant Link sans les propriétés de la redirection externe', () => {
+	describe('lien interne', () => {
+		it('avec href relatif, retourne le composant Link sans les propriétés de la redirection externe', () => {
 			const lienInterne = '/emplois';
 
 			render(
@@ -42,9 +36,8 @@ describe('Link', () => {
 			expect(linkComponent).not.toHaveAttribute('target');
 			expect(linkComponent).not.toHaveAttribute('rel');
 		});
-	});
-	describe('quand le lien est un lien interne avec href absolut', () => {
-		it('retourne le composant Link sans les propriétés de la redirection externe', () => {
+
+		it('avec href absolu, retourne le composant Link sans les propriétés de la redirection externe', () => {
 			Object.defineProperty(window, 'location', {
 				value: { origin: 'localhost' },
 			});
@@ -60,9 +53,8 @@ describe('Link', () => {
 			expect(linkComponent).not.toHaveAttribute('target');
 			expect(linkComponent).not.toHaveAttribute('rel');
 		});
-	});
-	describe('quand le lien est un lien interne vers une ancre', () => {
-		it('retourne le composant Link sans les propriétés de la redirection externe', () => {
+
+		it('avec une ancre, retourne le composant Link sans les propriétés de la redirection externe', () => {
 			const lienInterne = '#emplois';
 
 			render(
--- src/client/components/ui/Link/Link.tsx
+++ src/client/components/ui/Link/Link.tsx
@@ -1,25 +1,60 @@
 import classNames from 'classnames';
 import LinkNext from 'next/link';
-import React from 'react';
+import React, { PropsWithChildren, useMemo } from 'react';
 
+import { Icon } from '~/client/components/ui/Icon/Icon';
 import { useIsInternalLink } from '~/client/hooks/useIsInternalLink';
 import { getTextFromChildren } from '~/client/utils/getTextFromChildren.util';
 
+import styles from './Link.module.scss';
 
-type LinkProps = React.ComponentPropsWithoutRef<'a'>&{
+type ButtonAppearance = 'asPrimaryButton' | 'asSecondaryButton' | 'asTertiaryButton' | 'asQuaternaryButton';
+
+type IconPosition = 'top' | 'left' | 'right';
+
+type IconProps = {
+	icon: React.ReactNode;
+	iconPosition: IconPosition;
+} | {
+	icon?: never;
+	iconPosition?: never;
+}
+
+interface LinkStyledAsButtonProps extends React.ComponentPropsWithoutRef<'a'> {
 	href: string
+	appearance?: ButtonAppearance
 	prefetch?: boolean
 }
 
-export function Link(props: LinkProps) {
+type LinkStyledAsButtonWithIconProps = LinkStyledAsButtonProps & IconProps
+
+
+export function Link(props: PropsWithChildren<LinkStyledAsButtonProps>) {
 	const {
 		className,
+		appearance,
 		children,
 		href,
 		prefetch = false,
 		title,
 		...rest
 	} = props;
+
+	const appearanceClass = useMemo(() => {
+		switch (appearance) {
+			case 'asPrimaryButton':
+				return styles.primaryButton;
+			case 'asSecondaryButton':
+				return styles.secondaryButton;
+			case 'asTertiaryButton':
+				return styles.tertiaryButton;
+			case 'asQuaternaryButton':
+				return styles.quaternaryButton;
+			default:
+				return ;
+		}
+	}, [appearance]);
+
 	const isInternalLink = useIsInternalLink(href);
 
 	function getTitle() {
@@ -30,12 +65,58 @@ export function Link(props: LinkProps) {
 	}
 
 	return isInternalLink ? (
-		<LinkNext href={href} title={getTitle()} prefetch={prefetch} className={classNames(className)} {...rest}>
+		<LinkNext href={href} title={getTitle()} prefetch={prefetch} className={classNames(className, appearanceClass)} {...rest}>
 			{children}
 		</LinkNext>
 	) : (
-		<a href={href} title={getTitle()} target="_blank" rel="noreferrer" className={classNames(className)} {...rest}>
+		<a href={href} title={getTitle()} target="_blank" rel="noreferrer" className={classNames(className, appearanceClass)} {...rest}>
 			{children}
 		</a>
 	);
 }
+
+export function LinkIcon(props: PropsWithChildren<LinkStyledAsButtonWithIconProps> ) {
+	const {
+		children,
+		className,
+		iconPosition,
+		icon,
+		href,
+		...rest
+	} = props;
+	const isInternalLink = useIsInternalLink(href);
+
+	const iconClass = useMemo(() => {
+		switch (iconPosition) {
+			case 'top':
+				return styles.linkWithTopIcon;
+			case 'left':
+				return styles.linkWithLeftIcon;
+			case 'right':
+				return styles.linkWithRightIcon;
+		}
+	}, [iconPosition]);
+
+	const iconBody = useMemo(() => {
+		switch (iconPosition) {
+			case 'top':
+			case 'left':
+				return <>{icon}<span className={styles.linkLabel}>{children}</span></>;
+			case 'right':
+				return <><span className={styles.linkLabel}>{children}</span>{icon}</>;
+			default:
+				return <>
+					<span className={styles.linkLabel}>{children}</span>
+					{	isInternalLink ? <Icon name="arrow-right"/> : <Icon name="external-redirection"/>}
+				</>;
+		}
+	}, [icon, iconPosition, children, isInternalLink]);
+
+	return (<span className={classNames(className, iconClass)} {...rest}>
+		{iconBody}
+	</span>
+	);
+}
+
+Link.Icon = LinkIcon;
+
