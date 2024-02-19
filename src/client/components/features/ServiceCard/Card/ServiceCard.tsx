267b7880fc2d4be0dd769cf128c3b93148a4e1a5
Guillaume Moizan <guillaume.moizan@octo.com>
2024-02-19 16:30:13 +0100

    fix: dirty hack pour la serviceCard

--- src/client/components/features/ServiceCard/Card/ServiceCard.tsx
+++ src/client/components/features/ServiceCard/Card/ServiceCard.tsx
@@ -5,7 +5,6 @@ import { HtmlHeadingTag } from '~/client/components/props';
 import { Card } from '~/client/components/ui/Card/Card';
 import { Icon } from '~/client/components/ui/Icon/Icon';
 import { Link } from '~/client/components/ui/Link/Link';
-import useBreakpoint from '~/client/hooks/useBreakpoint';
 import { useIsInternalLink } from '~/client/hooks/useIsInternalLink';
 
 import styles from './ServiceCard.module.scss';
@@ -41,7 +40,6 @@ export function ServiceCard(props: ServiceCardProps & React.HTMLAttributes<HTMLL
 		className, imageFit = 'contain', logo, link, linkLabel, title, titleAs, children,
 	} = props;
 	const isInternalLink = useIsInternalLink(link);
-	const { isLargeScreen } = useBreakpoint();
 
 	const icon = useMemo(function () {
 		return <Icon name={isInternalLink ? 'arrow-right' : 'external-redirection'}/>;
@@ -49,13 +47,32 @@ export function ServiceCard(props: ServiceCardProps & React.HTMLAttributes<HTMLL
 	const linkTitle = !isInternalLink ? `${linkLabel} - nouvelle fenêtre` : undefined;
 
 	// TODO remplacer link sur toute la card par un lien avec un ::before pour rendre toute la card cliquable
+	// TODO revoir le CSS pour éviter d'avoir deux fois la même card et supprimer les "largeScreen" et "smallScreen"
 	return (
 		<Link href={link} title={linkTitle} className={classNames(styles.cardContainer, className, 'underline-none')}>
 			<Card
-				layout={isLargeScreen ? 'horizontal' : 'vertical'}
+				layout={'horizontal'}
 				className={classNames(
 					styles.card,
-					isLargeScreen ? styles.cardHorizontal : styles.cardVertical,
+					styles.largeScreen,
+					styles.cardHorizontal,
+					imageFit === 'cover' && styles.cardCover)}
+			>
+				<Card.Image className={styles.cardLogo} src={logo} aria-hidden/>
+				<Card.Content className={styles.cardBody}>
+					<Card.Title titleAs={titleAs} className={styles.cardTitle}>{title}</Card.Title>
+					<p>{children}</p>
+					<span className={styles.cardAction}>
+						<Card.FakeLink appearance={'quaternary'} label={linkLabel} icon={icon}/>
+					</span>
+				</Card.Content>
+			</Card>
+			<Card
+				layout={'vertical'}
+				className={classNames(
+					styles.card,
+					styles.smallScreen,
+					styles.cardVertical,
 					imageFit === 'cover' && styles.cardCover)}
 			>
 				<Card.Image className={styles.cardLogo} src={logo} aria-hidden/>
