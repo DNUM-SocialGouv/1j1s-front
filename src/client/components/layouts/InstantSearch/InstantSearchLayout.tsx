23d8c6b6b7c6de20fba947f3a96dccfce2fd5f0b
Suxue LI <suxue.li@octo.com>
2024-02-20 11:15:17 +0100

    refacto: ne pas passer true mais juste router en props

--- src/client/components/layouts/InstantSearch/InstantSearchLayout.tsx
+++ src/client/components/layouts/InstantSearch/InstantSearchLayout.tsx
@@ -72,7 +72,7 @@ export function InstantSearchLayout<THit extends BaseHit = BaseHit>(props: Insta
 			<InstantSearch
 				searchClient={searchClient}
 				indexName={meilisearchIndex}
-				routing={true} /* NOTE (SULI 19-02-2024): Cf PR https://github.com/DNUM-SocialGouv/1j1s-front/pull/2711 pour réutiliser createInstantSearchRouterNext*/
+				routing /* NOTE (SULI 19-02-2024): Cf PR https://github.com/DNUM-SocialGouv/1j1s-front/pull/2711 pour réutiliser createInstantSearchRouterNext*/
 				future={{ preserveSharedStateOnUnmount: true }}
 			>
 				<InstantSearchErrorBoundary>
