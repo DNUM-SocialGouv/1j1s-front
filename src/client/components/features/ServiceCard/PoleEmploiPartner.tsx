acb0ad07d56ec68d5651d0d629023fc593d984c7
Suxue LI <suxue.li@octo.com>
2024-02-29 18:14:04 +0100

    feat(france-travail): remplacer logo sur recherche accompagnement, carte partenaire, recherche alternance

--- src/client/components/features/ServiceCard/PoleEmploiPartner.tsx
+++ src/client/components/features/ServiceCard/PoleEmploiPartner.tsx
@@ -6,13 +6,13 @@ export function PoleEmploiPartner() {
 	return (
 		<ServiceCard
 			title="France travail propose un accompagnement individualisé pour les jeunes de 16 à 30 ans"
-			logo="/images/logos/pole-emploi.svg"
-			link="/articles/pole-emploi"
+			logo="/images/logos/france-travail.svg"
+			link="/articles/pole-emploi" // todo changer slug de l'article ?
 			linkLabel="En savoir plus"
 			titleAs={'h3'}
 		>
-			Avec son dispositif d’accompagnement individualisé des jeunes (AIJ), Pôle
-			emploi propose à tous les jeunes demandeurs d’emploi de 16 à 30 ans, un
+			Avec son dispositif d’accompagnement individualisé des jeunes (AIJ), France
+			Travail propose à tous les jeunes demandeurs d’emploi de 16 à 30 ans, un
 			accompagnement personnalisé intensif d’une durée de 3 à 6 mois pour les
 			aider à trouver ou retrouver plus rapidement un emploi.
 		</ServiceCard>
