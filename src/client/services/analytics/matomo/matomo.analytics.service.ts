6c9c82100e4152a01631281ca0fabd65a02628b8
julie BRUNETTO <julie.brunetto@octo.com>
2024-03-07 09:52:49 +0100

    feat(matomo): ajout du service matomomt

--- src/client/services/analytics/matomo/matomo.analytics.service.ts
+++ src/client/services/analytics/matomo/matomo.analytics.service.ts
@@ -4,13 +4,18 @@ import { AnalyticsService } from '../analytics.service';
 
 export class MatomoAnalyticsService implements AnalyticsService {
 	private static MATOMO_SERVICE = 'matomocloud';
+	private static MATOMO_TM = 'matomotm';
 	private readonly cookiesService: CookiesService;
 
 	constructor(cookiesService: CookiesService) {
 		this.cookiesService = cookiesService;
+		this.cookiesService.addUser('matomotmUrl', process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_HOST);
+
 		this.cookiesService.addUser('matomoId', process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_SITE_ID);
 		this.cookiesService.addUser('matomoHost', process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_HOST);
 		this.cookiesService.addUser('matomoCustomJSPath', process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_CUSTOM_JS_PATH);
+
+		this.cookiesService.addService(MatomoAnalyticsService.MATOMO_TM);
 		this.cookiesService.addService(MatomoAnalyticsService.MATOMO_SERVICE);
 	}
 
