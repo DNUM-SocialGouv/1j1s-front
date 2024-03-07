329cb7edc66d9b93dd05751c95efaa6573ac955a
julie BRUNETTO <julie.brunetto@octo.com>
2024-03-07 11:02:40 +0100

    feat(matomo): ajout var denv url

--- src/client/services/analytics/matomo/matomo.analytics.service.ts
+++ src/client/services/analytics/matomo/matomo.analytics.service.ts
@@ -9,7 +9,7 @@ export class MatomoAnalyticsService implements AnalyticsService {
 
 	constructor(cookiesService: CookiesService) {
 		this.cookiesService = cookiesService;
-		this.cookiesService.addUser('matomotmUrl', process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_HOST);
+		this.cookiesService.addUser('matomotmUrl', process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_TM_CUSTOM_JS_PATH);
 
 		this.cookiesService.addUser('matomoId', process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_SITE_ID);
 		this.cookiesService.addUser('matomoHost', process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_HOST);
