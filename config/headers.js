ca31885dae569ad7f2324ee3920a97cca7d6f282
julie BRUNETTO <julie.brunetto@octo.com>
2024-03-08 16:54:54 +0100

    feat(matomo): test avec unsafe-inline et unsafe-eval

--- config/headers.js
+++ config/headers.js
@@ -5,7 +5,7 @@ const TRUSTED_SOURCES = '*.fabrique.social.gouv.fr *.meilisearch.io/multi-search
 const ANALYTICS_SOURCES = `${process.env.NEXT_PUBLIC_ANALYTICS_DOMAIN} ${process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_HOST}*`;
 const contentSecurityPolicy = `
   default-src 'self' ${TRUSTED_SOURCES};
-  script-src 'self' ${ANALYTICS_SOURCES} https://*.adform.net;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' ${ANALYTICS_SOURCES} https://*.adform.net;
   img-src 'self' *.google.com data: ${STRAPI_MEDIA_HOST} ${ANALYTICS_SOURCES} img.youtube.com jedonnemonavis.numerique.gouv.fr;
   style-src 'self' 'unsafe-inline' ${ANALYTICS_SOURCES};
   frame-ancestors 'none';
