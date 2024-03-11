989bfa109acd91317ada085fa6f83df483213776
Guillaume Moizan <guillaume.moizan@octo.com>
2024-03-11 14:19:18 +0100

    fix: generation sitemap avec variable d'env pour le domain

--- src/pages/api/sitemap/index.controller.ts
+++ src/pages/api/sitemap/index.controller.ts
@@ -4,7 +4,7 @@ import { ErrorHttpResponse } from '~/pages/api/utils/response/response.type';
 import { dependencies } from '~/server/start';
 
 export default async function générerSitemapXml(req: NextApiRequest, res: NextApiResponse<void | ErrorHttpResponse>) {
-	const baseUrl = `https://${req.headers.host}`;
+	const baseUrl = `https://${process.env.NEXT_PUBLIC_1J1S_DOMAIN}`;
 	const sitemap = await dependencies.sitemapDependencies.générerSitemapUseCase.handle(baseUrl);
 
 	res.setHeader('Content-Type', 'text/xml');
