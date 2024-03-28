a914bb78b775b411d7369596d9b29455cf61edd6
Suxue LI <suxue.li@octo.com>
2024-03-28 10:19:39 +0100

    refacto(cej): suppression des usages de useBreakpoint sur Demarrage et PasAccompagnement

--- src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/Démarrage.tsx
+++ src/client/components/features/ContratEngagementJeune/Accompagnement/Formulaires/Démarrage.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 
 import { FormulairesProps } from '~/client/components/features/ContratEngagementJeune/Accompagnement/Accompagnement';
 import styles from '~/client/components/features/ContratEngagementJeune/Accompagnement/Accompagnement.module.scss';
-import useBreakpoint from '~/client/hooks/useBreakpoint';
 
 type DemarrageProps = Pick<FormulairesProps, 'setTypeFormulaireAffiché' | 'setIsFranceTravailModalOpen' | 'setIsMissionLocaleModalOpen'>
 export default function Démarrage({ setTypeFormulaireAffiché, setIsFranceTravailModalOpen, setIsMissionLocaleModalOpen }: DemarrageProps) {
-	const { isSmallScreen, isMediumScreen } = useBreakpoint();
-	const isMobile = isSmallScreen || isMediumScreen;
 
 	return <>
 		<p className={styles.accompagnementQuestion}>Bénéficiez-vous actuellement d‘un accompagnement ?</p>
 		<div>
-			{isMobile && <span>Sélectionnez l‘option qui vous correspond :</span>}
+			<span>Sélectionnez l‘option qui vous correspond :</span>
 			<button className={styles.optionBouton} onClick={() => setIsMissionLocaleModalOpen(true)}>
 				Oui, je suis accompagné(e) par la Mission Locale
 			</button>
