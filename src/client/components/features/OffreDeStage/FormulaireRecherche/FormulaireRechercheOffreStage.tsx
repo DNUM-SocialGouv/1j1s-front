3c45f339a86ead6a8e7a807607aa3191b99435d3
julie BRUNETTO <julie.brunetto@octo.com>
2024-04-08 11:30:18 +0200

    feat(stages): tri les durees par ordre logique

--- src/client/components/features/OffreDeStage/FormulaireRecherche/FormulaireRechercheOffreStage.tsx
+++ src/client/components/features/OffreDeStage/FormulaireRecherche/FormulaireRechercheOffreStage.tsx
@@ -10,15 +10,31 @@ import { DomainesStage } from '~/server/stages/repository/domainesStage';
 
 const LIMIT_MAX_FACETS = 100000;
 const LIMIT_MAX_DOMAINS = 100;
-const MEILISEARCH_SORT_BY_LABEL_ASC = 'name:asc';
 
-export function sortWithNonRenseigneInTheEnd(a: SearchResults.FacetValue, b: SearchResults.FacetValue) {
+export function sortWithNonRenseigneAtTheEnd(a: SearchResults.FacetValue, b: SearchResults.FacetValue) {
 	if (a.name === DomainesStage.NON_RENSEIGNE) {
 		return 1;
 	} else if (b.name === DomainesStage.NON_RENSEIGNE) {
 		return -1;
 	}
 	return a.name.localeCompare(b.name);
+}
+
+export function sortWithMoinsUnMoisAtTheBegining(a: SearchResults.FacetValue, b: SearchResults.FacetValue) {
+	const MOINS_D_UN_MOIS = '< 1 mois';
+	if (a.name === MOINS_D_UN_MOIS) {
+		return -1;
+	}
+	if (b.name === MOINS_D_UN_MOIS) {
+		return 1;
+	}
+	if (a.name < b.name) {
+		return -1;
+	}
+	if (a.name > b.name) {
+		return 1;
+	}
+	return 0;
 }
 
 export function FormulaireRechercheOffreStage() {
@@ -37,12 +53,12 @@ export function FormulaireRechercheOffreStage() {
 				attribute="domaines"
 				limit={LIMIT_MAX_DOMAINS}
 				label="Domaines"
-				sortBy={sortWithNonRenseigneInTheEnd}
+				sortBy={sortWithNonRenseigneAtTheEnd}
 			/>
 			<MeilisearchCustomRefinementList
 				attribute="dureeCategorisee"
 				label="Durée de stage"
-				sortBy={[MEILISEARCH_SORT_BY_LABEL_ASC]}
+				sortBy={sortWithMoinsUnMoisAtTheBegining}
 			/>
 		</form>
 	);
