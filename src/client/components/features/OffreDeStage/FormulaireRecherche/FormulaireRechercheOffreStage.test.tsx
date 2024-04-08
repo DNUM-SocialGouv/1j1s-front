ef5d11911f6277d22801aca61f90640999b0f660
juliebrunetto83 <122871677+juliebrunetto83@users.noreply.github.com>
2024-04-08 14:59:36 +0200

    feat(stages): tri les durees par ordre logique (#2883)

--- src/client/components/features/OffreDeStage/FormulaireRecherche/FormulaireRechercheOffreStage.test.tsx
+++ src/client/components/features/OffreDeStage/FormulaireRecherche/FormulaireRechercheOffreStage.test.tsx
@@ -5,7 +5,8 @@
 import type { SearchResults } from 'algoliasearch-helper';
 
 import {
-	sortWithNonRenseigneInTheEnd,
+	sortByDurationAscending,
+	sortWithNonRenseigneAtTheEnd,
 } from '~/client/components/features/OffreDeStage/FormulaireRecherche/FormulaireRechercheOffreStage';
 
 function anFacedValue(override ?: Partial<SearchResults.FacetValue>): SearchResults.FacetValue {
@@ -19,7 +20,7 @@ function anFacedValue(override ?: Partial<SearchResults.FacetValue>): SearchResu
 	};
 }
 
-describe('sortWithNonRenseigneInTheEnd', () => {
+describe('sortWithNonRenseigneAtTheEnd', () => {
 	it('tri les données par ordre ascendant avec Non rensigné à la fin', () => {
 		const data = [
 			anFacedValue({ name: 'ab' }),
@@ -29,13 +30,38 @@ describe('sortWithNonRenseigneInTheEnd', () => {
 			anFacedValue({ name: 'a' }),
 		];
 
-		const sortedData = data.sort(sortWithNonRenseigneInTheEnd);
+		const sortedData = data.sort(sortWithNonRenseigneAtTheEnd);
 
 		expect(sortedData).toStrictEqual([
 			anFacedValue({ name: 'a' }),
 			anFacedValue({ name: 'ab' }),
 			anFacedValue({ name: 'n' }),
 			anFacedValue({ name: 's' }),
+			anFacedValue({ name: 'non renseigné' }),
+		]);
+	});
+});
+
+describe('sortByDurationAscending', () => {
+	it('tri les données par ordre ascendant avec < 1 mois au début', () => {
+		const data = [
+			anFacedValue({ name: '< 1 mois' }),
+			anFacedValue({ name: 'non renseigné' }),
+			anFacedValue({ name: '3 mois' }),
+			anFacedValue({ name: '1 mois' }),
+			anFacedValue({ name: '2 mois' }),
+			anFacedValue({ name: '> 6 mois' }),
+		];
+
+		const sortedData = data.sort(sortByDurationAscending);
+
+
+		expect(sortedData).toStrictEqual([
+			anFacedValue({ name: '< 1 mois' }),
+			anFacedValue({ name: '1 mois' }),
+			anFacedValue({ name: '2 mois' }),
+			anFacedValue({ name: '3 mois' }),
+			anFacedValue({ name: '> 6 mois' }),
 			anFacedValue({ name: 'non renseigné' }),
 		]);
 	});
