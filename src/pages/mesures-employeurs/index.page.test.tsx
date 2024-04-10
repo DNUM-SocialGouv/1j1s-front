bd455ad1725affa3bc0b456d11d54499fa45ebe9
julie BRUNETTO <julie.brunetto@octo.com>
2024-04-10 17:17:07 +0200

    feat(mesures empl): ajout des tests d'accessibilité

---
+++ src/pages/mesures-employeurs/index.page.test.tsx
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '~/test-utils';
+
+import { render } from '@testing-library/react';
+
+import { mockUseRouter } from '~/client/components/useRouter.mock';
+import { DependenciesProvider } from '~/client/context/dependenciesContainer.context';
+import { aManualAnalyticsService } from '~/client/services/analytics/analytics.service.fixture';
+import MesuresEmployeursPage from '~/pages/mesures-employeurs/index.page';
+import { aMesuresEmployeursList } from '~/server/mesures-employeurs/domain/mesureEmployeur.fixture';
+
+describe('<MesuresEmployeursPage />', () => {
+	it('doit rendre du HTML respectant la specification', () => {
+		const { container } = render(<DependenciesProvider analyticsService={aManualAnalyticsService()}>
+			<MesuresEmployeursPage mesureEmployeurList={aMesuresEmployeursList()}/>
+		</DependenciesProvider> );
+
+		expect(container.outerHTML).toHTMLValidate();
+	});
+
+	it('n‘a pas de défaut d‘accessibilité', async () => {
+		mockUseRouter({});
+		const { container } = render(
+			<DependenciesProvider
+				analyticsService={aManualAnalyticsService()}
+			>
+				<MesuresEmployeursPage mesureEmployeurList={aMesuresEmployeursList()}/>
+			</DependenciesProvider>,
+		);
+
+		await expect(container).toBeAccessible();
+	});
+});
