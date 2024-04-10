4764ecf17efa48f23fcf59956a712c55a3a7b108
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-04-10 15:26:58 +0200

    wip: Supprime la TODO

--- src/client/components/ui/Img/Image.tsx
+++ src/client/components/ui/Img/Image.tsx
@@ -1,15 +1,6 @@
 import NextImage from 'next/image';
 import React from 'react';
 
-/*
-* TODO:
-*  - [X] add index.tsx
-*  - [X] Add wrapper for Next/Image
-*  - [] Add placeholder on error
-*  - [] Add placeholder as component
-*  - [] Renommer en Image ?
-* */
-
 type ImageProps = React.ComponentPropsWithoutRef<typeof NextImage>;
 
 type ImageRef = React.ComponentRef<typeof NextImage>;
