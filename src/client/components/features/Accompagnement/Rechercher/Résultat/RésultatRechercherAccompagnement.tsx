1dcf35535cf46142d383c97f2a511f8436a7c337
julie BRUNETTO <julie.brunetto@octo.com>
2024-04-11 17:59:39 +0200

    feat(accompagnement): ordonnancement cohérent des informations

--- src/client/components/features/Accompagnement/Rechercher/Résultat/RésultatRechercherAccompagnement.tsx
+++ src/client/components/features/Accompagnement/Rechercher/Résultat/RésultatRechercherAccompagnement.tsx
@@ -1,48 +1,119 @@
+import classNames from 'classnames';
 import React, { useCallback, useState } from 'react';
 
 import {
 	ModalDemandeDeContactAccompagnement,
 } from '~/client/components/features/Accompagnement/DemandeDeContact/ModalDemandeDeContactAccompagnement';
 import {
-	RésultatRechercherAccompagnementDesktop,
-} from '~/client/components/features/Accompagnement/Rechercher/Résultat/RésultatRechercherAccompagnementDesktop';
+	HorairesResultatRechercherAccompagnement,
+} from '~/client/components/features/Accompagnement/Rechercher/Résultat/Horaires/HorairesResultatRechercherAccompagnement';
 import {
-	RésultatRechercherAccompagnementMobile,
-} from '~/client/components/features/Accompagnement/Rechercher/Résultat/RésultatRechercherAccompagnementMobile';
-import useBreakpoint from '~/client/hooks/useBreakpoint';
+	RésultatRechercherAccompagnementTagsList,
+} from '~/client/components/features/Accompagnement/Rechercher/Résultat/RésultatRechercherAccompagnementTagsList';
+import {
+	useAccompagnementLogo,
+} from '~/client/components/features/Accompagnement/Rechercher/Résultat/useAccompagnementLogo';
+import { ButtonComponent } from '~/client/components/ui/Button/ButtonComponent';
+import { Card } from '~/client/components/ui/Card/Card';
+import { Link } from '~/client/components/ui/Link/Link';
 import {
 	EtablissementAccompagnement,
 	TypeÉtablissement,
 } from '~/server/etablissement-accompagnement/domain/etablissementAccompagnement';
+
+import styles from './RésultatRechercherAccompagnement.module.scss';
 
 export interface RésultatRechercherAccompagnementProps {
 	établissement: EtablissementAccompagnement
 }
 
 export function RésultatRechercherAccompagnement({ établissement }: RésultatRechercherAccompagnementProps) {
-	const { isLargeScreen } = useBreakpoint();
 
 	const isMissionLocale = établissement.type === TypeÉtablissement.MISSION_LOCALE;
 	const [isPopInOpen, setIsPopInOpen] = useState(false);
+	const logoÉtablissement = useAccompagnementLogo(établissement.type);
 
 	const openContactÉtablissementModal = useCallback(() => {
 		setIsPopInOpen(true);
 	}, []);
 
+
+	function ContactButton() {
+		const label = 'Je souhaite être contacté(e)';
+		return <>
+			<ButtonComponent
+				className={classNames(styles.contactFormulaireÉtablissement, styles.contactFormulaireÉtablissementDesktopOnly)}
+				label={label}
+				appearance={'quaternary'}
+				onClick={openContactÉtablissementModal}/>
+
+			<ButtonComponent
+				className={classNames(styles.contactFormulaireÉtablissement, styles.contactFormulaireÉtablissementMobileOnly)}
+				label={label}
+				appearance={'primary'}
+				onClick={openContactÉtablissementModal}/>
+		</>;
+	}
+
+	function MailLink(props: { établissement: EtablissementAccompagnement }) {
+		const label = 'Contacter l‘agence';
+		const mailTo = `mailto:${props.établissement.email}`;
+		const title = 'Contacter l‘agence - adresse mail';
+
+		return <>
+			<Link
+				appearance={'asQuaternaryButton'}
+				href={mailTo}
+				className={classNames(styles.contactMailÉtablissement, styles.contactMailÉtablissementDektop)}
+				title={title}>
+				{label}
+				<Link.Icon name="mail"/>
+			</Link>
+			<Link
+				appearance={'asPrimaryButton'}
+				href={mailTo}
+				className={classNames(styles.contactMailÉtablissement, styles.contactMailÉtablissementMobile)}
+				title={title}>
+				{label}
+				<Link.Icon name="mail"/>
+			</Link>
+		</>;
+	}
+
 	return (
 		<>
-			{
-				isLargeScreen ?
-					<RésultatRechercherAccompagnementDesktop
-						établissement={établissement}
-						onContactClick={openContactÉtablissementModal}
-					/>
-					:
-					<RésultatRechercherAccompagnementMobile
-						établissement={établissement}
-						onContactClick={openContactÉtablissementModal}
-					/>
-			}
+			<Card layout={'vertical'} className={styles.card}>
+				<Card.Content className={styles.content}>
+					<Card.Image className={styles.logo} src={logoÉtablissement} aria-hidden/>
+					<div className={styles.mainInfoEtablissement}>
+						<Card.Title className={styles.title} titleAs={'h3'}>
+							{établissement.nom}
+						</Card.Title>
+						{établissement.adresse && <span className={styles.address}>{établissement.adresse}</span>}
+					</div>
+					<RésultatRechercherAccompagnementTagsList etablissement={établissement}/>
+
+					{établissement.horaires && établissement.horaires.length > 0 &&
+						<details className={styles.details}>
+							<summary className={styles.summary}>Voir les horaires d‘ouverture</summary>
+							<div className={styles.horaireBackground}>
+								<ol className={styles.listeHoraire}>
+									{établissement.horaires.map((horaire) => (
+										<li key={horaire.jour} className={styles.horaireElement}>
+											<HorairesResultatRechercherAccompagnement horaire={horaire}/>
+										</li>
+									))}
+								</ol>
+							</div>
+						</details>
+					}
+
+					{
+						établissement.email && (isMissionLocale ? <ContactButton/>
+							: <MailLink établissement={établissement}/>)
+					}
+				</Card.Content>
+			</Card>
 			{
 				isMissionLocale && établissement.email &&
 				<ModalDemandeDeContactAccompagnement
