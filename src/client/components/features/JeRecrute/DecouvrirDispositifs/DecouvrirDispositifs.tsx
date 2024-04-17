cb996f2561e137bd28dccf9272232229c9cae45d
gmoocto <128365508+gmoocto@users.noreply.github.com>
2024-04-17 14:04:30 +0200

    fix: supprime le MAIL_TO sur le depot d'offre de stage et le feature flipping (#2921)

--- src/client/components/features/JeRecrute/DecouvrirDispositifs/DecouvrirDispositifs.tsx
+++ src/client/components/features/JeRecrute/DecouvrirDispositifs/DecouvrirDispositifs.tsx
@@ -5,17 +5,6 @@ import { Container } from '~/client/components/layouts/Container/Container';
 import { Link } from '~/client/components/ui/Link/Link';
 
 export function DécouvrirDispositifs () {
-	const bulletList = '%E2%80%A2';
-	const lineBreak = '%0D%0A';
-	const body = `Afin de recevoir des candidatures correspondant au besoin de la mission proposée, nous vous conseillons de bien détailler votre offre, en n’oubliant pas de préciser : ${lineBreak}${lineBreak}${bulletList} Un titre pour votre offre de stage ; ${lineBreak}${bulletList} La description des missions (n’hésitez pas à faire une liste) ; ${lineBreak}${bulletList} Le lieu du stage (ville, code postal, département, région, pays) ; ${lineBreak}${bulletList} Votre secteur d’activité ; ${lineBreak}${bulletList} Les dates de début et de fin du stage, ainsi que la rémunération prévue ;${lineBreak}${bulletList} Les coordonnées et le SIRET de votre entreprise ;${lineBreak}${bulletList} Vos coordonnées ;${lineBreak}${bulletList} L’URL ou le mail pour envoyer sa candidature. ${lineBreak}${lineBreak}${lineBreak}Votre offre sera visible sur la plateforme après modération.`;
-	const subject = '[Déposer une offre de stage]';
-	const mail = 'contact-1j1s@sg.social.gouv.fr';
-	let MAIL_TO = `mailto:${mail}?subject=${subject}&body=${body}`;
-
-	if (process.env.NEXT_PUBLIC_DEPOT_STAGE_FEATURE === '1') {
-		MAIL_TO = '/stages/deposer-offre';
-	}
-
 	return (
 		<section>
 			<Container className={styles.recruter}>
@@ -29,7 +18,7 @@ export function DécouvrirDispositifs () {
 						<Link href="/apprentissage/deposer-offre" appearance='asPrimaryButton' className={styles.offresLien}>Déposer une offre d’alternance<Link.Icon/></Link>
 					</li>
 					<li>
-						<Link href={MAIL_TO} appearance='asPrimaryButton' className={styles.offresLien}>Déposer une offre de stage<Link.Icon name="mail"/></Link>
+						<Link href="/stages/deposer-offre" appearance='asPrimaryButton' className={styles.offresLien}>Déposer une offre de stage<Link.Icon/></Link>
 					</li>
 				</ul>
 			</Container>
