597405fadd6593bed279ba757565deda134b04e7
Guillaume Moizan <guillaume.moizan@octo.com>
2024-04-23 14:44:54 +0200

    chore: change var env IS_REVIEW_APP en DUREE_VALIDITE_CACHE_CMS_EN_SECONDES

--- src/server/cms/configuration/dependencies.container.ts
+++ src/server/cms/configuration/dependencies.container.ts
@@ -4,11 +4,9 @@ export interface CmsDependencies {
 	duréeDeValiditéEnSecondes: () => number
 }
 
-const UN_JOUR_EN_SECONDES = 60 * 60 * 24;
-
 export function cmsDependenciesContainer(configurationService: ConfigurationService): CmsDependencies {
-	const { IS_REVIEW_APP } = configurationService.getConfiguration();
-	const duréeDeValiditéEnSecondes = IS_REVIEW_APP ? 20 : UN_JOUR_EN_SECONDES;
+	const { DUREE_VALIDITE_CACHE_CMS_EN_SECONDES } = configurationService.getConfiguration();
+	const duréeDeValiditéEnSecondes = DUREE_VALIDITE_CACHE_CMS_EN_SECONDES;
 
 	return {
 		duréeDeValiditéEnSecondes: () => duréeDeValiditéEnSecondes,
