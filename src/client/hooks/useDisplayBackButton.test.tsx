30b2d669e9a7441b11ad94c2180908fd471ba685
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-05-14 12:06:39 +0200

    fix: Fix l'affichage du BackButton au premier chargement

--- src/client/hooks/useDisplayBackButton.test.tsx
+++ src/client/hooks/useDisplayBackButton.test.tsx
@@ -5,7 +5,7 @@
 import { render } from '@testing-library/react';
 
 import { mockUseRouter } from '~/client/components/useRouter.mock';
-import useDisplayBackButton from '~/client/hooks/useDisplayBackButton';
+import useDisplayBackButton, { PREVIOUS_PAGE } from '~/client/hooks/useDisplayBackButton';
 
 function TestComponent() {
 	useDisplayBackButton();
@@ -17,38 +17,20 @@ describe('useDisplayBackButton', () => {
 		sessionStorage.clear();
 		jest.resetAllMocks();
 	});
-	describe('quand la page actuel est la première sur laquelle on navigue', () => {
-		it('stocke le pathname de la page dans sessionStorage', () => {
-			// Given
-			mockUseRouter({
-				pathname: '/',
-			});
-			const setItem = jest.spyOn(Object.getPrototypeOf(sessionStorage), 'setItem');
-			jest.spyOn(Object.getPrototypeOf(sessionStorage), 'getItem').mockReturnValue(null);
-
-			// When
-			render(<TestComponent />);
-
-			// Then
-			expect(setItem).toHaveBeenCalledWith('current-page', '/');
-		});
-	});
-
-	describe('quand la page actuel n’est pas la première sur laquelle on navigue', () => {
+	describe('quand on quitte la page', () => {
 		it('stocke le pathname de la page dans sessionStorage', () => {
 			// Given
 			mockUseRouter({
 				pathname: '/other-page',
 			});
 			const setItem = jest.spyOn(Object.getPrototypeOf(sessionStorage), 'setItem');
-			jest.spyOn(Object.getPrototypeOf(sessionStorage), 'getItem').mockReturnValue('/');
+			const { unmount } = render(<TestComponent />);
 
 			// When
-			render(<TestComponent />);
+			unmount();
 
 			// Then
-			expect(setItem).toHaveBeenCalledWith('previous-page', '/');
-			expect(setItem).toHaveBeenLastCalledWith('current-page', '/other-page');
+			expect(setItem).toHaveBeenCalledWith(PREVIOUS_PAGE, 'true');
 		});
 	});
 });
--- src/client/hooks/useDisplayBackButton.ts
+++ src/client/hooks/useDisplayBackButton.ts
@@ -1,24 +1,18 @@
-import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 import { isStorageAvailable } from '~/client/utils/isStorageAvailable';
 
 
-const CURRENT_PAGE = 'current-page';
 export const PREVIOUS_PAGE = 'previous-page';
 
 function useDisplayBackButton(): void {
-	const router = useRouter();
 	useEffect(() => {
-		if (isStorageAvailable('sessionStorage')) {
-			const currentPage = sessionStorage.getItem(CURRENT_PAGE);
-			if (currentPage && currentPage !== router.pathname) {
-				sessionStorage.setItem(PREVIOUS_PAGE, currentPage);
+		return () => {
+			if (isStorageAvailable('sessionStorage')) {
+				sessionStorage.setItem(PREVIOUS_PAGE, 'true');
 			}
-			sessionStorage.setItem(CURRENT_PAGE, router.pathname);
-
-		}
-	}, [router.pathname]);
+		};
+	}, []);
 }
 
 export default useDisplayBackButton;
