f45cdad3497fb8c673b7bdcc26cb7b7eef8ec89d
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-05-24 17:35:08 +0200

    fix: Fix le BackButton sur le build
    Affiche le back button même lorsque Next tourne en SPA

--- src/client/hooks/usePageHistory.ts
+++ src/client/hooks/usePageHistory.ts
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 import { isStorageAvailable } from '~/client/utils/isStorageAvailable';
@@ -6,11 +7,12 @@ import { isStorageAvailable } from '~/client/utils/isStorageAvailable';
 export const IS_PREVIOUS_PAGE_LOCAL = 'is-previous-page-local';
 
 export default function usePageHistory(): void {
+	const router = useRouter();
 	useEffect(() => {
 		return () => {
 			if (isStorageAvailable('sessionStorage')) {
 				sessionStorage.setItem(IS_PREVIOUS_PAGE_LOCAL, 'true');
 			}
 		};
-	}, []);
+	}, [router.pathname]);
 }
