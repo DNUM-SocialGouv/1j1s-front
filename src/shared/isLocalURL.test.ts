e9d474f6660a76cab6b0e3460450bc63d792c11c
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-05-27 18:30:08 +0200

    feat: Gère les erreurs pour isLocalURL

--- src/shared/isLocalURL.test.ts
+++ src/shared/isLocalURL.test.ts
@@ -3,15 +3,17 @@ import isLocalURL from '~/shared/isLocalURL';
 describe('isLocalURL()', () => {
 	it('renvoie true si l‘url est relative', () => {
 		const url = '/home';
+		const origin = 'https://www.1jeune1solution.gouv.fr';
 
-		const isLocal = isLocalURL(url, '');
+		const isLocal = isLocalURL(url, origin);
 
 		expect(isLocal).toBe(true);
 	});
 	it('renvoie true si l‘url est une ancre', () => {
 		const url = '#section1';
+		const origin = 'https://www.1jeune1solution.gouv.fr';
 
-		const isLocal = isLocalURL(url, '');
+		const isLocal = isLocalURL(url, origin);
 
 		expect(isLocal).toBe(true);
 	});
@@ -40,5 +42,17 @@ describe('isLocalURL()', () => {
 		const isLocal = isLocalURL(url, origin);
 
 		expect(isLocal).toBe(true);
+	});
+	it('throw un erreur si l‘url est invalide', () => {
+		const url = 'url invalide';
+		const origin = 'https://www.1jeune1solution.gouv.fr';
+
+		expect(() => isLocalURL(url, origin)).toThrow(new TypeError('Invalid URL'));
+	});
+	it('throw un erreur si l‘origine est invalide', () => {
+		const url = '/toot';
+		const origin = 'www.test.com';
+
+		expect(() => isLocalURL(url, origin)).toThrow(new TypeError('Invalid Origin'));
 	});
 });
