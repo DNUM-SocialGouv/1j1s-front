6d316180421731825b5327a28d8c8ae5dcb40820
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-06-04 14:52:18 +0200

    feat: Ajoute la gestion d'erreur au runtime pour les services de stockage

--- src/client/services/storage/browser.storage.service.test.ts
+++ src/client/services/storage/browser.storage.service.test.ts
@@ -10,7 +10,7 @@ function aStorage(overrides?: Partial<BrowserStorage>): BrowserStorage {
 }
 
 describe('browserStorageService', () => {
-	it('devrait parser la donnée renvoyée quand on get()', () => {
+	it('parse la donnée renvoyée quand on get()', () => {
 		const expectedData = { test: 'value' };
 		const storage = aStorage({
 			getItem: jest.fn().mockReturnValue(JSON.stringify(expectedData)),
@@ -21,7 +21,7 @@ describe('browserStorageService', () => {
 
 		expect(result).toEqual(expectedData);
 	});
-	it('devrait serialiser la donnée quand on set()', () => {
+	it('serialise la donnée quand on set()', () => {
 		const data = { test: 'value' };
 		const storage = aStorage();
 		const service = new BrowserStorageService(storage);
@@ -29,5 +29,29 @@ describe('browserStorageService', () => {
 		service.set('key', data);
 
 		expect(storage.setItem).toHaveBeenCalledWith('key', JSON.stringify(data));
+	});
+	it('throw une erreur quand on get(), mais que le stockage n‘est pas disponible', () => {
+		const storage = aStorage({
+			getItem: jest.fn().mockImplementation(() => { throw new TypeError('getItem is not a function');}),
+		});
+		const service = new BrowserStorageService(storage);
+
+		expect(() => service.get('key')).toThrow(new Error('storage unavailable'));
+	});
+	it('throw une erreur quand on set(), mais que le stockage n‘est pas disponible', () => {
+		const storage = aStorage({
+			setItem: jest.fn().mockImplementation(() => { throw new TypeError('setItem is not a function');}),
+		});
+		const service = new BrowserStorageService(storage);
+
+		expect(() => service.set('key', 'value')).toThrow(new Error('storage unavailable'));
+	});
+	it('throw une erreur quand on remove(), mais que le stockage n‘est pas disponible', () => {
+		const storage = aStorage({
+			removeItem: jest.fn().mockImplementation(() => { throw new TypeError('removeItem is not a function');}),
+		});
+		const service = new BrowserStorageService(storage);
+
+		expect(() => service.remove('key')).toThrow(new Error('storage unavailable'));
 	});
 });
