3438f4ebdc7909df71439e61d7ca6783040d8f8f
symo_octo <sylvia.moreno@octo.com>
2024-06-26 09:47:03 +0200

    feat(a11y): Transformer le lien d’accès rapide 'Contenu' en 'Contenu principal' pour le rendre plus explicite

--- src/client/components/ui/SkipLink/SkipLink.tsx
+++ src/client/components/ui/SkipLink/SkipLink.tsx
@@ -9,7 +9,7 @@ export default function SkipLink() {
 		<div className={styles.skipLinkWrapper}>
 			<Container>
 				<ul className={styles.skipLinkList} id={TOP_ELEMENT_ID} tabIndex={-1}>
-					<li><a className={styles.skipLink} href="#contenu">Contenu</a></li>
+					<li><a className={styles.skipLink} href="#contenu">Contenu principal</a></li>
 					<li><a className={styles.skipLink} href="#header-navigation">Menu</a></li>
 					<li><a className={styles.skipLink} href="#footer">Pied de page</a></li>
 				</ul>
