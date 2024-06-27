ba482e8c7596f6225cba48e2ad78077cbf7bbad3
juliebrunetto83 <122871677+juliebrunetto83@users.noreply.github.com>
2024-06-27 14:50:39 +0200

    fix(a11y): lien d'évitement du menu en mobile (#3117)

--- src/client/components/ui/Bouée/Bouée.tsx
+++ src/client/components/ui/Bouée/Bouée.tsx
@@ -1,26 +1,27 @@
 import React, { RefObject, useEffect } from 'react';
 
 import styles from '~/client/components/ui/Bouée/Bouée.module.scss';
+import { Link } from '~/client/components/ui/Link/Link';
 import useDebouncedState from '~/client/hooks/useDebouncedState';
 
-import { Link } from '../Link/Link';
 
-export const TOP_ELEMENT_ID = 'top';
+export const ID_TOP_ELEMENT = 'top';
 
-export default function Bouée ({ surface }: { surface: RefObject<HTMLElement> }) {
+export default function Bouée({ surface }: { surface: RefObject<HTMLElement> }) {
 	const [isDown, setIsDown] = useDebouncedState(false, 50);
 
 	useEffect(() => {
-		function onScroll () {
+		function onScroll() {
 			if (surface.current) {
 				const { y } = surface.current.getBoundingClientRect();
 				y < 0 ? setIsDown(true) : setIsDown(false);
 			}
 		}
+
 		window.addEventListener('scroll', onScroll);
 		onScroll();
 		return () => window.removeEventListener('scroll', onScroll);
-  }, [surface]); // eslint-disable-line
+	}, [surface]); // eslint-disable-line
 	// eslint croit que c'est une bonne idée de mettre `setIsDown` dans les dépendances ¯\_(ツ)_/¯
 	// alors qu'en pratique, ça fait une boucle infinie.
 
@@ -28,10 +29,10 @@ export default function Bouée ({ surface }: { surface: RefObject<HTMLElement> }
 		<Link
 			className={styles.bouée}
 			appearance="asPrimaryButton"
-			hidden={ !isDown }
-			href={`#${TOP_ELEMENT_ID}`}
+			hidden={!isDown}
+			href={`#${ID_TOP_ELEMENT}`}
 		>
-			<Link.Icon name={'angle-up'} aria-hidden={false} aria-label='Remonter en haut de la page' />
+			<Link.Icon name={'angle-up'} aria-hidden={false} aria-label="Remonter en haut de la page"/>
 		</Link>
 	);
 }
--- src/client/components/ui/SkipLink/SkipLink.tsx
+++ src/client/components/ui/SkipLink/SkipLink.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
 import { Container } from '~/client/components/layouts/Container/Container';
-import { TOP_ELEMENT_ID } from '~/client/components/ui/Bouée/Bouée';
+import { ID_NAV_MOBILE } from '~/client/components/layouts/Header/HeaderBody';
+import { ID_NAV_DESKTOP } from '~/client/components/layouts/Header/Navigation/NavDesktop';
+import { ID_TOP_ELEMENT } from '~/client/components/ui/Bouée/Bouée';
 import styles from '~/client/components/ui/SkipLink/SkipLink.module.scss';
 
 export default function SkipLink() {
 	return (
 		<div className={styles.skipLinkWrapper}>
 			<Container>
-				<ul className={styles.skipLinkList} id={TOP_ELEMENT_ID} tabIndex={-1}>
-					<li><a className={styles.skipLink} href="#contenu">Contenu principal</a></li>
-					<li><a className={styles.skipLink} href="#header-navigation">Menu</a></li>
-					<li><a className={styles.skipLink} href="#footer">Pied de page</a></li>
+				<ul id={ID_TOP_ELEMENT} tabIndex={-1}>
+					<li><a href="#contenu">Contenu principal</a></li>
+					<li className={styles.desktopOnly}><a href={`#${ID_NAV_DESKTOP}`}>Menu</a></li>
+					<li className={styles.mobileOnly}><a href={`#${ID_NAV_MOBILE}`}>Menu</a></li>
+					<li><a href="#footer">Pied de page</a></li>
 				</ul>
 			</Container>
 		</div>
