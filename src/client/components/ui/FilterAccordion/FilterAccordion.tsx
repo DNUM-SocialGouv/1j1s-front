bfa2cefbe4fc910b7f158374719858ef9530d1bf
julie BRUNETTO <julie.brunetto@octo.com>
2024-07-15 11:45:16 +0200

    feat(UI): passer en composition FilterAccordion

--- src/client/components/ui/FilterAccordion/FilterAccordion.tsx
+++ src/client/components/ui/FilterAccordion/FilterAccordion.tsx
@@ -4,16 +4,28 @@ import React from 'react';
 import styles from '~/client/components/ui/FilterAccordion/FilterAccordion.module.scss';
 
 interface FilterAccordionProps extends React.ComponentPropsWithoutRef<'details'> {
-	title: string
 	open?: boolean
 }
 
 export function FilterAccordion(props: React.PropsWithChildren<FilterAccordionProps>) {
-	const { children, title, open, className, ...rest } = props;
+	const { children, open, className, ...rest } = props;
 	return (
 		<details className={classNames(styles.details, className)} {...rest} open={open || false}>
-			<summary>{title}</summary>
-			<div className={styles.detailsContent}>{children}</div>
+			{children}
 		</details>
 	);
 }
+
+export function FilterAccordionTitle(props: React.ComponentPropsWithoutRef<'summary'>) {
+	const { children, ...rest } = props;
+	return (<summary {...rest}>{children}</summary>
+	);
+}
+
+export function FilterAccordionContent(props: React.ComponentPropsWithoutRef<'div'>) {
+	const { className, children, ...rest } = props;
+	return <div className={classNames(styles.detailsContent, className)} {...rest}>{children}</div>;
+}
+
+FilterAccordion.Title = FilterAccordionTitle;
+FilterAccordion.Content = FilterAccordionContent;
