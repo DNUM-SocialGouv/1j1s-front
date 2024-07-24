65fa8df874ab99137f27ad8c5bed10166352cf52
juliebrunetto83 <122871677+juliebrunetto83@users.noreply.github.com>
2024-07-24 09:37:40 +0200

    feat(UI): zoom 200% sur la flipping card (#3201)

--- src/client/components/ui/Card/Flipping/FlippingCard.tsx
+++ src/client/components/ui/Card/Flipping/FlippingCard.tsx
@@ -33,37 +33,23 @@ export function FlippingCard(props: FlippingCardProps) {
 	const categoryClass = useMemo(() => {
 		switch (category) {
 			case ServiceJeune.Categorie.ACCOMPAGNEMENT:
-				return styles.cardCategoryAccompagnement;
+				return styles.categoryAccompagnement;
 			case ServiceJeune.Categorie.ORIENTATION_FORMATION:
-				return styles.cardCategoryOrienterFormer;
+				return styles.categoryOrienterFormer;
 			case ServiceJeune.Categorie.ENTREE_VIE_PROFESSIONELLE:
-				return styles.cardCategoryVieProfessionnelle;
+				return styles.categoryVieProfessionnelle;
 			case ServiceJeune.Categorie.AIDES_FINANCIERES:
-				return styles.cardCategoryAidesFinancieres;
+				return styles.categoryAidesFinancieres;
 		}
 	}, [category]);
 
-
 	useEffect(function setFocusOnFlip() {
 		if (isCardFlipped) {
-			const currentItem = cardFlipRef.current as HTMLDivElement;
-			const firstElement = currentItem.getElementsByTagName('button')[0];
-			firstElement.focus();
+			const currentItem = cardFlipRef.current;
+			const firstElement = currentItem?.getElementsByTagName('button')[0];
+			firstElement?.focus();
 		}
 	}, [isCardFlipped]);
-
-
-	const linkAsButton = useMemo(function () {
-		return <Link
-			href={link}
-			prefetch={false}
-			className={styles.cardAction}
-			appearance="asPrimaryButton"
-		>
-			{isInternalLink ? 'Lire l‘article' : 'En savoir plus'}
-			<Link.Icon/>
-		</Link>;
-	}, [isInternalLink, link]);
 
 	const flipCard = useCallback((reverse = false) => {
 		setIsAnimationOn(!isAnimationOn);
@@ -80,38 +66,38 @@ export function FlippingCard(props: FlippingCardProps) {
 	}, [isAnimationOn, flipButton, isCardFlipped]);
 
 	return (
-		<div className={classNames(styles.cardWrapper, { [styles.animate]: isAnimationOn }, className)} {...rest}>
-			<div className={classNames(styles.card, styles.cardFlip)}>
+		<div className={classNames(styles.cardWrapper, { [styles.flipped]: isAnimationOn }, className)} {...rest}>
+			<div className={classNames(styles.card, styles.recto)}>
 				<Image src={imageUrl ?? '/images/image-par-defaut-carte.webp'} alt="" width={360} height={180}/>
-				{category && <div className={classNames(styles.cardCategory, categoryClass)}>{category}</div>}
+				{category && <div className={classNames(styles.category, categoryClass)}>{category}</div>}
 
-				<div className={styles.cardBody}>
-					<CardTitle className={styles.cardBodyTitle} titleAs={titleAs}>{title}</CardTitle>
+				<div className={styles.body}>
+					<CardTitle className={styles.bodyTitle} titleAs={titleAs}>{title}</CardTitle>
 					<div
-						className={classNames(styles.cardActionWrapper, hasFlipCardContent ? styles.cardActionWrapperSpaceBetween : styles.cardActionWrapperFlexEnd)}>
+						className={styles.actionWrapper}>
 						{hasFlipCardContent &&
-                <ButtonComponent
-                	label="Pour qui ?"
-                	appearance={'quaternary'}
-                	className={styles.cardActionWrapperButton}
-                	ref={flipButton}
-                	onClick={() => flipCard()}>
-                </ButtonComponent>
+							<ButtonComponent
+								label="Pour qui ?"
+								appearance={'quaternary'}
+								ref={flipButton}
+								onClick={() => flipCard()}/>
 						}
-						{linkAsButton}
+						<Link href={link} prefetch={false} appearance="asPrimaryButton">
+							{isInternalLink ? 'Lire l‘article' : 'En savoir plus'}
+							<Link.Icon/>
+						</Link>
 					</div>
 				</div>
 			</div>
-			{isCardFlipped && (
-				<div ref={cardFlipRef} className={classNames(styles.card, styles.cardFlipBack)}>
-					<button onClick={() => flipCard(true)} className={styles.cardFlipBackAction}>
-						<span className="sr-only">masquer la section pour qui</span>
-						<Icon name="angle-left" aria-hidden="true"/>
-					</button>
-					<div className={styles.cardFlipBackTitle}>Pour qui ?</div>
-					<div className={styles.cardFlipBackContent}><MarkdownToHtml markdown={flippingCardContent}/></div>
-				</div>)
-			}
+			<div ref={cardFlipRef} className={classNames(styles.card, styles.verso)}>
+				<button
+					onClick={() => flipCard(true)}
+					aria-label="masquer la section pour qui">
+					<Icon name="angle-left" aria-hidden="true"/>
+				</button>
+				<div className={styles.title}>Pour qui ?</div>
+				<MarkdownToHtml markdown={flippingCardContent} className={styles.content}/>
+			</div>
 		</div>
 	);
 }
