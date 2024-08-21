c6873e8c7badfdb64dc032a1fb0a15fc37caec7b
LIG <124597730+LIG-OCTO@users.noreply.github.com>
2024-08-21 11:52:48 +0200

    feat(header): modifier la bannière d'accueil (#3269)
    * feat(modifier la banniere de campagne)
    
    * feat(modification bannière)
    
    * feat(retour review)

--- src/client/components/layouts/Header/Banner/Campagne/CampagneBanner.tsx
+++ src/client/components/layouts/Header/Banner/Campagne/CampagneBanner.tsx
@@ -4,15 +4,15 @@ import { Link } from '~/client/components/ui/Link/Link';
 
 import styles from './CampagneBanner.module.scss';
 
-export const ENCART_CAMPAGNE_URL = 'https://stagedeseconde.1jeune1solution.gouv.fr/professionnels';
-const TITRE = 'Vous souhaitez accueillir des élèves de 2de et contribuer à leur orientation\u00A0?';
-const SOUS_TITRE = 'Faites la différence et déposez facilement une offre de stage.';
+export const ENCART_CAMPAGNE_URL = 'https://www.1jeune1solution.gouv.fr/articles/competition-mondiale-des-metiers-world-skills-lyon-2024';
+const TITRE = 'Retrouvez le programme de la 47e édition des WorldSkills Lyon 2024';
+//const SOUS_TITRE = 'Faites la différence et déposez facilement une offre de stage.';
 
 export function CampagneBannerMobile() {
 	const displayCampagneEnCoursBanner = process.env.NEXT_PUBLIC_CAMPAGNE_COM_EN_COURS_FEATURE === '1';
 	return (displayCampagneEnCoursBanner &&
 		<Link href={ENCART_CAMPAGNE_URL} className={styles.headerBannerMobile} data-testid="mobile-encart-campagne">
-			{TITRE} {SOUS_TITRE}
+			{TITRE} {/*SOUS_TITRE*/}
 			<Link.Icon name="angle-right"/>
 		</Link>
 	);
@@ -24,7 +24,7 @@ export function CampagneBannerDesktop() {
 		<Link href={ENCART_CAMPAGNE_URL} className={styles.headerBannerDektop} data-testid="desktop-encart-campagne">
 			<p>
 				<span className={styles.title}>{TITRE}</span>
-				<span className={styles.content}>{SOUS_TITRE}</span>
+				{/*<span className={styles.content}>{SOUS_TITRE}</span>*/}
 			</p>
 			<Link.Icon className={styles.icon} name="angle-right"/>
 		</Link>
