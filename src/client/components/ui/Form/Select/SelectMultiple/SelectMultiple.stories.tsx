4c29f810e23c17280238ad8d71aa01525a8cbf61
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-08-26 17:16:15 +0200

    docs: Ajoute des stories de base pour les selects (#3282)
    * docs: Ajoute des stories pour le Select Simple
    
    * docs: Ajoute des stories pour le Select Multiple
    
    * style: Fix lint

---
+++ src/client/components/ui/Form/Select/SelectMultiple/SelectMultiple.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { SelectMultiple } from './SelectMultiple';
+
+const meta: Meta<typeof SelectMultiple> = {
+	argTypes: {
+		children: {
+			control: 'array',
+		},
+	},
+	args: {
+		children: ['France', 'Suisse', 'Allemagne', 'Royaume-Uni', 'Espagne', 'Belgique', 'Japon', 'Australie', 'Chine', 'Canada', 'États-Unis'],
+	},
+	component: SelectMultiple,
+	title: 'Components/Form/Select/SelectMultiple',
+};
+
+export default meta;
+type Story = StoryObj<typeof SelectMultiple>;
+export const exemple: Story = {
+	args: {},
+	render: ({ children, ...args }) => (
+		<>
+			<label htmlFor="pays">Pays</label>
+			<SelectMultiple id="pays" {...args}>
+				{children.map((child) => <SelectMultiple.Option value={child} key={child}>{child}</SelectMultiple.Option>)}
+			</SelectMultiple>
+		</>
+	),
+};
+export const disabled: Story = {
+	args: {
+		disabled: true,
+	},
+	render: ({ children, ...args }) => (
+		<>
+			<label htmlFor="pays">Pays</label>
+			<SelectMultiple id="pays" {...args}>
+				{children.map((child) => <SelectMultiple.Option value={child} key={child}>{child}</SelectMultiple.Option>)}
+			</SelectMultiple>
+		</>
+	),
+};
