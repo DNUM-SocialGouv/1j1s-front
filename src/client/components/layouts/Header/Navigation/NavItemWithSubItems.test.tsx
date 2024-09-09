b41eff0efbb35db3a531f1b9865361c03bea6934
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-09-09 18:45:32 +0200

    refactor: Refactor les tests sur le aria-current

--- src/client/components/layouts/Header/Navigation/NavItemWithSubItems.test.tsx
+++ src/client/components/layouts/Header/Navigation/NavItemWithSubItems.test.tsx
@@ -20,8 +20,8 @@ import { NavItemWithSubItems } from './NavItemWithSubItems';
 function aNavigationItem(overrides?: Partial<NavigationItemWithChildren>) {
 	return {
 		children: [
-			{ label: 'Sub Item 1', link: '/sub1' },
-			{ label: 'Sub Item 2', link: '/sub2' },
+			{ label: 'Sub Item 1', link: '#sub1' },
+			{ label: 'Sub Item 2', link: '#sub2' },
 		],
 		label: 'Test Menu',
 		...overrides,
@@ -30,7 +30,7 @@ function aNavigationItem(overrides?: Partial<NavigationItemWithChildren>) {
 function aNavigationSubItem(overrides?: Partial<NavigationItem>) {
 	return {
 		label: 'Sub Item 1',
-		link: '/sub1',
+		link: '#sub1',
 		...overrides,
 	};
 }
@@ -81,7 +81,6 @@ describe('NavItemWithSubItems', () => {
 			expect(menuButton).toHaveAttribute('aria-expanded', 'true');
 			expect(screen.getByRole('link', { name: /SubItem/i })).toBeVisible();
 		});
-
 		describe('et que le menu est déjà déplié', () => {
 			it('masque le menu', async () => {
 				const user = userEvent.setup();
@@ -99,61 +98,77 @@ describe('NavItemWithSubItems', () => {
 				expect(screen.queryByText('SubItem')).not.toBeInTheDocument();
 			});
 		});
-
-		it('lorsque l‘utilisateur clique sur un lien, appelle la props onClick une fois', async () => {
-			const mockOnClick = jest.fn();
-			const user = userEvent.setup();
-				
-			render(
-				<NavItemWithSubItems 
-					navigationItemWithChildren={aNavigationItem()}
-					onClick={mockOnClick} />,
-			);
-		
-			const button = screen.getByRole('button', { name: 'Test Menu' });
-			await user.click(button);
-		
-			const subItem = screen.getByRole('link', { name: /Sub Item 1/i });
-			await user.click(subItem);
-		
-			expect(mockOnClick).toHaveBeenCalledTimes(1);
-		});
 	});
 
-	describe('lorsque la page du sous-menu est visité', () => {
-		beforeEach(() => {
-			mockUseRouter({ pathname: '/sub1' });
-		});
+	describe('lorsque la page actuelle appartient au sous-menu', () => {
+		// FIXME (GAFI 09-09-2024): le aria-current est actuellement sur le span
+		it.skip('marque le bouton comme section actuelle', async () => {
+			mockUseRouter({ pathname: '/current-page' });
+			const nav = aNavigationItem({
+				children: [ aNavigationSubItem({
+					label: 'Current Page',
+					link: '/current-page',
+				}) ],
+				label: 'Menu',
+			});
+			render(<NavItemWithSubItems navigationItemWithChildren={nav} />);
 
-		afterEach(() => {
-			mockUseRouter({ pathname: '/' });
+			const menuButton = screen.getByRole('button', { name: 'Menu' });
+			expect(menuButton).toHaveAttribute('aria-current', 'true');
 		});
-
-		it('doit afficher l‘attribut aria-current à true', async () => {
+		it('marque le lien comme page actuelle', async () => {
+			mockUseRouter({ pathname: '/current-page' });
 			const user = userEvent.setup();
-			render(<NavItemWithSubItems navigationItemWithChildren={aNavigationItem()} />);
-    
-			const menuButton = screen.getByRole('button', { name: 'Test Menu' });
+			const nav = aNavigationItem({
+				children: [ aNavigationSubItem({
+					label: 'Current Page',
+					link: '/current-page',
+				}) ],
+				label: 'Menu',
+			});
+			render(<NavItemWithSubItems navigationItemWithChildren={nav} />);
+
+			const menuButton = screen.getByRole('button', { name: 'Menu' });
 			await user.click(menuButton);
 
-			const linkMenu = screen.getByRole('link', { name: /Sub Item 1/i });
-
+			const linkMenu = screen.getByRole('link', { name: 'Current Page' });
+			// FIXME (GAFI 09-09-2024): Idéalement aria-current="page"
 			expect(linkMenu).toHaveAttribute('aria-current', 'true');
 		});
 	});
 
-	describe('lorsque la page du sous-menu n‘est pas est visité', () => {
-		it('doit afficher l‘attribut aria-current à false', async () => {
-			const user = userEvent.setup();
+	describe('lorsque la page actuelle n’appartient pas au sous-menu', () => {
+		// FIXME (GAFI 09-09-2024): le aria-current est actuellement sur le span
+		it.skip('marque le bouton comme section pas actuelle', async () => {
+			mockUseRouter({ pathname: '/current-page' });
+			const nav = aNavigationItem({
+				children: [ aNavigationSubItem({
+					label: 'Other Page',
+					link: '/other-page',
+				}) ],
+				label: 'Menu',
+			});
+			render(<NavItemWithSubItems navigationItemWithChildren={nav} />);
 
-			render(<NavItemWithSubItems navigationItemWithChildren={aNavigationItem()} />);
-		
-			const menuButton = screen.getByRole('button', { name: 'Test Menu' });
+			const menuButton = screen.getByRole('button', { name: 'Menu' });
+			expect(menuButton).not.toHaveAttribute('aria-current', 'false');
+		});
+		it('marque le lien comme page actuelle', async () => {
+			const user = userEvent.setup();
+			mockUseRouter({ pathname: '/current-page' });
+			const nav = aNavigationItem({
+				children: [ aNavigationSubItem({
+					label: 'Other Page',
+					link: '/other-page',
+				}) ],
+				label: 'Menu',
+			});
+			render(<NavItemWithSubItems navigationItemWithChildren={nav} />);
+
+			const menuButton = screen.getByRole('button', { name: 'Menu' });
 			await user.click(menuButton);
 
-
-			const linkMenu = screen.getByRole('link', { name: /Sub Item 1/i });
-
+			const linkMenu = screen.getByRole('link', { name: 'Other Page' });
 			expect(linkMenu).toHaveAttribute('aria-current', 'false');
 		});
 	});
@@ -223,5 +238,24 @@ describe('NavItemWithSubItems', () => {
 			await user.click(screen.getByTestId('outside'));
 			expect(screen.queryByText('Sub Item 1')).not.toBeInTheDocument();
 		});
+	});
+
+	it('lorsque l‘utilisateur clique sur un lien, appelle la props onClick une fois', async () => {
+		const mockOnClick = jest.fn();
+		const user = userEvent.setup();
+
+		render(
+			<NavItemWithSubItems
+				navigationItemWithChildren={aNavigationItem()}
+				onClick={mockOnClick} />,
+		);
+
+		const button = screen.getByRole('button', { name: 'Test Menu' });
+		await user.click(button);
+
+		const subItem = screen.getByRole('link', { name: /Sub Item 1/i });
+		await user.click(subItem);
+
+		expect(mockOnClick).toHaveBeenCalledTimes(1);
 	});
 });
--- src/client/hooks/useIsInternalLink.ts
+++ src/client/hooks/useIsInternalLink.ts
@@ -2,14 +2,16 @@ import { useEffect, useMemo, useState } from 'react';
 
 import isInternalURL from '~/shared/isInternalURL';
 
+const RESERVED_ORIGIN = 'https://www.example.com';
+
 export function useIsInternalLink(href: string): boolean {
-	const [origin, setOrigin] = useState<string>('');
+	const [origin, setOrigin] = useState<string | undefined>(undefined);
 
 	useEffect(() => {
 		setOrigin(window.location.origin);
 	}, []);
 
 	return useMemo(function () {
-		return Boolean(origin) && isInternalURL(href, origin);
+		return origin ? isInternalURL(href, origin) : isInternalURL(href, RESERVED_ORIGIN);
 	}, [href, origin]);
 }
