2a4c1b6f236225388570a88bd2b2aaa7d876eb70
symo_octo <sylvia.moreno@octo.com>
2024-09-17 00:43:54 +0200

    feat(ServiceCard): correction du clic sur la carte

--- src/client/components/features/ServiceCard/Card/ServiceCard.tsx
+++ src/client/components/features/ServiceCard/Card/ServiceCard.tsx
@@ -1,9 +1,8 @@
 import classNames from 'classnames';
-import React, { useMemo } from 'react';
+import React, { useId } from 'react';
 
 import { HtmlHeadingTag } from '~/client/components/props';
 import { Card } from '~/client/components/ui/Card/Card';
-import { Icon } from '~/client/components/ui/Icon/Icon';
 import { Link } from '~/client/components/ui/Link/Link';
 import { useIsInternalLink } from '~/client/hooks/useIsInternalLink';
 
@@ -45,33 +44,37 @@ export function ServiceCard(props: ServiceCardProps & React.HTMLAttributes<HTMLL
 	const {
 		className, imageFit = 'contain', logo, logoAlt = '', link, linkLabel, title, titleAs, children,
 	} = props;
+	const idLink = useId();
+	const idIntitulé = useId();
 
 	const isInternalLink = useIsInternalLink(link);
-	const linkTitle = !isInternalLink ? `${linkLabel} - nouvelle fenêtre` : undefined;
-	const icon = useMemo(function () {
-		return <Icon name={isInternalLink ? 'arrow-right' : 'external-redirection'} />;
-	}, [isInternalLink]);
+	const linkTitle = !isInternalLink ? `${linkLabel} - nouvelle fenêtre` : 'En savoir plus';
 
-
-	// TODO remplacer link sur toute la card par un lien avec un ::before pour rendre toute la card cliquable
 	return (
-		<Link href={link} title={linkTitle} className={classNames(styles.cardContainer, className, 'underline-none')}>
-			<Card
-				layout={'vertical'}
-				className={classNames(
-					styles.card,
-					className,
-					styles.serviceCard,
-					imageFit === 'cover' && styles.cardCover)}>
-				<Card.Image className={styles.cardLogo} src={logo} alt={logoAlt} />
-				<Card.Content className={styles.cardBody}>
-					<Card.Title titleAs={titleAs} className={styles.cardTitle}>{title}</Card.Title>
-					<p>{children}</p>
-					<span className={styles.cardAction}>
-						<Card.FakeLink appearance={'quaternary'} label={linkLabel} icon={icon} />
-					</span>
-				</Card.Content>
-			</Card>
-		</Link>
+		<Card
+			layout={'vertical'}
+			className={classNames(
+				styles.card,
+				className,
+				styles.serviceCard,
+				'underline-none',
+				imageFit === 'cover' && styles.cardCover)}>
+			<Card.Image className={styles.cardLogo} src={logo} alt={logoAlt} />
+			<Card.Content className={styles.cardBody}>
+				<Card.Title id={idIntitulé} titleAs={titleAs} className={styles.cardTitle}>{title}</Card.Title>
+				<p>{children}</p>
+				<span className={styles.cardAction}>
+					<Link
+						id={idLink}
+						href={link}
+						appearance={'asQuaternaryButton'}
+						aria-labelledby={`${idIntitulé} ${idLink}`}
+						className={styles.cardAction}>
+						{linkTitle}
+						<Link.Icon name={isInternalLink ? 'arrow-right' : 'external-redirection'} />
+					</Link>
+				</span>
+			</Card.Content>
+		</Card>
 	);
 }
