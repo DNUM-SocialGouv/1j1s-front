4bbe8592a1e236af9b6ebc849b859e513454390f
LIG-OCTO <laurent.igout@octo.com>
2024-09-27 18:08:24 +0200

    fix: Modification du lien

--- src/client/components/layouts/Header/Navigation/NavigationStructure.tsx
+++ src/client/components/layouts/Header/Navigation/NavigationStructure.tsx
@@ -84,7 +84,7 @@ const employeurNav = (): NavigationItemWithChildren => ({
 				{ label: 'Je deviens mentor', link: '/je-deviens-mentor' },
 				{ label: 'Je propose des immersions', link: '/immersions' },
 				{ label: 'Je forme les jeunes grâce à l‘emploi', link: '/je-recrute-afpr-poei' },
-				...(process.env.NEXT_PUBLIC_CAMPAGNE_ALTERNANCE_FEATURE === '1' ? [{ label: 'Je recrute un apprenti', link: '/choisir-apprentissage' }] : []),
+				...(process.env.NEXT_PUBLIC_CAMPAGNE_ALTERNANCE_FEATURE === '1' ? [{ label: 'Je recrute un apprenti', link: '/apprentissage-entreprises' }] : []),
 			],
 			label: 'Recruter et agir pour les jeunes',
 		},
