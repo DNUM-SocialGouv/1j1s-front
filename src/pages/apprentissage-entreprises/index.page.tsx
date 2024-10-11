142673f846cd72a2cdc2da88909c7f15ab2f0a86
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-10-11 10:37:44 +0200

    wip: Fix pagename pas reset quand on change de page

--- src/pages/apprentissage-entreprises/index.page.tsx
+++ src/pages/apprentissage-entreprises/index.page.tsx
@@ -21,6 +21,10 @@ export default function ApprentissageEntreprises ({ videos }: ApprentissageEntre
 	useAnalytics(analyticsPageConfig);
 	const adformService: MarketingService = useDependency('marketingService');
 	adformService.trackPage('2024-09-1jeune1solution.gouv.fr-PageAccueil-PageAccueil');
+	useEffect(() => {
+		// @ts-ignore
+		return () => adformService.trackPage(undefined);
+	}, []);
 	const seedtagService: MarketingService = useDependency('seedtagService');
 	useEffect(() => {
 		seedtagService.trackPage('');
