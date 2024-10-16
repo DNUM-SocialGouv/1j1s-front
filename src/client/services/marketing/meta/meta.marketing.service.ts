d657575d43c9a1c0434270e2030a36310a8d6a73
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-10-16 17:51:20 +0200

    wip: "Fix" le tracker Meta

--- src/client/services/marketing/meta/meta.marketing.service.ts
+++ src/client/services/marketing/meta/meta.marketing.service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable */
+// @ts-nocheck
+
 import { CookiesService } from '../../cookies/cookies.service';
 import { MarketingService } from '../marketing.service';
 
@@ -12,47 +15,22 @@ export default class MetaMarketingService implements MarketingService {
 			cookies: ['xs', 'sb', 'fr', 'datr', 'dpr', 'c_user'],
 			js: function () {
 				'use strict';
-				// eslint-disable-next-line
-				// @ts-ignore
+				if (!window.tarteaucitron.user.metaId) return;
 				if (!window.fbq) {
-					// eslint-disable-next-line
-					// @ts-ignore
-					// eslint-disable-next-line
 					const n = window.fbq = function (...args: any[]) {
-						// eslint-disable-next-line
-						// @ts-ignore
-						// eslint-disable-next-line
 						n.callMethod ? n.callMethod.apply(n, args) : n.queue.push(args);
 					};
-					// eslint-disable-next-line
-					// @ts-ignore
 					if (!window._fbq) {
-						// eslint-disable-next-line
-						// @ts-ignore
 						window._fbq = n;
 					}
-					// eslint-disable-next-line
-					// @ts-ignore
 					n.push = n;
-					// eslint-disable-next-line
-					// @ts-ignore
 					n.loaded = !0;
-					// eslint-disable-next-line
-					// @ts-ignore
 					n.version = '2.0';
-					// eslint-disable-next-line
-					// @ts-ignore
 					n.queue = [];
 				}
 
-				// eslint-disable-next-line
-				// @ts-ignore
 				window.tarteaucitron.addScript(`https://connect.facebook.net/${window.tarteaucitron.getLocale()}/fbevents.js`, '', function () {
-					// eslint-disable-next-line
-					// @ts-ignore
 					window.fbq('init', window.tarteaucitron.user.metaId);
-					// eslint-disable-next-line
-					// @ts-ignore
 					window.fbq('track', 'PageView');
 				});
 			},
