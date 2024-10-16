50901c68bb6bb426eb30d5bca92a5f26966df40b
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2024-10-16 12:11:31 +0200

    wip: Extrait les 3 scripts Amnet dans des services tarteaucitron

--- src/client/services/marketing/amnet/amnet.marketing.service.ts
+++ src/client/services/marketing/amnet/amnet.marketing.service.ts
@@ -6,72 +6,21 @@ import { MarketingService } from '../marketing.service';
 
 export default class AmnetMarketingService implements MarketingService {
 	private cookieService: CookiesService;
-	private static readonly amnetId = '0cfe1200-c50d-4658-a08d-8967a78bfaeb';
+	private static readonly xandrId = '0cfe1200-c50d-4658-a08d-8967a78bfaeb';
 	private static readonly zemTagId = '118693';
 
-	constructor(cookieService: CookiesService) {
-		this.cookieService = cookieService;
+	private addXandr() {
+		this.cookieService.addService('xandr');
+	}
+	private addOutbrain() {
 		const config = {
-			// FIXME ajouter cookies issus de adsrvr et de zemanta
+			// FIXME ajouter cookies issus de zemanta
 			cookies: ['anj', 'icu', 'sess', 'uids', 'usersync', 'uuid2'],
 			js: function () {
 				'use strict';
-				if (window.tarteaucitron.user.amnetId === undefined) {
-					return;
-				}
-				// eslint-disable-next-line
-				// @ts-ignore
-				if (!window.pixie) {
-					// eslint-disable-next-line
-					// @ts-ignore
-					const n = window.pixie = function (e, i, a) {
-						// eslint-disable-next-line
-						// @ts-ignore
-						n.actionQueue.push({
-							action: e,
-							actionValue: i,
-							params: a,
-						});
-					};
-					// eslint-disable-next-line
-					// @ts-ignore
-					n.actionQueue = [];
-				}
-
-				// eslint-disable-next-line
-				// @ts-ignore
-				window.tarteaucitron.addScript('https://acdn.adnxs.com/dmp/up/pixie.js', '', function () {
-					// eslint-disable-next-line
-					// @ts-ignore
-					window.pixie('init', window.tarteaucitron.user.amnetId);
-					// eslint-disable-next-line
-					// @ts-ignore
-					window.pixie('event', 'PageView');
-				});
-
-				// eslint-disable-next-line
-				// @ts-ignore
-				window.tarteaucitron.addScript('https://js.adsrvr.org/up_loader.1.1.0.js', '', function () {
-					// eslint-disable-next-line
-					// @ts-ignore
-					// eslint-disable-next-line
-					ttd_dom_ready(function () {
-						// eslint-disable-next-line
-						// @ts-ignore
-						// eslint-disable-next-line
-						if (typeof TTDUniversalPixelApi === 'function') {
-							// eslint-disable-next-line
-							// @ts-ignore
-							// eslint-disable-next-line
-							const universalPixelApi = new TTDUniversalPixelApi();
-							universalPixelApi.init('qj8wadw', ['muuud1z'], 'https://insight.adsrvr.org/track/up');
-						}
-					});
-				});
-
-
+				if (!window.tarteaucitron.user.xandrId) return;
 				const pixel = document.createElement('img');
-				pixel.src = 'https://p1.zemanta.com/v2/p/ns/118693/PAGE_VIEW/';
+				pixel.src = `https://p1.zemanta.com/v2/p/ns/${AmnetMarketingService.zemTagId}/PAGE_VIEW/`;
 				pixel.width = 1;
 				pixel.height = 1;
 				pixel.alt = '';
@@ -82,21 +31,52 @@ export default class AmnetMarketingService implements MarketingService {
 					pixel.remove();
 				});
 			},
-			key: 'amnet',
-			name: 'Amnet',
+			key: 'amnet-outbrain',
+			name: 'Amnet - Outbrain',
+			needConsent: true,
+			type: 'ads',
+			uri: 'https://support.google.com/displayvideo/topic/3528231?hl=en&ref_topic=9059505&sjid=9933903973918710720-EU',
+		};
+		this.cookieService.addService('amnet-outbrain', config);
+	}
+	private addAdsrvr() {
+		const config = {
+			// FIXME ajouter cookies issus de adsrvr
+			cookies: ['anj', 'icu', 'sess', 'uids', 'usersync', 'uuid2'],
+			js: function () {
+				'use strict';
+				if (!window.tarteaucitron.user.xandrId) return;
+				window.tarteaucitron.addScript('https://js.adsrvr.org/up_loader.1.1.0.js', '', function () {
+					ttd_dom_ready(function () {
+						if (typeof TTDUniversalPixelApi === 'function') {
+							const universalPixelApi = new TTDUniversalPixelApi();
+							universalPixelApi.init('qj8wadw', ['muuud1z'], 'https://insight.adsrvr.org/track/up');
+						}
+					});
+				});
+			},
+			key: 'adsrvr',
+			name: 'Amnet - Adsrvr',
 			needConsent: true,
 			type: 'ads',
 			uri: 'https://support.google.com/displayvideo/topic/3528231?hl=en&ref_topic=9059505&sjid=9933903973918710720-EU',
 		};
 
-		this.cookieService.addService('amnet', config);
+		this.cookieService.addService('adsrvr', config);
+	}
+
+	constructor(cookieService: CookiesService) {
+		this.cookieService = cookieService;
+		this.addAdsrvr();
+		this.addOutbrain();
+		this.addXandr();
 	}
 
 	trackPage(pagename: string): void {
 		if (pagename === 'off') {
-			this.cookieService.addUser('amnetId', undefined);
+			this.cookieService.addUser('xandrId', undefined);
 		} else {
-			this.cookieService.addUser('amnetId', AmnetMarketingService.amnetId);
+			this.cookieService.addUser('xandrId', AmnetMarketingService.xandrId);
 		}
 	}
 }
