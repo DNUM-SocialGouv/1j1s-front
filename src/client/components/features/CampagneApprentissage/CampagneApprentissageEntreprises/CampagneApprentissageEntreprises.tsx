970e7cdc3daec46752c3bb5d0f7ac376fcfe7eb0
Sonny Klotz <165301800+sokl-octo@users.noreply.github.com>
2024-10-21 17:40:56 +0200

    fix(campagne): ajoute guillemets aux verbatims (#3383)

--- src/client/components/features/CampagneApprentissage/CampagneApprentissageEntreprises/CampagneApprentissageEntreprises.tsx
+++ src/client/components/features/CampagneApprentissage/CampagneApprentissageEntreprises/CampagneApprentissageEntreprises.tsx
@@ -87,32 +87,32 @@ export function CampagneApprentissageEntreprises({ videos }: CampagneApprentissa
 		{
 			imageUrl: '/images/campagne-verbatim-fabrice.webp',
 			nomApprenti: 'Fabrice, maître d’apprentissage d’Emma',
-			verbatim: 'En venant en entreprise, les jeunes vont apprendre à connaître et respecter ses règles, ses processus, mais aussi l’esprit d’entreprise. C’est tout aussi important que les compétences techniques.',
+			verbatim: '« En venant en entreprise, les jeunes vont apprendre à connaître et respecter ses règles, ses processus, mais aussi l’esprit d’entreprise. C’est tout aussi important que les compétences techniques. »',
 		},
 		{
 			imageUrl: '/images/campagne-verbatim-gael.webp',
 			nomApprenti: 'Gaël, maître d’apprentissage de Benoît',
-			verbatim: 'J’ai été apprenti il y a 20 ans, j’ai été très bien formé et j’ai à cœur de bien transmettre à mon tour.',
+			verbatim: '« J’ai été apprenti il y a 20 ans, j’ai été très bien formé et j’ai à cœur de bien transmettre à mon tour. »',
 		},
 		{
 			imageUrl: '/images/campagne-verbatim-julien.webp',
 			nomApprenti: 'Julien, maître d’apprentissage de Céline',
-			verbatim: 'Nous, notre travail, c’est leur donner la confiance aussi, leur faire comprendre qu’ils peuvent y arriver. Il n’y a pas d’âge pour apprendre.',
+			verbatim: '« Nous, notre travail, c’est leur donner la confiance aussi, leur faire comprendre qu’ils peuvent y arriver. Il n’y a pas d’âge pour apprendre. »',
 		},
 		{
 			imageUrl: '/images/campagne-verbatim-emma.webp',
 			nomApprenti: 'Emma, en apprentissage avec Fabrice',
-			verbatim: 'L’apprentissage, c’est que du plus : j’ai un diplôme et de l’expérience et c’est ça qui est important pour la suite.',
+			verbatim: '« L’apprentissage, c’est que du plus : j’ai un diplôme et de l’expérience et c’est ça qui est important pour la suite. »',
 		},
 		{
 			imageUrl: '/images/campagne-verbatim-benoit.webp',
 			nomApprenti: 'Benoît, en apprentissage avec Gaël',
-			verbatim: 'L’entreprise, c’est là où on s’amuse le plus, c’est là où on voit le plus de choses, que ce soit côté humain ou côté professionnel.',
+			verbatim: '« L’entreprise, c’est là où on s’amuse le plus, c’est là où on voit le plus de choses, que ce soit côté humain ou côté professionnel. »',
 		},
 		{
 			imageUrl: '/images/campagne-verbatim-celine.webp',
 			nomApprenti: 'Céline, en apprentissage avec Julien',
-			verbatim: 'Avec l’apprentissage, je suis une vraie salariée. C’est une fierté de voir le salaire arriver sur mon compte chaque mois grâce à mon travail.',
+			verbatim: '« Avec l’apprentissage, je suis une vraie salariée. C’est une fierté de voir le salaire arriver sur mon compte chaque mois grâce à mon travail. »',
 		},
 	];
 
