2b8a75407537dd3ef95207aa2dae72f261bcffd9
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-07-31 11:14:35 +0200

    feat: Remet la bannière de campagne apprentissage

--- src/client/components/features/BannieresCampagnes/index.tsx
+++ src/client/components/features/BannieresCampagnes/index.tsx
@@ -1,6 +1,7 @@
 import FeteDesMetiersBanner from './FeteDesMetiers';
 import StageDeSecondeBanner from './StageDeSeconde';
 import WorldSkillsBanner from './WorldSkills';
+import ApprentissageBanner from './Apprentissage';
 
 
 export default function Bannieres() {
@@ -9,6 +10,7 @@ export default function Bannieres() {
 			{process.env.NEXT_PUBLIC_FETE_DE_METIERS_CAMPAGNE === '1' && <FeteDesMetiersBanner />}
 			{process.env.NEXT_PUBLIC_STAGES_SECONDE_FEATURE === '1' && <StageDeSecondeBanner />}
 			{process.env.NEXT_PUBLIC_WORLD_SKILLS_FEATURE === '1' && <WorldSkillsBanner />}
+			{process.env.NEXT_PUBLIC_CAMPAGNE_APPRENTISSAGE_FEATURE === '1' && <ApprentissageBanner />}
 		</>
 	);
 }
