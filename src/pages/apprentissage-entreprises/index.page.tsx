cbce78a68824c736bb9d2486aac214337f15a397
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-08-05 17:29:18 +0200

    feat: Ré-active le tracker Adform

--- src/pages/apprentissage-entreprises/index.page.tsx
+++ src/pages/apprentissage-entreprises/index.page.tsx
@@ -1,13 +1,25 @@
+import { useEffect } from 'react';
+
 import {
 	CampagneApprentissageEntreprises,
 } from '~/client/components/features/CampagneApprentissage/CampagneApprentissageEntreprises/CampagneApprentissageEntreprises';
 import { Head } from '~/client/components/head/Head';
+import { useDependency } from '~/client/context/dependenciesContainer.context';
 import useAnalytics from '~/client/hooks/useAnalytics';
+import { MarketingService } from '~/client/services/marketing/marketing.service';
 import styles from '~/pages/apprentissage/index.module.scss';
 import analyticsPageConfig from '~/pages/apprentissage-entreprises/index.analytics';
 
 export default function ApprentissageEntreprises () {
 	useAnalytics(analyticsPageConfig);
+
+	const adformService = useDependency<MarketingService>('marketingService');
+	adformService.trackPage('2024-09-1jeune1solution.gouv.fr-PageAccueil-PageAccueil');
+	useEffect(() => {
+		// @ts-expect-error
+		return () => adformService.trackPage(undefined);
+		// eslint-disable-next-line
+	}, []);
 
 	return (
 		<>
