7a7bde3f97871576b43a9bdf24b5f6904cc65152
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-08-27 15:06:19 +0200

    fix: Fix les CSPs

--- config/headers.js
+++ config/headers.js
@@ -1,13 +1,11 @@
-const LOCAL_MODE_HEADERS = [];
-
 const STRAPI_MEDIA_HOST = new URL(process.env.STRAPI_MEDIA_URL).hostname;
 const TRUSTED_SOURCES = '*.fabrique.social.gouv.fr *.meilisearch.io/multi-search *.meilisearch.com/multi-search 1j1s-front.osc-fr1.scalingo.io *.1jeune1solution.gouv.fr';
 const ANALYTICS_SOURCES = `${process.env.NEXT_PUBLIC_ANALYTICS_DOMAIN} ${process.env.NEXT_PUBLIC_ANALYTICS_MATOMO_HOST}`;
 const contentSecurityPolicy = `
-  default-src 'self' ${TRUSTED_SOURCES} https://*.linkedin.com;
-  script-src 'self' ${ANALYTICS_SOURCES} https://*.adform.net https://*.zemanta.com https://*.licdn.com;
-  img-src 'self' *.google.com data: ${STRAPI_MEDIA_HOST} ${ANALYTICS_SOURCES} img.youtube.com jedonnemonavis.numerique.gouv.fr https://*.linkedin.com;
-  style-src 'self' 'unsafe-inline' ${ANALYTICS_SOURCES};
+  default-src 'self' ${TRUSTED_SOURCES} *.myjobglasses.com https://fonts.gstatic.com/;
+  script-src 'self' ${ANALYTICS_SOURCES} https://*.adform.net *.myjobglasses.com;
+  img-src 'self' *.google.com data: ${STRAPI_MEDIA_HOST} ${ANALYTICS_SOURCES} img.youtube.com jedonnemonavis.numerique.gouv.fr *.myjobglasses.com;
+  style-src 'self' 'unsafe-inline' ${ANALYTICS_SOURCES} *.myjobglasses.com https://fonts.googleapis.com;
   frame-ancestors 'none';
   frame-src 'self' *.apprentissage.beta.gouv.fr immersion-facile.beta.gouv.fr deposer-offre.www.1jeune1solution.gouv.fr *.youtube-nocookie.com simulateur-alternance.1jeune1solution.gouv.fr https://*.adform.net mes-aides.francetravail.fr;
   form-action 'self';
@@ -30,6 +28,26 @@ const SECURITY_MODE_HEADERS = [{
 	}, {
 		key: 'Content-Security-Policy',
 		value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+	}],
+	source: '/:path*',
+}];
+
+const LOCAL_MODE_HEADERS = [{
+	headers: [{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on',
+	}, {
+		key: 'Strict-Transport-Security',
+		value: 'max-age=63072000; includeSubDomains; preload',
+	}, {
+		key: 'X-Content-Type-Options',
+		value: 'nosniff',
+	}, {
+		key: 'Referrer-Policy',
+		value: 'no-referrer, strict-origin-when-cross-origin',
+	}, {
+		key: 'Content-Security-Policy',
+		value: contentSecurityPolicy.replace('script-src', "script-src 'unsafe-eval'").replace(/\s{2,}/g, ' ').trim(),
 	}],
 	source: '/:path*',
 }];
