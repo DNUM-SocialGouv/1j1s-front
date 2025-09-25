8a99b5b740dd434e0f1cac89cc0c216cd32a5601
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-09-25 16:39:45 +0200

    feat: Ajoute le paragraphe d'introduction à MyJobGlasses

--- src/pages/myjobglasses/index.page.tsx
+++ src/pages/myjobglasses/index.page.tsx
@@ -2,8 +2,10 @@ import Script from 'next/script';
 import React from 'react';
 
 import { Head } from '~/client/components/head/Head';
+import { Container } from '~/client/components/layouts/Container/Container';
 
-import { LightHero, LightHeroPrimaryText, LightHeroSecondaryText } from '../../client/components/ui/Hero/LightHero';
+import { LightHero, LightHeroPrimaryText, LightHeroSecondaryText } from '~/client/components/ui/Hero/LightHero';
+import styles from './index.module.scss'
 
 export default function MyJobGlassesPage() {
 	return (
@@ -16,9 +18,19 @@ export default function MyJobGlassesPage() {
 				<LightHero>
 					<h1>
 						<LightHeroPrimaryText>Échangez avec des professionnels</LightHeroPrimaryText>
-						<LightHeroSecondaryText>avec MyJobGlasses</LightHeroSecondaryText>
+						<LightHeroSecondaryText>avec <i>My Job Glasses</i></LightHeroSecondaryText>
 					</h1>
 				</LightHero>
+				<Container>
+					<p className={styles.description}>
+						Un réseau professionnel à votre disposition&nbsp;: <b><i>1Jeune1Solution</i></b> s&apos;est associé à <b><i>My Job Glasses</i></b> pour
+						vous permettre de découvrir les métiers grâce à un réseau de <strong>plus de 82&nbsp;000
+						professionnels</strong> qui se rendent disponibles pour répondre à toutes vos questions sur leur métier. Ce service
+						est <strong>gratuit</strong> et disponible pour tous, sans condition d&apos;âge. Rien de tel que d&apos;échanger avec une
+						personne qui vous présentera son métier, son environnement professionnel et son parcours pour vous aider
+						dans votre choix d&apos;orientation !
+					</p>
+				</Container>
 				<div suppressHydrationWarning id="widget-my-job-glasses"></div>
 
 				<Script async
