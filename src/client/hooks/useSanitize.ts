0e00174227ec21742f7256bff652239163ec1353
Thibaut <thibaut.poullain@ydrazil.fr>
2026-03-09 09:38:01 +0100

    fix: 🐛 résout le conflit de merge dans useSanitize

--- src/client/hooks/useSanitize.ts
+++ src/client/hooks/useSanitize.ts
@@ -1,15 +1,10 @@
 import DOMPurify from 'dompurify';
 import { useMemo } from 'react';
 
-<<<<<<< HEAD
 export default function useSanitize(dirty: string | undefined | null): string {
 	return useMemo(() => {
 		if (!dirty) return '';
 		if (typeof window === 'undefined') return dirty;
 		return DOMPurify.sanitize(dirty);
 	}, [dirty]);
-=======
-export default function useSanitize(dirty: string | undefined | null) {
-	return useMemo(() => dirty ? DOMPurify.sanitize(dirty) : '', [dirty]);
->>>>>>> main
 }
