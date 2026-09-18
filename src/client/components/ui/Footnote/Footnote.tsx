2b81ab3b611bcca3cd8548846cbfd33aa6b96f51
Juliette Ferrer <122263079+octojufe@users.noreply.github.com>
2026-09-18 14:21:29 +0200

    fix: Corrections UI DSFR (#3666)
    * fix: marge mobile banner dsfr avec illustration
    
    * feat: note de bas de page dsfr
    
    * fix: padding liste de résultat combobox pays dsfr
    
    (cherry picked from commit 9ec7e3fcb44eb34e59896baab04d4fc31cc7ae5a)

--- src/client/components/ui/Footnote/Footnote.tsx
+++ src/client/components/ui/Footnote/Footnote.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames';
 import { ComponentPropsWithoutRef, forwardRef } from 'react';
 
 import { Icon } from '~/client/components/ui/Icon/Icon';
@@ -14,11 +13,10 @@ type FootnoteProps = Omit<ComponentPropsWithoutRef<'p'>, 'id'> & {
 const FootnoteComponent = forwardRef<HTMLParagraphElement, FootnoteProps>(function Footnote({
 	children,
 	htmlFor,
-	className,
 	...pProps
 }, ref) {
 	return (
-		<p {...pProps} className={classNames(className, styles.footnote)} ref={ref}>
+		<p {...pProps} ref={ref}>
 			<abbr title="note de pied de page">*</abbr> {children} <Link href={`#${htmlFor}`} title="Retour à la référence"><Icon name="arrow-up" className={styles.icon} /></Link>
 		</p>
 	);
@@ -31,11 +29,10 @@ type ReferenceProps = Omit<ComponentPropsWithoutRef<'a'>, 'id'> & {
 
 const Reference = forwardRef<HTMLAnchorElement, ReferenceProps>(function Reference({
 	to,
-	className,
 	...aProps
 }, ref) {
 	return (
-		<a href={`#${to}`} className={classNames(className, styles.reference)} aria-label="note de pied de page" {...aProps} ref={ref}>
+		<a href={`#${to}`} aria-label="note de pied de page" {...aProps} ref={ref}>
 			<abbr title="note de pied de page">*</abbr>
 		</a>
 	);
